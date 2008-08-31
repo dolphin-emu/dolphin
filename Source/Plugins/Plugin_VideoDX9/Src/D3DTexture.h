9a7b828eba97f2ec87dd066d047b86bb4f639064
Maarten ter Huurne <maarten@treewalker.org>
2008-08-31 21:52:28 +0000

    Removed function FilterDown(), which was neither used nor implemented.
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@412 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Plugins/Plugin_VideoDX9/Src/D3DTexture.h
+++ Source/Plugins/Plugin_VideoDX9/Src/D3DTexture.h
@@ -4,13 +4,6 @@
 
 namespace D3D
 {
-	//////////////////////////////////////////////////////////////////////////
-	//Simple box filter mipmap generator: should in theory be replaced with say a 
-	//gaussian or something, but even this makes textures look great, especially
-	//with anisotropic filtering enabled
-	void FilterDown(DWORD *buffer, int w, int h, const int pitch);
-	// __________________________________________________________________________________________________
-	// calls filterdown which will trash _pBuffer as temp storage for mips
 	LPDIRECT3DTEXTURE9 CreateTexture2D(const u8* buffer, const int width, const int height, const int pitch, D3DFORMAT fmt = D3DFMT_A8R8G8B8);
 	void ReplaceTexture2D(LPDIRECT3DTEXTURE9 pTexture, const u8* buffer, const int width, const int height,const int pitch,  D3DFORMAT fmt);
 	LPDIRECT3DTEXTURE9 CreateRenderTarget(const int width, const int height);
