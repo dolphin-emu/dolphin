70eebcb3a3d747b4aeb7096ab2c04968a9d2b3f5
fires.gc <fires.gc@gmail.com>
2008-10-15 13:08:55 +0000

    reverted stuff that doesn't compile...
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@878 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/Core/Src/PatchEngine.cpp
+++ Source/Core/Core/Src/PatchEngine.cpp
@@ -36,7 +36,6 @@
 #include "PatchEngine.h"
 #include "IniFile.h"
 #include "HW/Memmap.h"
-#include "ActionReplay.h"
 
 
 enum PatchType
@@ -67,7 +66,22 @@ struct Patch
 std::vector<Patch> onLoad;
 std::vector<Patch> onFrame;
 
+struct AREntry {
+	u32 cmd_addr;
+	u32 value;
+};
+
+struct ARCode {
+	std::string name;
+	std::vector<AREntry> ops;
+	bool active;
+};
+
+std::vector<ARCode> arCodes;
+
 using namespace Common;
+
+void RunActionReplayCode(const ARCode &code, bool nowIsBootup);
 
 void LoadPatchSection(const char *section, std::vector<Patch> &patches, IniFile &ini)
 {
@@ -95,6 +109,8 @@ void LoadPatchSection(const char *section, std::vector<Patch> &patches, IniFile 
 		}
 	}
 }
+
+void LoadActionReplayCodes(IniFile &ini);
 
 void PatchEngine_LoadPatches(const char *gameID)
 {
