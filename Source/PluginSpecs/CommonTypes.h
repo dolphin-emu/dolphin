a56cb7c0bba37a844e4e34c0e3f4bb3e005d17bc
tmator <tmator@gmail.com>
2008-10-25 17:48:54 +0000

    BOOL is defined on apple 64
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@964 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/PluginSpecs/CommonTypes.h
+++ Source/PluginSpecs/CommonTypes.h
@@ -43,7 +43,9 @@ typedef signed __int64 s64;
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
+#if !defined(OSX64)
 typedef unsigned int BOOL;
+#endif
 typedef unsigned long long u64;
 
 typedef char s8;
