e2f687470b1d60ac929aac562c2e743a44943786
John Peterson <jpeterson57@gmail.com>
2008-11-11 17:24:37 +0000

    Added Wii Sports AX CRC
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@1125 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Plugins/Plugin_DSP_HLE/Src/UCodes/UCodes.cpp
+++ Source/Plugins/Plugin_DSP_HLE/Src/UCodes/UCodes.cpp
@@ -74,6 +74,7 @@ IUCode* UCodeFactory(u32 _CRC, CMailHandler& _rMailHandler)
 
 		case 0x5ef56da3: // AX demo
 	    case 0x347112ba: // raving rabbits
+		case 0xfa450138: // wii sports - PAL
 			DebugLog("Wii - AXWii chosen");
 		    return new CUCode_AXWii(_rMailHandler);
 
