3f52c6951375fe93095ccdc72814933d7d15f4e2
gigaherz <gigaherz@gmail.com>
2008-11-26 02:07:29 +0000

    Added some ugly shitty code which will let ppl paste the masterkey in hex into a masterkey.txt file and let the emu convert it to binary. Feel free to make it nicer if you want to. Requested by ector.
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@1300 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/DiscIO/Src/VolumeCreator.cpp
+++ Source/Core/DiscIO/Src/VolumeCreator.cpp
@@ -127,8 +127,38 @@ IVolume* CreateVolumeFromCryptedWiiImage(IBlobReader& _rReader, u32 _VolumeType)
 		FILE* pT = fopen(WII_MASTERKEY_FILE, "rb");
 		if (pT == NULL)
 		{
-			PanicAlert("Can't open " WII_MASTERKEY_FILE);
-			return NULL;
+			PanicAlert("Can't open '" WII_MASTERKEY_FILE "'.\n If you know the key, now it's the time to paste it into '" WII_MASTERKEY_FILE_HEX "' before pressing [OK].");
+
+			pT = fopen(WII_MASTERKEY_FILE_HEX, "r");
+			if(pT==NULL)
+				return NULL;
+
+			static char hexkey[32];
+			if(fread(hexkey,1,32,pT)<32)
+			{
+				fclose(pT);
+				return NULL;
+			}
+			fclose(pT);
+
+			static char binkey[16];
+			char *t=hexkey;
+			for(int i=0;i<16;i++)
+			{
+				char h[3]={*(t++),*(t++),0};
+				binkey[i] = strtol(h,NULL,16);
+			}
+
+			pT = fopen(WII_MASTERKEY_FILE, "wb");
+			if(pT==NULL)
+				return NULL;
+
+			fwrite(binkey,16,1,pT);
+			fclose(pT);
+
+			pT = fopen(WII_MASTERKEY_FILE, "rb");
+			if(pT==NULL)
+				return NULL;
 		}
 
 		fread(g_MasterKey, 16, 1, pT);
