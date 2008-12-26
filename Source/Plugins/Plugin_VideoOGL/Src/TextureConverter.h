bd9ebbf8c98df60f50314f850bfe8dc46ba3221b
nakeee <nakeee@gmail.com>
2008-12-26 12:39:53 +0000

    compile fix
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@1687 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Plugins/Plugin_VideoOGL/Src/TextureConverter.h
+++ Source/Plugins/Plugin_VideoOGL/Src/TextureConverter.h
@@ -19,7 +19,7 @@
 #define _TEXTURECONVERTER_H
 
 #include "VideoCommon.h"
-#include "GLutil.h"
+#include "GLUtil.h"
 
 // Converts textures between formats
 // TODO: support multiple texture formats
