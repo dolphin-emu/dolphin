96d6fc6e85f10fb5a5ac0a3f8d5c34c53669b4ec
nakeee <nakeee@gmail.com>
2009-01-04 20:05:34 +0000

    More files
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@1767 8ced0084-cf51-0410-be5f-012b33b47a6e

---
+++ Source/Core/InputCommon/Src/InputCommon.cpp
@@ -0,0 +1,17 @@
+#include "EventHandler.h"
+
+EventHandler *eventHandler = NULL;
+
+namespace InputCommon {
+    void Init() {
+#if defined GLTEST && GLTEST
+	// init the event handler
+	eventHandler = new EventHandler();
+#endif
+    }
+
+    void Shutdown() {
+	if (eventHandler)
+	    delete eventHandler;
+    }
+}
---
+++ Source/Core/InputCommon/Src/InputCommon.h
@@ -0,0 +1,8 @@
+#include "EventHandler.h"
+
+extern EventHandler *eventHandler;
+
+namespace InputCommon {
+    void Init(); 
+    void Shutdown();
+}
