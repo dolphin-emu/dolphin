88291fe8bef2ec9b9558ed867505da82979682ee
John Peterson <jpeterson57@gmail.com>
2009-01-05 03:01:22 +0000

    CodeWindow: Missing file
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@1785 8ced0084-cf51-0410-be5f-012b33b47a6e

---
+++ Source/Core/DebuggerWX/Src/CodeWindowSJP.cpp
@@ -0,0 +1,500 @@
+// Copyright (C) 2003-2008 Dolphin Project.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2.0.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License 2.0 for more details.
+
+// A copy of the GPL 2.0 should have been included with the program.
+// If not, see http://www.gnu.org/licenses/
+
+// Official SVN repository and contact information can be found at
+// http://code.google.com/p/dolphin-emu/
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Include
+// ¯¯¯¯¯¯¯¯¯¯
+#include "Common.h"
+
+#include <wx/button.h>
+#include <wx/textctrl.h>
+#include <wx/textdlg.h>
+#include <wx/listctrl.h>
+#include <wx/thread.h>
+#include <wx/mstream.h>
+#include <wx/tipwin.h>
+
+// ugly that this lib included code from the main
+#include "../../DolphinWX/Src/Globals.h"
+
+#include "IniFile.h"
+#include "Host.h"
+
+#include "Debugger.h"
+
+#include "RegisterWindow.h"
+#include "LogWindow.h"
+#include "BreakpointWindow.h"
+#include "MemoryWindow.h"
+#include "JitWindow.h"
+
+#include "CodeWindow.h"
+#include "CodeView.h"
+
+#include "FileUtil.h"
+#include "Core.h"
+#include "HLE/HLE.h"
+#include "Boot/Boot.h"
+#include "LogManager.h"
+#include "HW/CPU.h"
+#include "PowerPC/PowerPC.h"
+#include "Debugger/PPCDebugInterface.h"
+#include "Debugger/Debugger_SymbolMap.h"
+#include "PowerPC/PPCAnalyst.h"
+#include "PowerPC/Profiler.h"
+#include "PowerPC/SymbolDB.h"
+#include "PowerPC/SignatureDB.h"
+#include "PowerPC/PPCTables.h"
+#include "PowerPC/Jit64/Jit.h"
+#include "PowerPC/Jit64/JitCache.h" // for ClearCache()
+
+#include "Plugins/Plugin_DSP.h" // new stuff, to let us open the DLLDebugger
+#include "Plugins/Plugin_Video.h" // new stuff, to let us open the DLLDebugger
+#include "../../DolphinWX/Src/PluginManager.h"
+#include "../../DolphinWX/Src/Config.h"
+
+
+extern "C"  // Bitmaps
+{
+	#include "../resources/toolbar_play.c"
+	#include "../resources/toolbar_pause.c"
+	#include "../resources/toolbar_add_memorycheck.c"
+	#include "../resources/toolbar_delete.c"
+	#include "../resources/toolbar_add_breakpoint.c"
+}
+///////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Declarations and definitions
+// ¯¯¯¯¯¯¯¯¯¯
+// and here are the classes
+class CPluginInfo;
+class CPluginManager;
+//extern DynamicLibrary Common::CPlugin;
+//extern CPluginManager CPluginManager::m_Instance;
+///////////////////////////////////
+
+
+
+
+void CCodeWindow::CreateSymbolsMenu()
+{
+	wxMenu *pSymbolsMenu = new wxMenu;
+	pSymbolsMenu->Append(IDM_CLEARSYMBOLS, _T("&Clear symbols"));
+	// pSymbolsMenu->Append(IDM_CLEANSYMBOLS, _T("&Clean symbols (zz)"));
+	pSymbolsMenu->Append(IDM_SCANFUNCTIONS, _T("&Generate symbol map"));
+	pSymbolsMenu->AppendSeparator();
+	pSymbolsMenu->Append(IDM_LOADMAPFILE, _T("&Load symbol map"));
+	pSymbolsMenu->Append(IDM_SAVEMAPFILE, _T("&Save symbol map"));
+	pSymbolsMenu->AppendSeparator();
+	pSymbolsMenu->Append(IDM_SAVEMAPFILEWITHCODES, _T("Save code"),
+		wxString::FromAscii("Save the entire disassembled code. This may take a several seconds"
+		" and may require between 50 and 100 MB of hard drive space. It will only save code"
+		" that are in the first 4 MB of memory, if you are debugging a game that load .rel"
+		" files with code to memory you may want to increase that to perhaps 8 MB, you can do"
+		" that from SymbolDB::SaveMap().")
+		);
+	
+	pSymbolsMenu->AppendSeparator();
+	pSymbolsMenu->Append(IDM_CREATESIGNATUREFILE, _T("&Create signature file..."));
+	pSymbolsMenu->Append(IDM_USESIGNATUREFILE, _T("&Use signature file..."));
+	pSymbolsMenu->AppendSeparator();
+	pSymbolsMenu->Append(IDM_PATCHHLEFUNCTIONS, _T("&Patch HLE functions"));
+	pMenuBar->Append(pSymbolsMenu, _T("&Symbols"));
+
+
+	wxMenu *pJitMenu = new wxMenu;
+	pJitMenu->Append(IDM_CLEARCODECACHE, _T("&Clear code cache"));
+	pJitMenu->Append(IDM_LOGINSTRUCTIONS, _T("&Log JIT instruction coverage"));
+	pMenuBar->Append(pJitMenu, _T("&JIT"));
+
+	wxMenu *pProfilerMenu = new wxMenu;
+	pProfilerMenu->Append(IDM_PROFILEBLOCKS, _T("&Profile blocks"), wxEmptyString, wxITEM_CHECK);
+	pProfilerMenu->AppendSeparator();
+	pProfilerMenu->Append(IDM_WRITEPROFILE, _T("&Write to profile.txt, show"));
+	pMenuBar->Append(pProfilerMenu, _T("&Profiler"));
+}
+
+
+
+void CCodeWindow::OnProfilerMenu(wxCommandEvent& event)
+{
+	if (Core::GetState() == Core::CORE_RUN) {
+		event.Skip();
+		return;
+	}
+	switch (event.GetId())
+	{
+	case IDM_PROFILEBLOCKS:
+		jit.ClearCache();
+		Profiler::g_ProfileBlocks = GetMenuBar()->IsChecked(IDM_PROFILEBLOCKS);
+		break;
+	case IDM_WRITEPROFILE:
+		Profiler::WriteProfileResults("profiler.txt");
+		File::Launch("profiler.txt");
+		break;
+	}
+}
+
+void CCodeWindow::OnSymbolsMenu(wxCommandEvent& event) 
+{
+	if (Core::GetState() == Core::CORE_UNINITIALIZED)
+	{
+		// TODO: disable menu items instead :P
+		return;
+	}
+	std::string mapfile = CBoot::GenerateMapFilename();
+	switch (event.GetId())
+	{
+	case IDM_CLEARSYMBOLS:
+		g_symbolDB.Clear();
+		Host_NotifyMapLoaded();
+		break;
+	case IDM_CLEANSYMBOLS:
+		g_symbolDB.Clear("zz");
+		Host_NotifyMapLoaded();
+		break;
+	case IDM_SCANFUNCTIONS:
+		{
+		PPCAnalyst::FindFunctions(0x80000000, 0x80400000, &g_symbolDB);
+		SignatureDB db;
+		if (db.Load(TOTALDB_FILE))
+			db.Apply(&g_symbolDB);
+
+		// HLE::PatchFunctions();
+		NotifyMapLoaded();
+		break;
+		}
+	case IDM_LOADMAPFILE:
+		if (!File::Exists(mapfile.c_str()))
+		{
+			g_symbolDB.Clear();
+			PPCAnalyst::FindFunctions(0x80000000, 0x80400000, &g_symbolDB);
+			SignatureDB db;
+			if (db.Load(TOTALDB_FILE))
+				db.Apply(&g_symbolDB);
+		} else {
+			g_symbolDB.LoadMap(mapfile.c_str());
+		}
+		NotifyMapLoaded();
+		break;
+	case IDM_SAVEMAPFILE:
+		g_symbolDB.SaveMap(mapfile.c_str());
+		break;
+	case IDM_SAVEMAPFILEWITHCODES:
+		g_symbolDB.SaveMap(mapfile.c_str(), true);
+		break;
+	case IDM_CREATESIGNATUREFILE:
+		{
+		wxTextEntryDialog input_prefix(this, wxString::FromAscii("Only export symbols with prefix:"), wxGetTextFromUserPromptStr, _T("."));
+		if (input_prefix.ShowModal() == wxID_OK) {
+			std::string prefix(input_prefix.GetValue().mb_str());
+
+			wxString path = wxFileSelector(
+					_T("Save signature as"), wxEmptyString, wxEmptyString, wxEmptyString,
+					_T("Dolphin Signature File (*.dsy)|*.dsy;"), wxFD_SAVE,
+					this);
+			if (path) {
+				SignatureDB db;
+				db.Initialize(&g_symbolDB, prefix.c_str());
+				std::string filename(path.ToAscii());		// PPCAnalyst::SaveSignatureDB(
+				db.Save(path.ToAscii());
+			}
+		}
+		}
+		break;
+	case IDM_USESIGNATUREFILE:
+		{
+		wxString path = wxFileSelector(
+				_T("Apply signature file"), wxEmptyString, wxEmptyString, wxEmptyString,
+				_T("Dolphin Signature File (*.dsy)|*.dsy;"), wxFD_OPEN | wxFD_FILE_MUST_EXIST,
+				this);
+		if (path) {
+			SignatureDB db;
+			db.Load(path.ToAscii());
+			db.Apply(&g_symbolDB);
+		}
+		}
+		NotifyMapLoaded();
+		break;
+	case IDM_PATCHHLEFUNCTIONS:
+		HLE::PatchFunctions();
+		Update();
+		break;
+	}
+}
+
+
+void CCodeWindow::NotifyMapLoaded()
+{
+	g_symbolDB.FillInCallers();
+	symbols->Show(false); // hide it for faster filling
+	symbols->Clear();
+	for (SymbolDB::XFuncMap::iterator iter = g_symbolDB.GetIterator(); iter != g_symbolDB.End(); iter++)
+	{
+		int idx = symbols->Append(wxString::FromAscii(iter->second.name.c_str()));
+		symbols->SetClientData(idx, (void*)&iter->second);
+	}
+	symbols->Show(true);
+	Update();
+}
+
+
+void CCodeWindow::OnSymbolListChange(wxCommandEvent& event)
+{
+	int index = symbols->GetSelection();
+	if (index >= 0) {
+		Symbol* pSymbol = static_cast<Symbol *>(symbols->GetClientData(index));
+		if (pSymbol != NULL)
+		{
+			if(pSymbol->type == Symbol::SYMBOL_DATA)
+			{
+				if(m_MemoryWindow && m_MemoryWindow->IsVisible())
+					m_MemoryWindow->JumpToAddress(pSymbol->address);
+			}
+			else
+			{
+				JumpToAddress(pSymbol->address);
+			}
+		}
+	}
+}
+
+void CCodeWindow::OnSymbolListContextMenu(wxContextMenuEvent& event)
+{
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Show and hide windows
+/////////////////////////////////////////////////////////////////////////////////////////////////
+void CCodeWindow::OnToggleLogWindow(wxCommandEvent& event)
+{
+	if (LogManager::Enabled())
+	{
+		bool show = GetMenuBar()->IsChecked(event.GetId());
+
+		if (show)
+		{
+			if (!m_LogWindow)
+			{
+				m_LogWindow = new CLogWindow(this);
+			}
+
+			m_LogWindow->Show(true);
+		}
+		else // hide
+		{
+			// If m_dialog is NULL, then possibly the system
+			// didn't report the checked menu item status correctly.
+			// It should be true just after the menu item was selected,
+			// if there was no modeless dialog yet.
+			wxASSERT(m_LogWindow != NULL);
+
+			if (m_LogWindow)
+			{
+				m_LogWindow->Hide();
+			}
+		}
+	}
+}
+
+
+void CCodeWindow::OnToggleRegisterWindow(wxCommandEvent& event)
+{
+	bool show = GetMenuBar()->IsChecked(event.GetId());
+
+	if (show)
+	{
+		if (!m_RegisterWindow)
+		{
+			m_RegisterWindow = new CRegisterWindow(this);
+		}
+
+		m_RegisterWindow->Show(true);
+	}
+	else // hide
+	{
+		// If m_dialog is NULL, then possibly the system
+		// didn't report the checked menu item status correctly.
+		// It should be true just after the menu item was selected,
+		// if there was no modeless dialog yet.
+		wxASSERT(m_RegisterWindow != NULL);
+
+		if (m_RegisterWindow)
+		{
+			m_RegisterWindow->Hide();
+		}
+	}
+}
+
+
+// =======================================================================================
+// Toggle Sound Debugging Window
+// ------------
+void CCodeWindow::OnToggleSoundWindow(wxCommandEvent& event)
+{
+	bool show = GetMenuBar()->IsChecked(event.GetId());
+
+	if (show)
+	{
+		// TODO: add some kind of if() check here to?
+		CPluginManager::GetInstance().OpenDebug(
+			GetHandle(),
+			SConfig::GetInstance().m_LocalCoreStartupParameter.m_strDSPPlugin.c_str(),
+			false, true // DSP, show
+			);
+	}
+	else // hide
+	{
+		// Close the sound dll that has an open debugger
+		CPluginManager::GetInstance().OpenDebug(
+			GetHandle(),
+			SConfig::GetInstance().m_LocalCoreStartupParameter.m_strDSPPlugin.c_str(),
+			false, false // DSP, hide
+			);
+	}
+}
+// ===========
+
+
+// =======================================================================================
+// Toggle Video Debugging Window
+// ------------
+void CCodeWindow::OnToggleVideoWindow(wxCommandEvent& event)
+{
+	bool show = GetMenuBar()->IsChecked(event.GetId());
+	//GetMenuBar()->Check(event.GetId(), false); // Turn off
+
+	if (show)
+	{
+		// It works now, but I'll keep this message in case the problem reappears
+		/*if(Core::GetState() == Core::CORE_UNINITIALIZED)
+		{
+			wxMessageBox(_T("Warning, opening this window before a game is started \n\
+may cause a crash when a game is later started. Todo: figure out why and fix it."), wxT("OpenGL Debugging Window"));	
+		}*/
+
+		// TODO: add some kind of if() check here to?
+		CPluginManager::GetInstance().OpenDebug(
+		GetHandle(),
+		SConfig::GetInstance().m_LocalCoreStartupParameter.m_strVideoPlugin.c_str(),
+		true, true // Video, show
+		);
+	}
+	else // hide
+	{
+		// Close the video dll that has an open debugger
+		CPluginManager::GetInstance().OpenDebug(
+			GetHandle(),
+			SConfig::GetInstance().m_LocalCoreStartupParameter.m_strVideoPlugin.c_str(),
+			true, false // Video, hide
+			);
+	}
+}
+// ===========
+
+
+void CCodeWindow::OnToggleJitWindow(wxCommandEvent& event)
+{
+	bool show = GetMenuBar()->IsChecked(event.GetId());
+
+	if (show)
+	{
+		if (!m_JitWindow)
+		{
+			m_JitWindow = new CJitWindow(this);
+		}
+
+		m_JitWindow->Show(true);
+	}
+	else // hide
+	{
+		// If m_dialog is NULL, then possibly the system
+		// didn't report the checked menu item status correctly.
+		// It should be true just after the menu item was selected,
+		// if there was no modeless dialog yet.
+		wxASSERT(m_JitWindow != NULL);
+
+		if (m_JitWindow)
+		{
+			m_JitWindow->Hide();
+		}
+	}
+}
+
+
+void CCodeWindow::OnToggleBreakPointWindow(wxCommandEvent& event)
+{
+	bool show = GetMenuBar()->IsChecked(event.GetId());
+
+	if (show)
+	{
+		if (!m_BreakpointWindow)
+		{
+			m_BreakpointWindow = new CBreakPointWindow(this, this);
+		}
+
+		m_BreakpointWindow->Show(true);
+	}
+	else // hide
+	{
+		// If m_dialog is NULL, then possibly the system
+		// didn't report the checked menu item status correctly.
+		// It should be true just after the menu item was selected,
+		// if there was no modeless dialog yet.
+		wxASSERT(m_BreakpointWindow != NULL);
+
+		if (m_BreakpointWindow)
+		{
+			m_BreakpointWindow->Hide();
+		}
+	}
+}
+
+void CCodeWindow::OnToggleMemoryWindow(wxCommandEvent& event)
+{
+	bool show = GetMenuBar()->IsChecked(event.GetId());
+
+	if (show)
+	{
+		if (!m_MemoryWindow)
+		{
+			m_MemoryWindow = new CMemoryWindow(this);
+		}
+
+		m_MemoryWindow->Show(true);
+	}
+	else // hide
+	{
+		// If m_dialog is NULL, then possibly the system
+		// didn't report the checked menu item status correctly.
+		// It should be true just after the menu item was selected,
+		// if there was no modeless dialog yet.
+		wxASSERT(m_MemoryWindow != NULL);
+
+		if (m_MemoryWindow)
+		{
+			m_MemoryWindow->Hide();
+		}
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
