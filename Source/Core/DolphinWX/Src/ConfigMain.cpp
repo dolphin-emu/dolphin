cbd7804044e77d346cc4846d8f6eebf0798516f5
hrydgard <hrydgard@gmail.com>
2009-02-28 08:37:12 +0000

    allow user to clear "default iso"
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@2465 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/DolphinWX/Src/ConfigMain.cpp
+++ Source/Core/DolphinWX/Src/ConfigMain.cpp
@@ -440,7 +440,7 @@ void CConfigMain::CreateGUIControls()
 	DefaultISOText = new wxStaticText(PathsPage, ID_DEFAULTISO_TEXT, wxT("Default ISO:"), wxDefaultPosition, wxDefaultSize);
 	DefaultISO = new wxFilePickerCtrl(PathsPage, ID_DEFAULTISO, wxEmptyString, wxT("Choose a default ISO:"),
 		wxString::Format(wxT("All GC/Wii images (gcm, iso, gcz)|*.gcm;*.iso;*.gcz|All files (%s)|%s"), wxFileSelectorDefaultWildcardStr, wxFileSelectorDefaultWildcardStr),
-		wxDefaultPosition, wxDefaultSize, wxFLP_USE_TEXTCTRL|wxFLP_FILE_MUST_EXIST|wxFLP_OPEN);
+		wxDefaultPosition, wxDefaultSize, wxFLP_USE_TEXTCTRL|wxFLP_OPEN);
 	DefaultISO->SetPath(wxString::FromAscii(SConfig::GetInstance().m_LocalCoreStartupParameter.m_strDefaultGCM.c_str()));
 
 	DVDRootText = new wxStaticText(PathsPage, ID_DVDROOT_TEXT, wxT("DVD Root:"), wxDefaultPosition, wxDefaultSize);
