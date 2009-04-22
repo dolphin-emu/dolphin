8cb7ab559ae58d058e782da4be5abe7b0e928279
XTra.KrazzY <XTra.KrazzY@gmail.com>
2009-04-22 11:11:58 +0000

    Compile fix
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@3037 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/DSPTool/Src/main.cpp
+++ Source/DSPTool/Src/main.cpp
@@ -208,9 +208,9 @@ void RunAsmTests()
 // So far, all this binary can do is test partially that itself works correctly.
 int main(int argc, const char *argv[])
 {
-	if(argc == 1 || (argc == 2 && (!strcmp(argv[1], "--help") || (!strcmp(argv[1], "-h"))))
+	if(argc == 1 || (argc == 2 && (!strcmp(argv[1], "--help") || (!strcmp(argv[1], "-?")))))
 	{
-		printf("DSPTool [-d] [-o <FILE>] [-h <FILE>] <DSP ASSEMBLER FILE>\n");
+		printf("USAGE: DSPTool [-d] [-o <FILE>] [-h <FILE>] <DSP ASSEMBLER FILE>\n");
 		printf("-d: Disassemble\n");
 		printf("-o <OUTPUT FILE>: Results from stdout redirected to a file\n");
 		printf("-h <HEADER FILE>: Output assembly results to a header\n");
