36eec8d305656b541659163d700ede3ba1d27ac1
Marcus Wanners <marcus@wanners.net>
2009-04-26 17:38:31 +0000

    Revert r3083, we'll find a better way.
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@3084 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/DolphinWX/Src/InfoWindow.cpp
+++ Source/Core/DolphinWX/Src/InfoWindow.cpp
@@ -21,8 +21,6 @@
 #include "Core.h"
 #include "ConfigManager.h"
 #include "CDUtils.h"
-#include "OGLconfig.h"
-#include "D3Dconfig.h"
 
 BEGIN_EVENT_TABLE(wxInfoWindow, wxWindow)
 	EVT_SIZE(                                            wxInfoWindow::OnEvent_Window_Resize)
@@ -55,27 +53,12 @@ std::string Summarize_Plug()
 		"Default GFX Plugin: %s\n"
 		"Default DSP Plugin: %s\n"
 		"Default PAD Plugin: %s\n"
-		"Default WiiMote Plugin: %s\n\n"
-		"Current GFX Plugin: %s\n"
-		"Current DSP Plugin: %s\n"
-		"Current PAD Plugin[0]: %s\n"
-		"Current PAD Plugin[1]: %s\n"
-		"Current PAD Plugin[2]: %s\n"
-		"Current PAD Plugin[3]: %s\n"
-		"Current WiiMote Plugin[0]: %s\n",
+		"Default WiiMote Plugin: %s\n",
 		SConfig::GetInstance().m_DefaultGFXPlugin.c_str(),
 		SConfig::GetInstance().m_DefaultDSPPlugin.c_str(),
 		SConfig::GetInstance().m_DefaultPADPlugin.c_str(),
-		SConfig::GetInstance().m_DefaultWiiMotePlugin.c_str(),
-		Core::GetStartupParameter().m_strVideoPlugin.c_str(),
-		Core::GetStartupParameter().m_strDSPPlugin.c_str(),
-		Core::GetStartupParameter().m_strPadPlugin[0].c_str(),
-		Core::GetStartupParameter().m_strPadPlugin[1].c_str(),
-		Core::GetStartupParameter().m_strPadPlugin[2].c_str(),
-		Core::GetStartupParameter().m_strPadPlugin[3].c_str(),
-		Core::GetStartupParameter().m_strWiimotePlugin[0].c_str()
+		SConfig::GetInstance().m_DefaultWiiMotePlugin.c_str()
 		);
-
 }
 
 std::string Summarize_Settings()
@@ -99,200 +82,30 @@ std::string Summarize_Settings()
 		"Slot A: %d\n"
 		"Slot B: %d\n"
 		"Serial Port 1: %d\n"
-		"[Wii]Widescreen: %s\n"
-		"[Wii]Progressive Scan: %s\n"
-		"Run Compare Server: %s\n"
-		"Run Compare Client: %s\n"
-		"TLB Hack: %s\n",
-
-		Core::GetStartupParameter().bHLEBios?"True":"False",
-		Core::GetStartupParameter().bUseJIT?"True":"False",
-		Core::GetStartupParameter().bUseDualCore?"True":"False",
-		Core::GetStartupParameter().bDSPThread?"True":"False",
-		Core::GetStartupParameter().bSkipIdle?"True":"False",
-		Core::GetStartupParameter().bLockThreads?"True":"False",
-		Core::GetStartupParameter().bUseDualCore?"True":"False",
+		"Widescreen: %s\n"
+		"Progressive Scan: %s\n",
+		Core::GetStartupParameter().bHLEBios?"true":"false",
+		Core::GetStartupParameter().bUseJIT?"true":"false",
+		Core::GetStartupParameter().bUseDualCore?"true":"false",
+		Core::GetStartupParameter().bDSPThread?"true":"false",
+		Core::GetStartupParameter().bSkipIdle?"true":"false",
+		Core::GetStartupParameter().bLockThreads?"true":"false",
+		Core::GetStartupParameter().bUseDualCore?"true":"false",
 		Core::GetStartupParameter().m_strDefaultGCM.c_str(),
 		Core::GetStartupParameter().m_strDVDRoot.c_str(),
-		Core::GetStartupParameter().bOptimizeQuantizers?"True":"False",
-		Core::GetStartupParameter().bEnableCheats?"True":"False",
+		Core::GetStartupParameter().bOptimizeQuantizers?"true":"false",
+		Core::GetStartupParameter().bEnableCheats?"true":"false",
 		Core::GetStartupParameter().SelectedLanguage, //FIXME show language based on index
 		SConfig::GetInstance().m_strMemoryCardA.c_str(),
 		SConfig::GetInstance().m_strMemoryCardB.c_str(),
 		SConfig::GetInstance().m_EXIDevice[0], //FIXME
 		SConfig::GetInstance().m_EXIDevice[1], //FIXME
 		SConfig::GetInstance().m_EXIDevice[2], //FIXME
-		Core::GetStartupParameter().bWidescreen?"True":"False",
-		Core::GetStartupParameter().bProgressiveScan?"True":"False",
-		Core::GetStartupParameter().bRunCompareServer?"True":"False",
-		Core::GetStartupParameter().bRunCompareClient?"True":"False",
-		Core::GetStartupParameter().iTLBHack?"True":"False"//Fix me(Ment to be interger but caues problems with no ?"True":"False")
+		Core::GetStartupParameter().bWidescreen?"true":"false",
+		Core::GetStartupParameter().bProgressiveScan?"true":"false"
 		);
 }
 
-std::string Summarize_OpenGlPlug()
-{
-	OGLConfig OGLconfig;
-	OGLconfig.Load();
-	return StringFromFormat(
-		"OpenGl Plugin Settings\n\n"
-//General
-		"Fullscreen: %s\n"
-		"Hide Cursor: %s\n"
-		"Render to main frame: %s\n"
-		"V Sync: %s\n"
-//Resolution control
-
-//char iFSResolution[16];
-//char iWindowedRes[16];
-
-        "Use Native Resolution: %s\n"
-		"Keep 4:3 Aspect Ratio: %s\n"
-		"Keep 16:9 Aspect Ratio: %s\n"
-		"Crop screen: %s\n"
-//Enhancements
-		"Multi Sample Mode: %s\n"
-		"Force Filtering: %s\n"
-		"Max Anisotropy: %s\n"
-//Information
-        "Show FPS: %s\n"
-		"Overlay Stats: %s\n"
-		"Overlay Blend Stats: %s\n"
-		"Overlay Proj Stats: %s\n"
-		"Tex Fmt Overlay Enable: %s\n"
-		"Tex Fmt Overlay Center: %s\n"
-		"Show EFB Copy Regions: %s\n"
-//Render
-        "Wire Frame: %s\n"
-		"Disable Lighting: %s\n"
-		"Disable Texturing: %s\n"
-		"Dst Alpha Pass: %s\n"
-		"Disable Fog: %s\n"
-//Utility
-		"Dump Textures: %s\n"
-		"Dump EFB Target: %s\n"
-		"Dump Frames: %s\n"
-		"Free Look: %s\n"
-//Hacks
-		"EFB Copy Disable: %s\n"
-		"EFB Copy Disable Hot Key: %s\n"
-		"Projection Hax 1: %s\n"
-		"SMG hack: %s\n"
-		"Dump Frames: %s\n"
-		"Free Look: %s\n",
-
-//--------------------------------------\\
-///////////////////\\\\\\\\\\\\\\\\\\\\\\\
-
-
-//General
-		OGLconfig.bFullscreen?"True":"False",
-		OGLconfig.bHideCursor?"True":"False",
-		OGLconfig.renderToMainframe?"True":"False",
-		OGLconfig.bVSync?"True":"False",
-//Resolution control
-//char iFSResolution[16];
-//char iWindowedRes[16];
-		OGLconfig.bNativeResolution?"True":"False",
-		OGLconfig.bKeepAR43?"True":"False",
-		OGLconfig.bKeepAR169?"True":"False",
-		OGLconfig.bCrop?"True":"False",
-		OGLconfig.bAutoScale?"True":"False",
-//Enhancements
-		OGLconfig.iMultisampleMode?"True":"False",//Fix me(Ment to be interger but caues problems with no ?"True":"False")
-		OGLconfig.bForceFiltering?"True":"False",
-		OGLconfig.iMaxAnisotropy?"True":"False",//Fix me(Ment to be interger but caues problems with no ?"True":"False")
-//Information
-		OGLconfig.bShowFPS?"True":"False",
-		OGLconfig.bOverlayStats?"True":"False",
-		OGLconfig.bOverlayBlendStats?"True":"False",
-		OGLconfig.bOverlayProjStats?"True":"False",
-		OGLconfig.bTexFmtOverlayEnable?"True":"False",
-		OGLconfig.bTexFmtOverlayCenter?"True":"False",
-		OGLconfig.bShowEFBCopyRegions?"True":"False",
-//Render
-		OGLconfig.bWireFrame?"True":"False",
-		OGLconfig.bDisableTexturing?"True":"False",
-		OGLconfig.bDstAlphaPass?"True":"False",
-		OGLconfig.bDisableFog?"True":"False",
-//Utility
-		OGLconfig.bDumpTextures?"True":"False",
-		OGLconfig.bDumpEFBTarget?"True":"False",
-		OGLconfig.bDumpFrames?"True":"False",		
-		OGLconfig.bFreeLook?"True":"False",
-//Hacks
-		OGLconfig.bEFBCopyDisable?"True":"False",
-		OGLconfig.bEFBCopyDisableHotKey?"True":"False",
-		OGLconfig.bProjectionHax1?"True":"False",
-		OGLconfig.bSMGhack?"True":"False",
-		OGLconfig.bCopyEFBToRAM?"True":"False",		
-		OGLconfig.bSafeTextureCache?"True":"False"
-		);
-}
-
-std::string Summarize_D3DPlug()
-{
-	D3DConfig D3Dconfig;
-
-	D3Dconfig.Load();
-
-	return StringFromFormat(
-		"D3D Plugin Settings\n\n"
-
-		"Adapter: %s\n"
-		"Full Screen Resolution: %s\n"
-		"Post Process Effect: %s\n"
-		"Render to main frame: %s\n"
-		"Full Screen: %s\n"
-		"Vsync: %s\n"
-		"Wire frame: %s\n"
-		"Overlay Stats: %s\n"
-		"Overlay Proj Stats: %s\n"
-		"Dump Textures: %s\n"
-		"Dump Frames: %s\n"
-		"Old Card: %s\n"
-		"Show Shader Errors: %s\n"
-		"Force Max Aniso: %s\n"
-		"Pre Upscale: %s\n"
-		"Pre Upscale Filter: %s\n"
-		"True form: %s\n"
-		"True form Level: %s\n"
-		"Window Resolution: %s\n"
-//psProfile[16]
-//vsProfile[16]
-		"Tex Fmt Overlay Enable: %s\n"
-		"Tex Fmt Overlay Center: %s\n",
-//--------------------------------------\\
-
-		D3Dconfig.iAdapter?"True":"False",
-		D3Dconfig.iFSResolution?"True":"False",
-		D3Dconfig.iMultisampleMode?"True":"False",
-		D3Dconfig.iPostprocessEffect?"True":"False",
-		//Above have problems since they use integers and i have no idea how to get the working properly removing ?"True":"False" creates problems
-		D3Dconfig.renderToMainframe?"True":"False",
-		D3Dconfig.bFullscreen?"True":"False",
-		D3Dconfig.bVsync?"True":"False",
-		D3Dconfig.bWireFrame?"True":"False",
-		D3Dconfig.bOverlayStats?"True":"False",
-		D3Dconfig.bOverlayProjStats?"True":"False",
-		D3Dconfig.bDumpTextures?"True":"False",
-		D3Dconfig.bDumpFrames?"True":"False",
-		D3Dconfig.bOldCard?"True":"False",
-		D3Dconfig.bShowShaderErrors?"True":"False",
-		//Enhancements
-		D3Dconfig.bForceFiltering?"True":"False",
-		D3Dconfig.bForceMaxAniso?"True":"False",
-		D3Dconfig.bPreUpscale?"True":"False",
-		D3Dconfig.iPreUpscaleFilter?"True":"False",//May not work because of integar removing ?"True":"False" causes problems
-		D3Dconfig.bTruform?"True":"False",
-	    D3Dconfig.iTruformLevel?"True":"False",//May not work because of integar removing ?"True":"False" causes problems
-	    D3Dconfig.iWindowedRes?"True":"False",//May not work because of integar removing ?"True":"False" causes problems
-//psProfile[16]
-//vsProfile[16]
-		D3Dconfig.bTexFmtOverlayEnable?"True":"False",
-		D3Dconfig.bTexFmtOverlayCenter?"True":"False"
-		);
-}
 
 void wxInfoWindow::Init_ChildControls()
 {
@@ -303,15 +116,13 @@ void wxInfoWindow::Init_ChildControls()
 	for (int i = 0; drives[i] != NULL && i < 24; i++)
 	{
 
-		Info.append(StringFromFormat("\nCD/DVD Drive %d: %s", i+1, drives[i]));
+		Info.append(StringFromFormat("\nCD/DVD Drive%d: %s", i+1, drives[i]));
 	}
 	Info.append(StringFromFormat(
-		"\n\nPlugin Information\n\n%s\n%s\n%s\n%s\n"
+		"\n\nPlugin Information\n\n%s\n%s\n"
 		"Processor Information:\n%s\n",
 		Summarize_Plug().c_str(),
 		Summarize_Settings().c_str(),
-		Summarize_OpenGlPlug().c_str(),
-		Summarize_D3DPlug().c_str(),
 		cpu_info.Summarize().c_str()));
 	
 	// Main Notebook
@@ -361,5 +172,4 @@ void wxInfoWindow::OnEvent_Window_Close(wxCloseEvent& WXUNUSED (event))
 {
 	Destroy();
 }
-
 
