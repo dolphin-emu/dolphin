81354ea368710a7001b51a8bd947bcc892a18f6a
death2droid <death2droid@gmail.com>
2009-05-30 05:52:12 +0000

    Some clean ups to make it easier to work with the InfoWindow
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@3298 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/DolphinWX/Src/InfoWindow.cpp
+++ Source/Core/DolphinWX/Src/InfoWindow.cpp
@@ -47,102 +47,17 @@ wxInfoWindow::~wxInfoWindow()
 	// On Disposal
 }
 
-std::string Summarize_Plug()
-{
-	return StringFromFormat(
-		"Plugin Information\n\n"
-		"Default GFX Plugin: %s\n"
-		"Default DSP Plugin: %s\n"
-		"Default PAD Plugin: %s\n"
-		"Default WiiMote Plugin: %s\n\n"
-		"Current GFX Plugin: %s\n"
-		"Current DSP Plugin: %s\n"
-		"Current PAD Plugin[0]: %s\n"
-		"Current PAD Plugin[1]: %s\n"
-		"Current PAD Plugin[2]: %s\n"
-		"Current PAD Plugin[3]: %s\n"
-		"Current WiiMote Plugin[0]: %s\n",
-		SConfig::GetInstance().m_DefaultGFXPlugin.c_str(),
-		SConfig::GetInstance().m_DefaultDSPPlugin.c_str(),
-		SConfig::GetInstance().m_DefaultPADPlugin.c_str(),
-		SConfig::GetInstance().m_DefaultWiiMotePlugin.c_str(),
-		Core::GetStartupParameter().m_strVideoPlugin.c_str(),
-		Core::GetStartupParameter().m_strDSPPlugin.c_str(),
-		Core::GetStartupParameter().m_strPadPlugin[0].c_str(),
-		Core::GetStartupParameter().m_strPadPlugin[1].c_str(),
-		Core::GetStartupParameter().m_strPadPlugin[2].c_str(),
-		Core::GetStartupParameter().m_strPadPlugin[3].c_str(),
-		Core::GetStartupParameter().m_strWiimotePlugin[0].c_str()
-		);
-}
-
-std::string Summarize_Settings()
-{
-	return StringFromFormat(
-		"Dolphin Settings\n\n"
-		"Always HLE Bios: %s\n"
-		"Use Dynarec: %s\n"
-		"Use Dual Core: %s\n"
-		"DSP Thread: %s\n"
-		"Skip Idle: %s\n"
-		"Lock Threads: %s\n"
-		"Use Dual Core: %s\n"
-		"Default GCM: %s\n"
-		"DVD Root: %s\n"
-		"Optimize Quantizers: %s\n"
-		"Enable Cheats: %s\n"
-		"Selected Language: %d\n"
-		"Memcard A: %s\n"
-		"Memcard B: %s\n"
-		"Slot A: %d\n"
-		"Slot B: %d\n"
-		"Serial Port 1: %d\n"
-		"Run Compare Server: %s\n"
-		"Run Compare Client: %s\n"
-		"TLB Hack: %s\n"
-		"[Wii]Widescreen: %s\n"
-		"[Wii]Progressive Scan: %s\n",
-		Core::GetStartupParameter().bHLEBios?"True":"False",
-		Core::GetStartupParameter().bUseJIT?"True":"False",
-		Core::GetStartupParameter().bUseDualCore?"True":"False",
-		Core::GetStartupParameter().bDSPThread?"True":"False",
-		Core::GetStartupParameter().bSkipIdle?"True":"False",
-		Core::GetStartupParameter().bLockThreads?"True":"False",
-		Core::GetStartupParameter().bUseDualCore?"True":"False",
-		Core::GetStartupParameter().m_strDefaultGCM.c_str(),
-		Core::GetStartupParameter().m_strDVDRoot.c_str(),
-		Core::GetStartupParameter().bOptimizeQuantizers?"True":"False",
-		Core::GetStartupParameter().bEnableCheats?"True":"False",
-		Core::GetStartupParameter().SelectedLanguage, //FIXME show language based on index
-		SConfig::GetInstance().m_strMemoryCardA.c_str(),
-		SConfig::GetInstance().m_strMemoryCardB.c_str(),
-		SConfig::GetInstance().m_EXIDevice[0], //FIXME
-		SConfig::GetInstance().m_EXIDevice[1], //FIXME
-		SConfig::GetInstance().m_EXIDevice[2], //FIXME
-		Core::GetStartupParameter().bRunCompareServer?"True":"False",
-		Core::GetStartupParameter().bRunCompareClient?"True":"False",
-		Core::GetStartupParameter().iTLBHack?"True":"False",
-		Core::GetStartupParameter().bWidescreen?"True":"False",
-		Core::GetStartupParameter().bProgressiveScan?"True":"False"
-		);
-}
-
+#include "Summarize.h";//Just used this to keep stuff clean.
 
 void wxInfoWindow::Init_ChildControls()
 {
 	std::string Info;
 	Info = StringFromFormat("Dolphin Revision: %s", SVN_REV_STR);
 
-	char ** drives = cdio_get_devices();
-	for (int i = 0; drives[i] != NULL && i < 24; i++)
-	{
-
-		Info.append(StringFromFormat("\nCD/DVD Drive%d: %s", i+1, drives[i]));
-	}
 	Info.append(StringFromFormat(
-		"\n\nProcessor Information:\n%s\n"
-		"\n%s\n\n%s\n",
-		cpu_info.Summarize().c_str(),
+		"\n\n%s\n\n%s\n%s\n\n%s\n\n",
+		Summarize_Drives().c_str(),
+		Summarize_CPU().c_str(),
 		Summarize_Plug().c_str(),
 		Summarize_Settings().c_str()
 		));
