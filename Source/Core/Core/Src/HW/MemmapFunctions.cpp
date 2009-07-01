0905dbc833a8a8126da8b6e303ca616217893d7d
nakeee <nakeee@gmail.com>
2009-07-01 19:41:19 +0000

    in gcc enum is not a namespace
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@3633 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/Core/Src/HW/MemmapFunctions.cpp
+++ Source/Core/Core/Src/HW/MemmapFunctions.cpp
@@ -129,10 +129,10 @@ void ReadFromHardware(T &_var, u32 em_address, u32 effective_address, Memory::XC
 			int x = (em_address & 0xfff) >> 2;
 			int y = (em_address >> 12) & 0x3ff;
 			if (em_address & 0x00400000) {
-				_var = CPluginManager::GetInstance().GetVideo()->Video_AccessEFB(EFBAccessType::PEEK_Z, x, y);
+				_var = CPluginManager::GetInstance().GetVideo()->Video_AccessEFB(PEEK_Z, x, y);
 				DEBUG_LOG(MEMMAP, "EFB Z Read @ %i, %i\t= 0x%08x", x, y, _var);
 			} else {
-				_var = CPluginManager::GetInstance().GetVideo()->Video_AccessEFB(EFBAccessType::PEEK_COLOR, x, y);
+				_var = CPluginManager::GetInstance().GetVideo()->Video_AccessEFB(PEEK_COLOR, x, y);
 				DEBUG_LOG(MEMMAP, "EFB Color Read @ %i, %i\t= 0x%08x", x, y, _var);
 			}
 		}
@@ -204,10 +204,10 @@ void WriteToHardware(u32 em_address, const T data, u32 effective_address, Memory
 			int y = (em_address >> 12) & 0x3ff;
 			// TODO figure out a way to send data without falling into the template trap
 			if (em_address & 0x00400000) {
-				CPluginManager::GetInstance().GetVideo()->Video_AccessEFB(EFBAccessType::POKE_Z, x, y);
+				CPluginManager::GetInstance().GetVideo()->Video_AccessEFB(POKE_Z, x, y);
 				DEBUG_LOG(MEMMAP, "EFB Z Write %08x @ %i, %i", data, x, y);
 			} else {
-				CPluginManager::GetInstance().GetVideo()->Video_AccessEFB(EFBAccessType::POKE_COLOR, x, y);
+				CPluginManager::GetInstance().GetVideo()->Video_AccessEFB(POKE_COLOR, x, y);
 				DEBUG_LOG(MEMMAP, "EFB Color Write %08x @ %i, %i", data, x, y);
 			}
 			return;
