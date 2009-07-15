4533cc39b3b97a2ead51ebc00647d21b22c9f8bd
hrydgard <hrydgard@gmail.com>
2009-07-15 19:09:02 +0000

    Revert the ridiculously wrong r3797 (bad sonicadvance1)
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@3806 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/Core/Src/PowerPC/Interpreter/Interpreter_FloatingPoint.cpp
+++ Source/Core/Core/Src/PowerPC/Interpreter/Interpreter_FloatingPoint.cpp
@@ -35,11 +35,7 @@
 #include "../../Core.h"
 #include "Interpreter.h"
 #include "MathUtil.h"
-#if !defined(_mm_cvttsd_si32) // No SSE2 support
-#define _mm_set_sd
-#define _mm_cvttsd_si32 truncl
-#define _mm_cvtsd_si32 lrint
-#endif
+
 // F-ZERO IS BEING A ROYAL PAIN
 // POSSIBLE APPROACHES:
 // * Full SW FPU. Urgh.
