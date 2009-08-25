2e24e7db06ce685f8132d8b0d9c8bd97d2a74f56
XTra.KrazzY <XTra.KrazzY@gmail.com>
2009-08-25 19:48:38 +0000

    Pikmin 2 for the Wii now has correct sound thanks to LordMark. Too bad the mail stops coming after a while
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4064 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Plugins/Plugin_DSP_HLE/Src/UCodes/UCode_Zelda.h
+++ Source/Plugins/Plugin_DSP_HLE/Src/UCodes/UCode_Zelda.h
@@ -184,6 +184,9 @@ private:
 	{
 		switch (m_CRC)
 		{
+			case 0xb7eb9a9c: // Wii Pikmin - PAL
+			case 0xeaeb38cc: // Wii Pikmin 2 - PAL
+			case 0x6c3f6f94: // zelda - PAL
 			case 0xD643001F: // Super Mario Galaxy
 				return true;
 			default:
