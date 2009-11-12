013a27b13b05e3af4cdf0781b755d1bddbb606f5
John Peterson <jpeterson57@gmail.com>
2009-11-12 19:26:25 +0000

    Externals > Fixed SDL include
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4538 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/DolphinWX/Src/Frame.cpp
+++ Source/Core/DolphinWX/Src/Frame.cpp
@@ -232,6 +232,7 @@ EVT_MENU(IDM_BROWSE, CFrame::OnBrowse)
 EVT_MENU(IDM_MEMCARD, CFrame::OnMemcard)
 EVT_MENU(IDM_CHEATS, CFrame::OnShow_CheatsWindow)
 EVT_MENU(IDM_INFO, CFrame::OnShow_InfoWindow)
+EVT_MENU(IDM_RESTART, CFrame::OnRestart)
 EVT_MENU(IDM_CHANGEDISC, CFrame::OnChangeDisc)
 EVT_MENU(IDM_LOAD_WII_MENU, CFrame::OnLoadWiiMenu)
 EVT_MENU(IDM_TOGGLE_FULLSCREEN, CFrame::OnToggleFullscreen)
@@ -491,6 +492,24 @@ CFrame::~CFrame()
 
 void CFrame::OnQuit(wxCommandEvent& WXUNUSED (event))
 {
+	Close(true);
+}
+
+void CFrame::OnRestart(wxCommandEvent& WXUNUSED (event))
+{
+	if (Core::GetState() != Core::CORE_UNINITIALIZED)
+	{
+		wxMessageBox(wxT("Please stop the current game before restarting."), wxT("Notice"), wxOK, this);
+		return;
+	}
+	// Get exe path and restart
+	#ifdef _WIN32
+		char Str[MAX_PATH + 1];
+		DWORD Size = sizeof(Str)/sizeof(char);
+		DWORD n = GetModuleFileNameA(NULL, Str, Size);
+		ShellExecuteA(NULL, "open", Str, g_pCodeWindow ? "" : "-d", NULL, SW_SHOW);
+	#endif
+
 	Close(true);
 }
 
