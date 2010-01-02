dolphin-emu/dolphin

commit 6e87ef943f8bd084d21fe565841f4f6c01744136

Sorry, this is the working fix for r4759
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4762 8ced0084-cf51-0410-be5f-012b33b47a6e

Source/Core/VideoCommon/Src/CommandProcessor.cpp
@@ -184,7 +184,6 @@ void Read16(u16& _rReturnValue, const u32 _Address)
 		//m_CPStatusReg.ReadIdle = 1;
 		//m_CPStatusReg.CommandIdle = 1;
 
-		//m_CPStatusReg.ReadIdle = fifo.CPReadIdle; // This seems not necessary though
 		m_CPStatusReg.Breakpoint = fifo.bFF_Breakpoint;
 		_rReturnValue = m_CPStatusReg.Hex;
 
@@ -198,6 +197,7 @@ void Read16(u16& _rReturnValue, const u32 _Address)
 			, m_CPStatusReg.OverflowHiWatermark ?	"ON" : "OFF"
 			, m_CPStatusReg.UnderflowLoWatermark ?	"ON" : "OFF"
 				);
+
 		return;
 
 	case CTRL_REGISTER:		_rReturnValue = m_CPCtrlReg.Hex; return;
@@ -411,11 +411,11 @@ void Write16(const u16 _Value, const u32 _Address)
 			}
 
 			DEBUG_LOG(COMMANDPROCESSOR,"\t write to CTRL_REGISTER : %04x", _Value);
-			DEBUG_LOG(COMMANDPROCESSOR, "\t GPREAD %s | LINK %s | BP %s || Int %s | OvF %s | UndF %s"
+			DEBUG_LOG(COMMANDPROCESSOR, "\t GPREAD %s | LINK %s | BP %s || Init %s | OvF %s | UndF %s"
 				, fifo.bFF_GPReadEnable ?				"ON" : "OFF"
 				, fifo.bFF_GPLinkEnable ?				"ON" : "OFF"
 				, fifo.bFF_BPEnable ?					"ON" : "OFF"
-				, m_CPCtrlReg.CPIntEnable ?				"ON" : "OFF"
+				, m_CPCtrlReg.BPInit ?					"ON" : "OFF"
 				, m_CPCtrlReg.FifoOverflowIntEnable ?	"ON" : "OFF"
 				, m_CPCtrlReg.FifoUnderflowIntEnable ?	"ON" : "OFF"
 				);
@@ -649,7 +649,7 @@ void CatchUpGPU()
 				if (
 					(fifo.CPReadPointer == fifo.CPBreakpoint) ||
 					(fifo.CPReadPointer == fifo.CPWritePointer) ||
-					(fifo.CPWritePointer < fifo.CPBreakpoint)
+					(fifo.CPReadPointer > fifo.CPBreakpoint && fifo.CPBreakpoint > fifo.CPWritePointer)
 					)
 				{
 					//_assert_msg_(POWERPC,0,"BP: %08x",fifo.CPBreakpoint);

Source/Core/VideoCommon/Src/Fifo.cpp
@@ -126,19 +126,19 @@ void Fifo_SendFifoData(u8* _uData, u32 len)
 // Purpose: Keep the Core HW updated about the CPU-GPU distance
 void Fifo_EnterLoop(const SVideoInitialize &video_initialize)
 {
-    fifoStateRun = true;
-    SCPFifoStruct &_fifo = CommandProcessor::fifo;
+	fifoStateRun = true;
+	SCPFifoStruct &_fifo = CommandProcessor::fifo;
 	s32 distToSend;
 
-    while (fifoStateRun)
-    {
+	while (fifoStateRun)
+	{
 		video_initialize.pPeekMessages();
 
 		VideoFifo_CheckEFBAccess();
 		VideoFifo_CheckSwapRequest();
 
-        // check if we are able to run this buffer
-		while (_fifo.bFF_GPReadEnable && _fifo.CPReadWriteDistance && !(_fifo.bFF_BPEnable && _fifo.bFF_Breakpoint))
+		// check if we are able to run this buffer
+		while (_fifo.bFF_GPReadEnable && ((!_fifo.bFF_BPEnable && _fifo.CPReadWriteDistance) || (_fifo.bFF_BPEnable && !_fifo.bFF_Breakpoint)))
 		{
 			if (!fifoStateRun)
 				break;
@@ -157,7 +157,7 @@ void Fifo_EnterLoop(const SVideoInitialize &video_initialize)
 				if (
 					(readPtr == _fifo.CPBreakpoint) ||
 					(readPtr == _fifo.CPWritePointer) ||
-					(_fifo.CPWritePointer < _fifo.CPBreakpoint)
+					(readPtr > _fifo.CPBreakpoint && _fifo.CPBreakpoint > _fifo.CPWritePointer)
 					)
 				{
 					Common::AtomicStore(_fifo.bFF_Breakpoint, 1);
@@ -172,6 +172,7 @@ void Fifo_EnterLoop(const SVideoInitialize &video_initialize)
 				else
 					readPtr += 32;
 			}
+			// If we are not in BP mode we send all the chunk we have to speed up
 			else
 			{
 				distToSend = _fifo.CPReadWriteDistance;
@@ -202,8 +203,7 @@ void Fifo_EnterLoop(const SVideoInitialize &video_initialize)
 			VideoFifo_CheckSwapRequest();
 
 			CommandProcessor::SetFifoIdleFromVideoPlugin();
-        }
-
+		}
 		Common::YieldCPU();
 	}
 	fifo_exit_event.Set();

commit 92ef4f115638a677f2e6a1f2baff6da537b82a19

1. Fixed the freeze issue in "Super Mario Galaxy" and other games, which was introduced since r4762
2. Fixed Issue 1909
   Multi-Nunchuck control problem

3. Updated SYSCONF, which should enable games that require MotionPlus, such as "Wii Sports Resort"

git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4773 8ced0084-cf51-0410-be5f-012b33b47a6e

Source/Core/VideoCommon/Src/CommandProcessor.cpp
@@ -158,6 +158,7 @@ void Init()
 	memset(&fifo,0,sizeof(fifo));
 	fifo.CPCmdIdle  = 1 ;
 	fifo.CPReadIdle = 1;
+	fifo.bFF_Breakpoint = 1;
 
 	s_fifoIdleEvent.Init();
 
@@ -564,7 +565,7 @@ bool AllowIdleSkipping()
 // if not then lock CPUThread until GP finish a frame.
 void WaitForFrameFinish()
 {
-	while ((fake_GPWatchdogLastToken == fifo.Fake_GPWDToken) && fifo.bFF_GPReadEnable && fifo.CPReadWriteDistance && !fifo.bFF_Breakpoint)
+	while ((fake_GPWatchdogLastToken == fifo.Fake_GPWDToken) && fifo.bFF_GPReadEnable && ((!fifo.bFF_BPEnable && fifo.CPReadWriteDistance) || (fifo.bFF_BPEnable && !fifo.bFF_Breakpoint)));
 		s_fifoIdleEvent.MsgWait();
 	
 	fake_GPWatchdogLastToken = fifo.Fake_GPWDToken;
@@ -605,7 +606,7 @@ void STACKALIGN GatherPipeBursted()
 			//		- CPU can write to fifo
 			//		- disable Underflow interrupt
 
-			INFO_LOG(COMMANDPROCESSOR, "(GatherPipeBursted): CPHiWatermark reached, 0x%04X, 0x%04X", fifo.CPReadWriteDistance, fifo.CPLoWatermark);
+			INFO_LOG(COMMANDPROCESSOR, "(GatherPipeBursted): CPHiWatermark (Hi: 0x%04x, Lo: 0x%04x) reached (RWDistance: 0x%04x)", fifo.CPHiWatermark, fifo.CPLoWatermark, fifo.CPReadWriteDistance);
 			// Wait for GPU to catch up
 			while (fifo.CPReadWriteDistance > fifo.CPLoWatermark && !fifo.bFF_Breakpoint)
 				s_fifoIdleEvent.MsgWait();

Source/Core/VideoCommon/Src/Fifo.cpp
@@ -164,7 +164,6 @@ void Fifo_EnterLoop(const SVideoInitialize &video_initialize)
 					CommandProcessor::UpdateInterruptsFromVideoPlugin(true);
 					break;
 				}
-
 				distToSend = 32;
 
 				if ( readPtr >= _fifo.CPEnd) 
@@ -201,9 +200,9 @@ void Fifo_EnterLoop(const SVideoInitialize &video_initialize)
 			// leading the CPU thread to wait in Video_BeginField or Video_AccessEFB thus slowing things down.
 			VideoFifo_CheckEFBAccess();
 			VideoFifo_CheckSwapRequest();
-
-			CommandProcessor::SetFifoIdleFromVideoPlugin();
 		}
+
+		CommandProcessor::SetFifoIdleFromVideoPlugin();
 		Common::YieldCPU();
 	}
 	fifo_exit_event.Set();

Source/Plugins/Plugin_Wiimote/Src/EmuDefinitions.cpp
@@ -54,7 +54,7 @@ bool g_ReportingAuto[MAX_WIIMOTES]; // Auto report or passive report
 u8 g_ReportingMode[MAX_WIIMOTES]; // The reporting mode and channel id
 u16 g_ReportingChannel[MAX_WIIMOTES];
 
-wiimote_key g_ExtKey; // The extension encryption key
+wiimote_key g_ExtKey[MAX_WIIMOTES]; // The extension encryption key
 bool g_Encryption; // Encryption on or off
 
 // Gamepad input

Source/Plugins/Plugin_Wiimote/Src/EmuDefinitions.h
@@ -86,7 +86,7 @@ extern bool g_ReportingAuto[MAX_WIIMOTES];
 extern u8 g_ReportingMode[MAX_WIIMOTES];
 extern u16 g_ReportingChannel[MAX_WIIMOTES];
 
-extern wiimote_key g_ExtKey; // extension encryption key
+extern wiimote_key g_ExtKey[MAX_WIIMOTES]; // extension encryption key
 extern bool g_Encryption;
 
 /* An example of a factory default first bytes of the Eeprom memory. There are differences between

Source/Plugins/Plugin_Wiimote/Src/EmuMain.cpp
@@ -481,7 +481,6 @@ void DoState(PointerWrap &p)
 	p.DoArray(g_RegExtTmp, WIIMOTE_REG_EXT_SIZE);
 	p.DoArray(g_RegIr, WIIMOTE_REG_IR_SIZE);
 
-	p.Do(g_ExtKey);
 	p.Do(g_Encryption);
 
 	//p.Do(NumPads);
@@ -500,6 +499,7 @@ void DoState(PointerWrap &p)
 		p.Do(g_ReportingChannel[i]);
 		//p.Do(g_IR[i]);
 		p.Do(g_Leds[i]);
+		p.Do(g_ExtKey[i]);
 	}
 	return;
 }

Source/Plugins/Plugin_Wiimote/Src/EmuSubroutines.cpp
@@ -267,7 +267,7 @@ void WmReadData(u16 _channelID, wm_read_data* rd)
 				memcpy(g_RegExtTmp, g_RegExt[g_ID], sizeof(g_RegExt[0]));
 
 				// Encrypt g_RegExtTmp at that location
-				wiimote_encrypt(&g_ExtKey, &g_RegExtTmp[address & 0xffff], (address & 0xffff), (u8)size);
+				wiimote_encrypt(&g_ExtKey[g_ID], &g_RegExtTmp[address & 0xffff], (address & 0xffff), (u8)size);
 
 				// Update the block that SendReadDataReply will eventually send to the Wii
 				block = g_RegExtTmp;
@@ -446,7 +446,7 @@ void WmWriteData(u16 _channelID, wm_write_data* wd)
 			   that we send it parts of a key, only the last full key will have an
 			   effect */
 			if(address >= 0x40 && address <= 0x4c)
-				wiimote_gen_key(&g_ExtKey, &g_RegExt[g_ID][0x40]);
+				wiimote_gen_key(&g_ExtKey[g_ID], &g_RegExt[g_ID][0x40]);
 		}
 
 	}

Source/Plugins/Plugin_Wiimote/Src/FillReport.cpp
@@ -815,7 +815,7 @@ void FillReportExtension(wm_extension& _ext)
 	// Copy the data to it
 	memcpy(Tmp, &_ext, sizeof(_ext));
 	// Encrypt it
-	wiimote_encrypt(&g_ExtKey, Tmp, 0x00, sizeof(_ext));
+	wiimote_encrypt(&g_ExtKey[g_ID], Tmp, 0x00, sizeof(_ext));
 	// Write it back to the struct
 	memcpy(&_ext, Tmp, sizeof(_ext));
 }
@@ -1077,7 +1077,7 @@ void FillReportClassicExtension(wm_classic_extension& _ext)
 	// Copy the data to it
 	memcpy(Tmp, &_ext, sizeof(_ext));
 	// Encrypt it
-	wiimote_encrypt(&g_ExtKey, Tmp, 0x00, sizeof(_ext));
+	wiimote_encrypt(&g_ExtKey[g_ID], Tmp, 0x00, sizeof(_ext));
 	// Write it back to the struct
 	memcpy(&_ext, Tmp, sizeof(_ext));
 }
@@ -1172,7 +1172,7 @@ void FillReportGuitarHero3Extension(wm_GH3_extension& _ext)
 	// Copy the data to it
 	memcpy(Tmp, &_ext, sizeof(_ext));
 	// Encrypt it
-	wiimote_encrypt(&g_ExtKey, Tmp, 0x00, sizeof(_ext));
+	wiimote_encrypt(&g_ExtKey[g_ID], Tmp, 0x00, sizeof(_ext));
 	// Write it back to the struct
 	memcpy(&_ext, Tmp, sizeof(_ext));
 }