0ec6d85b2f488d4c5c3cd084576985c19cf66bc4
LPFaint99 <lpfaint99@gmail.com>
2010-01-14 16:30:20 +0000

    add correct copyright to WiiSaveCrypted.cpp
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4835 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/DolphinWX/Src/MemoryCards/WiiSaveCrypted.cpp
+++ Source/Core/DolphinWX/Src/MemoryCards/WiiSaveCrypted.cpp
@@ -16,6 +16,9 @@
 // http://code.google.com/p/dolphin-emu/
 
 // Based off of tachtig/twintig http://git.infradead.org/?p=users/segher/wii.git
+// Copyright 2007,2008  Segher Boessenkool  <segher@kernel.crashing.org>
+// Licensed under the terms of the GNU GPL, version 2
+// http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 
 #include "WiiSaveCrypted.h"
