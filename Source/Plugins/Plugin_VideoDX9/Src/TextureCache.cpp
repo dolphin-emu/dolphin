cb1038dd9995e4af3651f24548bf5e4e93b06f6c
death2droid <death2droid@gmail.com>
2010-01-21 10:23:13 +0000

    DX:Change the DirectX plugins texture dumping format back to PNG
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4914 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Plugins/Plugin_VideoDX9/Src/TextureCache.cpp
+++ Source/Plugins/Plugin_VideoDX9/Src/TextureCache.cpp
@@ -453,10 +453,10 @@ TextureCache::TCacheEntry *TextureCache::Load(int stage, u32 address, int width,
 			bCheckedDumpDir = true;
 		}
 
-		sprintf(szTemp, "%s/%s_%08x_%i.bmp", szDir, uniqueId, texHash, tex_format);
+		sprintf(szTemp, "%s/%s_%08x_%i.png", szDir, uniqueId, texHash, tex_format);
 
 		if (!File::Exists(szTemp))
-			D3DXSaveTextureToFileA(szTemp,D3DXIFF_BMP,entry.texture,0);
+			D3DXSaveTextureToFileA(szTemp,D3DXIFF_PNG,entry.texture,0);
 	}
 
 	INCSTAT(stats.numTexturesCreated);
