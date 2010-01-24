dolphin-emu/dolphin

commit 2f8a6f86b97bd6c78a27f86af5247666f2db47f5

quiet some compile-time warnings
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4941 8ced0084-cf51-0410-be5f-012b33b47a6e

Source/Core/Core/Src/Debugger/PPCDebugInterface.h
@@ -13,7 +13,7 @@ public:
 	PPCDebugInterface(){} 
 	virtual void disasm(unsigned int address, char *dest, int max_size);
 	virtual void getRawMemoryString(int memory, unsigned int address, char *dest, int max_size);
-	virtual int getInstructionSize(int instruction) {return 4;}
+	virtual int getInstructionSize(int inst_size = 4) {return inst_size;}
 	virtual bool isAlive();
 	virtual bool isBreakpoint(unsigned int address);
 	virtual void setBreakpoint(unsigned int address);

Source/Core/Common/Src/LinearDiskCache.cpp
@@ -79,8 +79,8 @@ int LinearDiskCache::OpenAndRead(const char *filename, LinearDiskCacheReader *re
 			// We're past the header already thanks to ValidateHeader.
 			while (!feof(file_)) {
 				int key_size, value_size;
-				int key_size_size = fread(&key_size, 1, sizeof(key_size), file_);
-				int value_size_size = fread(&value_size, 1, sizeof(value_size), file_);
+				size_t key_size_size = fread(&key_size, 1, sizeof(key_size), file_);
+				size_t value_size_size = fread(&value_size, 1, sizeof(value_size), file_);
 				if (key_size_size == 0 && value_size_size == 0) {
 					// I guess feof isn't doing it's job - we're at the end.
 					break;

Source/Core/Core/Src/HW/ProcessorInterface.cpp
@@ -39,7 +39,7 @@ enum
 	PI_FIFO_RESET           = 0x18, // ??? - GXAbortFrame writes to it
 	PI_RESET_CODE           = 0x24,
 	PI_FLIPPER_REV          = 0x2C,
-	PI_UNKNOWN				= 0x30 // ??? - BS1 writes to it
+	PI_UNKNOWN				= 0x30 // BS1 writes 0x0245248A to it - prolly some bootstrap thing
 };
 
 

Source/Core/Core/Src/IPC_HLE/WII_IPC_HLE_Device.h
@@ -34,22 +34,33 @@ public:
 		m_Active(false)
 	{}
 
-	virtual ~IWII_IPC_HLE_Device()
-	{}
+	virtual ~IWII_IPC_HLE_Device() { }
 
-	virtual void DoState(PointerWrap &p)
-	{}
+	virtual void DoState(PointerWrap&) { }
 
-	const std::string& GetDeviceName() const { return m_Name; } 
-    u32 GetDeviceID() const { return m_DeviceID; } 
+	const std::string& GetDeviceName() const { return m_Name; }
+    u32 GetDeviceID() const { return m_DeviceID; }
 
-    virtual bool Open(u32 _CommandAddress, u32 _Mode)  { _dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support Open()", m_Name.c_str()); m_Active = true; return true; }
-    virtual bool Close(u32 _CommandAddress, bool _bForce = false)  { _dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support Close()", m_Name.c_str()); m_Active = false; return true; }
-    virtual bool Seek(u32 _CommandAddress) { _dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support Seek()", m_Name.c_str()); return true; }
-	virtual bool Read(u32 _CommandAddress) { _dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support Read()", m_Name.c_str()); return true; }
-	virtual bool Write(u32 _CommandAddress) { _dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support Write()", m_Name.c_str()); return true; }
-	virtual bool IOCtl(u32 _CommandAddress) { _dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support IOCtl()", m_Name.c_str()); return true; }
-	virtual bool IOCtlV(u32 _CommandAddress) { _dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support IOCtlV()", m_Name.c_str()); return true; }
+    virtual bool Open(u32 _CommandAddress, u32 _Mode) {
+		(void)_CommandAddress; (void)_Mode;
+		_dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support Open()", m_Name.c_str());
+		m_Active = true;
+		return true;
+	}
+    virtual bool Close(u32 _CommandAddress, bool _bForce = false) {
+		(void)_CommandAddress; (void)_bForce;
+		_dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support Close()", m_Name.c_str());
+		m_Active = false;
+		return true;
+	}
+
+#define UNIMPLEMENTED_CMD(cmd) _dbg_assert_msg_(WII_IPC_HLE, 0, "%s does not support "##cmd##"()", m_Name.c_str()); return true;
+	virtual bool Seek	(u32) { UNIMPLEMENTED_CMD("Seek") }
+	virtual bool Read	(u32) { UNIMPLEMENTED_CMD("Read") }
+	virtual bool Write	(u32) { UNIMPLEMENTED_CMD("Write") }
+	virtual bool IOCtl	(u32) { UNIMPLEMENTED_CMD("IOCtl") }
+	virtual bool IOCtlV	(u32) { UNIMPLEMENTED_CMD("IOCtlV") }
+#undef UNIMPLEMENTED_CMD
 
 	virtual u32 Update() { return 0; }
 
@@ -152,8 +163,8 @@ protected:
     }
 
 	
-    void DumpAsync(u32 BufferVector, u32 _CommandAddress, u32 NumberInBuffer, u32 NumberOutBuffer
-		,LogTypes::LOG_TYPE LogType = LogTypes::WII_IPC_HLE,
+    void DumpAsync(u32 BufferVector, u32 NumberInBuffer, u32 NumberOutBuffer,
+		LogTypes::LOG_TYPE LogType = LogTypes::WII_IPC_HLE,
 		LogTypes::LOG_LEVELS Verbosity = LogTypes::LDEBUG)
     {
 		GENERIC_LOG(LogType, Verbosity, "======= DumpAsync ======");

Source/Core/Core/Src/IPC_HLE/WII_IPC_HLE_Device_sdio_slot0.cpp
@@ -225,7 +225,7 @@ bool CWII_IPC_HLE_Device_sdio_slot0::IOCtlV(u32 _CommandAddress)
 		break;
 	}
 
-	//DumpAsync(CommandBuffer.BufferVector, _CommandAddress, CommandBuffer.NumberInBuffer, CommandBuffer.NumberPayloadBuffer, LogTypes::WII_IPC_SD);
+	//DumpAsync(CommandBuffer.BufferVector, CommandBuffer.NumberInBuffer, CommandBuffer.NumberPayloadBuffer, LogTypes::WII_IPC_SD);
 
 	Memory::Write_U32(ReturnValue, _CommandAddress + 0x4);
 

Source/Core/Core/Src/IPC_HLE/WII_IPC_HLE_Device_usb.cpp
@@ -233,7 +233,7 @@ bool CWII_IPC_HLE_Device_usb_oh1_57e_305::IOCtlV(u32 _CommandAddress)
 					m_ACLSetup = CommandBuffer.m_Address;
 
 					#if defined(_DEBUG) || defined(DEBUGFAST)
-					DumpAsync(CommandBuffer.BufferVector, _CommandAddress, CommandBuffer.NumberInBuffer, CommandBuffer.NumberPayloadBuffer);
+					DumpAsync(CommandBuffer.BufferVector, CommandBuffer.NumberInBuffer, CommandBuffer.NumberPayloadBuffer);
 					#endif
 
 					CtrlBuffer BulkBuffer(_CommandAddress);
@@ -308,7 +308,7 @@ bool CWII_IPC_HLE_Device_usb_oh1_57e_305::IOCtlV(u32 _CommandAddress)
 			DEBUG_LOG(WII_IPC_WIIMOTE, "    PayloadAddr: 0x%08x", CommandBuffer.PayloadBuffer[0].m_Address);
 			DEBUG_LOG(WII_IPC_WIIMOTE, "    PayloadSize: 0x%08x", CommandBuffer.PayloadBuffer[0].m_Size);
 			#if defined(_DEBUG) || defined(DEBUGFAST)
-			DumpAsync(CommandBuffer.BufferVector, _CommandAddress, CommandBuffer.NumberInBuffer, CommandBuffer.NumberPayloadBuffer);
+			DumpAsync(CommandBuffer.BufferVector, CommandBuffer.NumberInBuffer, CommandBuffer.NumberPayloadBuffer);
 			#endif
 		}
 		break;

commit 675e9845147243011036d1639682dab1452925cb

fix issue caused by r4941: ppc debugger would think the instruction size was 0, which is rarely a good thing
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@4952 8ced0084-cf51-0410-be5f-012b33b47a6e

Source/Core/Core/Src/Debugger/PPCDebugInterface.h
@@ -13,7 +13,7 @@ public:
 	PPCDebugInterface(){} 
 	virtual void disasm(unsigned int address, char *dest, int max_size);
 	virtual void getRawMemoryString(int memory, unsigned int address, char *dest, int max_size);
-	virtual int getInstructionSize(int inst_size = 4) {return inst_size;}
+	virtual int getInstructionSize(int /*instruction*/) {return 4;}
 	virtual bool isAlive();
 	virtual bool isBreakpoint(unsigned int address);
 	virtual void setBreakpoint(unsigned int address);