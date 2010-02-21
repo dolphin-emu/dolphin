b6a3df2fcf8eabffa6cb91b8d88d291b6239ce11
Shawn Hoffman <godisgovernment@gmail.com>
2010-02-21 18:50:44 +0000

    commit current GBA link cable work. no touching (unless you're skid_au or ask first) :P
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@5096 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/Core/Src/HW/SI_DeviceGBA.cpp
+++ Source/Core/Core/Src/HW/SI_DeviceGBA.cpp
@@ -17,135 +17,93 @@
 
 #include "SI_Device.h"
 #include "SI_DeviceGBA.h"
-#include "GBAPipe.h"
 
+#include "SFML/Network.hpp"
+#include <algorithm>
 
-// --- GameBoy Advance ---
-
-
-CSIDevice_GBA::CSIDevice_GBA(int _iDeviceNumber) :
-	ISIDevice(_iDeviceNumber)
+// --- GameBoy Advance "Link Cable" ---
+GBASockServer::GBASockServer()
 {
-	GBAPipe::StartServer();
-	js.U16 = 0;
+	if (!server.Listen(8080))
+		return;
+
+	server.Accept(client, &client_addr);
+}
+
+GBASockServer::~GBASockServer()
+{
+	client.Close();
+	server.Close();
+}
+
+// Blocking, since GBA must always send lower byte of REG_JOYSTAT
+void GBASockServer::Transfer(char* si_buffer)
+{
+	current_data[0] = si_buffer[3];
+	current_data[1] = si_buffer[2];
+	current_data[2] = si_buffer[1];
+	current_data[3] = si_buffer[0];
+	current_data[4] = si_buffer[7];
+
+	u8 cmd = *current_data;
+
+	if (cmd == CMD_WRITE)
+		client.Send(current_data, sizeof(current_data));
+	else
+		client.Send(current_data, 1);
+
+	DEBUG_LOG(SERIALINTERFACE, "> cmd %02x %02x%02x%02x%02x",
+		(u8)current_data[0], (u8)current_data[1], (u8)current_data[2],
+		(u8)current_data[3], (u8)current_data[4]);
+
+	memset(current_data, 0, sizeof(current_data));
+	size_t num_received = 0;
+	client.Receive(current_data, sizeof(current_data), num_received);
+
+	DEBUG_LOG(SERIALINTERFACE, "< %02x%02x%02x%02x%02x",
+		(u8)current_data[0], (u8)current_data[1], (u8)current_data[2],
+		(u8)current_data[3], (u8)current_data[4]);
+
+#ifdef _DEBUG
+	size_t num_expecting = 3;
+	if (cmd == CMD_READ)
+		num_expecting = 5;
+	else if (cmd == CMD_WRITE)
+		num_expecting = 1;
+	if (num_received != num_expecting)
+		ERROR_LOG(SERIALINTERFACE, "%x:%x:%x", (u8)cmd, num_received, num_expecting);
+#endif
+
+	si_buffer[0] = current_data[3];
+	si_buffer[1] = current_data[2];
+	si_buffer[2] = current_data[1];
+	si_buffer[3] = current_data[0];
+	si_buffer[7] = current_data[4];
+}
+
+CSIDevice_GBA::CSIDevice_GBA(int _iDeviceNumber)
+	: ISIDevice(_iDeviceNumber)
+	, GBASockServer()
+{
 }
 
 CSIDevice_GBA::~CSIDevice_GBA()
 {
-	GBAPipe::Stop();
 }
 
 int CSIDevice_GBA::RunBuffer(u8* _pBuffer, int _iLength)
 {
-	// for debug logging only
-	ISIDevice::RunBuffer(_pBuffer, _iLength);
-
-	int iPosition = 0;
-
-	// read the command
-	EBufferCommands command = static_cast<EBufferCommands>(_pBuffer[3]);
-	iPosition++;
-
-	// handle it
-	switch(command)
-	{
-		// NOTE: All "Send/Recieve" mentioned here is from dolphin's perspective,
-		// NOT the GBA's
-		// This means the first "Send"s are seen as CMDs to the GBA, and are therefor IMPORTANT :p
-		// Also this means that we can randomly fill recieve bits and try for a fake gba...
-		// (try playing with the fake joystat)
-		// It seems like JOYSTAT is polled all the time, it's value may or may not matter
-	case CMD_RESET:
-		{
-			//Send 0xFF
-			GBAPipe::Write(CMD_RESET);
-			//Recieve 0x00
-			//Recieve 0x04
-			//Recieve from lower 8bits of JOYSTAT register
-			for (;iPosition < _iLength; ++iPosition)
-				GBAPipe::Read(_pBuffer[iPosition]);
-
-			js.U16 = 0; // FAKE
-			js.stat_send = 1; // FAKE
-			*(u32*)&_pBuffer[0] |= SI_GBA | js.U16; // hax for now
-			WARN_LOG(SERIALINTERFACE, "GBA %i CMD_RESET", this->m_iDeviceNumber);
-		}
-		break;
-	case CMD_STATUS:
-		{
-			//Send 0x00
-			GBAPipe::Write(CMD_STATUS);
-			//Recieve 0x00
-			//Recieve 0x04
-			//Recieve from lower 8bits of JOYSTAT register
-			for (;iPosition < _iLength; ++iPosition)
-				GBAPipe::Read(_pBuffer[iPosition]);
-
-			js.U16 = 0; // FAKE
-			js.stat_rec = 1; // FAKE
-			*(u32*)&_pBuffer[0] |= SI_GBA | js.U16; // hax for now
-			WARN_LOG(SERIALINTERFACE, "GBA %i CMD_STATUS", this->m_iDeviceNumber);
-		}
-		break;
-	// Probably read and write belong in getdata and sendcommand
-	case CMD_WRITE:
-		{
-			//Send 0x15
-			GBAPipe::Write(CMD_WRITE);
-			//Send to Lower 8bits of JOY_RECV_L
-			//Send to Upper 8bits of JOY_RECV_L
-			//Send to Lower 8bits of JOY_RECV_H
-			//Send to Upper 8bits of JOY_RECV_H
-			for (;iPosition < _iLength-1; ++iPosition)
-				GBAPipe::Write(_pBuffer[iPosition]);
-			//Receive from lower 8bits of JOYSTAT register
-			GBAPipe::Read(_pBuffer[++iPosition]);
-
-			WARN_LOG(SERIALINTERFACE, "GBA %i CMD_WRITE", this->m_iDeviceNumber);
-		}
-		break;
-	case CMD_READ:
-		{
-			//Send 0x14
-			GBAPipe::Write(CMD_READ);
-			//Receive from Lower 8bits of JOY_TRANS_L
-			//Receive from Upper 8bits of JOY_TRANS_L
-			//Receive from Lower 8bits of JOY_TRANS_H
-			//Receive from Upper 8bits of JOY_TRANS_H
-			//Receive from lower 8bits of JOYSTAT register
-			for (;iPosition < _iLength; ++iPosition)
-				GBAPipe::Read(_pBuffer[iPosition]);
-
-			WARN_LOG(SERIALINTERFACE, "GBA %i CMD_READ", this->m_iDeviceNumber);;
-		}
-		break;
-	default:
-		{
-			WARN_LOG(SERIALINTERFACE, "GBA %i CMD_UNKNOWN     (0x%x)", this->m_iDeviceNumber, command);
-		}
-		break;
-	}
-	INFO_LOG(SERIALINTERFACE, "GBA buffer %08x",*(u32*)&_pBuffer[0]);
-
-	return iPosition;
+	Transfer((char*)_pBuffer);
+	return _iLength;
 }
 
-
-// GetData
-
-bool 
-CSIDevice_GBA::GetData(u32& _Hi, u32& _Low)
+bool CSIDevice_GBA::GetData(u32& _Hi, u32& _Low)
 {
-	INFO_LOG(SERIALINTERFACE, "GBA %i GetData Hi: 0x%08x Low: 0x%08x", this->m_iDeviceNumber, _Hi, _Low);
-
+	DEBUG_LOG(SERIALINTERFACE, "GBA %i GetData Hi: 0x%08x Low: 0x%08x", m_iDeviceNumber, _Hi, _Low);
 	return true;
 }
 
-
-// SendCommand
-
-void
-CSIDevice_GBA::SendCommand(u32 _Cmd, u8 _Poll)
+void CSIDevice_GBA::SendCommand(u32 _Cmd, u8 _Poll)
 {
-	INFO_LOG(SERIALINTERFACE, "GBA %i SendCommand: (0x%08x)", this->m_iDeviceNumber, _Cmd);
+	DEBUG_LOG(SERIALINTERFACE, "GBA %i SendCommand: (0x%08x)", m_iDeviceNumber, _Cmd);
 }
--- Source/Core/Core/Src/HW/SI_DeviceGBA.h
+++ Source/Core/Core/Src/HW/SI_DeviceGBA.h
@@ -18,63 +18,38 @@
 #ifndef _SI_DEVICEGBA_H
 #define _SI_DEVICEGBA_H
 
+#include "SFML/Network.hpp"
 
-// GameBoy Advance
+// GameBoy Advance "Link Cable"
 
-
-class CSIDevice_GBA : public ISIDevice
+class GBASockServer : public sf::SocketTCP
 {
-private:
+public:
+	GBASockServer();
+	~GBASockServer();
 
-	// Commands
+	void Transfer(char* si_buffer);
+
+	char current_data[5];
+
+private:
 	enum EBufferCommands
 	{
-		CMD_RESET		= 0xFF,
-		CMD_STATUS		= 0x00,
-		CMD_WRITE		= 0x15,
-		CMD_READ		= 0x14
+		CMD_RESET	= 0xff,
+		CMD_STATUS	= 0x00,
+		CMD_READ	= 0x14,
+		CMD_WRITE	= 0x15		
 	};
 
-	union FAKE_JOYSTAT
-	{
-		u16 U16;
-		struct{
-			unsigned			:1;
-			unsigned stat_rec	:1; // GBA-side reception status flag
-			unsigned			:1;
-			unsigned stat_send	:1; // GBA-side transmission status flag
-			unsigned gp0		:1; // General-purpose flag 0
-			unsigned gp1		:1; // General-purpose flag 1
-			unsigned			:2;
+	sf::SocketTCP server;
+	sf::SocketTCP client;
+	sf::IPAddress client_addr;
+};
 
-			unsigned			:8;
-		};
-	};
-	FAKE_JOYSTAT js;
-	
-	//0x4000158 - JOYSTAT - Receive Status Register (R/W) (ON THE GBA)
-	
-	// NOTE: there is a typo in GBATEK!
-	// in the JOY BUS command descriptions, SIOSTAT==JOYSTAT
-	//Bit   Expl.
-	//0     Not used
-	//1     Receive Status Flag   (0=Remote GBA is/was receiving) (Read Only?)
-	//2     Not used
-	//3     Send Status Flag      (1=Remote GBA is/was sending)   (Read Only?)
-	//4-5   General Purpose Flag  (Not assigned, may be used for whatever purpose)
-	//6-15  Not used
-	//--------------------------------------
-	//0b0000000000 00 0 0 0 0
-	//______________________________________
-	//Bit 1 is automatically set when writing to local JOY_TRANS.
-	//Bit 3 is automatically reset when reading from local JOY_RECV.
-
+class CSIDevice_GBA : public ISIDevice, private GBASockServer
+{
 public:
-
-	// Constructor
 	CSIDevice_GBA(int _iDeviceNumber);
-
-	// Destructor
 	~CSIDevice_GBA();
 
 	// Run the SI Buffer
@@ -86,4 +61,5 @@ public:
 	// Send a command directly
 	virtual void SendCommand(u32 _Cmd, u8 _Poll);
 };
+
 #endif
