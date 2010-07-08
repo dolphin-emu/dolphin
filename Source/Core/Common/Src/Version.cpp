1d535c9e5096960e2c822bc2d6ee6a6ef8558713
Soren Jorvang <soren.jorvang@gmail.com>
2010-07-08 15:44:39 +0000

    Forgot to add Version.cpp in previous.
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@5853 8ced0084-cf51-0410-be5f-012b33b47a6e

---
+++ Source/Core/Common/Src/Version.cpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2003 Dolphin Project.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2.0.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License 2.0 for more details.
+
+// A copy of the GPL 2.0 should have been included with the program.
+// If not, see http://www.gnu.org/licenses/
+
+// Official SVN repository and contact information can be found at
+// http://code.google.com/p/dolphin-emu/
+
+#include "Common.h"
+#include "svnrev.h"
+
+#ifdef _DEBUG
+const char *svn_rev_str = "Dolphin Debug SVN R " SVN_REV_STR;
+#elif defined DEBUGFAST
+const char *svn_rev_str = "Dolphin Debugfast SVN R " SVN_REV_STR;
+#else
+const char *svn_rev_str = "Dolphin SVN R " SVN_REV_STR;
+#endif   
+
+#ifdef _M_X64
+#define NP_ARCH "x64"
+#else
+#define NP_ARCH "x86"
+#endif
+
+#ifdef _WIN32
+const char *netplay_dolphin_ver = SVN_REV_STR " W" NP_ARCH;
+#elif __APPLE__
+const char *netplay_dolphin_ver = SVN_REV_STR " M" NP_ARCH;
+#else
+const char *netplay_dolphin_ver = SVN_REV_STR " L" NP_ARCH;
+#endif
