dolphin-emu/dolphin

commit 074fe9c9ea5e377a2a50ee943e76a2136271760e

Implemented jit'ing of floating point compare operations in the IL
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@3312 8ced0084-cf51-0410-be5f-012b33b47a6e

Source/Core/Core/Src/PowerPC/Jit64IL/IR.h
@@ -119,6 +119,7 @@ namespace IREmitter {
 		ExpandPackedToMReg,
 		CompactMRegToPacked,
 		LoadFReg,
+		LoadFRegDENToZero,
 		FSMul,
 		FSAdd,
 		FSSub,
@@ -410,6 +411,9 @@ namespace IREmitter {
 		}
 		InstLoc EmitLoadFReg(unsigned freg) {
 			return FoldZeroOp(LoadFReg, freg);
+		}
+		InstLoc EmitLoadFRegDENToZero(unsigned freg) {
+			return FoldZeroOp(LoadFRegDENToZero, freg);
 		}
 		InstLoc EmitStoreFReg(InstLoc val, unsigned freg) {
 			return FoldUOp(StoreFReg, val, freg);

Source/Core/Core/Src/PowerPC/Jit64IL/IR.cpp
@@ -257,6 +257,10 @@ InstLoc IRBuilder::FoldZeroOp(unsigned Opcode, unsigned extra) {
 			FRegCache[extra] = EmitZeroOp(LoadFReg, extra);
 		return FRegCache[extra];
 	}
+	if (Opcode == LoadFRegDENToZero) {
+		// cant use cache here
+		return EmitZeroOp(LoadFRegDENToZero, extra);
+	}
 	if (Opcode == LoadCarry) {
 		if (!CarryCache)
 			CarryCache = EmitZeroOp(LoadCarry, extra);
@@ -1313,6 +1317,7 @@ static void DoWriteCode(IRBuilder* ibuild, Jit64* Jit, bool UseProfile) {
 		case LoadCTR:
 		case LoadMSR:
 		case LoadFReg:
+		case LoadFRegDENToZero:
 		case LoadGQR:
 		case BlockEnd:
 		case BlockStart:
@@ -1978,6 +1983,22 @@ static void DoWriteCode(IRBuilder* ibuild, Jit64* Jit, bool UseProfile) {
 			if (!thisUsed) break;
 			X64Reg reg = fregFindFreeReg(RI);
 			unsigned ppcreg = *I >> 8;
+			Jit->MOVAPD(reg, M(&PowerPC::ppcState.ps[ppcreg]));
+			RI.fregs[reg] = I;
+			break;
+		}	    
+		case LoadFRegDENToZero: {
+			if (!thisUsed) break;
+			X64Reg reg = fregFindFreeReg(RI);
+			unsigned ppcreg = *I >> 8;
+			char *p = (char*)&(PowerPC::ppcState.ps[ppcreg][0]);
+			Jit->MOV(32, R(ECX), M(p+4));
+			Jit->AND(32, R(ECX), Imm32(0x7ff00000));
+			Jit->CMP(32, R(ECX), Imm32(0x38000000));
+			FixupBranch ok = Jit->J_CC(CC_AE);
+			Jit->AND(32, M(p+4), Imm32(0x80000000));
+			Jit->MOV(32, M(p), Imm32(0));
+			Jit->SetJumpTarget(ok);
 			Jit->MOVAPD(reg, M(&PowerPC::ppcState.ps[ppcreg]));
 			RI.fregs[reg] = I;
 			break;

Source/Core/Core/Src/PowerPC/Jit64IL/Jit_FloatingPoint.cpp
@@ -102,14 +102,11 @@
 
 	void Jit64::fcmpx(UGeckoInstruction inst)
 	{
-		// @TODO: conform this to the new fcmpo
-		Default(inst); return;
-
 		INSTRUCTION_START
 		JITDISABLE(FloatingPoint)
 		IREmitter::InstLoc lhs, rhs, res;
-		lhs = ibuild.EmitLoadFReg(inst.FA);
-		rhs = ibuild.EmitLoadFReg(inst.FB);
+		lhs = ibuild.EmitLoadFRegDENToZero(inst.FA);
+		rhs = ibuild.EmitLoadFRegDENToZero(inst.FB);
 		res = ibuild.EmitFDCmpCR(lhs, rhs);
 		ibuild.EmitStoreFPRF(res);
 		ibuild.EmitStoreCR(res, inst.CRFD);

commit 26eac5d22a7581f406130056ba5f85d4b5635b26

JitIL: Fixed a bug (hack?) that NaN is considered as -0.0 in fcmpx. Implemented Nan check routine in FDCmpCR. The bug (hack?) was introduced in r3312. I could not determine whether it was a hack or not.
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@6165 8ced0084-cf51-0410-be5f-012b33b47a6e

Source/Core/Core/Src/PowerPC/Jit64IL/IR.h
@@ -518,8 +518,8 @@ public:
 	InstLoc EmitDoubleToSingle(InstLoc op1) {
 		return FoldUOp(DoubleToSingle, op1);
 	}
-	InstLoc EmitFDCmpCR(InstLoc op1, InstLoc op2) {
-		return FoldBiOp(FDCmpCR, op1, op2);
+	InstLoc EmitFDCmpCR(InstLoc op1, InstLoc op2, int ordered) {
+		return FoldBiOp(FDCmpCR, op1, op2, ordered);
 	}
 	InstLoc EmitLoadGQR(unsigned gqr) {
 		return FoldZeroOp(LoadGQR, gqr);

Source/Core/Core/Src/PowerPC/Jit64IL/IR_X86.cpp
@@ -48,6 +48,7 @@ The register allocation is linear scan allocation.
 #include "../../ConfigManager.h"
 #include "x64Emitter.h"
 #include "../../../../Common/Src/CPUDetect.h"
+#include "MathUtil.h"
 
 static ThunkManager thunks;
 
@@ -712,6 +713,12 @@ static void regWriteExit(RegInfo& RI, InstLoc dest) {
 	} else {
 		RI.Jit->WriteExitDestInOpArg(regLocForInst(RI, dest), RI.exitNumber++);
 	}
+}
+
+// Helper function to check floating point exceptions
+static double GC_ALIGNED16(isSNANTemp[2][2]);
+static bool checkIsSNAN() {
+	return MathUtil::IsSNAN(isSNANTemp[0][0]) || MathUtil::IsSNAN(isSNANTemp[1][0]);
 }
 
 static void DoWriteCode(IRBuilder* ibuild, JitIL* Jit, bool UseProfile, bool MakeProfile) {
@@ -1532,10 +1539,13 @@ static void DoWriteCode(IRBuilder* ibuild, JitIL* Jit, bool UseProfile, bool Mak
 			break;
 		}
 		case FDCmpCR: {
+			const u32 ordered = *I >> 24;
 			X64Reg destreg = regFindFreeReg(RI);
-			// TODO: Add case for NaN (CC_P)
-			Jit->MOVSD(XMM0, fregLocForInst(RI, getOp1(I)));
-			Jit->UCOMISD(XMM0, fregLocForInst(RI, getOp2(I)));
+			// TODO: Remove an extra MOVSD if loc1.IsSimpleReg()
+			OpArg loc1 = fregLocForInst(RI, getOp1(I));
+			OpArg loc2 = fregLocForInst(RI, getOp2(I));
+			Jit->MOVSD(XMM0, loc1);
+			Jit->UCOMISD(XMM0, loc2);
 			FixupBranch pNan     = Jit->J_CC(CC_P);
 			FixupBranch pEqual   = Jit->J_CC(CC_Z);
 			FixupBranch pLesser  = Jit->J_CC(CC_C);
@@ -1545,6 +1555,56 @@ static void DoWriteCode(IRBuilder* ibuild, JitIL* Jit, bool UseProfile, bool Mak
 			// NaN
 			Jit->SetJumpTarget(pNan);
 			Jit->MOV(32, R(destreg), Imm32(0x1));
+
+			static const u32 FPSCR_VE = (u32)1 << (31 - 24);
+			static const u32 FPSCR_VXVC = (u32)1 << (31 - 12);
+			static const u32 FPSCR_VXSNAN = (u32)1 << (31 - 7);
+			static const u32 FPSCR_FX = (u32)1 << (31 - 0);
+
+			if (ordered) {
+				// fcmpo
+				// TODO: Optimize the following code if slow.
+				//       SNAN check may not be needed
+				//       because it does not happen so much.
+				Jit->MOVSD(M(isSNANTemp[0]), XMM0);
+				if (loc2.IsSimpleReg()) {
+					Jit->MOVSD(M(isSNANTemp[1]), loc2.GetSimpleReg());
+				} else {
+					Jit->MOVSD(XMM0, loc2);
+					Jit->MOVSD(M(isSNANTemp[1]), XMM0);
+				}
+				Jit->ABI_CallFunction(checkIsSNAN);
+				Jit->TEST(8, R(EAX), R(EAX));
+				FixupBranch ok = Jit->J_CC(CC_Z);
+					Jit->OR(32, M(&FPSCR), Imm32(FPSCR_FX)); // FPSCR.FX = 1;
+					Jit->OR(32, M(&FPSCR), Imm32(FPSCR_VXSNAN)); // FPSCR.Hex |= mask;
+					Jit->TEST(32, M(&FPSCR), Imm32(FPSCR_VE));
+					FixupBranch finish0 = Jit->J_CC(CC_NZ);
+						Jit->OR(32, M(&FPSCR), Imm32(FPSCR_VXVC)); // FPSCR.Hex |= mask;
+						FixupBranch finish1 = Jit->J();
+				Jit->SetJumpTarget(ok);
+					Jit->OR(32, M(&FPSCR), Imm32(FPSCR_FX)); // FPSCR.FX = 1;
+					Jit->OR(32, M(&FPSCR), Imm32(FPSCR_VXVC)); // FPSCR.Hex |= mask;
+				Jit->SetJumpTarget(finish0);
+				Jit->SetJumpTarget(finish1);
+			} else {
+				// fcmpu
+				// TODO: Optimize the following code if slow
+				Jit->MOVSD(M(isSNANTemp[0]), XMM0);
+				if (loc2.IsSimpleReg()) {
+					Jit->MOVSD(M(isSNANTemp[1]), loc2.GetSimpleReg());
+				} else {
+					Jit->MOVSD(XMM0, loc2);
+					Jit->MOVSD(M(isSNANTemp[1]), XMM0);
+				}
+				Jit->ABI_CallFunction(checkIsSNAN);
+				Jit->TEST(8, R(EAX), R(EAX));
+				FixupBranch finish = Jit->J_CC(CC_Z);
+					Jit->OR(32, M(&FPSCR), Imm32(FPSCR_FX)); // FPSCR.FX = 1;
+					Jit->OR(32, M(&FPSCR), Imm32(FPSCR_VXVC)); // FPSCR.Hex |= mask;
+				Jit->SetJumpTarget(finish);
+			}
+
 			FixupBranch continue2 = Jit->J();
 			// Equal
 			Jit->SetJumpTarget(pEqual);

Source/Core/Core/Src/PowerPC/Jit64IL/JitIL_FloatingPoint.cpp
@@ -115,9 +115,10 @@ void JitIL::fcmpx(UGeckoInstruction inst)
 	INSTRUCTION_START
 	JITDISABLE(FloatingPoint)
 	IREmitter::InstLoc lhs, rhs, res;
-	lhs = ibuild.EmitLoadFRegDENToZero(inst.FA);
-	rhs = ibuild.EmitLoadFRegDENToZero(inst.FB);
-	res = ibuild.EmitFDCmpCR(lhs, rhs);
+	lhs = ibuild.EmitLoadFReg(inst.FA);
+	rhs = ibuild.EmitLoadFReg(inst.FB);
+	int ordered = (inst.SUBOP10 == 32) ? 1 : 0;
+	res = ibuild.EmitFDCmpCR(lhs, rhs, ordered);
 	ibuild.EmitStoreFPRF(res);
 	ibuild.EmitStoreCR(res, inst.CRFD);
 }