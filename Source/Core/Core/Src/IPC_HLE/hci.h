5948865bcc0ff079889bd21a69af1f5e73abd3a9
Shawn Hoffman <godisgovernment@gmail.com>
2010-09-06 13:47:18 +0000

    jiggle includes in hci.h/l2cap.h: msvc <= 9 doesn't provide stdint.h
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@6186 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/Core/Src/IPC_HLE/hci.h
+++ Source/Core/Core/Src/IPC_HLE/hci.h
@@ -79,12 +79,7 @@
 
 #pragma once
 
-#include <stdint.h>
-
-// All structs in this file are packed
-#pragma pack(push, 1)
-
-// This is not needed >= msvc10
+// msvc >= msvc10 has stdint.h/cstdint
 #ifdef _MSC_VER
 	typedef u8 uint8_t;
 	typedef s8 int8_t;
@@ -92,7 +87,12 @@
 	typedef s16 int16_t;
 	typedef u32 uint32_t;
 	typedef s32 int32_t;
+#else
+	#include <stdint.h>
 #endif
+
+// All structs in this file are packed
+#pragma pack(push, 1)
 
 /*
  * Bluetooth Address Family Protocol Numbers
