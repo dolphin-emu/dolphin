dolphin-emu/dolphin

commit 719521eeeba24f5a6324975208b6d8d52f6386cb

Added EFB Scale option to the OpenGL plug-in. Renamed EFB Scale to Internal Resolution. Removed Auto Scale option (it is now always on). Added on-the-fly changing of the Internal Resolution in the OpenGL and Direct3D9 plug-ins. Further consolidated the code in the video plug-ins.
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@6240 8ced0084-cf51-0410-be5f-012b33b47a6e

Source/Plugins/Plugin_VideoOGL/Src/VertexManager.cpp
@@ -86,7 +86,7 @@ bool Init()
 	g_nativeVertexFmt = NULL;
 	Flushed=false;
 	GL_REPORT_ERRORD();
-	
+
 	return true;
 }
 
@@ -181,17 +181,17 @@ void AddVertices(int primitive, int numVertices)
 
 inline void Draw()
 {
-	if(IndexGenerator::GetNumTriangles() > 0)
+	if (IndexGenerator::GetNumTriangles() > 0)
 	{
 		glDrawElements(GL_TRIANGLES, IndexGenerator::GetTriangleindexLen(), GL_UNSIGNED_SHORT, TIBuffer);
 		INCSTAT(stats.thisFrame.numIndexedDrawCalls);
 	}
-	if(IndexGenerator::GetNumLines() > 0)
+	if (IndexGenerator::GetNumLines() > 0)
 	{
 		glDrawElements(GL_LINES, IndexGenerator::GetLineindexLen(), GL_UNSIGNED_SHORT, LIBuffer);
 		INCSTAT(stats.thisFrame.numIndexedDrawCalls);
 	}
-	if(IndexGenerator::GetNumPoints() > 0)
+	if (IndexGenerator::GetNumPoints() > 0)
 	{
 		glDrawElements(GL_POINTS, IndexGenerator::GetPointindexLen(), GL_UNSIGNED_SHORT, PIBuffer);
 		INCSTAT(stats.thisFrame.numIndexedDrawCalls);
@@ -288,26 +288,23 @@ void Flush()
 		}
 	}
 
-	FRAGMENTSHADER* ps = PixelShaderCache::SetShader(false,g_nativeVertexFmt->m_components);
-	VERTEXSHADER* vs = VertexShaderCache::SetShader(g_nativeVertexFmt->m_components);
-
 	// set global constants
 	VertexShaderManager::SetConstants();
 	PixelShaderManager::SetConstants();
 
 	// finally bind
-
-	if (vs) VertexShaderCache::SetCurrentShader(vs->glprogid);
+	FRAGMENTSHADER* ps = PixelShaderCache::SetShader(false,g_nativeVertexFmt->m_components);
+	VERTEXSHADER* vs = VertexShaderCache::SetShader(g_nativeVertexFmt->m_components);
 	if (ps) PixelShaderCache::SetCurrentShader(ps->glprogid); // Lego Star Wars crashes here.
+	if (vs) VertexShaderCache::SetCurrentShader(vs->glprogid);
 
 	Draw();
-	
+
 	// run through vertex groups again to set alpha
-	if (!g_ActiveConfig.bDstAlphaPass && bpmem.dstalpha.enable && bpmem.blendmode.alphaupdate) 
+	if (bpmem.dstalpha.enable && bpmem.blendmode.alphaupdate)
 	{
 		ps = PixelShaderCache::SetShader(true,g_nativeVertexFmt->m_components);
-
-		if (ps)PixelShaderCache::SetCurrentShader(ps->glprogid);
+		if (ps) PixelShaderCache::SetCurrentShader(ps->glprogid);
 
 		// only update alpha
 		glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_TRUE);
@@ -353,6 +350,5 @@ void Flush()
 	g_Config.iSaveTargetId++;
 
 	GL_REPORT_ERRORD();
-
 }
 }  // namespace

Source/Core/VideoCommon/Src/Render.h
@@ -59,9 +59,6 @@ public:
 	static void SetLineWidth();
 	static void SetSamplerState(int stage,int texindex);
 	static void SetInterlacingMode();
-	// Live resolution change
-	static bool Allow2x();
-	static bool AllowCustom();
 
 	// Render target management
 	static int GetFrameBufferWidth();

Source/Core/VideoCommon/Src/VideoConfig.cpp
@@ -48,14 +48,11 @@ void VideoConfig::Load(const char *ini_file)
 	iniFile.Load(ini_file);
 	
 	iniFile.Get("Hardware", "VSync", &bVSync, 0); // Hardware
-	iniFile.Get("Settings", "StretchToFit", &bNativeResolution, true);
-	iniFile.Get("Settings", "2xResolution", &b2xResolution, false);
 	iniFile.Get("Settings", "wideScreenHack", &bWidescreenHack, false);
 	iniFile.Get("Settings", "AspectRatio", &iAspectRatio, (int)ASPECT_AUTO);
 	iniFile.Get("Settings", "Crop", &bCrop, false);
 	iniFile.Get("Settings", "UseXFB", &bUseXFB, 0);
 	iniFile.Get("Settings", "UseRealXFB", &bUseRealXFB, 0);
-	iniFile.Get("Settings", "AutoScale", &bAutoScale, true);
 	iniFile.Get("Settings", "UseNativeMips", &bUseNativeMips, true);
 	
 	iniFile.Get("Settings", "SafeTextureCache", &bSafeTextureCache, false); // Settings
@@ -165,14 +162,11 @@ void VideoConfig::Save(const char *ini_file)
 	IniFile iniFile;
 	iniFile.Load(ini_file);
 	iniFile.Set("Hardware", "VSync", bVSync);
-	iniFile.Set("Settings", "StretchToFit", bNativeResolution);
-	iniFile.Set("Settings", "2xResolution", b2xResolution);
 	iniFile.Set("Settings", "AspectRatio", iAspectRatio);
 	iniFile.Set("Settings", "Crop", bCrop);
 	iniFile.Set("Settings", "wideScreenHack", bWidescreenHack);
 	iniFile.Set("Settings", "UseXFB", bUseXFB);
 	iniFile.Set("Settings", "UseRealXFB", bUseRealXFB);
-	iniFile.Set("Settings", "AutoScale", bAutoScale);
 	iniFile.Set("Settings", "UseNativeMips", bUseNativeMips);
 
 	iniFile.Set("Settings", "SafeTextureCache", bSafeTextureCache);

Source/Core/VideoCommon/Src/VideoConfig.h
@@ -69,13 +69,12 @@ struct VideoConfig
 	// General
 	bool bVSync;
 
-	bool bNativeResolution, b2xResolution, bRunning;  // Should possibly be augmented with 2x, 4x native.
+	bool bRunning;
 	bool bWidescreenHack;
 	int iAspectRatio;
 	bool bCrop;   // Aspect ratio controls.
 	bool bUseXFB;
 	bool bUseRealXFB;
-	bool bAutoScale;  // Removes annoying borders without using XFB. Doesn't always work perfectly.
 	bool bUseNativeMips;
 
 	// OpenCL

Source/Plugins/Plugin_VideoDX11/Src/Render.cpp
@@ -924,17 +924,14 @@ void Renderer::Swap(u32 xfbAddr, FieldType field, u32 fbWidth, u32 fbHeight,cons
 				drawRc.left = -(xfbWidth / (float)fbWidth);
 				drawRc.right = (xfbWidth / (float)fbWidth);
 
-				if (!g_ActiveConfig.bAutoScale)
-				{
-					// scale draw area for a 1 to 1 pixel mapping with the draw target
-					float vScale = (float)fbHeight / (float)s_backbuffer_height;
-					float hScale = (float)fbWidth / (float)s_backbuffer_width;
-
-					drawRc.top *= vScale;
-					drawRc.bottom *= vScale;
-					drawRc.left *= hScale;
-					drawRc.right *= hScale;
-				}
+				// The following code disables auto stretch.  Kept for reference.
+				// scale draw area for a 1 to 1 pixel mapping with the draw target
+				//float vScale = (float)fbHeight / (float)s_backbuffer_height;
+				//float hScale = (float)fbWidth / (float)s_backbuffer_width;
+				//drawRc.top *= vScale;
+				//drawRc.bottom *= vScale;
+				//drawRc.left *= hScale;
+				//drawRc.right *= hScale;
 			}
 			else
 			{

Source/Plugins/Plugin_VideoDX9/Src/BPFunctions.cpp
@@ -59,7 +59,7 @@ void SetLineWidth(const BPCmd &bp)
 
 void SetDepthMode(const BPCmd &bp)
 {
-	Renderer::SetDepthMode();			
+	Renderer::SetDepthMode();
 }
 
 void SetBlendMode(const BPCmd &bp)

Source/Plugins/Plugin_VideoDX9/Src/DlgSettings.cpp
@@ -210,9 +210,9 @@ void GFXConfigDialogDX::CreateGUIControls()
 		arrayStringFor_MSAAModeCB.Add(wxString::FromAscii(adapter.aa_levels[i].name));
 	}
 	m_MSAAModeCB = new wxChoice( m_PageDirect3D, ID_ANTIALIASMODE, wxPoint( -1,-1 ), wxDefaultSize, arrayStringFor_MSAAModeCB, 0);
-	m_EFBScaleText = new wxStaticText( m_PageDirect3D, wxID_ANY, wxT("EFB scale:"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_EFBScaleText = new wxStaticText( m_PageDirect3D, wxID_ANY, wxT("Internal Resolution:"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_EFBScaleText->Wrap( -1 );
-	wxString m_EFBScaleModeChoices[] = { wxT("Auto (fractional)"), wxT("Auto (integral)"), wxT("1x"), wxT("2x"), wxT("3x") };
+	wxString m_EFBScaleModeChoices[] = { wxT("Auto (fractional)"), wxT("Auto (integral)"), wxT("Native"), wxT("2x"), wxT("3x") };
 	int m_EFBScaleModeNChoices = sizeof( m_EFBScaleModeChoices ) / sizeof( wxString );
 	m_EFBScaleMode = new wxChoice( m_PageDirect3D, ID_EFBSCALEMODE, wxDefaultPosition, wxDefaultSize, m_EFBScaleModeNChoices, m_EFBScaleModeChoices, 0 );
 

Source/Plugins/Plugin_VideoDX9/Src/EmuWindow.cpp
@@ -145,8 +145,8 @@ void OSDMenu(WPARAM wParam)
 	case '3':
 		OSDChoice = 1;
 		// Toggle native resolution
-		OSDInternalW = D3D::GetBackBufferWidth();
-		OSDInternalH = D3D::GetBackBufferHeight();
+		g_Config.iEFBScale = g_Config.iEFBScale + 1;
+		if (g_Config.iEFBScale > 4) g_Config.iEFBScale = 0;
 		break;
 	case '4':
 		OSDChoice = 2;

commit ccb4645f24bd8ea4f995d4babb13cc41fb8b3552

Fix a bug introduced by r6240. Fixes issue 3320.
git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@6265 8ced0084-cf51-0410-be5f-012b33b47a6e

Source/Plugins/Plugin_VideoOGL/Src/VertexManager.cpp
@@ -191,7 +191,7 @@ void VertexManager::vFlush()
 	Draw();
 
 	// run through vertex groups again to set alpha
-	if (bpmem.dstalpha.enable && bpmem.blendmode.alphaupdate)
+	if (!g_ActiveConfig.bDstAlphaPass && bpmem.dstalpha.enable && bpmem.blendmode.alphaupdate)
 	{
 		ps = PixelShaderCache::SetShader(true,g_nativeVertexFmt->m_components);
 		if (ps) PixelShaderCache::SetCurrentShader(ps->glprogid);