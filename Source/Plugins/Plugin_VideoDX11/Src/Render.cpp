e2543689b810c4b93b9a44cbd5ba6a018cc4a917
NeoBrainX <NeoBrainX@googlemail.com>
2010-12-05 22:28:46 +0000

    DX11: Fix a crash when accessing the EFB.
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@6536 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Plugins/Plugin_VideoDX11/Src/Render.cpp
+++ Source/Plugins/Plugin_VideoDX11/Src/Render.cpp
@@ -577,7 +577,7 @@ u32 Renderer::AccessEFB(EFBAccessType type, u32 x, u32 y, u32 poke_data)
 	else if (type == PEEK_COLOR)
 	{
 		// we can directly copy to system memory here
-		read_tex = FramebufferManager::GetResolvedEFBColorTexture()->GetTex();
+		read_tex = FramebufferManager::GetEFBColorStagingBuffer();
 		D3D11_BOX box = CD3D11_BOX(RectToLock.left, RectToLock.top, 0, RectToLock.right, RectToLock.bottom, 1);
 		D3D::context->CopySubresourceRegion(read_tex, 0, 0, 0, 0, FramebufferManager::GetEFBColorTexture()->GetTex(), 0, &box);
 
