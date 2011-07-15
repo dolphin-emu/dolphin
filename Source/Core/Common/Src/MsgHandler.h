b853d7de3db62ca3f0e36beb58a709e6e46b4421
Glenn Rice <glennricster@gmail.com>
2011-07-15 16:50:54 +0000

    Oops.  Forgot to add the CriticalAlert definitions for windows.
    git-svn-id: https://dolphin-emu.googlecode.com/svn/trunk@7679 8ced0084-cf51-0410-be5f-012b33b47a6e

--- Source/Core/Common/Src/MsgHandler.h
+++ Source/Core/Common/Src/MsgHandler.h
@@ -49,11 +49,13 @@ void SetEnableAlert(bool enable);
 	#define PanicAlert(format, ...) MsgAlert(false, WARNING, format, __VA_ARGS__) 
 	#define PanicYesNo(format, ...) MsgAlert(true, WARNING, format, __VA_ARGS__) 
 	#define AskYesNo(format, ...) MsgAlert(true, QUESTION, format, __VA_ARGS__) 
+	#define CriticalAlert(format, ...) MsgAlert(false, CRITICAL, format, __VA_ARGS__) 
 	// Use these macros (that do the same thing) if the message should be translated.
 	#define SuccessAlertT(format, ...) MsgAlert(false, INFORMATION, format, __VA_ARGS__) 
 	#define PanicAlertT(format, ...) MsgAlert(false, WARNING, format, __VA_ARGS__) 
 	#define PanicYesNoT(format, ...) MsgAlert(true, WARNING, format, __VA_ARGS__) 
 	#define AskYesNoT(format, ...) MsgAlert(true, QUESTION, format, __VA_ARGS__) 
+	#define CriticalAlertT(format, ...) MsgAlert(false, CRITICAL, format, __VA_ARGS__) 
 #else
 	#define SuccessAlert(format, ...) MsgAlert(false, INFORMATION, format, ##__VA_ARGS__) 
 	#define PanicAlert(format, ...) MsgAlert(false, WARNING, format, ##__VA_ARGS__) 
@@ -73,10 +75,12 @@ void SetEnableAlert(bool enable);
 	#define PanicAlert(format, ...) ;
 	#define PanicYesNo(format, ...) ;
 	#define AskYesNo(format, ...) ;
+	#define CriticalAlert(format, ...) ;
 	#define SuccessAlertT(format, ...) ;
 	#define PanicAlertT(format, ...) ;
 	#define PanicYesNoT(format, ...) ;
 	#define AskYesNoT(format, ...) ;
+	#define CriticalAlertT(format, ...) ;
 #endif
 
 #endif // _MSGHANDLER_H_
