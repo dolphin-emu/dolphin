f567782b4732646c619fc9508cbd1cfa779914bb
Pierre Bourdon <delroth@gmail.com>
2012-05-12 01:48:56 +0200

    Implement a better heuristic to detect whether an ELF is for GC or Wii

--- Source/Core/Core/Src/Boot/Boot_ELF.cpp
+++ Source/Core/Core/Src/Boot/Boot_ELF.cpp
@@ -34,12 +34,35 @@ bool CBoot::IsElfWii(const char *filename)
 	File::IOFile f(filename, "rb");
 	f.ReadBytes(mem, (size_t)filesize);
 	}
-
-	ElfReader reader(mem);
-	// TODO: Find a more reliable way to distinguish.
-	bool isWii = reader.GetEntryPoint() >= 0x80004000;
-	delete[] mem;
 	
+	// Use the same method as the DOL loader uses: search for mfspr from HID4,
+	// which should only be used in Wii ELFs.
+	// 
+	// Likely to have some false positives/negatives, patches implementing a
+	// better heuristic are welcome.
+
+	u32 HID4_pattern = 0x7c13fba6;
+	u32 HID4_mask = 0xfc1fffff;
+	ElfReader reader(mem);
+	bool isWii = false;
+
+	for (int i = 0; i < reader.GetNumSections(); ++i)
+	{
+		if (reader.IsCodeSection(i))
+		{
+			for (unsigned int j = 0; j < reader.GetSectionSize(i) / sizeof (u32); ++j)
+			{
+				u32 word = Common::swap32(((u32*)reader.GetSectionDataPtr(i))[j]);
+				if ((word & HID4_mask) == HID4_pattern)
+				{
+					isWii = true;
+					break;
+				}
+			}
+		}
+	}
+
+	delete[] mem;
     return isWii;
 }
 
