1bc5f41e40e604ef6c1d1e84617c13fdf40e04d1
Matthew Parlane <parlane@gmail.com>
2012-05-18 17:57:28 +1200

    Fixes issue 5428. Thanks delroth.

--- Source/Core/Core/Src/IPC_HLE/WII_IPC_HLE.cpp
+++ Source/Core/Core/Src/IPC_HLE/WII_IPC_HLE.cpp
@@ -83,7 +83,7 @@ static ipc_msg_queue reply_queue;	// arm -> ppc
 void Init()
 {
     _dbg_assert_msg_(WII_IPC_HLE, g_DeviceMap.empty(), "DeviceMap isnt empty on init");
-	
+	CWII_IPC_HLE_Device_es::m_ContentFile = "";
 	u32 i;
 	for (i=0; i<IPC_MAX_FDS; i++)
 	{
