603bd9982d89b917af83bae2e45b3eaa8e2accec
LPFaint99 <lpfaint99@gmail.com>
2012-08-19 20:44:09 -0700

    bugfix for memorycard manager. fixes exporting from page > 1 on slot b, thanks to suloku for reporting
    Signed-off-by: LPFaint99 <lpfaint99@gmail.com>

--- Source/Core/DolphinWX/Src/MemcardManager.cpp
+++ Source/Core/DolphinWX/Src/MemcardManager.cpp
@@ -480,12 +480,12 @@ void CMemcardManager::CopyDeleteClick(wxCommandEvent& event)
 	int index_B = m_MemcardList[SLOT_B]->GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
 	int slot = SLOT_B;
 	int slot2 = SLOT_A;
-	int index = index_B;
 	std::string fileName2("");
 
 	if (index_A != wxNOT_FOUND && page[SLOT_A]) index_A += itemsPerPage * page[SLOT_A];
 	if (index_B != wxNOT_FOUND && page[SLOT_B]) index_B += itemsPerPage * page[SLOT_B];
 
+	int index = index_B;
 	switch (event.GetId())
 	{
 	case ID_COPYFROM_B:
