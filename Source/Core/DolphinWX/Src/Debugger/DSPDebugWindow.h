f865450b5aa620db070e300f4ca8524c95d193f8
Matthew Parlane <parlane@gmail.com>
2012-12-24 00:00:45 +1300

    Changed to non conflicting overload.

--- Source/Core/DolphinWX/Src/Debugger/DSPDebugWindow.h
+++ Source/Core/DolphinWX/Src/Debugger/DSPDebugWindow.h
@@ -50,7 +50,7 @@ public:
 	DSPDebuggerLLE(wxWindow *parent, wxWindowID id = wxID_ANY);
 	virtual ~DSPDebuggerLLE();
 
-	virtual void Refresh();
+	virtual void Update();
 
 private:
 	DECLARE_EVENT_TABLE();
