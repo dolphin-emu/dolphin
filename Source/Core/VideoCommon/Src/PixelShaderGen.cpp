dolphin-emu/dolphin

commit 4925a28f94d8a9925a85824ec89513c6774bdc30

PixelShaderGen: Shader uid maintainance

Source/Core/VideoCommon/Src/PixelShaderGen.cpp
@@ -151,23 +151,17 @@ void GetPixelShaderId(PIXELSHADERUID *uid, DSTALPHA_MODE dstAlphaMode, u32 compo
 	ptr[0] |= bpmem.alpha_test.comp1 << 3; // 3
 	ptr[0] |= bpmem.alpha_test.logic << 6; // 2
 
-	if (alphaPreTest != AlphaTest::FAIL)
-	{
-		ptr[0] |= bpmem.fog.c_proj_fsel.fsel << 8; // 3
-		ptr[0] |= bpmem.ztex2.op << 11; // 2
-//		ptr[0] |= bpmem.zcontrol.early_ztest << 13; // 1
-		ptr[0] |= bpmem.zmode.testenable << 14; // 1
-	}
-
-	ptr[0] |= bpmem.zcontrol.early_ztest << 13; // 1
-	ptr[0] |= bpmem.zmode.updateenable << 15; // 1
+	ptr[0] |= bpmem.ztex2.op << 8; // 2
+	ptr[0] |= bpmem.zcontrol.early_ztest << 10; // 1
+	ptr[0] |= bpmem.zmode.testenable << 11; // 1
+	ptr[0] |= bpmem.zmode.updateenable << 12; // 1
 
 	if (dstAlphaMode != DSTALPHA_ALPHA_PASS)
 	{
 		if (bpmem.fog.c_proj_fsel.fsel != 0)
 		{
-			ptr[0] |= bpmem.fog.c_proj_fsel.proj << 16; // 1
-			ptr[0] |= bpmem.fogRange.Base.Enabled << 17; // 1
+			ptr[0] |= bpmem.fog.c_proj_fsel.proj << 13; // 1
+			ptr[0] |= bpmem.fogRange.Base.Enabled << 14; // 1
 		}
 	}
 

Source/Core/Common/Src/LinearDiskCache.h
@@ -24,7 +24,7 @@
 // Increment this every time you change shader generation code.
 enum
 {
-	LINEAR_DISKCACHE_VER = 6978
+	LINEAR_DISKCACHE_VER = 6979
 };
 
 // On disk format:

commit 6c0f6ffecff631344fe52972e267753e19e8d121

Fix a dumb regression from revision 4925a28f94d8.

Source/Core/VideoCommon/Src/PixelShaderGen.cpp
@@ -158,10 +158,11 @@ void GetPixelShaderId(PIXELSHADERUID *uid, DSTALPHA_MODE dstAlphaMode, u32 compo
 
 	if (dstAlphaMode != DSTALPHA_ALPHA_PASS)
 	{
+		ptr[0] |= bpmem.fog.c_proj_fsel.fsel << 13; // 3
 		if (bpmem.fog.c_proj_fsel.fsel != 0)
 		{
-			ptr[0] |= bpmem.fog.c_proj_fsel.proj << 13; // 1
-			ptr[0] |= bpmem.fogRange.Base.Enabled << 14; // 1
+			ptr[0] |= bpmem.fog.c_proj_fsel.proj << 16; // 1
+			ptr[0] |= bpmem.fogRange.Base.Enabled << 17; // 1
 		}
 	}
 