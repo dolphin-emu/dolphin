45a7fa293bebd520d86f5068dc866a35793d7af8
Rachel Bryk <RachelBryk@gmail.com>
2013-01-20 19:15:48 -0500

    Set the default memory card path as relative too.

--- Source/Core/Core/Src/CoreParameter.cpp
+++ Source/Core/Core/Src/CoreParameter.cpp
@@ -348,7 +348,11 @@ void SCoreStartupParameter::CheckMemcardPath(std::string& memcardPath, std::stri
 	{
 		// Use default memcard path if there is no user defined name
 		std::string defaultFilename = isSlotA ? GC_MEMCARDA : GC_MEMCARDB;
-		memcardPath = File::GetUserPath(D_GCUSER_IDX) + defaultFilename + ext;
+		#ifdef _WIN32
+			memcardPath = "." + File::GetUserPath(D_GCUSER_IDX).substr(File::GetExeDirectory().size()) + defaultFilename + ext;
+		#else
+			memcardPath = File::GetUserPath(D_GCUSER_IDX) + defaultFilename + ext;
+		#endif
 	}
 	else
 	{
