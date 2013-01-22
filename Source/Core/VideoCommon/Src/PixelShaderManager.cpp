d5748ebaef93cd601ae6ab5e12f39dd201593ad0
degasus <wickmarkus@web.de>
2013-01-22 10:49:46 +0100

    fix small dx9 slowdown

--- Source/Core/VideoCommon/Src/PixelShaderManager.cpp
+++ Source/Core/VideoCommon/Src/PixelShaderManager.cpp
@@ -85,7 +85,7 @@ void PixelShaderManager::Shutdown()
 
 void PixelShaderManager::SetConstants()
 {
-	if (!g_ActiveConfig.backend_info.bSupportsGLSLUBO)
+	if (g_ActiveConfig.backend_info.APIType == API_OPENGL && !g_ActiveConfig.backend_info.bSupportsGLSLUBO)
 		Dirty();
     for (int i = 0; i < 2; ++i)
 	{
