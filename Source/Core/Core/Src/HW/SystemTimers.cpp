286392af9d4166c2e376a9667249101a12313255
Jordan Woyak <jordan.woyak@gmail.com>
2013-02-01 23:44:17 -0600

    Reduce ACL max queue size back to 10. Increase IPC_HLE freq to 8 KHz to compensate.

--- Source/Core/Core/Src/HW/SystemTimers.cpp
+++ Source/Core/Core/Src/HW/SystemTimers.cpp
@@ -248,10 +248,10 @@ void Init()
 			DSP_PERIOD = (int)(GetTicksPerSecond() * 0.003f);
 
 		// AyuanX: TO BE TWEAKED
-		// Now the 1500 is a pure assumption
+		// Now the 1500 is (was) a pure assumption
 		// We need to figure out the real frequency though
 
-		const int freq = 4000;
+		const int freq = 8000;
 		const int fields = SConfig::GetInstance().m_LocalCoreStartupParameter.
 				bVBeam ? 2 : 1;
 		IPC_HLE_PERIOD = GetTicksPerSecond() / (freq * fields);
