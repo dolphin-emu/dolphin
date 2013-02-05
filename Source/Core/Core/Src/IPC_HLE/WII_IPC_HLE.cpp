fb5b5e8b63cb3484753c75d3e44205b0b8f33a00
Jordan Woyak <jordan.woyak@gmail.com>
2013-02-04 19:39:31 -0600

    Only delay DI command replies.

--- Source/Core/Core/Src/IPC_HLE/WII_IPC_HLE.cpp
+++ Source/Core/Core/Src/IPC_HLE/WII_IPC_HLE.cpp
@@ -405,6 +405,7 @@ void ExecuteCommand(u32 _Address)
 				else
 				{
 					delete pDevice;
+					pDevice = NULL;
 				}
 			}
 
@@ -435,7 +436,10 @@ void ExecuteCommand(u32 _Address)
 
 			// Don't delete hardware
 			if (!pDevice->IsHardware())
+			{
 				delete pDevice;
+				pDevice = NULL;
+			}
 		}
 		else
 		{
@@ -515,8 +519,8 @@ void ExecuteCommand(u32 _Address)
     if (CmdSuccess)
     {
 		// Generate a reply to the IPC command
-		// TODO: should probably figure out which commands need delayed replies and which don't
-		EnqReply(_Address, SystemTimers::GetTicksPerSecond() / 100);
+		int const reply_delay = pDevice ? pDevice->GetCmdDelay(_Address) : 0;
+		EnqReply(_Address, reply_delay);
     }
 	else
 	{
