4d27315cd1c0de85df93bfa859d19741483c06ea
Pierre Bourdon <delroth@gmail.com>
2013-03-31 01:55:17 +0100

    Initialize the AX Thread after the sync objects are initialized

--- Source/Core/Core/Src/HW/DSPHLE/UCodes/UCode_AX.h
+++ Source/Core/Core/Src/HW/DSPHLE/UCodes/UCode_AX.h
@@ -107,12 +107,12 @@ protected:
 	volatile u16 m_cmdlist[512];
 	volatile u32 m_cmdlist_size;
 
-	std::thread m_axthread;
-
 	// Sync objects
 	std::mutex m_processing;
 	std::condition_variable m_cmdlist_cv;
 	std::mutex m_cmdlist_mutex;
+
+	std::thread m_axthread;
 
 	// Copy a command list from memory to our temp buffer
 	void CopyCmdList(u32 addr, u16 size);
