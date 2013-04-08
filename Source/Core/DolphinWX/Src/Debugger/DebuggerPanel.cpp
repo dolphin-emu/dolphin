3ed0a9636785ba487be5f0e7b9f91d9fba4a7809
Lioncash <mathew1800@gmail.com>
2013-04-08 01:47:51 -0400

    Remove an extraneous comment indicator from DebuggerPanel.cpp in DolphinWX.
    Was asked to remove it, so... yeah.

--- Source/Core/DolphinWX/Src/Debugger/DebuggerPanel.cpp
+++ Source/Core/DolphinWX/Src/Debugger/DebuggerPanel.cpp
@@ -79,7 +79,7 @@ void GFXDebuggerPanel::SaveSettings() const
 
 	// TODO: make this work when we close the entire program too, currently on total close we get
 	// weird values, perhaps because of some conflict with the rendering window
-	//
+
 	// TODO: get the screen resolution and make limits from that
 	if (GetPosition().x < 1000 && GetPosition().y < 1000
 							   && GetSize().GetWidth() < 1000 
