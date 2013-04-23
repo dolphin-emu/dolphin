bd72e13dd42a2ad276e5836570972c601a74e562
Ryan Houdek <Sonicadvance1@gmail.com>
2013-04-23 14:21:48 -0500

    Generally make the Android UI better.

--- Source/Android/src/org/dolphinemu/dolphinemu/SideMenuAdapter.java
+++ Source/Android/src/org/dolphinemu/dolphinemu/SideMenuAdapter.java
@@ -9,20 +9,20 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-public class FileArrayAdapter extends ArrayAdapter<Option>{
+public class SideMenuAdapter extends ArrayAdapter<SideMenuItem>{
 
 	private Context c;
 	private int id;
-	private List<Option>items;
+	private List<SideMenuItem>items;
 	
-	public FileArrayAdapter(Context context, int textViewResourceId,
-			List<Option> objects) {
+	public SideMenuAdapter(Context context, int textViewResourceId,
+			List<SideMenuItem> objects) {
 		super(context, textViewResourceId, objects);
 		c = context;
 		id = textViewResourceId;
 		items = objects;
 	}
-	public Option getItem(int i)
+	public SideMenuItem getItem(int i)
 	 {
 		 return items.get(i);
 	 }
@@ -33,16 +33,12 @@ public class FileArrayAdapter extends ArrayAdapter<Option>{
                   LayoutInflater vi = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                   v = vi.inflate(id, null);
               }
-              final Option o = items.get(position);
+              final SideMenuItem o = items.get(position);
               if (o != null) {
                       TextView t1 = (TextView) v.findViewById(R.id.TextView01);
-                      TextView t2 = (TextView) v.findViewById(R.id.TextView02);
                       
                       if(t1!=null)
                       	t1.setText(o.getName());
-                      if(t2!=null)
-                      	t2.setText(o.getData());
-                      
               }
               return v;
       }
