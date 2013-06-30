dolphin-emu/dolphin

commit f524312fd17e62cbc761bb01953c67763fc9242b

VertexShaderGen: Fix a potential bug where vertex shader uids don't change when pixel lighting is toggled.

Source/Core/VideoCommon/Src/VertexShaderGen.cpp
@@ -279,8 +279,8 @@ static void GenerateVertexShader(T& out, u32 components, API_TYPE api_type)
 			out.Write("o.colors_0 = float4(1.0f, 1.0f, 1.0f, 1.0f);\n");
 	}
 
-	// TODO: This probably isn't necessary if pixel lighting is enabled.
-	GenerateLightingShader<T>(out, uid_data.lighting, components, I_MATERIALS, I_LIGHTS, "color", "o.colors_");
+	if (g_ActiveConfig.bEnablePixelLighting && g_ActiveConfig.backend_info.bSupportsPixelLighting)
+		GenerateLightingShader<T>(out, uid_data.lighting, components, I_MATERIALS, I_LIGHTS, "color", "o.colors_");
 
 	if (xfregs.numChan.numColorChans < 2)
 	{

commit b8b5427ba4cead27a52de2780f6e03bfcc34fd22

VertexShaderGen: Fix a dumb regression from revision f524312fd17e.

Source/Core/VideoCommon/Src/VertexShaderGen.cpp
@@ -279,7 +279,7 @@ static void GenerateVertexShader(T& out, u32 components, API_TYPE api_type)
 			out.Write("o.colors_0 = float4(1.0f, 1.0f, 1.0f, 1.0f);\n");
 	}
 
-	if (g_ActiveConfig.bEnablePixelLighting && g_ActiveConfig.backend_info.bSupportsPixelLighting)
+	if (!(g_ActiveConfig.bEnablePixelLighting && g_ActiveConfig.backend_info.bSupportsPixelLighting))
 		GenerateLightingShader<T>(out, uid_data.lighting, components, I_MATERIALS, I_LIGHTS, "color", "o.colors_");
 
 	if (xfregs.numChan.numColorChans < 2)