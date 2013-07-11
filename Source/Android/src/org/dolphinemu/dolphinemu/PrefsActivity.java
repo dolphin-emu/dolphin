e1f641424de268385649be6982d90182a8bfab2f
Ryan Houdek <Sonicadvance1@gmail.com>
2013-07-11 10:18:03 -0500

    [Android] Show JIT cores based on host arch.

--- Source/Android/src/org/dolphinemu/dolphinemu/PrefsActivity.java
+++ Source/Android/src/org/dolphinemu/dolphinemu/PrefsActivity.java
@@ -2,8 +2,11 @@ package org.dolphinemu.dolphinemu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 
 /**
@@ -12,6 +15,7 @@ import android.preference.PreferenceFragment;
  * Refer to the license.txt file included.
  */
 public class PrefsActivity extends PreferenceActivity {
+	private PrefsActivity m_activity;
 
     public class PrefsFragment extends PreferenceFragment {
 
@@ -21,12 +25,53 @@ public class PrefsActivity extends PreferenceActivity {
 
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.layout.prefs);
+
+	        final ListPreference etp = new ListPreference(m_activity);
+	        CharSequence[] _entries;
+	        CharSequence[] _entryvalues;
+
+	        if (Build.CPU_ABI.contains("x86"))
+	        {
+		        _entries = new CharSequence[] {
+			        "Interpreter",
+			        "JIT64 Recompiler",
+			        "JITIL Recompiler",
+		        };
+		        _entryvalues = new CharSequence[] {"0", "1", "2"};
+	        }
+	        else if (Build.CPU_ABI.contains("arm"))
+	        {
+		        _entries = new CharSequence[] {
+				        "Interpreter",
+				        "JIT ARM Recompiler",
+		        };
+		        _entryvalues = new CharSequence[] {"0", "3"};
+	        }
+	        else
+	        {
+		        _entries = new CharSequence[] {
+				        "Interpreter",
+		        };
+		        _entryvalues = new CharSequence[] {"0"};
+	        }
+
+	        etp.setEntries(_entries);
+	        etp.setEntryValues(_entryvalues);
+	        etp.setKey("cpupref");
+	        etp.setTitle("CPU Core");
+	        etp.setSummary("Emulation core to use");
+
+	        PreferenceCategory mCategory = (PreferenceCategory) findPreference("cpuprefcat");
+	        mCategory.addPreference(etp);
         }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+	    m_activity = this;
+
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new PrefsFragment()).commit();
     }
