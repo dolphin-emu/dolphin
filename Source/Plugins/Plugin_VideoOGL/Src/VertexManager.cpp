4aba0135e1c9137c551267f274abfbb7d78d975f
Ryan Houdek <Sonicadvance1@gmail.com>
2013-07-26 21:49:48 -0500

    [Android] Qualcomm Swap hack isn't needed anymore due to the new StreamBuffer type.

--- Source/Plugins/Plugin_VideoOGL/Src/VertexManager.cpp
+++ Source/Plugins/Plugin_VideoOGL/Src/VertexManager.cpp
@@ -268,8 +268,6 @@ void VertexManager::vFlush()
 
 	g_perf_query->EnableQuery(bpmem.zcontrol.early_ztest ? PQG_ZCOMP_ZCOMPLOC : PQG_ZCOMP);
 	Draw(stride);
-	if (DriverDetails::HasBug(DriverDetails::BUG_BROKENBUFFERS))
-		GLInterface->Swap();
 	g_perf_query->DisableQuery(bpmem.zcontrol.early_ztest ? PQG_ZCOMP_ZCOMPLOC : PQG_ZCOMP);
 	//ERROR_LOG(VIDEO, "PerfQuery result: %d", g_perf_query->GetQueryResult(bpmem.zcontrol.early_ztest ? PQ_ZCOMP_OUTPUT_ZCOMPLOC : PQ_ZCOMP_OUTPUT));
 
