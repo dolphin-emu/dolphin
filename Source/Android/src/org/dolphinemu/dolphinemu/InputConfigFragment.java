00b034f991ffedd2fb30040dea715b2287563cc0
Lioncash <mathew1800@gmail.com>
2013-08-12 15:32:52 -0400

    [Android] Seems like InputConfigFragment.java also had explicit list indexing. Removed it from here too.

--- Source/Android/src/org/dolphinemu/dolphinemu/InputConfigFragment.java
+++ Source/Android/src/org/dolphinemu/dolphinemu/InputConfigFragment.java
@@ -47,29 +47,27 @@ public class InputConfigFragment extends Fragment
 	                         Bundle savedInstanceState)
 	{
 		List<InputConfigItem> Input = new ArrayList<InputConfigItem>();
-		int a = 0;
-
-		Input.add(a++, new InputConfigItem("Draw on-screen controls", "Android-ScreenControls", "True"));
-		Input.add(a++, new InputConfigItem("Button A", "Android-InputA"));
-		Input.add(a++, new InputConfigItem("Button B", "Android-InputB"));
-		Input.add(a++, new InputConfigItem("Button Start", "Android-InputStart"));
-		Input.add(a++, new InputConfigItem("Button X", "Android-InputX"));
-		Input.add(a++, new InputConfigItem("Button Y", "Android-InputY"));
-		Input.add(a++, new InputConfigItem("Button Z", "Android-InputZ"));
-		Input.add(a++, new InputConfigItem("D-Pad Up", "Android-DPadUp"));
-		Input.add(a++, new InputConfigItem("D-Pad Down", "Android-DPadDown"));
-		Input.add(a++, new InputConfigItem("D-Pad Left", "Android-DPadLeft"));
-		Input.add(a++, new InputConfigItem("D-Pad Right", "Android-DPadRight"));
-		Input.add(a++, new InputConfigItem("Main Stick Up", "Android-MainUp"));
-		Input.add(a++, new InputConfigItem("Main Stick Down", "Android-MainDown"));
-		Input.add(a++, new InputConfigItem("Main Stick Left", "Android-MainLeft"));
-		Input.add(a++, new InputConfigItem("Main Stick Right", "Android-MainRight"));
-		Input.add(a++, new InputConfigItem("C Stick Up", "Android-CStickUp"));
-		Input.add(a++, new InputConfigItem("C Stick Down", "Android-CStickDown"));
-		Input.add(a++, new InputConfigItem("C Stick Left", "Android-CStickLeft"));
-		Input.add(a++, new InputConfigItem("C Stick Right", "Android-CStickRight"));
-		Input.add(a++, new InputConfigItem("Trigger L", "Android-InputL"));
-		Input.add(a++, new InputConfigItem("Trigger R", "Android-InputR"));
+		Input.add(new InputConfigItem("Draw on-screen controls", "Android-ScreenControls", "True"));
+		Input.add(new InputConfigItem("Button A", "Android-InputA"));
+		Input.add(new InputConfigItem("Button B", "Android-InputB"));
+		Input.add(new InputConfigItem("Button Start", "Android-InputStart"));
+		Input.add(new InputConfigItem("Button X", "Android-InputX"));
+		Input.add(new InputConfigItem("Button Y", "Android-InputY"));
+		Input.add(new InputConfigItem("Button Z", "Android-InputZ"));
+		Input.add(new InputConfigItem("D-Pad Up", "Android-DPadUp"));
+		Input.add(new InputConfigItem("D-Pad Down", "Android-DPadDown"));
+		Input.add(new InputConfigItem("D-Pad Left", "Android-DPadLeft"));
+		Input.add(new InputConfigItem("D-Pad Right", "Android-DPadRight"));
+		Input.add(new InputConfigItem("Main Stick Up", "Android-MainUp"));
+		Input.add(new InputConfigItem("Main Stick Down", "Android-MainDown"));
+		Input.add(new InputConfigItem("Main Stick Left", "Android-MainLeft"));
+		Input.add(new InputConfigItem("Main Stick Right", "Android-MainRight"));
+		Input.add(new InputConfigItem("C Stick Up", "Android-CStickUp"));
+		Input.add(new InputConfigItem("C Stick Down", "Android-CStickDown"));
+		Input.add(new InputConfigItem("C Stick Left", "Android-CStickLeft"));
+		Input.add(new InputConfigItem("C Stick Right", "Android-CStickRight"));
+		Input.add(new InputConfigItem("Trigger L", "Android-InputL"));
+		Input.add(new InputConfigItem("Trigger R", "Android-InputR"));
 
 		adapter = new InputConfigAdapter(m_activity, R.layout.folderbrowser, Input);
 		View rootView = inflater.inflate(R.layout.gamelist_listview, container, false);
