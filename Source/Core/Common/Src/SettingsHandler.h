75c398118fb1d470a82735f93f5c0d1fe0a91664
Matthew Parlane <parlane@gmail.com>
2013-08-28 00:57:08 +1200

    Fix headings and more.

--- Source/Core/Common/Src/SettingsHandler.h
+++ Source/Core/Common/Src/SettingsHandler.h
@@ -1,19 +1,6 @@
-// Copyright (C) 2003 Dolphin Project.
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, version 2.0.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License 2.0 for more details.
-
-// A copy of the GPL 2.0 should have been included with the program.
-// If not, see http://www.gnu.org/licenses/
-
-// Official SVN repository and contact information can be found at
-// http://code.google.com/p/dolphin-emu/
+// Copyright 2013 Dolphin Emulator Project
+// Licensed under GPLv2
+// Refer to the license.txt file included.
 
 // Thanks to Treeki for writing the original class - 29/01/2012
 
@@ -30,7 +17,9 @@ public:
 
 	enum
 	{
-		SETTINGS_SIZE = 0x100
+		SETTINGS_SIZE = 0x100,
+		// Key used to encrypt/decrypt setting.txt contents
+		INITIAL_SEED = 0x73B5DBFA
 	};
 
 	void AddSetting(const char *key, const char *value);
