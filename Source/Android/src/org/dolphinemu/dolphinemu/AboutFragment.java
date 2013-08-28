8ca3ed13decd3062e8a509ff46d15fa4eccd086e
lioncash <mathew1900@hotmail.com>
2013-08-28 08:15:00 -0400

    [Android] Clean up AboutFragment's onAttach method.

--- Source/Android/src/org/dolphinemu/dolphinemu/AboutFragment.java
+++ Source/Android/src/org/dolphinemu/dolphinemu/AboutFragment.java
@@ -52,16 +52,7 @@ public final class AboutFragment extends Fragment
 	{
 		super.onAttach(activity);
 
-		// This makes sure that the container activity has implemented
-		// the callback interface. If not, it throws an exception
-		try
-		{
-			m_activity = activity;
-		}
-		catch (ClassCastException e)
-		{
-			throw new ClassCastException(activity.toString()
-					+ " must implement OnGameListZeroListener");
-		}
+		// Cache the activity instance.
+		m_activity = activity;
 	}
 }
