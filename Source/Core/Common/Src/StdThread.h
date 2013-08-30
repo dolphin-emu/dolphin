8bb16d8e989be8c753b95f35a9ac94bdfd7f5ff0
Ryan Houdek <Sonicadvance1@gmail.com>
2013-08-29 22:29:33 -0500

    [Android] Android NDK now supports full implementations of std::mutex, std::thread, and std::conditional_variable so there is no need to have our own implementations there now.

--- Source/Core/Common/Src/StdThread.h
+++ Source/Core/Common/Src/StdThread.h
@@ -9,7 +9,7 @@
 #define __has_include(s) 0
 #endif
 
-#if GCC_VERSION >= GCC_VER(4,4,0) && __GXX_EXPERIMENTAL_CXX0X__ && !ANDROID
+#if GCC_VERSION >= GCC_VER(4,4,0) && __GXX_EXPERIMENTAL_CXX0X__
 // GCC 4.4 provides <thread>
 #ifndef _GLIBCXX_USE_SCHED_YIELD
 #define _GLIBCXX_USE_SCHED_YIELD
