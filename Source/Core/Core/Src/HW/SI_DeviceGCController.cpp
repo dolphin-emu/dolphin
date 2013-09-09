772046647a7e9d65414137e4faecdebf41f667c8
Rachel Bryk <RachelBryk@gmail.com>
2013-09-09 03:09:45 -0400

    Revert "Revert "NetPlay: Use the correct pad mappings for rumble""
    This reverts commit 06140e8083921a61d00ba111f6a636da531f2c00.

--- Source/Core/Core/Src/HW/SI_DeviceGCController.cpp
+++ Source/Core/Core/Src/HW/SI_DeviceGCController.cpp
@@ -249,7 +249,7 @@ void CSIDevice_GCController::SendCommand(u32 _Cmd, u8 _Poll)
 			unsigned int uStrength = command.Parameter2;
 
 			// get the correct pad number that should rumble locally when using netplay
-			const u8 numPAD = NetPlay_GetPadNum(ISIDevice::m_iDeviceNumber);
+			const u8 numPAD = NetPlay_InGamePadToLocalPad(ISIDevice::m_iDeviceNumber);
 
 			if (numPAD < 4)
 				Pad::Rumble(numPAD, uType, uStrength);
