511de71736d499946a4280292f7cbee2052b1819
Lioncash <mathew1800@gmail.com>
2013-10-10 11:57:18 -0400

    [Android] Actually show the currently selected CPU core and video backend within the settings screen.

---
+++ Source/Android/src/org/dolphinemu/dolphinemu/settings/custom/UpdatingListPreference.java
@@ -0,0 +1,33 @@
+package org.dolphinemu.dolphinemu.settings.custom;
+
+import android.content.Context;
+import android.preference.ListPreference;
+import android.util.AttributeSet;
+
+/**
+ * A {@link ListPreference} that updates its summary upon it's selection being changed.
+ */
+public final class UpdatingListPreference extends ListPreference
+{
+	/**
+	 * Constructor
+	 * 
+	 * @param context the current {@link Context}.
+	 */
+	public UpdatingListPreference(Context context, AttributeSet attribs)
+	{
+		super(context, attribs);
+	}
+
+	@Override
+	public void onDialogClosed(boolean positiveResult)
+	{
+		super.onDialogClosed(positiveResult);
+
+		// If an entry was selected
+		if (positiveResult)
+		{
+			setSummary(getEntry());
+		}
+	}
+}
