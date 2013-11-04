zakk4223/dolphin-emu

Run code through clang-modernize -loop-convert to create range-based for loops, and manually fix some stuff up.

Source/Core/Common/Src/FileSearch.cpp
@@ -21,11 +21,11 @@
 CFileSearch::CFileSearch(const CFileSearch::XStringVector& _rSearchStrings, const CFileSearch::XStringVector& _rDirectories)
 {
 	// Reverse the loop order for speed?
-	for (size_t j = 0; j < _rSearchStrings.size(); j++)
+	for (auto& _rSearchString : _rSearchStrings)
 	{
-		for (size_t i = 0; i < _rDirectories.size(); i++)
+		for (auto& _rDirectorie : _rDirectories)
 		{
-			FindFiles(_rSearchStrings[j], _rDirectories[i]);
+			FindFiles(_rSearchString, _rDirectorie);
 		}
 	}
 }

Source/Core/DolphinWX/Src/GameListCtrl.cpp
@@ -475,15 +475,15 @@ void CGameListCtrl::ScanForISOs()
 		{
 			File::FSTEntry FST_Temp;
 			File::ScanDirectoryTree(Directories[i], FST_Temp);
-			for (u32 j = 0; j < FST_Temp.children.size(); j++)
+			for (auto& Entry : FST_Temp.children)
 			{
-				if (FST_Temp.children[j].isDirectory)
+				if (Entry.isDirectory)
 				{
 					bool duplicate = false;
-					for (u32 k = 0; k < Directories.size(); k++)
+					for (auto& Directorie : Directories)
 					{
-						if (strcmp(Directories[k].c_str(),
-									FST_Temp.children[j].physicalName.c_str()) == 0)
+						if (strcmp(Directorie.c_str(),
+									Entry.physicalName.c_str()) == 0)
 						{
 							duplicate = true;
 							break;
@@ -491,7 +491,7 @@ void CGameListCtrl::ScanForISOs()
 					}
 					if (!duplicate)
 						Directories.push_back(
-								FST_Temp.children[j].physicalName.c_str());
+								Entry.physicalName.c_str());
 				}
 			}
 		}
@@ -603,10 +603,10 @@ void CGameListCtrl::ScanForISOs()
 	{
 		const std::vector<std::string> drives = cdio_get_devices();
 
-		for (std::vector<std::string>::const_iterator iter = drives.begin(); iter != drives.end(); ++iter)
+		for (const auto& drive : drives)
 		{
 			#ifdef __APPLE__
-			std::auto_ptr<GameListItem> gli(new GameListItem(*iter));
+			std::auto_ptr<GameListItem> gli(new GameListItem(drive));
 			#else
 			std::unique_ptr<GameListItem> gli(new GameListItem(*iter));
 			#endif

Source/Core/VideoBackends/OGL/Src/main.cpp
@@ -110,16 +110,16 @@ void GetShaders(std::vector<std::string> &shaders)
 		File::GetUserPath(D_SHADERS_IDX),
 		File::GetSysDirectory() + SHADERS_DIR DIR_SEP,
 	};
-	for (size_t i = 0; i < ArraySize(directories); ++i)
+	for (auto& directorie : directories)
 	{
-		if (!File::IsDirectory(directories[i]))
+		if (!File::IsDirectory(directorie))
 			continue;
 
 		File::FSTEntry entry;
-		File::ScanDirectoryTree(directories[i], entry);
-		for (u32 j = 0; j < entry.children.size(); j++)
+		File::ScanDirectoryTree(directorie, entry);
+		for (auto& file : entry.children)
 		{
-			std::string name = entry.children[j].virtualName.c_str();
+			std::string name = file.virtualName.c_str();
 			if (name.size() < 5)
 				continue;
 			if (strcasecmp(name.substr(name.size() - 5).c_str(), ".glsl"))

Source/Core/VideoCommon/Src/HiresTextures.cpp
@@ -33,21 +33,21 @@ void Init(const char *gameCode)
 	{
 		File::FSTEntry FST_Temp;
 		File::ScanDirectoryTree(Directories[i], FST_Temp);
-		for (u32 j = 0; j < FST_Temp.children.size(); j++)
+		for (auto& entry : FST_Temp.children)
 		{
-			if (FST_Temp.children.at(j).isDirectory)
+			if (entry.isDirectory)
 			{
 				bool duplicate = false;
-				for (u32 k = 0; k < Directories.size(); k++)
+				for (auto& Directorie : Directories)
 				{
-					if (strcmp(Directories[k].c_str(), FST_Temp.children.at(j).physicalName.c_str()) == 0)
+					if (strcmp(Directorie.c_str(), entry.physicalName.c_str()) == 0)
 					{
 						duplicate = true;
 						break;
 					}
 				}
 				if (!duplicate)
-					Directories.push_back(FST_Temp.children.at(j).physicalName.c_str());
+					Directories.push_back(entry.physicalName.c_str());
 			}
 		}
 	}
@@ -66,13 +66,13 @@ void Init(const char *gameCode)
 
 	if (rFilenames.size() > 0)
 	{
-		for (u32 i = 0; i < rFilenames.size(); i++)
+		for (auto& rFilename : rFilenames)
 		{
 			std::string FileName;
-			SplitPath(rFilenames[i], NULL, &FileName, NULL);
+			SplitPath(rFilename, NULL, &FileName, NULL);
 
 			if (FileName.substr(0, strlen(code)).compare(code) == 0 && textureMap.find(FileName) == textureMap.end())
-				textureMap.insert(std::map<std::string, std::string>::value_type(FileName, rFilenames[i]));
+				textureMap.insert(std::map<std::string, std::string>::value_type(FileName, rFilename));
 		}
 	}
 }

dolphin-emu/dolphin

Source/Core/Common/Src/FileSearch.cpp
@@ -21,11 +21,11 @@
 CFileSearch::CFileSearch(const CFileSearch::XStringVector& _rSearchStrings, const CFileSearch::XStringVector& _rDirectories)
 {
 	// Reverse the loop order for speed?
-	for (size_t j = 0; j < _rSearchStrings.size(); j++)
+	for (auto& _rSearchString : _rSearchStrings)
 	{
-		for (size_t i = 0; i < _rDirectories.size(); i++)
+		for (auto& _rDirectory : _rDirectories)
 		{
-			FindFiles(_rSearchStrings[j], _rDirectories[i]);
+			FindFiles(_rSearchString, _rDirectory);
 		}
 	}
 }

Source/Core/DolphinWX/Src/GameListCtrl.cpp
@@ -475,15 +475,15 @@ void CGameListCtrl::ScanForISOs()
 		{
 			File::FSTEntry FST_Temp;
 			File::ScanDirectoryTree(Directories[i], FST_Temp);
-			for (u32 j = 0; j < FST_Temp.children.size(); j++)
+			for (auto& Entry : FST_Temp.children)
 			{
-				if (FST_Temp.children[j].isDirectory)
+				if (Entry.isDirectory)
 				{
 					bool duplicate = false;
-					for (u32 k = 0; k < Directories.size(); k++)
+					for (auto& Directory : Directories)
 					{
-						if (strcmp(Directories[k].c_str(),
-									FST_Temp.children[j].physicalName.c_str()) == 0)
+						if (strcmp(Directory.c_str(),
+									Entry.physicalName.c_str()) == 0)
 						{
 							duplicate = true;
 							break;
@@ -491,7 +491,7 @@ void CGameListCtrl::ScanForISOs()
 					}
 					if (!duplicate)
 						Directories.push_back(
-								FST_Temp.children[j].physicalName.c_str());
+								Entry.physicalName.c_str());
 				}
 			}
 		}
@@ -539,7 +539,7 @@ void CGameListCtrl::ScanForISOs()
 			if (dialog.WasCancelled())
 				break;
 
-			std::auto_ptr<GameListItem> iso_file(new GameListItem(rFilenames[i]));
+			std::unique_ptr<GameListItem> iso_file(new GameListItem(rFilenames[i]));
 			const GameListItem& ISOFile = *iso_file;
 
 			if (ISOFile.IsValid())
@@ -603,13 +603,9 @@ void CGameListCtrl::ScanForISOs()
 	{
 		const std::vector<std::string> drives = cdio_get_devices();
 
-		for (std::vector<std::string>::const_iterator iter = drives.begin(); iter != drives.end(); ++iter)
+		for (const auto& drive : drives)
 		{
-			#ifdef __APPLE__
-			std::auto_ptr<GameListItem> gli(new GameListItem(*iter));
-			#else
-			std::unique_ptr<GameListItem> gli(new GameListItem(*iter));
-			#endif
+			std::unique_ptr<GameListItem> gli(new GameListItem(drive));
 
 			if (gli->IsValid())
 				m_ISOFiles.push_back(gli.release());

Source/Core/VideoBackends/OGL/Src/main.cpp
@@ -110,16 +110,16 @@ void GetShaders(std::vector<std::string> &shaders)
 		File::GetUserPath(D_SHADERS_IDX),
 		File::GetSysDirectory() + SHADERS_DIR DIR_SEP,
 	};
-	for (size_t i = 0; i < ArraySize(directories); ++i)
+	for (auto& directory : directories)
 	{
-		if (!File::IsDirectory(directories[i]))
+		if (!File::IsDirectory(directory))
 			continue;
 
 		File::FSTEntry entry;
-		File::ScanDirectoryTree(directories[i], entry);
-		for (u32 j = 0; j < entry.children.size(); j++)
+		File::ScanDirectoryTree(directory, entry);
+		for (auto& file : entry.children)
 		{
-			std::string name = entry.children[j].virtualName.c_str();
+			std::string name = file.virtualName.c_str();
 			if (name.size() < 5)
 				continue;
 			if (strcasecmp(name.substr(name.size() - 5).c_str(), ".glsl"))

Source/Core/VideoCommon/Src/HiresTextures.cpp
@@ -33,21 +33,21 @@ void Init(const char *gameCode)
 	{
 		File::FSTEntry FST_Temp;
 		File::ScanDirectoryTree(Directories[i], FST_Temp);
-		for (u32 j = 0; j < FST_Temp.children.size(); j++)
+		for (auto& entry : FST_Temp.children)
 		{
-			if (FST_Temp.children.at(j).isDirectory)
+			if (entry.isDirectory)
 			{
 				bool duplicate = false;
-				for (u32 k = 0; k < Directories.size(); k++)
+				for (auto& Directory : Directories)
 				{
-					if (strcmp(Directories[k].c_str(), FST_Temp.children.at(j).physicalName.c_str()) == 0)
+					if (strcmp(Directory.c_str(), entry.physicalName.c_str()) == 0)
 					{
 						duplicate = true;
 						break;
 					}
 				}
 				if (!duplicate)
-					Directories.push_back(FST_Temp.children.at(j).physicalName.c_str());
+					Directories.push_back(entry.physicalName.c_str());
 			}
 		}
 	}
@@ -66,13 +66,13 @@ void Init(const char *gameCode)
 
 	if (rFilenames.size() > 0)
 	{
-		for (u32 i = 0; i < rFilenames.size(); i++)
+		for (auto& rFilename : rFilenames)
 		{
 			std::string FileName;
-			SplitPath(rFilenames[i], NULL, &FileName, NULL);
+			SplitPath(rFilename, NULL, &FileName, NULL);
 
 			if (FileName.substr(0, strlen(code)).compare(code) == 0 && textureMap.find(FileName) == textureMap.end())
-				textureMap.insert(std::map<std::string, std::string>::value_type(FileName, rFilenames[i]));
+				textureMap.insert(std::map<std::string, std::string>::value_type(FileName, rFilename));
 		}
 	}
 }