0720026dab7c1899cf788f4ed24514c5a89d8717
Ryan Houdek <Sonicadvance1@gmail.com>
2013-11-15 16:56:21 -0600

    [Android] Disable the workaround for Qualcomm devices with driver >= 53 for the rotated framebuffer since it is fixed now.

--- Source/Android/src/org/dolphinemu/dolphinemu/emulation/EmulationActivity.java
+++ Source/Android/src/org/dolphinemu/dolphinemu/emulation/EmulationActivity.java
@@ -68,12 +68,14 @@ public final class EmulationActivity extends Activity
 
 		// Due to a bug in Adreno, it renders the screen rotated 90 degrees when using OpenGL
 		// Flip the width and height when on Adreno to work around this.
+		// This bug is fixed in Qualcomm driver v53
 		// Mali isn't affected by this bug.
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		if (prefs.getString("gpuPref", "Software Rendering").equals("OGL")
 				&& VideoSettingsFragment.SupportsGLES3()
 				&& VideoSettingsFragment.m_GLVendor != null
-				&& VideoSettingsFragment.m_GLVendor.equals("Qualcomm"))
+				&& VideoSettingsFragment.m_GLVendor.equals("Qualcomm")
+				&& VideoSettingsFragment.m_QualcommVersion < 53.0f)
 			NativeLibrary.SetDimensions((int)screenHeight, (int)screenWidth);
 		else
 			NativeLibrary.SetDimensions((int)screenWidth, (int)screenHeight);
