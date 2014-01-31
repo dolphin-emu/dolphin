mirror/dolphin-emu

PortableVertexFormat: use AttributeFormat for normal attribute format

Source/Core/VideoBackends/D3D/NativeVertexFormat.cpp
@@ -96,12 +96,13 @@ void D3DVertexFormat::Initialize(const PortableVertexDeclaration &_vtx_decl)
 
 	for (int i = 0; i < 3; i++)
 	{
-		if (_vtx_decl.normal_offset[i] > 0)
+		format = &_vtx_decl.normals[i];
+		if (format->enable)
 		{
 			m_elems[m_num_elems].SemanticName = "NORMAL";
 			m_elems[m_num_elems].SemanticIndex = i;
-			m_elems[m_num_elems].AlignedByteOffset = _vtx_decl.normal_offset[i];
-			m_elems[m_num_elems].Format = VarToD3D(_vtx_decl.normal_gl_type, _vtx_decl.normal_gl_size, true);
+			m_elems[m_num_elems].AlignedByteOffset = format->offset;
+			m_elems[m_num_elems].Format = VarToD3D(format->type, format->components, format->normalize);
 			m_elems[m_num_elems].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 			++m_num_elems;
 		}

dolphin-emu/dolphin

Source/Core/VideoBackends/D3D/NativeVertexFormat.cpp
@@ -74,12 +74,13 @@ void D3DVertexFormat::Initialize(const PortableVertexDeclaration &_vtx_decl)
 
 	for (int i = 0; i < 3; i++)
 	{
-		if (_vtx_decl.normal_offset[i] > 0)
+		format = &_vtx_decl.normals[i];
+		if (format->enable)
 		{
 			m_elems[m_num_elems].SemanticName = "NORMAL";
 			m_elems[m_num_elems].SemanticIndex = i;
-			m_elems[m_num_elems].AlignedByteOffset = _vtx_decl.normal_offset[i];
-			m_elems[m_num_elems].Format = VarToD3D(_vtx_decl.normal_gl_type, _vtx_decl.normal_gl_size, false);
+			m_elems[m_num_elems].AlignedByteOffset = format->offset;
+			m_elems[m_num_elems].Format = VarToD3D(format->type, format->components, format->integer);
 			m_elems[m_num_elems].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 			++m_num_elems;
 		}