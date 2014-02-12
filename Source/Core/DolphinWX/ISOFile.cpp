82f4add0d1713ef76217f86cef629623468cc261
Tillmann Karras <tilkax@gmail.com>
2014-02-12 23:53:17 +0100

    One more warning fix

--- Source/Core/DolphinWX/ISOFile.cpp
+++ Source/Core/DolphinWX/ISOFile.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <wx/mstream.h>
+#include <cinttypes>
 
 #include "Common.h"
 #include "CommonPaths.h"
@@ -188,7 +189,7 @@ std::string GameListItem::CreateCacheFilename()
 
 	// Filename.extension_HashOfFolderPath_Size.cache
 	// Append hash to prevent ISO name-clashing in different folders.
-	Filename.append(StringFromFormat("%s_%x_%zx.cache",
+	Filename.append(StringFromFormat("%s_%x_%" PRIx64 ".cache",
 		extension.c_str(), HashFletcher((const u8 *)LegalPathname.c_str(), LegalPathname.size()),
 		File::GetSize(m_FileName)));
 
