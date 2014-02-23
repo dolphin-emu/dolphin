0f89060cf3914514992b1ac1b4dc5b0b278f6939
Pierre Bourdon <delroth@gmail.com>
2014-02-23 16:21:01 +0100

    Remove Source/{Unittests,TestSuite}.
    These directories have been unused for several years and without any automated
    way of running the tests, they are pretty much useless.
    
    While they might be useful as a reference, in their present state they should
    not be kept in the repository.

--- Source/TestSuite/ASM/source/Defines.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#define u32 unsigned int
-#define u16 unsigned short
-#define u8 unsigned char
-#define s8 signed char
-#define s16 signed short
-#define s32 signed int
--- Source/TestSuite/grind.sh
+++
@@ -1,2 +0,0 @@
-valgrind --gen-suppressions=yes --suppressions=d.supp ./Dolphin $1 $2 $3
-
