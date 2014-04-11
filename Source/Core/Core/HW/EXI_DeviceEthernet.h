891fb056a3c28b7ad46c30a2ae7dc4cb042d08a5
SlEePlEs5 <matthieu.gb@icloud.com>
2014-04-12 01:38:22 +0200

    Added BBA TAP backend for OS X.
    TUN/TAP OS X needs to be installed, and /dev/tap0 chown’d to the user.
    The tap0 network interface doesn’t appear until /dev/tap0 is opened by
    the program, so all bridging must be done once the game is already
    running.
    
    Unfortunately, bridging seems to be broken on OS X, so this does not
    actually work over LAN (yet!).

--- Source/Core/Core/HW/EXI_DeviceEthernet.h
+++ Source/Core/Core/HW/EXI_DeviceEthernet.h
@@ -319,7 +319,7 @@ public:
 	DWORD mMtu;
 	OVERLAPPED mReadOverlapped;
 	static VOID CALLBACK ReadWaitCallback(PVOID lpParameter, BOOLEAN TimerFired);
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__APPLE__)
 	int fd;
 	std::thread readThread;
 	volatile bool readEnabled;
