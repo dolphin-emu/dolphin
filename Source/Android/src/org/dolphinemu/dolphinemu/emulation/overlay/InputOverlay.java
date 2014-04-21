05acacef218871e96bcd26c064ea6d80060886a8
Sean <seanmaas27@gmail.com>
2014-04-21 11:47:09 -0400

    Android: Allow finger movement while pressing button

--- Source/Android/src/org/dolphinemu/dolphinemu/emulation/overlay/InputOverlay.java
+++ Source/Android/src/org/dolphinemu/dolphinemu/emulation/overlay/InputOverlay.java
@@ -116,7 +116,8 @@ public final class InputOverlay extends SurfaceView implements OnTouchListener
 		//
 		// TODO: Refactor this so we detect either Axis movements or button presses so we don't run two loops all the time.
 		//
-		int buttonState = (event.getAction() == MotionEvent.ACTION_DOWN) ? ButtonState.PRESSED : ButtonState.RELEASED;
+		int buttonState = (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE)
+				? ButtonState.PRESSED : ButtonState.RELEASED;
 		// Check if there was a touch within the bounds of a drawable.
 		for (InputOverlayDrawableButton button : overlayButtons)
 		{
