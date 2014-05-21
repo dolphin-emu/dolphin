9b022e023b62ec20a6b1946b9f79311f91ab777f
myownfriend <obiwankenobi23@live.com>
2014-05-21 14:55:45 -0400

    Update firewater.glsl

--- Data/Sys/Shaders/firewater.glsl
+++ Data/Sys/Shaders/firewater.glsl
@@ -11,9 +11,9 @@ void main()
 	float4 c1 = texture(samp9, uv0 + float2(1,1)*resolution.zw);
 	float4 c2 = texture(samp9, uv0 + float2(-1,-1)*resolution.zw);
 	float red	= c0.r;
-	float green = c0.g;
+	float green	= c0.g;
 	float blue	= c0.b;
-	float alpha = c0.a;
+	float alpha	= c0.a;
 
 	red = c0.r - c1.b;
 	blue = c0.b - c2.r + (c0.g - c0.r);
