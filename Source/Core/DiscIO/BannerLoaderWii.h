d800b5fb74c077522ef7a0752e49968df46e745e
Lioncash <mathew1800@gmail.com>
2014-06-29 15:31:44 -0400

    DiscIO: Fix extra tabs in the banner loader headers

--- Source/Core/DiscIO/BannerLoaderWii.h
+++ Source/Core/DiscIO/BannerLoaderWii.h
@@ -18,54 +18,52 @@ class IVolume;
 class CBannerLoaderWii
 	: public IBannerLoader
 {
-	public:
+public:
+	CBannerLoaderWii(DiscIO::IVolume *pVolume);
 
-		CBannerLoaderWii(DiscIO::IVolume *pVolume);
+	virtual ~CBannerLoaderWii();
 
-		virtual ~CBannerLoaderWii();
+	virtual bool IsValid() override;
 
-		virtual bool IsValid() override;
+	virtual std::vector<u32> GetBanner(int* pWidth, int* pHeight) override;
 
-		virtual std::vector<u32> GetBanner(int* pWidth, int* pHeight) override;
+	virtual std::vector<std::string> GetNames() override;
+	virtual std::string GetCompany() override;
+	virtual std::vector<std::string> GetDescriptions() override;
 
-		virtual std::vector<std::string> GetNames() override;
-		virtual std::string GetCompany() override;
-		virtual std::vector<std::string> GetDescriptions() override;
+private:
+	enum
+	{
+		TEXTURE_SIZE = 192 * 64 * 2,
+		ICON_SIZE    = 48 * 48 * 2,
+		COMMENT_SIZE = 32
+	};
 
-	private:
+	enum CommentIndex
+	{
+		NAME_IDX,
+		DESC_IDX
+	};
 
-		enum
-		{
-			TEXTURE_SIZE = 192 * 64 * 2,
-			ICON_SIZE    = 48 * 48 * 2,
-			COMMENT_SIZE = 32
-		};
+	struct SWiiBanner
+	{
+		u32 ID;
 
-		enum CommentIndex
-		{
-			NAME_IDX,
-			DESC_IDX
-		};
+		u32 m_Flag;
+		u16 m_Speed;
+		u8  m_Unknown[22];
 
-		struct SWiiBanner
-		{
-			u32 ID;
+		// Not null terminated!
+		u16 m_Comment[2][COMMENT_SIZE];
+		u8  m_BannerTexture[TEXTURE_SIZE];
+		u8  m_IconTexture[8][ICON_SIZE];
+	} ;
 
-			u32 m_Flag;
-			u16 m_Speed;
-			u8  m_Unknown[22];
+	u8* m_pBannerFile;
 
-			// Not null terminated!
-			u16 m_Comment[2][COMMENT_SIZE];
-			u8  m_BannerTexture[TEXTURE_SIZE];
-			u8  m_IconTexture[8][ICON_SIZE];
-		} ;
+	bool m_IsValid;
 
-		u8* m_pBannerFile;
-
-		bool m_IsValid;
-
-		bool GetStringFromComments(const CommentIndex index, std::string& s);
+	bool GetStringFromComments(const CommentIndex index, std::string& s);
 };
 
 } // namespace
