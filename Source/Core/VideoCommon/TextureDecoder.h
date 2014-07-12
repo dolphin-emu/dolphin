1a4d5ab1746bab862e8188a9f67512685b8f3bcb
Jasper St. Pierre <jstpierre@mecheye.net>
2014-07-12 11:14:28 -0400

    TextureDecoder: Rearrange header slightly
    Put the two Decode APIs together.

--- Source/Core/VideoCommon/TextureDecoder.h
+++ Source/Core/VideoCommon/TextureDecoder.h
@@ -72,9 +72,9 @@ enum PC_TexFormat
 };
 
 PC_TexFormat TexDecoder_Decode(u8 *dst, const u8 *src, int width, int height, int texformat, int tlutaddr, int tlutfmt);
+PC_TexFormat TexDecoder_DecodeRGBA8FromTmem(u8* dst, const u8 *src_ar, const u8 *src_gb, int width, int height);
 void TexDecoder_DecodeTexel(u8 *dst, const u8 *src, int s, int t, int imageWidth, int texformat, int tlutaddr, int tlutfmt);
 void TexDecoder_DecodeTexelRGBA8FromTmem(u8 *dst, const u8 *src_ar, const u8* src_gb, int s, int t, int imageWidth);
-PC_TexFormat TexDecoder_DecodeRGBA8FromTmem(u8* dst, const u8 *src_ar, const u8 *src_gb, int width, int height);
 
 void TexDecoder_SetTexFmtOverlayOptions(bool enable, bool center);
 void TexDecoder_DrawOverlay(u8 *dst, int width, int height, int texformat, PC_TexFormat pc_texformat);
