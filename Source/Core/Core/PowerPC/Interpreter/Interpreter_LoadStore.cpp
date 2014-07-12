f20c3e5799345a668e74a7d0d7e0a11cccb5e0e2
Jasper St. Pierre <jstpierre@mecheye.net>
2014-07-12 11:21:41 -0400

    PixelEngine: Remove unused AllowIdleSkipping and all references to it

--- Source/Core/Core/PowerPC/Interpreter/Interpreter_LoadStore.cpp
+++ Source/Core/Core/PowerPC/Interpreter/Interpreter_LoadStore.cpp
@@ -220,22 +220,6 @@ void Interpreter::lwz(UGeckoInstruction _inst)
 	{
 		m_GPR[_inst.RD] = temp;
 	}
-
-	// hack to detect SelectThread loop
-	// should probably run a pass through memory instead before execution
-	// but that would be dangerous
-
-	// Enable idle skipping?
-	/*
-	if ((_inst.hex & 0xFFFF0000)==0x800D0000 &&
-		Memory::ReadUnchecked_U32(PC+4)==0x28000000 &&
-		Memory::ReadUnchecked_U32(PC+8)==0x4182fff8)
-	{
-		if (CommandProcessor::AllowIdleSkipping() && PixelEngine::AllowIdleSkipping())
-		{
-			CoreTiming::Idle();
-		}
-	}*/
 }
 
 void Interpreter::lwzu(UGeckoInstruction _inst)
