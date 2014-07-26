c1673824bd1f2065c4f73a55a82546956ecc0c6c
Lioncash <mathew1800@gmail.com>
2014-07-26 17:38:02 -0400

    Core: Remove the ratio parameter in Zelda UCode's SizeForResampling func
    It's only ever passed PB.RatioInt

--- Source/Core/Core/HW/DSPHLE/UCodes/Zelda.h
+++ Source/Core/Core/HW/DSPHLE/UCodes/Zelda.h
@@ -285,7 +285,7 @@ private:
 	void Resample(ZeldaVoicePB &PB, int size, s16 *in, s32 *out, bool do_resample = false);
 
 	int ConvertRatio(int pb_ratio);
-	int SizeForResampling(ZeldaVoicePB &PB, int size, int ratio);
+	int SizeForResampling(ZeldaVoicePB &PB, int size);
 
 	// Renders a voice and mixes it into LeftBuffer, RightBuffer
 	void RenderAddVoice(ZeldaVoicePB& PB, s32* _LeftBuffer, s32* _RightBuffer, int _Size);
