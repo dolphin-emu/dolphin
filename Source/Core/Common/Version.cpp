22e9d6977b296fbbb7a39b854d05440c19918f7a
Lioncash <mathew1800@gmail.com>
2014-07-30 02:01:56 -0400

    Common: State OS instead of 32/64 bit in the netplay lobby

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -24,24 +24,12 @@ const char *scm_rev_str = "Dolphin "
 	BUILD_TYPE_STR SCM_DESC_STR;
 #endif
 
-#if _M_X86_64
-#define NP_ARCH "x64"
-#elif _M_ARM_32
-#define NP_ARCH "ARM32"
-#elif _M_ARM_64
-#define NP_ARCH "ARM64"
-#elif _M_X86_32
-#define NP_ARCH "x86"
-#else
-#define NP_ARCH "Unk"
-#endif
-
 #ifdef _WIN32
-const char *netplay_dolphin_ver = SCM_DESC_STR " W" NP_ARCH;
+const char *netplay_dolphin_ver = SCM_DESC_STR " Win";
 #elif __APPLE__
-const char *netplay_dolphin_ver = SCM_DESC_STR " M" NP_ARCH;
+const char *netplay_dolphin_ver = SCM_DESC_STR " Mac";
 #else
-const char *netplay_dolphin_ver = SCM_DESC_STR " L" NP_ARCH;
+const char *netplay_dolphin_ver = SCM_DESC_STR " Lin";
 #endif
 
 const char *scm_rev_git_str = SCM_REV_STR;
