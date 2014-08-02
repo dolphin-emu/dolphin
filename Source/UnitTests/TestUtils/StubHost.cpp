8b26d7bf1ef478ff2f06d3cf0b0712418d1a8627
Pierre Bourdon <delroth@gmail.com>
2014-08-01 23:23:52 -0700

    UnitTests: make it possible to build tests for code that has global dependencies

---
+++ Source/UnitTests/TestUtils/StubHost.cpp
@@ -0,0 +1,34 @@
+// Copyright 2014 Dolphin Emulator Project
+// Licensed under GPLv2
+// Refer to the license.txt file included.
+
+// Stub implementation of the Host_* callbacks for tests. These implementations
+// do nothing except return default values when required.
+
+#include <string>
+
+#include "Core/Host.h"
+#include "VideoBackends/OGL/GLInterfaceBase.h"
+
+void Host_NotifyMapLoaded() {}
+void Host_RefreshDSPDebuggerWindow() {}
+void Host_ShowJitResults(unsigned int) {}
+void Host_Message(int) {}
+void* Host_GetRenderHandle() { return nullptr; }
+void* Host_GetInstance() { return nullptr; }
+void Host_UpdateTitle(const std::string&) {}
+void Host_UpdateLogDisplay() {}
+void Host_UpdateDisasmDialog() {}
+void Host_UpdateMainFrame() {}
+void Host_UpdateBreakPointView() {}
+void Host_GetRenderWindowSize(int&, int&, int&, int&) {}
+void Host_RequestRenderWindowSize(int, int) {}
+void Host_SetStartupDebuggingParameters() {}
+bool Host_UIHasFocus() { return false; }
+bool Host_RendererHasFocus() { return false; }
+void Host_ConnectWiimote(int, bool) {}
+void Host_UpdateStatusBar(const std::string&, int) {}
+void Host_SysMessage(const char*, ...) {}
+void Host_SetWiiMoteConnectionState(int) {}
+void Host_ShowVideoConfig(void*, const std::string&, const std::string&) {}
+cInterfaceBase* HostGL_CreateGLInterface() { return nullptr; }
