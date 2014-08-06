e3a9ba30e379730a60aa569f653165d51e72b7c1
Jasper St. Pierre <jstpierre@mecheye.net>
2014-08-05 23:44:25 -0400

    GLX: Remove evdpy / dpy split
    Move to one display. There's no reason to have two displays here -- the
    comment stated that one should touch GLX and one should touch window
    events, and that they should be touched from different threads, but the
    current code wasn't this careful.
    
    Just use one Display connection.

--- Source/Core/DolphinWX/GLInterface/X11_Util.cpp
+++ Source/Core/DolphinWX/GLInterface/X11_Util.cpp
@@ -11,33 +11,33 @@ void cX11Window::CreateXWindow(void)
 	Atom wmProtocols[1];
 
 	// Setup window attributes
-	GLWin.attr.colormap = XCreateColormap(GLWin.evdpy,
+	GLWin.attr.colormap = XCreateColormap(GLWin.dpy,
 			GLWin.parent, GLWin.vi->visual, AllocNone);
 	GLWin.attr.event_mask = KeyPressMask | StructureNotifyMask | FocusChangeMask;
-	GLWin.attr.background_pixel = BlackPixel(GLWin.evdpy, GLWin.screen);
+	GLWin.attr.background_pixel = BlackPixel(GLWin.dpy, GLWin.screen);
 	GLWin.attr.border_pixel = 0;
 
 	// Create the window
-	GLWin.win = XCreateWindow(GLWin.evdpy, GLWin.parent,
+	GLWin.win = XCreateWindow(GLWin.dpy, GLWin.parent,
 			0, 0, 1, 1, 0,
 			GLWin.vi->depth, InputOutput, GLWin.vi->visual,
 			CWBorderPixel | CWBackPixel | CWColormap | CWEventMask, &GLWin.attr);
-	wmProtocols[0] = XInternAtom(GLWin.evdpy, "WM_DELETE_WINDOW", True);
-	XSetWMProtocols(GLWin.evdpy, GLWin.win, wmProtocols, 1);
-	XSetStandardProperties(GLWin.evdpy, GLWin.win, "GPU", "GPU", None, nullptr, 0, nullptr);
-	XMapRaised(GLWin.evdpy, GLWin.win);
-	XSync(GLWin.evdpy, True);
+	wmProtocols[0] = XInternAtom(GLWin.dpy, "WM_DELETE_WINDOW", True);
+	XSetWMProtocols(GLWin.dpy, GLWin.win, wmProtocols, 1);
+	XSetStandardProperties(GLWin.dpy, GLWin.win, "GPU", "GPU", None, nullptr, 0, nullptr);
+	XMapRaised(GLWin.dpy, GLWin.win);
+	XSync(GLWin.dpy, True);
 
 	GLWin.xEventThread = std::thread(&cX11Window::XEventThread, this);
 }
 
 void cX11Window::DestroyXWindow(void)
 {
-	XUnmapWindow(GLWin.evdpy, GLWin.win);
+	XUnmapWindow(GLWin.dpy, GLWin.win);
 	GLWin.win = 0;
 	if (GLWin.xEventThread.joinable())
 		GLWin.xEventThread.join();
-	XFreeColormap(GLWin.evdpy, GLWin.attr.colormap);
+	XFreeColormap(GLWin.dpy, GLWin.attr.colormap);
 }
 
 void cX11Window::XEventThread()
@@ -45,16 +45,16 @@ void cX11Window::XEventThread()
 	while (GLWin.win)
 	{
 		XEvent event;
-		for (int num_events = XPending(GLWin.evdpy); num_events > 0; num_events--)
+		for (int num_events = XPending(GLWin.dpy); num_events > 0; num_events--)
 		{
-			XNextEvent(GLWin.evdpy, &event);
+			XNextEvent(GLWin.dpy, &event);
 			switch (event.type) {
 				case ConfigureNotify:
 					GLInterface->SetBackBufferDimensions(event.xconfigure.width, event.xconfigure.height);
 					break;
 				case ClientMessage:
 					if ((unsigned long) event.xclient.data.l[0] ==
-							XInternAtom(GLWin.evdpy, "WM_DELETE_WINDOW", False))
+							XInternAtom(GLWin.dpy, "WM_DELETE_WINDOW", False))
 						Host_Message(WM_USER_STOP);
 					break;
 				default:
