27f496fa349304e3de010e65fdc242bf3726f44b
Shawn Hoffman <godisgovernment@gmail.com>
2014-08-12 01:04:51 -0700

    Fix regression with toggling the toolbar from PR761

--- Source/Core/DolphinWX/FrameTools.cpp
+++ Source/Core/DolphinWX/FrameTools.cpp
@@ -1874,6 +1874,7 @@ void CFrame::OnToggleToolbar(wxCommandEvent& event)
 void CFrame::DoToggleToolbar(bool _show)
 {
 	GetToolBar()->Show(_show);
+	m_Mgr->Update();
 }
 
 // Enable and disable the status bar
