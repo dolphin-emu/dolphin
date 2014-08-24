858296e1c7fe6a492a1e31d532bffea5361b0c39
Fiora <fioraaeterna@gmail.com>
2014-08-24 11:09:10 -0700

    JIT64: optimize multiplication by immediate constants
    Factor out common code and handle a few more common cases.

--- Source/Core/Core/PowerPC/Jit64/Jit.h
+++ Source/Core/Core/PowerPC/Jit64/Jit.h
@@ -117,6 +117,8 @@ public:
 	Gen::FixupBranch JumpIfCRFieldBit(int field, int bit, bool jump_if_set = true);
 	void SetFPRFIfNeeded(UGeckoInstruction inst, Gen::X64Reg xmm);
 
+	void MultiplyImmediate(u32 imm, int a, int d, bool overflow);
+
 	void tri_op(int d, int a, int b, bool reversible, void (Gen::XEmitter::*op)(Gen::X64Reg, Gen::OpArg), UGeckoInstruction inst, bool roundRHS = false);
 	typedef u32 (*Operation)(u32 a, u32 b);
 	void regimmop(int d, int a, bool binary, u32 value, Operation doop, void (Gen::XEmitter::*op)(int, const Gen::OpArg&, const Gen::OpArg&), bool Rc = false, bool carry = false);
