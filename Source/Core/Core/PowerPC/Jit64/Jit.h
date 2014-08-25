24124d62f0015c48793df35142035576a06a8a3b
Fiora <fioraaeterna@gmail.com>
2014-08-25 13:56:01 -0700

    JIT64: clean up and unify float load/store code
    While we're at it, support a bunch of float load/store variants that weren't
    implemented in the JIT. Might not have a big speed impact on typical games but
    they're used at least a bit in povray and luabench.
    
    694 -> 644 seconds on povray.

--- Source/Core/Core/PowerPC/Jit64/Jit.h
+++ Source/Core/Core/PowerPC/Jit64/Jit.h
@@ -192,11 +192,9 @@ public:
 
 	void cntlzwx(UGeckoInstruction inst);
 
-	void lfs(UGeckoInstruction inst);
-	void lfd(UGeckoInstruction inst);
-	void stfd(UGeckoInstruction inst);
-	void stfs(UGeckoInstruction inst);
-	void stfsx(UGeckoInstruction inst);
+	void lfXXX(UGeckoInstruction inst);
+	void stfXXX(UGeckoInstruction inst);
+	void stfiwx(UGeckoInstruction inst);
 	void psq_l(UGeckoInstruction inst);
 	void psq_st(UGeckoInstruction inst);
 
@@ -211,7 +209,6 @@ public:
 	void srwx(UGeckoInstruction inst);
 	void dcbst(UGeckoInstruction inst);
 	void dcbz(UGeckoInstruction inst);
-	void lfsx(UGeckoInstruction inst);
 
 	void subfic(UGeckoInstruction inst);
 	void subfcx(UGeckoInstruction inst);
