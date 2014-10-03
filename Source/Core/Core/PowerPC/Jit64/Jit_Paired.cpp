bf014636c8631f51c15ebfca5a4b3069e5670c3c
Fiora <fioraaeterna@gmail.com>
2014-10-03 10:06:37 -0700

    JIT: add more AVX support, reduce redundant XMM moves
    10-20% reduction in typical compiled block size for float-heavy JIT blocks.

--- Source/Core/Core/PowerPC/Jit64/Jit_Paired.cpp
+++ Source/Core/Core/PowerPC/Jit64/Jit_Paired.cpp
@@ -43,17 +43,15 @@ void Jit64::ps_sel(UGeckoInstruction inst)
 
 	if (cpu_info.bSSE4_1)
 	{
-		MOVAPD(XMM1, fpr.R(a));
 		PXOR(XMM0, R(XMM0));
-		CMPPD(XMM0, R(XMM1), NLE);
+		CMPPD(XMM0, fpr.R(a), NLE);
 		MOVAPD(XMM1, fpr.R(c));
 		BLENDVPD(XMM1, fpr.R(b));
 	}
 	else
 	{
-		MOVAPD(XMM0, fpr.R(a));
 		PXOR(XMM1, R(XMM1));
-		CMPPD(XMM1, R(XMM0), NLE);
+		CMPPD(XMM1, fpr.R(a), NLE);
 		MOVAPD(XMM0, R(XMM1));
 		PAND(XMM1, fpr.R(b));
 		PANDN(XMM0, fpr.R(c));
@@ -74,26 +72,18 @@ void Jit64::ps_sign(UGeckoInstruction inst)
 	int b = inst.FB;
 
 	fpr.Lock(d, b);
-	if (d != b)
-	{
-		fpr.BindToRegister(d, false);
-		MOVAPD(fpr.RX(d), fpr.R(b));
-	}
-	else
-	{
-		fpr.BindToRegister(d, true);
-	}
+	fpr.BindToRegister(d, d == b);
 
 	switch (inst.SUBOP10)
 	{
 	case 40: //neg
-		PXOR(fpr.RX(d), M((void*)&psSignBits));
+		avx_op(&XEmitter::VPXOR, &XEmitter::PXOR, fpr.RX(d), fpr.R(b), M((void*)&psSignBits));
 		break;
 	case 136: //nabs
-		POR(fpr.RX(d), M((void*)&psSignBits));
+		avx_op(&XEmitter::VPOR, &XEmitter::POR, fpr.RX(d), fpr.R(b), M((void*)&psSignBits));
 		break;
 	case 264: //abs
-		PAND(fpr.RX(d), M((void*)&psAbsMask));
+		avx_op(&XEmitter::VPAND, &XEmitter::PAND, fpr.RX(d), fpr.R(b), M((void*)&psAbsMask));
 		break;
 	}
 
@@ -101,56 +91,29 @@ void Jit64::ps_sign(UGeckoInstruction inst)
 }
 
 //There's still a little bit more optimization that can be squeezed out of this
-void Jit64::tri_op(int d, int a, int b, bool reversible, void (XEmitter::*op)(X64Reg, OpArg), UGeckoInstruction inst, bool roundRHS)
+void Jit64::tri_op(int d, int a, int b, bool reversible, void (XEmitter::*avxOp)(X64Reg, X64Reg, OpArg), void (XEmitter::*sseOp)(X64Reg, OpArg), UGeckoInstruction inst, bool roundRHS)
 {
 	fpr.Lock(d, a, b);
+	fpr.BindToRegister(d, d == a || d == b);
 
 	if (roundRHS)
 	{
 		if (d == a)
 		{
-			fpr.BindToRegister(d, true);
-			MOVAPD(XMM0, fpr.R(b));
-			Force25BitPrecision(XMM0, XMM1);
-			(this->*op)(fpr.RX(d), R(XMM0));
+			Force25BitPrecision(XMM0, fpr.R(b), XMM1);
+			(this->*sseOp)(fpr.RX(d), R(XMM0));
 		}
 		else
 		{
-			fpr.BindToRegister(d, d == b);
-			if (d != b)
-				MOVAPD(fpr.RX(d), fpr.R(b));
-			Force25BitPrecision(fpr.RX(d), XMM0);
-			(this->*op)(fpr.RX(d), fpr.R(a));
-		}
-	}
-	else if (d == a)
-	{
-		fpr.BindToRegister(d, true);
-		(this->*op)(fpr.RX(d), fpr.R(b));
-	}
-	else if (d == b)
-	{
-		if (reversible)
-		{
-			fpr.BindToRegister(d, true);
-			(this->*op)(fpr.RX(d), fpr.R(a));
-		}
-		else
-		{
-			MOVAPD(XMM0, fpr.R(b));
-			fpr.BindToRegister(d, false);
-			MOVAPD(fpr.RX(d), fpr.R(a));
-			(this->*op)(fpr.RX(d), R(XMM0));
+			Force25BitPrecision(fpr.RX(d), fpr.R(b), XMM0);
+			(this->*sseOp)(fpr.RX(d), fpr.R(a));
 		}
 	}
 	else
 	{
-		//sources different from d, can use rather quick solution
-		fpr.BindToRegister(d, false);
-		MOVAPD(fpr.RX(d), fpr.R(a));
-		(this->*op)(fpr.RX(d), fpr.R(b));
+		avx_op(avxOp, sseOp, fpr.RX(d), fpr.R(a), fpr.R(b), true, reversible);
 	}
-	ForceSinglePrecisionP(fpr.RX(d));
+	ForceSinglePrecisionP(fpr.RX(d), fpr.RX(d));
 	SetFPRFIfNeeded(inst, fpr.RX(d));
 	fpr.UnlockAll();
 }
@@ -164,16 +127,16 @@ void Jit64::ps_arith(UGeckoInstruction inst)
 	switch (inst.SUBOP5)
 	{
 	case 18: // div
-		tri_op(inst.FD, inst.FA, inst.FB, false, &XEmitter::DIVPD, inst);
+		tri_op(inst.FD, inst.FA, inst.FB, false, &XEmitter::VDIVPD, &XEmitter::DIVPD, inst);
 		break;
 	case 20: // sub
-		tri_op(inst.FD, inst.FA, inst.FB, false, &XEmitter::SUBPD, inst);
+		tri_op(inst.FD, inst.FA, inst.FB, false, &XEmitter::VSUBPD, &XEmitter::SUBPD, inst);
 		break;
 	case 21: // add
-		tri_op(inst.FD, inst.FA, inst.FB, true, &XEmitter::ADDPD, inst);
+		tri_op(inst.FD, inst.FA, inst.FB, true, &XEmitter::VADDPD, &XEmitter::ADDPD, inst);
 		break;
 	case 25: // mul
-		tri_op(inst.FD, inst.FA, inst.FC, true, &XEmitter::MULPD, inst, true);
+		tri_op(inst.FD, inst.FA, inst.FC, true, &XEmitter::VMULPD, &XEmitter::MULPD, inst, true);
 		break;
 	default:
 		_assert_msg_(DYNA_REC, 0, "ps_arith WTF!!!");
@@ -208,10 +171,9 @@ void Jit64::ps_sum(UGeckoInstruction inst)
 	default:
 		PanicAlert("ps_sum WTF!!!");
 	}
-	ForceSinglePrecisionP(XMM0);
-	SetFPRFIfNeeded(inst, XMM0);
 	fpr.BindToRegister(d, false);
-	MOVAPD(fpr.RX(d), R(XMM0));
+	ForceSinglePrecisionP(fpr.RX(d), XMM0);
+	SetFPRFIfNeeded(inst, fpr.RX(d));
 	fpr.UnlockAll();
 }
 
@@ -232,18 +194,16 @@ void Jit64::ps_muls(UGeckoInstruction inst)
 		MOVDDUP(XMM0, fpr.R(c));
 		break;
 	case 13:
-		MOVAPD(XMM0, fpr.R(c));
-		SHUFPD(XMM0, R(XMM0), 3);
+		avx_op(&XEmitter::VSHUFPD, &XEmitter::SHUFPD, XMM0, fpr.R(c), fpr.R(c), 3);
 		break;
 	default:
 		PanicAlert("ps_muls WTF!!!");
 	}
-	Force25BitPrecision(XMM0, XMM1);
+	Force25BitPrecision(XMM0, R(XMM0), XMM1);
 	MULPD(XMM0, fpr.R(a));
-	ForceSinglePrecisionP(XMM0);
-	SetFPRFIfNeeded(inst, XMM0);
 	fpr.BindToRegister(d, false);
-	MOVAPD(fpr.RX(d), R(XMM0));
+	ForceSinglePrecisionP(fpr.RX(d), XMM0);
+	SetFPRFIfNeeded(inst, fpr.RX(d));
 	fpr.UnlockAll();
 }
 
@@ -258,27 +218,25 @@ void Jit64::ps_mergeXX(UGeckoInstruction inst)
 	int a = inst.FA;
 	int b = inst.FB;
 	fpr.Lock(a,b,d);
+	fpr.BindToRegister(d, d == a || d == b);
 
-	MOVAPD(XMM0, fpr.R(a));
 	switch (inst.SUBOP10)
 	{
 	case 528:
-		UNPCKLPD(XMM0, fpr.R(b)); //unpck is faster than shuf
+		avx_op(&XEmitter::VUNPCKLPD, &XEmitter::UNPCKLPD, fpr.RX(d), fpr.R(a), fpr.R(b));
 		break; //00
 	case 560:
-		SHUFPD(XMM0, fpr.R(b), 2); //must use shuf here
+		avx_op(&XEmitter::VSHUFPD, &XEmitter::SHUFPD, fpr.RX(d), fpr.R(a), fpr.R(b), 2);
 		break; //01
 	case 592:
-		SHUFPD(XMM0, fpr.R(b), 1);
+		avx_op(&XEmitter::VSHUFPD, &XEmitter::SHUFPD, fpr.RX(d), fpr.R(a), fpr.R(b), 1);
 		break; //10
 	case 624:
-		UNPCKHPD(XMM0, fpr.R(b));
+		avx_op(&XEmitter::VUNPCKHPD, &XEmitter::UNPCKHPD, fpr.RX(d), fpr.R(a), fpr.R(b));
 		break; //11
 	default:
 		_assert_msg_(DYNA_REC, 0, "ps_merge - invalid op");
 	}
-	fpr.BindToRegister(d, false);
-	MOVAPD(fpr.RX(d), R(XMM0));
 	fpr.UnlockAll();
 }
 
@@ -303,7 +261,7 @@ void Jit64::ps_rsqrte(UGeckoInstruction inst)
 	CALL((void *)asm_routines.frsqrte);
 	MOVLHPS(fpr.RX(d), XMM0);
 
-	ForceSinglePrecisionP(fpr.RX(d));
+	ForceSinglePrecisionP(fpr.RX(d), fpr.RX(d));
 	SetFPRFIfNeeded(inst, fpr.RX(d));
 	fpr.UnlockAll();
 	gpr.UnlockAllX();
@@ -330,7 +288,7 @@ void Jit64::ps_res(UGeckoInstruction inst)
 	CALL((void *)asm_routines.fres);
 	MOVLHPS(fpr.RX(d), XMM0);
 
-	ForceSinglePrecisionP(fpr.RX(d));
+	ForceSinglePrecisionP(fpr.RX(d), fpr.RX(d));
 	SetFPRFIfNeeded(inst, fpr.RX(d));
 	fpr.UnlockAll();
 	gpr.UnlockAllX();
@@ -352,42 +310,35 @@ void Jit64::ps_maddXX(UGeckoInstruction inst)
 	switch (inst.SUBOP5)
 	{
 	case 14: //madds0
-		MOVDDUP(XMM1, fpr.R(c));
-		Force25BitPrecision(XMM1, XMM0);
-		MOVAPD(XMM0, fpr.R(a));
-		MULPD(XMM0, R(XMM1));
+		MOVDDUP(XMM0, fpr.R(c));
+		Force25BitPrecision(XMM0, R(XMM0), XMM1);
+		MULPD(XMM0, fpr.R(a));
 		ADDPD(XMM0, fpr.R(b));
 		break;
 	case 15: //madds1
-		MOVAPD(XMM1, fpr.R(c));
-		SHUFPD(XMM1, R(XMM1), 3); // copy higher to lower
-		Force25BitPrecision(XMM1, XMM0);
-		MOVAPD(XMM0, fpr.R(a));
-		MULPD(XMM0, R(XMM1));
+		avx_op(&XEmitter::VSHUFPD, &XEmitter::SHUFPD, XMM0, fpr.R(c), fpr.R(c), 3);
+		Force25BitPrecision(XMM0, R(XMM0), XMM1);
+		MULPD(XMM0, fpr.R(a));
 		ADDPD(XMM0, fpr.R(b));
 		break;
 	case 28: //msub
-		MOVAPD(XMM0, fpr.R(c));
-		Force25BitPrecision(XMM0, XMM1);
+		Force25BitPrecision(XMM0, fpr.R(c), XMM1);
 		MULPD(XMM0, fpr.R(a));
 		SUBPD(XMM0, fpr.R(b));
 		break;
 	case 29: //madd
-		MOVAPD(XMM0, fpr.R(c));
-		Force25BitPrecision(XMM0, XMM1);
+		Force25BitPrecision(XMM0, fpr.R(c), XMM1);
 		MULPD(XMM0, fpr.R(a));
 		ADDPD(XMM0, fpr.R(b));
 		break;
 	case 30: //nmsub
-		MOVAPD(XMM0, fpr.R(c));
-		Force25BitPrecision(XMM0, XMM1);
+		Force25BitPrecision(XMM0, fpr.R(c), XMM1);
 		MULPD(XMM0, fpr.R(a));
 		SUBPD(XMM0, fpr.R(b));
 		PXOR(XMM0, M((void*)&psSignBits));
 		break;
 	case 31: //nmadd
-		MOVAPD(XMM0, fpr.R(c));
-		Force25BitPrecision(XMM0, XMM1);
+		Force25BitPrecision(XMM0, fpr.R(c), XMM1);
 		MULPD(XMM0, fpr.R(a));
 		ADDPD(XMM0, fpr.R(b));
 		PXOR(XMM0, M((void*)&psSignBits));
@@ -399,9 +350,8 @@ void Jit64::ps_maddXX(UGeckoInstruction inst)
 		return;
 	}
 	fpr.BindToRegister(d, false);
-	ForceSinglePrecisionP(XMM0);
-	SetFPRFIfNeeded(inst, XMM0);
-	MOVAPD(fpr.RX(d), R(XMM0));
+	ForceSinglePrecisionP(fpr.RX(d), XMM0);
+	SetFPRFIfNeeded(inst, fpr.RX(d));
 	fpr.UnlockAll();
 }
 
