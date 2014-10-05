41ea4a28b2eacde51858f419e8f326d32be1cacd
Lioncash <mathew1800@gmail.com>
2014-10-05 05:31:45 -0400

    DolphinWX: Make WXInputBase part of the WxUtils namespace.
    There's no need for the preprocessor checks for wx, since this is used in wx code. Also, this being a part of the InputCommon namespace is kind of wrong.

--- Source/Core/DolphinWX/FrameTools.cpp
+++ Source/Core/DolphinWX/FrameTools.cpp
@@ -511,8 +511,8 @@ wxString CFrame::GetMenuLabel(int Id)
 
 	hotkeymodifier &= wxMOD_CONTROL | wxMOD_ALT | wxMOD_SHIFT;
 
-	Modifier = InputCommon::WXKeymodToString(hotkeymodifier);
-	Hotkey = InputCommon::WXKeyToString(hotkey);
+	Modifier = WxUtils::WXKeymodToString(hotkeymodifier);
+	Hotkey = WxUtils::WXKeyToString(hotkey);
 	if (Modifier.Len() + Hotkey.Len() > 0)
 		Label += '\t';
 
