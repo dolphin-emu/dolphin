ef5ecd04ab50a76f508fbfe9726b32d76a10a518
Stevoisiak <Stevoisiak@gmail.com>
2014-10-17 16:45:09 -0400

    Variable naming consistency

--- Source/Core/DolphinWX/FrameTools.cpp
+++ Source/Core/DolphinWX/FrameTools.cpp
@@ -1512,10 +1512,10 @@ void CFrame::UpdateWiiMenuChoice(wxMenuItem *WiiMenuItem)
 	const DiscIO::INANDContentLoader & SysMenu_Loader = DiscIO::CNANDContentManager::Access().GetNANDLoader(TITLEID_SYSMENU, true);
 	if (SysMenu_Loader.IsValid())
 	{
-		int sysmenuVersion = SysMenu_Loader.GetTitleVersion();
-		char sysmenuRegion = SysMenu_Loader.GetCountryChar();
+		int sys_menu_version = SysMenu_Loader.GetTitleVersion();
+		char sys_menu_region = SysMenu_Loader.GetCountryChar();
 		WiiMenuItem->Enable();
-		WiiMenuItem->SetItemLabel(wxString::Format(_("Load Wii System Menu %d%c"), sysmenuVersion, sysmenuRegion));
+		WiiMenuItem->SetItemLabel(wxString::Format(_("Load Wii System Menu %d%c"), sys_menu_version, sys_menu_region));
 	}
 	else
 	{
