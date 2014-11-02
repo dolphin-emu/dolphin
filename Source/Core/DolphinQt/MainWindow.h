3d63d22e539fb6aaf4bad3c4d9e51f30d122fd56
Augustin Cavalier <waddlesplash@gmail.com>
2014-11-02 13:53:48 -0500

    DolphinQt: adapt so that it can boot games.

--- Source/Core/DolphinQt/MainWindow.h
+++ Source/Core/DolphinQt/MainWindow.h
@@ -7,6 +7,10 @@
 #include <memory>
 #include <QMainWindow>
 
+#include "Core/Core.h"
+
+#include "DolphinQt/VideoInterface/RenderWidget.h"
+
 // Predefinitions
 namespace Ui
 {
@@ -21,7 +25,23 @@ public:
 	explicit DMainWindow(QWidget* parent_widget = nullptr);
 	~DMainWindow();
 
+	// DRenderWidget
+	void RenderWidgetSize(int& x_pos, int& y_pos, int& w, int& h);
+	bool RenderWidgetHasFocus();
+	DRenderWidget* GetRenderWidget() { return m_render_widget.get(); }
+
+signals:
+	void CoreStateChanged(Core::EState state);
+
 private slots:
+	// Emulation
+	void StartGame(const QString filename);
+	void OnCoreStateChanged(Core::EState state);
+
+	// Main toolbar
+	void on_actOpen_triggered();
+	void on_actPlay_triggered();
+	void on_actStop_triggered();
 
 	// Help menu
 	void on_actWebsite_triggered();
@@ -30,6 +50,20 @@ private slots:
 	void on_actSystemInfo_triggered();
 	void on_actAbout_triggered();
 
+	// Misc.
+	void UpdateIcons();
+
 private:
 	std::unique_ptr<Ui::DMainWindow> m_ui;
+
+	// Emulation
+	QString RequestBootFilename();
+	QString ShowFileDialog();
+	void DoStartPause();
+
+	std::unique_ptr<DRenderWidget> m_render_widget;
+	bool m_isStopping = false;
 };
+
+// Pointer to the only instance of DMainWindow, used by Host_*
+extern DMainWindow* g_main_window;
