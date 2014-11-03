363223d2e3a29d42bbfbed8c10d5463081d54541
comex <comexk@gmail.com>
2014-11-02 23:05:43 -0500

    Revert "Defer processing of expansion interface interrupts (fixes audio in Super Mario Sunshine on lower-end systems)"

--- Source/Core/Core/HW/EXI.h
+++ Source/Core/Core/HW/EXI.h
@@ -27,7 +27,6 @@ void PauseAndLock(bool doLock, bool unpauseOnUnlock);
 
 void RegisterMMIO(MMIO::Mapping* mmio, u32 base);
 
-void UpdateInterruptsCallback(u64 userdata, int cyclesLate);
 void UpdateInterrupts();
 
 void ChangeDeviceCallback(u64 userdata, int cyclesLate);
