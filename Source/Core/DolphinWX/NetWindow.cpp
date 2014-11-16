dolphin-emu/dolphin

commit 3fed975bac11956ffc4d3eae86c928c7e0c921db

NetPlayDiag: Get rid of event table

Source/Core/DolphinWX/NetWindow.cpp
@@ -49,10 +49,6 @@
 
 #define INITIAL_PAD_BUFFER_SIZE 5
 
-BEGIN_EVENT_TABLE(NetPlayDiag, wxFrame)
-	EVT_COMMAND(wxID_ANY, wxEVT_THREAD, NetPlayDiag::OnThread)
-END_EVENT_TABLE()
-
 static NetPlayServer* netplay_server = nullptr;
 static NetPlayClient* netplay_client = nullptr;
 NetPlayDiag *NetPlayDiag::npd = nullptr;
@@ -309,6 +305,8 @@ NetPlayDiag::NetPlayDiag(wxWindow* const parent, const CGameListCtrl* const game
 	, m_start_btn(nullptr)
 	, m_game_list(game_list)
 {
+	Bind(wxEVT_THREAD, &NetPlayDiag::OnThread, this);
+
 	wxPanel* const panel = new wxPanel(this);
 
 	// top crap
@@ -534,7 +532,7 @@ void NetPlayDiag::OnQuit(wxCommandEvent&)
 }
 
 // update gui
-void NetPlayDiag::OnThread(wxCommandEvent& event)
+void NetPlayDiag::OnThread(wxThreadEvent& event)
 {
 	// player list
 	m_playerids.clear();
@@ -582,7 +580,9 @@ void NetPlayDiag::OnThread(wxCommandEvent& event)
 		// update selected game :/
 		{
 		m_selected_game.assign(WxStrToStr(event.GetString()));
-		m_game_btn->SetLabel(event.GetString().Prepend(_(" Game : ")));
+
+		wxString button_label = event.GetString();
+		m_game_btn->SetLabel(button_label.Prepend(_(" Game : ")));
 		}
 		break;
 	case NP_GUI_EVT_START_GAME :

Source/Core/DolphinWX/NetWindow.h
@@ -82,11 +82,9 @@ public:
 	bool IsRecording() override;
 
 private:
-    DECLARE_EVENT_TABLE()
-
 	void OnChat(wxCommandEvent& event);
 	void OnQuit(wxCommandEvent& event);
-	void OnThread(wxCommandEvent& event);
+	void OnThread(wxThreadEvent& event);
 	void OnChangeGame(wxCommandEvent& event);
 	void OnAdjustBuffer(wxCommandEvent& event);
 	void OnConfigPads(wxCommandEvent& event);

commit 37b14bdec99c70aa47e9bf79fa690d6ce246eda4

Fix wxCommandEvent/wxThreadEvent subclass mismatch
Regression by 3fed975bac11956ffc4d3eae86c928c7e0c921db caused netplay to
crash on OS X.  While I'm at it, fix the long-standing "unsafe i guess"
AddPendingEvent, since we depend on wx 3 now...

Source/Core/DolphinWX/NetWindow.cpp
@@ -474,7 +474,7 @@ void NetPlayDiag::StopGame()
 // NetPlayUI methods called from ---NETPLAY--- thread
 void NetPlayDiag::Update()
 {
-	wxCommandEvent evt(wxEVT_THREAD, 1);
+	wxThreadEvent evt(wxEVT_THREAD, 1);
 	GetEventHandler()->AddPendingEvent(evt);
 }
 
@@ -487,15 +487,14 @@ void NetPlayDiag::AppendChat(const std::string& msg)
 
 void NetPlayDiag::OnMsgChangeGame(const std::string& filename)
 {
-	wxCommandEvent evt(wxEVT_THREAD, NP_GUI_EVT_CHANGE_GAME);
-	// TODO: using a wxString in AddPendingEvent from another thread is unsafe i guess?
-	evt.SetString(StrToWxStr(filename));
-	GetEventHandler()->AddPendingEvent(evt);
+	wxThreadEvent* evt = new wxThreadEvent(wxEVT_THREAD, NP_GUI_EVT_CHANGE_GAME);
+	evt->SetString(StrToWxStr(filename));
+	GetEventHandler()->QueueEvent(evt);
 }
 
 void NetPlayDiag::OnMsgStartGame()
 {
-	wxCommandEvent evt(wxEVT_THREAD, NP_GUI_EVT_START_GAME);
+	wxThreadEvent evt(wxEVT_THREAD, NP_GUI_EVT_START_GAME);
 	GetEventHandler()->AddPendingEvent(evt);
 	if (m_start_btn)
 		m_start_btn->Disable();
@@ -504,7 +503,7 @@ void NetPlayDiag::OnMsgStartGame()
 
 void NetPlayDiag::OnMsgStopGame()
 {
-	wxCommandEvent evt(wxEVT_THREAD, NP_GUI_EVT_STOP_GAME);
+	wxThreadEvent evt(wxEVT_THREAD, NP_GUI_EVT_STOP_GAME);
 	GetEventHandler()->AddPendingEvent(evt);
 	if (m_start_btn)
 		m_start_btn->Enable();