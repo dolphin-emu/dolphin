634699f001f221017d439a68c27dd46d7b19a5e4
comex <comexk@gmail.com>
2014-11-15 23:04:09 -0500

    Disable /dev/usb/hid too.

--- Source/Core/Core/IPC_HLE/WII_IPC_HLE_Device_hid.cpp
+++ Source/Core/Core/IPC_HLE/WII_IPC_HLE_Device_hid.cpp
@@ -128,6 +128,12 @@ bool CWII_IPC_HLE_Device_hid::Close(u32 _CommandAddress, bool _bForce)
 
 bool CWII_IPC_HLE_Device_hid::IOCtl(u32 _CommandAddress)
 {
+	if (Core::g_want_determinism)
+	{
+		Memory::Write_U32(-1, _CommandAddress + 0x4);
+		return true;
+	}
+
 	u32 Parameter     = Memory::Read_U32(_CommandAddress + 0xC);
 	u32 BufferIn      = Memory::Read_U32(_CommandAddress + 0x10);
 	u32 BufferInSize  = Memory::Read_U32(_CommandAddress + 0x14);
