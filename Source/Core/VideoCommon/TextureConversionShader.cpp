9bcadc8029b615d3d447a2a875fd720a73255d11
Lioncash <mathew1800@gmail.com>
2014-12-05 20:54:41 -0500

    Common: Remove locale based functions from CommonFuncs.
    Since %f isn't used anymore in the shader generators, these can go.

--- Source/Core/VideoCommon/TextureConversionShader.cpp
+++ Source/Core/VideoCommon/TextureConversionShader.cpp
@@ -4,10 +4,6 @@
 
 #include <cmath>
 #include <cstdio>
-#include <locale.h>
-#ifdef __APPLE__
-	#include <xlocale.h>
-#endif
 
 #include "Common/MathUtil.h"
 #include "VideoCommon/BPMemory.h"
@@ -579,10 +575,6 @@ static void WriteZ24Encoder(char*& p, API_TYPE ApiType)
 
 const char *GenerateEncodingShader(u32 format,API_TYPE ApiType)
 {
-#ifndef ANDROID
-	locale_t locale = newlocale(LC_NUMERIC_MASK, "C", nullptr); // New locale for compilation
-	locale_t old_locale = uselocale(locale); // Apply the locale for this thread
-#endif
 	text[sizeof(text) - 1] = 0x7C;  // canary
 
 	char *p = text;
@@ -666,10 +658,6 @@ const char *GenerateEncodingShader(u32 format,API_TYPE ApiType)
 	if (text[sizeof(text) - 1] != 0x7C)
 		PanicAlert("TextureConversionShader generator - buffer too small, canary has been eaten!");
 
-#ifndef ANDROID
-	uselocale(old_locale); // restore locale
-	freelocale(locale);
-#endif
 	return text;
 }
 
