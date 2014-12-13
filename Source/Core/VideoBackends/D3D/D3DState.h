14792c34029ca9202728f9fbd9ff0a9786e546f7
Jules Blok <jules.blok@gmail.com>
2014-12-13 22:09:34 +0100

    D3DState: Set the pixel shader in m_current when linking dynamically.

--- Source/Core/VideoBackends/D3D/D3DState.h
+++ Source/Core/VideoBackends/D3D/D3DState.h
@@ -198,6 +198,7 @@ public:
 	void SetPixelShaderDynamic(ID3D11PixelShader* shader, ID3D11ClassInstance * const * classInstances, u32 classInstancesCount)
 	{
 		D3D::context->PSSetShader(shader, classInstances, classInstancesCount);
+		m_current.pixelShader = shader;
 		m_pending.pixelShader = shader;
 	}
 
