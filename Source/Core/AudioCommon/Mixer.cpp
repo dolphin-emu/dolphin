9252b63d9a7d1d53accca756ca7ae193776d6b28
cegli <cegli@users.noreply.github.com>
2014-12-21 17:29:21 -0800

    Timewarp: Fix Audio in Dual Core Determinism Mode. Frame Limiter must be 60 for XAudio, 60/75 for OpenAL.

--- Source/Core/AudioCommon/Mixer.cpp
+++ Source/Core/AudioCommon/Mixer.cpp
@@ -53,7 +53,7 @@ unsigned int CMixer::MixerFifo::Mix(short* samples, unsigned int numSamples, boo
 		// VR requires a head-tracking rate greater than 60fps per second. This is solved by 
 		// running the game at 100%, but the head-tracking frame rate at 125%. To bring the audio 
 		// back to 100% speed, it must be slowed down by 25%
-		if (g_has_hmd && !SConfig::GetInstance().m_LocalCoreStartupParameter.bSyncGPU && (g_ActiveConfig.bPullUp20fps || g_ActiveConfig.bPullUp30fps || g_ActiveConfig.bPullUp60fps || g_ActiveConfig.bPullUp20fpsTimewarp || g_ActiveConfig.bPullUp30fpsTimewarp || g_ActiveConfig.bPullUp60fpsTimewarp))
+		if (g_has_hmd && !SConfig::GetInstance().m_LocalCoreStartupParameter.bSyncGPU && SConfig::GetInstance().m_LocalCoreStartupParameter.m_GPUDeterminismMode != GPU_DETERMINISM_FAKE_COMPLETION && (g_ActiveConfig.bPullUp20fps || g_ActiveConfig.bPullUp30fps || g_ActiveConfig.bPullUp60fps || g_ActiveConfig.bPullUp20fpsTimewarp || g_ActiveConfig.bPullUp30fpsTimewarp || g_ActiveConfig.bPullUp60fpsTimewarp))
 			aid_sample_rate = aid_sample_rate * (framelimit - 1) * 4 / VideoInterface::TargetRefreshRate;
 		else
 			aid_sample_rate = aid_sample_rate * (framelimit - 1) * 5 / VideoInterface::TargetRefreshRate;
