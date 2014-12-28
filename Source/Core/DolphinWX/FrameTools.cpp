b996649073c28072590ee2d502e4c1cc929c4d33
riking <rikingcoding@gmail.com>
2014-12-28 12:55:03 -0800

    Reword "Boot from DVD Drive" -> "Backup" menu option

--- Source/Core/DolphinWX/FrameTools.cpp
+++ Source/Core/DolphinWX/FrameTools.cpp
@@ -138,7 +138,7 @@ wxMenuBar* CFrame::CreateMenu()
 	fileMenu->Append(IDM_CHANGEDISC, GetMenuLabel(HK_CHANGE_DISC));
 
 	wxMenu *externalDrive = new wxMenu;
-	fileMenu->Append(IDM_DRIVES, _("&Boot from DVD Drive..."), externalDrive);
+	fileMenu->Append(IDM_DRIVES, _("&Boot from DVD Backup..."), externalDrive);
 
 	drives = cdio_get_devices();
 	// Windows Limitation of 24 character drives
