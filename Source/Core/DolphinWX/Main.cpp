2560d89576461a290816ebbb968e21d2e13e2583
JosJuice <josjuice@gmail.com>
2015-01-03 15:42:28 +0100

    Fix "Lionux" typo

--- Source/Core/DolphinWX/Main.cpp
+++ Source/Core/DolphinWX/Main.cpp
@@ -199,7 +199,7 @@ bool DolphinApp::OnInit()
 		},
 		{
 			wxCMD_LINE_OPTION, "P", "perf_dir",
-			"Directory for Lionux perf perf-$pid.map file",
+			"Directory for Linux perf perf-$pid.map file",
 			wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL
 		},
 		{
