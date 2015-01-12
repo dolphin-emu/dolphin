213eb730d033f2c6181cb40ff1658dbec44538fa
magumagu <magumagu9@gmail.com>
2015-01-11 19:06:27 -0800

    For idle loops, don't explicitly call CoreTiming::Advance.
    This is more consistent with the way Dolphin behaves with idle skipping off.

--- Source/Core/Core/PowerPC/Jit64IL/IR_X86.cpp
+++ Source/Core/Core/PowerPC/Jit64IL/IR_X86.cpp
@@ -2088,7 +2088,7 @@ static void DoWriteCode(IRBuilder* ibuild, JitIL* Jit, u32 exitAddress)
 			FixupBranch noidle = Jit->J_CC(CC_NZ);
 
 			RI.Jit->Cleanup(); // is it needed?
-			Jit->ABI_CallFunction((void *)&PowerPC::OnIdleIL);
+			Jit->ABI_CallFunction((void *)&PowerPC::OnIdle);
 
 			Jit->MOV(32, PPCSTATE(pc), Imm32(ibuild->GetImmValue( getOp2(I) )));
 			Jit->WriteExceptionExit();
