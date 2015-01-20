a25b07ec56fdac4ab635f93a61142529774baf75
Benjamin Przybocki <buddybenj@gmail.com>
2015-01-20 17:57:42 -0600

    Change Order of Some Hotkeys

--- Source/Core/DolphinWX/HotkeyDlg.cpp
+++ Source/Core/DolphinWX/HotkeyDlg.cpp
@@ -219,8 +219,8 @@ void HotkeyConfigDialog::CreateHotkeyGUIControls()
 		_("Connect Wiimote 4"),
 		_("Connect Balance Board"),
 
-		_("Volume Up"),
 		_("Volume Down"),
+		_("Volume Up"),
 		_("Volume Toggle Mute"),
 
 		_("Toggle IR"),
@@ -228,8 +228,8 @@ void HotkeyConfigDialog::CreateHotkeyGUIControls()
 		_("Toggle EFB Copies"),
 		_("Toggle Fog"),
 		_("Toggle Frame limit"),
-		_("Increase Frame limit"),
 		_("Decrease Frame limit"),
+		_("Increase Frame limit"),
 
 		_("Freelook Increase Speed"),
 		_("Freelook Decrease Speed"),
@@ -242,10 +242,10 @@ void HotkeyConfigDialog::CreateHotkeyGUIControls()
 		_("Freelook Zoom Out"),
 		_("Freelook Reset"),
 
-		_("Increase Depth"),
 		_("Decrease Depth"),
-		_("Increase Convergence"),
+		_("Increase Depth"),
 		_("Decrease Convergence"),
+		_("Increase Convergence"),
 
 		_("Load State Slot 1"),
 		_("Load State Slot 2"),
