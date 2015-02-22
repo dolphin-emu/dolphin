56795efd425fdbd080deeb0469f518c96bcc0eac
Tillmann Karras <tilkax@gmail.com>
2015-02-22 21:30:43 +0100

    Common: add single header for all intrinsics

---
+++ Source/Core/Common/Intrinsics.h
@@ -0,0 +1,15 @@
+// Copyright 2015 Dolphin Emulator Project
+// Licensed under GPLv2
+// Refer to the license.txt file included.
+
+#pragma once
+
+#ifdef _M_X86
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#endif
+
+#endif // _M_X86
