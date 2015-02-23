daf4aa09471a0113315fa49aa8409bfc9ba947fd
Tillmann Karras <tilkax@gmail.com>
2015-02-23 21:50:35 +0100

    Common: move _M_SSE macro to Intrinsics.h

--- Source/Core/Common/Intrinsics.h
+++ Source/Core/Common/Intrinsics.h
@@ -32,4 +32,20 @@ _mm_shuffle_epi8(__m128i a, __m128i mask)
 }
 #endif
 
+#if defined _M_GENERIC
+#  define _M_SSE 0
+#elif _MSC_VER || __INTEL_COMPILER
+#  define _M_SSE 0x402
+#elif defined __GNUC__
+# if defined __SSE4_2__
+#  define _M_SSE 0x402
+# elif defined __SSE4_1__
+#  define _M_SSE 0x401
+# elif defined __SSSE3__
+#  define _M_SSE 0x301
+# elif defined __SSE3__
+#  define _M_SSE 0x300
+# endif
+#endif
+
 #endif // _M_X86
