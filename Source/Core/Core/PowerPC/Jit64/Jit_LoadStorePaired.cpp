45dbcf0ed20219f77d2cece9ba5c48ceba23fe2d
Tillmann Karras <tilkax@gmail.com>
2015-03-05 19:20:50 +0100

    Jit64: use MRegSum where appropriate

--- Source/Core/Core/PowerPC/Jit64/Jit_LoadStorePaired.cpp
+++ Source/Core/Core/PowerPC/Jit64/Jit_LoadStorePaired.cpp
@@ -57,7 +57,7 @@ void Jit64::psq_stXX(UGeckoInstruction inst)
 				addr = RSCRATCH2;
 				if (a && gpr.R(a).IsSimpleReg() && gpr.R(b).IsSimpleReg())
 				{
-					LEA(32, addr, MComplex(gpr.RX(a), gpr.RX(b), SCALE_1, 0));
+					LEA(32, addr, MRegSum(gpr.RX(a), gpr.RX(b)));
 				}
 				else
 				{
@@ -105,7 +105,7 @@ void Jit64::psq_stXX(UGeckoInstruction inst)
 	if (gpr.R(a).IsSimpleReg() && gpr.R(b).IsSimpleReg() && (indexed || offset))
 	{
 		if (indexed)
-			LEA(32, RSCRATCH_EXTRA, MComplex(gpr.RX(a), gpr.RX(b), SCALE_1, 0));
+			LEA(32, RSCRATCH_EXTRA, MRegSum(gpr.RX(a), gpr.RX(b)));
 		else
 			LEA(32, RSCRATCH_EXTRA, MDisp(gpr.RX(a), offset));
 	}
@@ -190,7 +190,7 @@ void Jit64::psq_lXX(UGeckoInstruction inst)
 				addr = RSCRATCH2;
 				if (a && gpr.R(a).IsSimpleReg() && gpr.R(b).IsSimpleReg())
 				{
-					LEA(32, addr, MComplex(gpr.RX(a), gpr.RX(b), SCALE_1, 0));
+					LEA(32, addr, MRegSum(gpr.RX(a), gpr.RX(b)));
 				}
 				else
 				{
@@ -282,7 +282,7 @@ void Jit64::psq_lXX(UGeckoInstruction inst)
 	if (gpr.R(a).IsSimpleReg() && gpr.R(b).IsSimpleReg() && (indexed || offset))
 	{
 		if (indexed)
-			LEA(32, RSCRATCH_EXTRA, MComplex(gpr.RX(a), gpr.RX(b), SCALE_1, 0));
+			LEA(32, RSCRATCH_EXTRA, MRegSum(gpr.RX(a), gpr.RX(b)));
 		else
 			LEA(32, RSCRATCH_EXTRA, MDisp(gpr.RX(a), offset));
 	}
