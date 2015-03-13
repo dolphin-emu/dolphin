c03d3764c26c0eadd75ff072e94fc06c11af42ab
cegli <aegli214@gmail.com>
2015-03-13 14:15:10 -0700

    Revert commit with bad line endings: "AR Bruteforcer: Fix .map file parsing errors and lockups."

--- Source/Core/Core/Boot/Boot.cpp
+++ Source/Core/Core/Boot/Boot.cpp
@@ -270,13 +270,8 @@ bool CBoot::BootUp()
 			{
 				while (getline(myfile, line))
 				{
-					std::string::size_type loc = line.find("80");
-					if (loc != std::string::npos && line.find("__start") == std::string::npos)
-					{
-						line = line.substr(loc + 2, 6);
-						if (line.find_first_not_of("0123456789abcdefABCDEF") == std::string::npos && line.size() == 6)
-							Core::ch_map.push_back("04" + line);
-					}
+					line = line.substr(2, 6);
+					Core::ch_map.push_back("04" + line);
 				}
 				myfile.close();
 			}
