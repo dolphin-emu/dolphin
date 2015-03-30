1e5197820fa72f0230369b23815bd86d6891e2d6
cegli <aegli214@gmail.com>
2015-03-30 01:37:21 -0700

    Fix hotkeys changes quashed in last merge.

--- Source/Core/DolphinWX/Config/InterfaceConfigPane.cpp
+++ Source/Core/DolphinWX/Config/InterfaceConfigPane.cpp
@@ -17,6 +17,9 @@
 #include "Core/ConfigManager.h"
 #include "Core/HotkeyManager.h"
 #include "DolphinWX/Frame.h"
+#ifndef NEW_HOTKEYS
+#include "DolphinWX/HotkeyDlg.h"
+#endif
 #include "DolphinWX/InputConfigDiag.h"
 #include "DolphinWX/Main.h"
 #include "DolphinWX/WxUtils.h"
@@ -187,6 +190,7 @@ void InterfaceConfigPane::LoadThemes()
 
 void InterfaceConfigPane::OnHotkeyConfigButtonClicked(wxCommandEvent& event)
 {
+#ifdef NEW_HOTKEYS
 	bool was_init = false;
 
 	InputConfig* const hotkey_plugin = HotkeyManagerEmu::GetConfig();
@@ -212,6 +216,11 @@ void InterfaceConfigPane::OnHotkeyConfigButtonClicked(wxCommandEvent& event)
 	// if game isn't running
 	if (!was_init)
 		HotkeyManagerEmu::Shutdown();
+
+#else
+	HotkeyConfigDialog m_HotkeyDialog(this);
+	m_HotkeyDialog.ShowModal();
+#endif
 
 	// Update the GUI in case menu accelerators were changed
 	main_frame->UpdateGUI();
