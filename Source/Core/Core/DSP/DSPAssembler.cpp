6d9986846ceeaf805ba70b7014127bfbbd66beea
Tillmann Karras <tilkax@gmail.com>
2015-05-25 13:11:41 +0200

    Simplify some more license headers

--- Source/Core/Core/DSP/DSPAssembler.cpp
+++ Source/Core/Core/DSP/DSPAssembler.cpp
@@ -1,41 +1,7 @@
-/*====================================================================
-
-$Id: assemble.cpp,v 1.3 2008-11-11 01:04:26 wntrmute Exp $
-
-project:      GameCube DSP Tool (gcdsp)
-mail:         duddie@walla.com
-
-Copyright (c) 2005 Duddie
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-Revision 1.4  2008/10/04 10:30:00  Hermes
-added function to export the code to .h file
-added support for '/ *' '* /' and '//' for comentaries
-added some sintax detection when use registers
-
-$Log: not supported by cvs2svn $
-Revision 1.2  2005/09/14 02:19:29  wntrmute
-added header guards
-use standard main function
-
-Revision 1.1  2005/08/24 22:13:34  wntrmute
-Initial import
-
-
-====================================================================*/
+// Copyright 2008 Dolphin Emulator Project
+// Copyright 2005 Duddie, wntrmute, Hermes
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
 
 #include <cstdio>
 #include <cstdlib>
--- Source/Core/Core/PowerPC/MMU.cpp
+++ Source/Core/Core/PowerPC/MMU.cpp
@@ -1,19 +1,6 @@
-// Copyright (C) 2003 Dolphin Project.
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, version 2.0.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License 2.0 for more details.
-
-// A copy of the GPL 2.0 should have been included with the program.
-// If not, see http://www.gnu.org/licenses/
-
-// Official Git repository and contact information can be found at
-// https://github.com/dolphin-emu/dolphin
+// Copyright 2003 Dolphin Emulator Project
+// Licensed under GPLv2
+// Refer to the license.txt file included.
 
 #include "Common/Atomic.h"
 #include "Common/BitSet.h"
