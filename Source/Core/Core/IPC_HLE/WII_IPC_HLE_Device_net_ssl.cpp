01f4492867f79ab819dc0492d92675f78dbf5248
Sepalani <sepalani@hotmail.fr>
2015-06-07 00:49:18 +0200

    Fixed: Allowed unknown certificates

--- Source/Core/Core/IPC_HLE/WII_IPC_HLE_Device_net_ssl.cpp
+++ Source/Core/Core/IPC_HLE/WII_IPC_HLE_Device_net_ssl.cpp
@@ -176,7 +176,7 @@ IPCCommandResult CWII_IPC_HLE_Device_net_ssl::IOCtlV(u32 _CommandAddress)
 			ssl_set_session(&ssl->ctx, &ssl->session);
 
 			ssl_set_endpoint(&ssl->ctx, SSL_IS_CLIENT);
-			ssl_set_authmode(&ssl->ctx, SSL_VERIFY_NONE);
+			ssl_set_authmode(&ssl->ctx, SSL_VERIFY_REQUIRED);
 			ssl_set_renegotiation(&ssl->ctx, SSL_RENEGOTIATION_ENABLED);
 
 			ssl->hostname = hostname;
