29b1132f9fac2a74edae44b925bcc229b6ad0987
Jules Blok <jules.blok@gmail.com>
2015-06-06 20:09:22 +0200

    Revert "Revert "OGL: Change the depth buffer type to GL_FLOAT.""
    This reverts commit 05d60f4fef5fb7377ed1f2f905b48e93926501b3.
    
    # Conflicts:
    #	Source/Core/VideoBackends/OGL/Render.cpp

--- Source/Core/VideoBackends/OGL/Render.h
+++ Source/Core/VideoBackends/OGL/Render.h
@@ -98,7 +98,7 @@ public:
 	int GetMaxTextureSize() override;
 
 private:
-	void UpdateEFBCache(EFBAccessType type, u32 cacheRectIdx, const EFBRectangle& efbPixelRc, const TargetRectangle& targetPixelRc, const u32* data);
+	void UpdateEFBCache(EFBAccessType type, u32 cacheRectIdx, const EFBRectangle& efbPixelRc, const TargetRectangle& targetPixelRc, const void* data);
 
 	void BlitScreen(TargetRectangle src, TargetRectangle dst, GLuint src_texture, int src_width, int src_height);
 };
