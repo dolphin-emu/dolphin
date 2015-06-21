cc221514954a26cd293d29c93d013357ccf552a1
rukai <rubickent@gmail.com>
2015-06-21 13:34:09 +1000

    Fixes translation on Linux and potentially Mac OSX, by specifying location of *.gmo files

--- Source/Core/DolphinWX/Main.cpp
+++ Source/Core/DolphinWX/Main.cpp
@@ -332,8 +332,13 @@ void DolphinApp::InitLanguageSupport()
 	{
 		m_locale = new wxLocale(language);
 
+        // Specify where dolphins *.gmo files are located on each operating system
 #ifdef _WIN32
-		m_locale->AddCatalogLookupPathPrefix(StrToWxStr(File::GetExeDirectory() + DIR_SEP "Languages"));
+        m_locale->AddCatalogLookupPathPrefix(StrToWxStr(File::GetExeDirectory() + DIR_SEP "Languages"));
+#elif defined(__LINUX__)
+		m_locale->AddCatalogLookupPathPrefix(StrToWxStr(DATA_DIR "../locale"));
+#elif defined(__APPLE__)
+		m_locale->AddCatalogLookupPathPrefix(StrToWxStr(GetBundleDirectory() + "Contents/Resources"));
 #endif
 
 		m_locale->AddCatalog("dolphin-emu");
