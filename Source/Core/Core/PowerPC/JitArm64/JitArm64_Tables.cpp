a8b1347ea99383955c36c93475d5fe7608f59630
degasus <wickmarkus@web.de>
2015-08-23 17:40:39 +0200

    JitArm64: Implement srawx.

--- Source/Core/Core/PowerPC/JitArm64/JitArm64_Tables.cpp
+++ Source/Core/Core/PowerPC/JitArm64/JitArm64_Tables.cpp
@@ -209,23 +209,23 @@ static GekkoOPTemplate table31[] = {
     {200, &JitArm64::FallBackToInterpreter},   // subfzex
     {712, &JitArm64::FallBackToInterpreter},   // subfzeox
 
-    {28, &JitArm64::boolX},                   // andx
-    {60, &JitArm64::boolX},                   // andcx
-    {444, &JitArm64::boolX},                  // orx
-    {124, &JitArm64::boolX},                  // norx
-    {316, &JitArm64::boolX},                  // xorx
-    {412, &JitArm64::boolX},                  // orcx
-    {476, &JitArm64::boolX},                  // nandx
-    {284, &JitArm64::boolX},                  // eqvx
-    {0, &JitArm64::cmp},                      // cmp
-    {32, &JitArm64::cmpl},                    // cmpl
-    {26, &JitArm64::cntlzwx},                 // cntlzwx
-    {922, &JitArm64::extsXx},                 // extshx
-    {954, &JitArm64::extsXx},                 // extsbx
-    {536, &JitArm64::srwx},                   // srwx
-    {792, &JitArm64::FallBackToInterpreter},  // srawx
-    {824, &JitArm64::srawix},                 // srawix
-    {24, &JitArm64::slwx},                    // slwx
+    {28, &JitArm64::boolX},    // andx
+    {60, &JitArm64::boolX},    // andcx
+    {444, &JitArm64::boolX},   // orx
+    {124, &JitArm64::boolX},   // norx
+    {316, &JitArm64::boolX},   // xorx
+    {412, &JitArm64::boolX},   // orcx
+    {476, &JitArm64::boolX},   // nandx
+    {284, &JitArm64::boolX},   // eqvx
+    {0, &JitArm64::cmp},       // cmp
+    {32, &JitArm64::cmpl},     // cmpl
+    {26, &JitArm64::cntlzwx},  // cntlzwx
+    {922, &JitArm64::extsXx},  // extshx
+    {954, &JitArm64::extsXx},  // extsbx
+    {536, &JitArm64::srwx},    // srwx
+    {792, &JitArm64::srawx},   // srawx
+    {824, &JitArm64::srawix},  // srawix
+    {24, &JitArm64::slwx},     // slwx
 
     {54, &JitArm64::dcbx},        // dcbst
     {86, &JitArm64::dcbx},        // dcbf
