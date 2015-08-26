ca51f1a4f67445a69632c3f53d1b62f074cb7414
Ryan Houdek <Sonicadvance1@gmail.com>
2015-08-26 04:18:08 -0500

    [AArch64] Optimize paired registers being used in double operations.
    In particular this optimizes the case where a 32bit float is loaded via lfs, and then used in double operations.
    This happens very often in Gekko based code because the best way to load a 32bit value as a double is lfs since it automatically turns in to a double value.
    
    There are a few other implications of this in practice as well. Like if both of the paired registers are loaded via psq_l and then used in double
    operations it would be improved.
    Also if we implement a double register we've got to be careful to make sure we understand if it is in "lower" register or the full 128bit register.

--- Source/Core/Core/PowerPC/JitArm64/JitArm64_RegCache.cpp
+++ Source/Core/Core/PowerPC/JitArm64/JitArm64_RegCache.cpp
@@ -330,6 +330,7 @@ void Arm64FPRCache::BindToRegister(u32 preg, bool do_load, bool only_lower)
 {
 	OpArg& reg = m_guest_registers[preg];
 
+	bool was_dirty = reg.IsDirty();
 	reg.SetDirty(true);
 	switch (reg.GetType())
 	{
@@ -369,6 +370,20 @@ void Arm64FPRCache::BindToRegister(u32 preg, bool do_load, bool only_lower)
 
 			// Change it over to a full 128bit register
 			reg.LoadToReg(reg.GetReg());
+		}
+	}
+	break;
+	case REG_REG:
+	{
+		if (only_lower)
+		{
+			// If we only want the lower bits, let's store away the high bits and drop to a lower only register
+			// We are doing a full 128bit store because it takes 2 cycles on a Cortex-A57 to do a 128bit store.
+			// It would take longer to do an insert to a temporary and a 64bit store than to just do this.
+			ARM64Reg host_reg = reg.GetReg();
+			if (was_dirty)
+				m_float_emit->STR(128, INDEX_UNSIGNED, host_reg, X29, PPCSTATE_OFF(ps[preg][0]));
+			reg.LoadLowerReg(host_reg);
 		}
 	}
 	break;
