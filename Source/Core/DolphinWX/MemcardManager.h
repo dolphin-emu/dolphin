d58550e874fa25187c1376efc9671a6f411c0a3b
Lioncash <mathew1800@gmail.com>
2015-08-29 05:17:33 -0400

    MemcardManager: Minor cleanup of header code

--- Source/Core/DolphinWX/MemcardManager.h
+++ Source/Core/DolphinWX/MemcardManager.h
@@ -17,19 +17,11 @@ class wxFileDirPickerEvent;
 class wxFilePickerCtrl;
 class wxStaticText;
 
-#undef MEMCARD_MANAGER_STYLE
-#define MEMCARD_MANAGER_STYLE wxCAPTION | wxSYSTEM_MENU | wxDIALOG_NO_PARENT | wxCLOSE_BOX | wxRESIZE_BORDER | wxMAXIMIZE_BOX
-#define MEMCARDMAN_TITLE _trans("Memory Card Manager WARNING-Make backups before using, should be fixed but could mangle stuff!")
-
-#define FIRSTPAGE 0
-
-class CMemcardManager : public wxDialog
+class CMemcardManager final : public wxDialog
 {
 public:
-
-	CMemcardManager(wxWindow *parent, wxWindowID id = wxID_ANY, const wxString& title = wxGetTranslation(MEMCARDMAN_TITLE),
-		const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = MEMCARD_MANAGER_STYLE);
-	virtual ~CMemcardManager();
+	CMemcardManager(wxWindow* parent);
+	~CMemcardManager();
 
 private:
 	DECLARE_EVENT_TABLE();
