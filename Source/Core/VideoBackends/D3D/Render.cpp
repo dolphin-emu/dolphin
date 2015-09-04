102a2a975d9cf5c1a514582d54b9a85948c0d260
Lioncash <mathew1800@gmail.com>
2015-09-03 21:53:45 -0400

    BitField: Enable ifdef'd out code for Windows

--- Source/Core/VideoBackends/D3D/Render.cpp
+++ Source/Core/VideoBackends/D3D/Render.cpp
@@ -1077,7 +1077,7 @@ void Renderer::SetGenerationMode()
 
 void Renderer::SetDepthMode()
 {
-	gx_state.zmode = bpmem.zmode;
+	gx_state.zmode.hex = bpmem.zmode.hex;
 }
 
 void Renderer::SetLogicOpMode()
