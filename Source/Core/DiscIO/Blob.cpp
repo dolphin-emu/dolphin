00aefa5e08a9aa6865a14706dcf35346993fa517
Tillmann Karras <tilkax@gmail.com>
2015-09-16 21:06:54 +0200

    DolphinWX: decompress discs to calculate MD5 hash

--- Source/Core/DiscIO/Blob.cpp
+++ Source/Core/DiscIO/Blob.cpp
@@ -77,23 +77,19 @@ bool SectorReader::Read(u64 offset, u64 size, u8* out_ptr)
 			continue;
 		}
 
+		const u8* data = GetBlockData(block);
+		if (!data)
+			return false;
+
 		u32 toCopy = m_blocksize - positionInBlock;
 		if (toCopy >= remain)
 		{
-			const u8* data = GetBlockData(block);
-			if (!data)
-				return false;
-
 			// Yay, we are done!
 			memcpy(out_ptr, data + positionInBlock, (size_t)remain);
 			return true;
 		}
 		else
 		{
-			const u8* data = GetBlockData(block);
-			if (!data)
-				return false;
-
 			memcpy(out_ptr, data + positionInBlock, toCopy);
 			out_ptr += toCopy;
 			remain -= toCopy;
