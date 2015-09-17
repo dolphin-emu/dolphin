efe71e686b8b89e87e2c10237e4d33bc4bfdf54f
Tillmann Karras <tilkax@gmail.com>
2015-09-17 10:58:38 +0200

    DiscIO: implement CISOBlob::GetDataSize()
    This is a best-effort approach, since the CISO format does not save the
    original file size.

--- Source/Core/DiscIO/CISOBlob.h
+++ Source/Core/DiscIO/CISOBlob.h
@@ -37,7 +37,11 @@ public:
 	static CISOFileReader* Create(const std::string& filename);
 
 	BlobType GetBlobType() const override { return BlobType::CISO; }
+
+	// The CISO format does not save the original file size.
+	// This function returns an upper bound.
 	u64 GetDataSize() const override;
+
 	u64 GetRawSize() const override;
 	bool Read(u64 offset, u64 nbytes, u8* out_ptr) override;
 
