c1fcb0c38d24bc74097bc6fc52246c08d3351556
Scott Mansell <phiren@gmail.com>
2015-10-12 12:36:02 +1300

    UberShader: Implement Alpha Combiner and dual source blending.

--- Source/Core/VideoBackends/OGL/ProgramShaderCache.cpp
+++ Source/Core/VideoBackends/OGL/ProgramShaderCache.cpp
@@ -221,8 +221,8 @@ SHADER* ProgramShaderCache::SetShader(DSTALPHA_MODE dstAlphaMode, u32 primitive_
 	if (g_ActiveConfig.backend_info.bSupportsGeometryShaders && !uid.guid.GetUidData()->IsPassthrough())
 		gcode = GenerateGeometryShaderCode(API_OPENGL, uid.guid.GetUidData());
 
-//#if defined(_DEBUG) || defined(DEBUGFAST)
-	if (true || g_ActiveConfig.iLog & CONF_SAVESHADERS)
+#if defined(_DEBUG) || defined(DEBUGFAST)
+	if (g_ActiveConfig.iLog & CONF_SAVESHADERS)
 	{
 		static int counter = 0;
 		std::string filename =  StringFromFormat("%svs_%04i.txt", File::GetUserPath(D_DUMP_IDX).c_str(), counter++);
@@ -237,7 +237,7 @@ SHADER* ProgramShaderCache::SetShader(DSTALPHA_MODE dstAlphaMode, u32 primitive_
 			SaveData(filename, gcode.GetBuffer());
 		}
 	}
-//#endif
+#endif
 
 	if (!CompileShader(newentry.shader, vcode.GetBuffer(), pcode.GetBuffer(), gcode.GetBuffer()))
 	{
