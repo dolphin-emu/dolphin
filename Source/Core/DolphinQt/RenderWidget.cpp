d5505a812721dace4da80fb78c06ffe5a276578c
spxtr <spextear@gmail.com>
2015-11-14 19:52:10 -0800

    Move RenderWidget into its own file, update Host.
    RenderWidget now listens for events such as WinId changed and focus
    changed and tells Host about them. RenderWidget is still managed by
    MainWindow in terms of placement and size, since it sometimes goes in
    the main window and sometimes doesn't.
    
    Tested on my box and the stop confirmation dialog appears over the
    fullscreen render widget. Does this work for everyone else?

---
+++ Source/Core/DolphinQt/RenderWidget.cpp
@@ -0,0 +1,50 @@
+// Copyright 2015 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#include <QDebug>
+#include <QEvent>
+#include <QKeyEvent>
+
+#include "DolphinQt/Host.h"
+#include "DolphinQt/RenderWidget.h"
+
+RenderWidget::RenderWidget(QWidget* parent)
+	: QWidget(parent)
+{
+	setAttribute(Qt::WA_OpaquePaintEvent, true);
+	setAttribute(Qt::WA_NoSystemBackground, true);
+
+	connect(Host::GetInstance(), &Host::RequestTitle, this, &RenderWidget::setWindowTitle);
+	connect(this, &RenderWidget::FocusChanged, Host::GetInstance(), &Host::SetRenderFocus);
+	connect(this, &RenderWidget::StateChanged, Host::GetInstance(), &Host::SetRenderFullscreen);
+	connect(this, &RenderWidget::HandleChanged, Host::GetInstance(), &Host::SetRenderHandle);
+	emit HandleChanged((void*) winId());
+}
+
+bool RenderWidget::event(QEvent* event)
+{
+	switch (event->type())
+	{
+	case QEvent::KeyPress:
+	{
+		QKeyEvent* ke = static_cast<QKeyEvent*>(event);
+		if (ke->key() == Qt::Key_Escape)
+			emit EscapePressed();
+		break;
+	}
+	case QEvent::WinIdChange:
+		emit HandleChanged((void*) winId());
+		break;
+	case QEvent::FocusIn:
+	case QEvent::FocusOut:
+		emit FocusChanged(hasFocus());
+		break;
+	case QEvent::WindowStateChange:
+		emit StateChanged(isFullScreen());
+		break;
+	default:
+		break;
+	}
+	return QWidget::event(event);
+}
