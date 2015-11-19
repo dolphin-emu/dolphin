ed5e3c054e58b7547cead222048676f32ce8858d
Ryan Houdek <Sonicadvance1@gmail.com>
2015-11-19 00:46:26 -0600

    Enable Anisotropic filtering in ES.
    This adds a check to make sure the vendor supports anisotropic filtering.
    Pretty much all vendors support this, even the mobile ones.

--- Source/Core/VideoBackends/OGL/SamplerCache.cpp
+++ Source/Core/VideoBackends/OGL/SamplerCache.cpp
@@ -129,12 +129,10 @@ void SamplerCache::SetParameters(GLuint sampler_id, const Params& params)
 	glSamplerParameterf(sampler_id, GL_TEXTURE_MAX_LOD, tm1.max_lod / 16.f);
 
 	if (GLInterface->GetMode() == GLInterfaceMode::MODE_OPENGL)
-	{
 		glSamplerParameterf(sampler_id, GL_TEXTURE_LOD_BIAS, (s32)tm0.lod_bias / 32.f);
 
-		if (g_ActiveConfig.iMaxAnisotropy > 0)
-			glSamplerParameterf(sampler_id, GL_TEXTURE_MAX_ANISOTROPY_EXT, (float)(1 << g_ActiveConfig.iMaxAnisotropy));
-	}
+	if (g_ActiveConfig.iMaxAnisotropy > 0 && g_ogl_config.bSupportsAniso)
+		glSamplerParameterf(sampler_id, GL_TEXTURE_MAX_ANISOTROPY_EXT, (float)(1 << g_ActiveConfig.iMaxAnisotropy));
 }
 
 void SamplerCache::Clear()
