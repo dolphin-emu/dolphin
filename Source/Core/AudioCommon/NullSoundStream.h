0d4baa0744be1405168782122832f563a6a3bd84
Lioncash <mathew1800@gmail.com>
2015-12-05 14:31:36 -0500

    NullSound: Replace header define with private constant

--- Source/Core/AudioCommon/NullSoundStream.h
+++ Source/Core/AudioCommon/NullSoundStream.h
@@ -4,16 +4,10 @@
 
 #pragma once
 
-#include <cstdlib>
 #include "AudioCommon/SoundStream.h"
-
-#define BUF_SIZE (48000 * 4 / 32)
 
 class NullSound final : public SoundStream
 {
-	// playback position
-	short realtimeBuffer[BUF_SIZE / sizeof(short)];
-
 public:
 	bool Start() override;
 	void SoundLoop() override;
@@ -23,4 +17,10 @@ public:
 	void Update() override;
 
 	static bool isValid() { return true; }
+
+private:
+	static constexpr size_t BUFFER_SIZE = 48000 * 4 / 32;
+
+	// Playback position
+	short realtimeBuffer[BUFFER_SIZE / sizeof(short)];
 };
