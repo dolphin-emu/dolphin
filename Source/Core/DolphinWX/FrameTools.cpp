dolphin-emu/dolphin

commit 33487ab5f20309e33b4734dbea9201b5b0d893a2

DolphinWX: Fix label change for the play/pause button
This fixes changing the play/pause button's label depending on the
emulation state. Before, wxToolBarToolBase's SetLabel() function was
used. This function, however, is not implemented in wxGTK which leads to
the label not changing on linux when the button is clicked. Although the preferred
method (according to the wxWidgets documentation) to change the properties
of a tool is to use the toolbar's setters, there is no such setter for
the label. Therefore, this implements a workaround where the
button is deleted and readded afterwards with the updated properties.

Thanks to linkmauve for noticing this!

Source/Core/DolphinWX/FrameTools.cpp
@@ -1792,17 +1792,19 @@ void CFrame::UpdateGUI()
 
 		if (PlayTool)
 		{
+			int position = m_ToolBar->GetToolPos(IDM_PLAY);
+
 			if (Running)
 			{
-				PlayTool->SetLabel(_("Pause"));
-				PlayTool->SetShortHelp(_("Pause"));
-				m_ToolBar->SetToolNormalBitmap(IDM_PLAY, m_Bitmaps[Toolbar_Pause]);
+				m_ToolBar->DeleteTool(IDM_PLAY);
+				m_ToolBar->InsertTool(position, IDM_PLAY, _("Pause"), m_Bitmaps[Toolbar_Pause],
+				                      wxNullBitmap, wxITEM_NORMAL, _("Pause"));
 			}
 			else
 			{
-				PlayTool->SetLabel(_("Play"));
-				PlayTool->SetShortHelp(_("Play"));
-				m_ToolBar->SetToolNormalBitmap(IDM_PLAY, m_Bitmaps[Toolbar_Play]);
+				m_ToolBar->DeleteTool(IDM_PLAY);
+				m_ToolBar->InsertTool(position, IDM_PLAY, _("Play"), m_Bitmaps[Toolbar_Play],
+				                      wxNullBitmap, wxITEM_NORMAL, _("Play"));
 			}
 		}
 	}

commit 9dd9a75dbe275ea707162557102d83a10e41a0ba

DolphinWX: Fix items vanishing from toolbar
Commit 33487ab5f20309e33b4734dbea9201b5b0d893a2 introduced a regression
where items would vanish from the toolbar. This adds a call to Realize()
after the reinsertions of the play/pause button as required per
documentation.

Thanks to Simonwayneee for noticing this!

Source/Core/DolphinWX/FrameTools.cpp
@@ -1804,6 +1804,7 @@ void CFrame::UpdateGUI()
 				m_ToolBar->InsertTool(position, IDM_PLAY, _("Play"), m_Bitmaps[Toolbar_Play],
 				                      wxNullBitmap, wxITEM_NORMAL, _("Play"));
 			}
+			m_ToolBar->Realize();
 		}
 	}
 