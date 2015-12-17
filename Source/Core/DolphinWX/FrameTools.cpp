9a0b70cc51e3496cc34efa2ed48284baf03b00c2
JosJuice <josjuice@gmail.com>
2015-12-17 11:25:14 +0100

    Fix encoding for savestate slots
    StrToWxStr is needed, otherwise the string will not be read as UTF-8.

--- Source/Core/DolphinWX/FrameTools.cpp
+++ Source/Core/DolphinWX/FrameTools.cpp
@@ -465,7 +465,8 @@ wxString CFrame::GetMenuLabel(int Id)
 		case HK_LOAD_STATE_SLOT_9:
 		case HK_LOAD_STATE_SLOT_10:
 			Label = wxString::Format(_("Slot %i - %s"),
-			        Id - HK_LOAD_STATE_SLOT_1 + 1, State::GetInfoStringOfSlot(Id - HK_LOAD_STATE_SLOT_1 + 1).c_str());
+			        Id - HK_LOAD_STATE_SLOT_1 + 1,
+			        StrToWxStr(State::GetInfoStringOfSlot(Id - HK_LOAD_STATE_SLOT_1 + 1)));
 			break;
 
 		case HK_SAVE_STATE_SLOT_1:
@@ -479,7 +480,8 @@ wxString CFrame::GetMenuLabel(int Id)
 		case HK_SAVE_STATE_SLOT_9:
 		case HK_SAVE_STATE_SLOT_10:
 			Label = wxString::Format(_("Slot %i - %s"),
-			        Id - HK_SAVE_STATE_SLOT_1 + 1, State::GetInfoStringOfSlot(Id - HK_SAVE_STATE_SLOT_1 + 1).c_str());
+			        Id - HK_SAVE_STATE_SLOT_1 + 1,
+			        StrToWxStr(State::GetInfoStringOfSlot(Id - HK_SAVE_STATE_SLOT_1 + 1)));
 			break;
 		case HK_SAVE_STATE_FILE:
 			Label = _("Save State...");
@@ -525,7 +527,8 @@ wxString CFrame::GetMenuLabel(int Id)
 		case HK_SELECT_STATE_SLOT_9:
 		case HK_SELECT_STATE_SLOT_10:
 			Label = wxString::Format(_("Select Slot %i - %s"),
-			        Id - HK_SELECT_STATE_SLOT_1 + 1, State::GetInfoStringOfSlot(Id - HK_SELECT_STATE_SLOT_1 + 1).c_str());
+			        Id - HK_SELECT_STATE_SLOT_1 + 1,
+			        StrToWxStr(State::GetInfoStringOfSlot(Id - HK_SELECT_STATE_SLOT_1 + 1)));
 			break;
 
 
