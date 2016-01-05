12cba281dd2bec5edfa12e4cf9ece55c25444160
Matt Mastracci <matthew@mastracci.com>
2016-01-04 20:58:31 -0700

    Refactor the paired load/store code Simplification/reduction of duplicated code. Detect other constant GQR values and inline loads (5-10% speedup) and do direct dispatch to AOT methods for stores.

--- Source/Core/Core/PowerPC/Jit64Common/Jit64AsmCommon.h
+++ Source/Core/Core/PowerPC/Jit64Common/Jit64AsmCommon.h
@@ -7,16 +7,31 @@
 #include "Core/PowerPC/JitCommon/Jit_Util.h"
 #include "Core/PowerPC/JitCommon/JitAsmCommon.h"
 
-class CommonAsmRoutines : public CommonAsmRoutinesBase, public EmuCodeBlock
-{
-protected:
-	void GenQuantizedLoads();
-	void GenQuantizedStores();
-	void GenQuantizedSingleStores();
+enum EQuantizeType : u32;
 
+class QuantizedMemoryRoutines : public EmuCodeBlock
+{
+public:
+	void GenQuantizedLoad(bool single, EQuantizeType type, int quantize);
+	void GenQuantizedStore(bool single, EQuantizeType type, int quantize);
+
+private:
+	void GenQuantizedLoadFloat(bool single, bool isInline);
+	void GenQuantizedStoreFloat(bool single, bool isInline);
+};
+
+class CommonAsmRoutines : public CommonAsmRoutinesBase, public QuantizedMemoryRoutines
+{
 public:
 	void GenFifoWrite(int size);
 	void GenFrsqrte();
 	void GenFres();
 	void GenMfcr();
+
+protected:
+	const u8* GenQuantizedLoadRuntime(bool single, EQuantizeType type);
+	const u8* GenQuantizedStoreRuntime(bool single, EQuantizeType type);
+	void GenQuantizedLoads();
+	void GenQuantizedStores();
+	void GenQuantizedSingleStores();
 };
