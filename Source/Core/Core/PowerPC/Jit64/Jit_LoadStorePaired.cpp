13e2fd7d38de528e10bca6991de1737f9f4ee8d6
Matt Mastracci <matthew@mastracci.com>
2016-01-09 14:25:21 -0700

    Refactor fastmem/trampoline code. Simplication to avoid reading back the generated instructions, allowing us to handle all possible cases.

--- Source/Core/Core/PowerPC/Jit64/Jit_LoadStorePaired.cpp
+++ Source/Core/Core/PowerPC/Jit64/Jit_LoadStorePaired.cpp
@@ -40,72 +40,6 @@ void Jit64::psq_stXX(UGeckoInstruction inst)
 	u32 gqrValue = gqrIsConstant ? it->second & 0xffff : 0;
 
 	gpr.Lock(a, b);
-	if (gqrIsConstant && gqrValue == 0)
-	{
-		int storeOffset = 0;
-		gpr.BindToRegister(a, true, update);
-		X64Reg addr = gpr.RX(a);
-		// TODO: this is kind of ugly :/ we should probably create a universal load/store address calculation
-		// function that handles all these weird cases, e.g. how non-fastmem loadstores clobber addresses.
-		bool storeAddress = (update && jo.memcheck) || !jo.fastmem;
-		if (storeAddress)
-		{
-			addr = RSCRATCH2;
-			MOV(32, R(addr), gpr.R(a));
-		}
-		if (indexed)
-		{
-			if (update)
-			{
-				ADD(32, R(addr), gpr.R(b));
-			}
-			else
-			{
-				addr = RSCRATCH2;
-				if (a && gpr.R(a).IsSimpleReg() && gpr.R(b).IsSimpleReg())
-				{
-					LEA(32, addr, MRegSum(gpr.RX(a), gpr.RX(b)));
-				}
-				else
-				{
-					MOV(32, R(addr), gpr.R(b));
-					if (a)
-						ADD(32, R(addr), gpr.R(a));
-				}
-			}
-		}
-		else
-		{
-			if (update)
-				ADD(32, R(addr), Imm32(offset));
-			else
-				storeOffset = offset;
-		}
-
-		fpr.Lock(s);
-		if (w)
-		{
-			CVTSD2SS(XMM0, fpr.R(s));
-			MOVD_xmm(R(RSCRATCH), XMM0);
-		}
-		else
-		{
-			CVTPD2PS(XMM0, fpr.R(s));
-			MOVQ_xmm(R(RSCRATCH), XMM0);
-			ROL(64, R(RSCRATCH), Imm8(32));
-		}
-
-		BitSet32 registersInUse = CallerSavedRegistersInUse();
-		if (update && storeAddress)
-			registersInUse[addr] = true;
-		SafeWriteRegToReg(RSCRATCH, addr, w ? 32 : 64, storeOffset, registersInUse);
-		MemoryExceptionCheck();
-		if (update && storeAddress)
-			MOV(32, gpr.R(a), R(addr));
-		gpr.UnlockAll();
-		fpr.UnlockAll();
-		return;
-	}
 	gpr.FlushLockX(RSCRATCH_EXTRA);
 	if (update)
 		gpr.BindToRegister(a, true, true);
@@ -128,43 +62,35 @@ void Jit64::psq_stXX(UGeckoInstruction inst)
 	if (update && !jo.memcheck)
 		MOV(32, gpr.R(a), R(RSCRATCH_EXTRA));
 
+	if (w)
+		CVTSD2SS(XMM0, fpr.R(s)); // one
+	else
+		CVTPD2PS(XMM0, fpr.R(s)); // pair
+
 	if (gqrIsConstant)
 	{
-		// Paired stores don't yield any real change in performance right now, but if we can
-		// improve fastmem support this might change
-		//#define INLINE_PAIRED_STORES
-#ifdef INLINE_PAIRED_STORES
-		if (w)
-		{
-			// One value
-			CVTSD2SS(XMM0, fpr.R(s));
-			GenQuantizedStore(true, static_cast<EQuantizeType>(gqrValue & 0x7), (gqrValue & 0x3F00) >> 8);
-		}
-		else
-		{
-			// Pair of values
-			CVTPD2PS(XMM0, fpr.R(s));
-			GenQuantizedStore(false, static_cast<EQuantizeType>(gqrValue & 0x7), (gqrValue & 0x3F00) >> 8);
-		}
-#else
-		// We know what GQR is here, so we can load RSCRATCH2 and call into the store method directly
-		// with just the scale bits.
 		int type = gqrValue & 0x7;
-		MOV(32, R(RSCRATCH2), Imm32(gqrValue & 0x3F00));
 
-		if (w)
+		// Paired stores (other than w/type zero) don't yield any real change in
+		// performance right now, but if we can improve fastmem support this might change
+		if (gqrValue == 0)
 		{
-			// One value
-			CVTSD2SS(XMM0, fpr.R(s));
-			CALL(asm_routines.singleStoreQuantized[type]);
+			if (w)
+				GenQuantizedStore(true, static_cast<EQuantizeType>(type), (gqrValue & 0x3F00) >> 8);
+			else
+				GenQuantizedStore(false, static_cast<EQuantizeType>(type), (gqrValue & 0x3F00) >> 8);
 		}
 		else
 		{
-			// Pair of values
-			CVTPD2PS(XMM0, fpr.R(s));
-			CALL(asm_routines.pairedStoreQuantized[type]);
+			// We know what GQR is here, so we can load RSCRATCH2 and call into the store method directly
+			// with just the scale bits.
+			MOV(32, R(RSCRATCH2), Imm32(gqrValue & 0x3F00));
+
+			if (w)
+				CALL(asm_routines.singleStoreQuantized[type]);
+			else
+				CALL(asm_routines.pairedStoreQuantized[type]);
 		}
-#endif
 	}
 	else
 	{
@@ -177,22 +103,13 @@ void Jit64::psq_stXX(UGeckoInstruction inst)
 		MOVZX(32, 8, RSCRATCH, R(RSCRATCH2));
 
 		if (w)
-		{
-			// One value
-			CVTSD2SS(XMM0, fpr.R(s));
 			CALLptr(MScaled(RSCRATCH, SCALE_8, (u32)(u64)asm_routines.singleStoreQuantized));
-		}
 		else
-		{
-			// Pair of values
-			CVTPD2PS(XMM0, fpr.R(s));
 			CALLptr(MScaled(RSCRATCH, SCALE_8, (u32)(u64)asm_routines.pairedStoreQuantized));
-		}
 	}
 
 	if (update && jo.memcheck)
 	{
-		MemoryExceptionCheck();
 		if (indexed)
 			ADD(32, gpr.R(a), gpr.R(b));
 		else
@@ -223,113 +140,6 @@ void Jit64::psq_lXX(UGeckoInstruction inst)
 
 	gpr.Lock(a, b);
 
-	if (gqrIsConstant && gqrValue == 0)
-	{
-		s32 loadOffset = 0;
-		gpr.BindToRegister(a, true, update);
-		X64Reg addr = gpr.RX(a);
-		if (update && jo.memcheck)
-		{
-			addr = RSCRATCH2;
-			MOV(32, R(addr), gpr.R(a));
-		}
-		if (indexed)
-		{
-			if (update)
-			{
-				ADD(32, R(addr), gpr.R(b));
-			}
-			else
-			{
-				addr = RSCRATCH2;
-				if (a && gpr.R(a).IsSimpleReg() && gpr.R(b).IsSimpleReg())
-				{
-					LEA(32, addr, MRegSum(gpr.RX(a), gpr.RX(b)));
-				}
-				else
-				{
-					MOV(32, R(addr), gpr.R(b));
-					if (a)
-						ADD(32, R(addr), gpr.R(a));
-				}
-			}
-		}
-		else
-		{
-			if (update)
-				ADD(32, R(addr), Imm32(offset));
-			else
-				loadOffset = offset;
-		}
-
-		fpr.Lock(s);
-		if (jo.memcheck)
-		{
-			fpr.StoreFromRegister(s);
-			js.revertFprLoad = s;
-		}
-		fpr.BindToRegister(s, false);
-
-		// Let's mirror the JitAsmCommon code and assume all non-MMU loads go to RAM.
-		if (!jo.memcheck)
-		{
-			if (w)
-			{
-				if (cpu_info.bSSSE3)
-				{
-					MOVD_xmm(XMM0, MComplex(RMEM, addr, SCALE_1, loadOffset));
-					PSHUFB(XMM0, M(pbswapShuffle1x4));
-					UNPCKLPS(XMM0, M(m_one));
-				}
-				else
-				{
-					LoadAndSwap(32, RSCRATCH, MComplex(RMEM, addr, SCALE_1, loadOffset));
-					MOVD_xmm(XMM0, R(RSCRATCH));
-					UNPCKLPS(XMM0, M(m_one));
-				}
-			}
-			else
-			{
-				if (cpu_info.bSSSE3)
-				{
-					MOVQ_xmm(XMM0, MComplex(RMEM, addr, SCALE_1, loadOffset));
-					PSHUFB(XMM0, M(pbswapShuffle2x4));
-				}
-				else
-				{
-					LoadAndSwap(64, RSCRATCH, MComplex(RMEM, addr, SCALE_1, loadOffset));
-					ROL(64, R(RSCRATCH), Imm8(32));
-					MOVQ_xmm(XMM0, R(RSCRATCH));
-				}
-			}
-			CVTPS2PD(fpr.RX(s), R(XMM0));
-		}
-		else
-		{
-			BitSet32 registersInUse = CallerSavedRegistersInUse();
-			registersInUse[fpr.RX(s) << 16] = false;
-			if (update)
-				registersInUse[addr] = true;
-			SafeLoadToReg(RSCRATCH, R(addr), w ? 32 : 64, loadOffset, registersInUse, false);
-			MemoryExceptionCheck();
-			if (w)
-			{
-				MOVD_xmm(XMM0, R(RSCRATCH));
-				UNPCKLPS(XMM0, M(m_one));
-			}
-			else
-			{
-				ROL(64, R(RSCRATCH), Imm8(32));
-				MOVQ_xmm(XMM0, R(RSCRATCH));
-			}
-			CVTPS2PD(fpr.RX(s), R(XMM0));
-			if (update)
-				MOV(32, gpr.R(a), R(addr));
-		}
-		gpr.UnlockAll();
-		fpr.UnlockAll();
-		return;
-	}
 	gpr.FlushLockX(RSCRATCH_EXTRA);
 	gpr.BindToRegister(a, true, update);
 	fpr.BindToRegister(s, false, true);
@@ -370,7 +180,6 @@ void Jit64::psq_lXX(UGeckoInstruction inst)
 		CALLptr(MScaled(RSCRATCH, SCALE_8, (u32)(u64)(&asm_routines.pairedLoadQuantized[w * 8])));
 	}
 
-	MemoryExceptionCheck();
 	CVTPS2PD(fpr.RX(s), R(XMM0));
 	if (update && jo.memcheck)
 	{
