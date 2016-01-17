d4b93b12954a6ba104e912ee6655dfc2fe5673c1
TinyChironex <Absolome@gmail.com>
2016-01-17 01:36:05 -0500

    Update SymbolDB.cpp

--- Source/Core/Common/SymbolDB.cpp
+++ Source/Core/Common/SymbolDB.cpp
@@ -44,38 +44,33 @@ void SymbolDB::PopulateSymbolComments()
 	std::string filename = File::GetUserPath(D_MAPS_IDX) + SConfig::GetInstance().m_strUniqueID + "_comments.txt";
 	if (!File::Exists(filename))
 		return;
-	std::fstream line_parse;
-	line_parse.open(filename);
+	std::fstream line_parse(filename);
 	std::string line;
 	u32 address;
-	while(!(line_parse>>std::ws).eof())
+	while (std::getline(line_parse, line))
 	{
-		std::getline(line_parse, line);
 		if (line.empty())
 			continue;
-		AsciiToHex(line.substr(0, 8),address);
-		std::string comment = line.substr(9, std::string::npos);
-		if (address < 0x80000010)
-			continue;
+		AsciiToHex(line.substr(0,8),address);
+		std::string comment = line.substr(9);
 		Symbol* f = GetSymbolFromAddr(address);
 		if (f)
 		{
-			char Offset = (address - f->address)/4;
-			f->comments[Offset] = comment;
+			char offset = (address - f->address) / 4;
+			f->comments[offset] = comment;
 		}
 	}
 }
 
-const std::string SymbolDB::GetCommentFromAddr(u32 addr)
+std::string SymbolDB::GetCommentFromAddr(u32 address)
 {
-	Symbol *symbol = GetSymbolFromAddr(addr);
+	Symbol* symbol = GetSymbolFromAddr(address);
 	if (symbol)
 	{
-		unsigned int Offset = (addr - symbol->address)/4;
-		return symbol->comments[Offset];
+		unsigned int offset = (address - symbol->address) / 4;
+		return symbol->comments[offset];
 	}
-	else
-		return "";
+	return "";
 }
 
 Symbol* SymbolDB::GetSymbolFromName(const std::string& name)
