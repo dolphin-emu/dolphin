40698bb2359a7bf74d29eecc8b0a324da9116c3d
sigmabeta <bastos.eder@gmail.com>
2016-01-17 13:42:22 -0500

    Implement Java-based Config file read/save

---
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/model/settings/IntSetting.java
@@ -0,0 +1,28 @@
+package org.dolphinemu.dolphinemu.model.settings;
+
+public final class IntSetting extends Setting
+{
+	private int mValue;
+
+	public IntSetting(String key, SettingSection section, int value)
+	{
+		super(key, section);
+		mValue = value;
+	}
+
+	public int getValue()
+	{
+		return mValue;
+	}
+
+	public void setValue(int value)
+	{
+		mValue = value;
+	}
+
+	@Override
+	public String getValueAsString()
+	{
+		return Integer.toString(mValue);
+	}
+}
