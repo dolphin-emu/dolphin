f6fac29aec1edf173eb38d0a44f8a348fad72c63
Pierre Bourdon <delroth@gmail.com>
2016-01-18 02:08:17 +0100

    DSPLLE: Remove deprecated ReadAnnotatedAssembly
    It has some fairly terrible code doing manual string parsing through char*
    manipulation, and frankly I'd be amazed if anyone has used this for the past 5
    years. I've been the main person doing DSP stuff for the past 3+ years and I
    have never had a use for this.
    
    That's also one less user of IOFile::GetHandle().

--- Source/Core/Core/HW/DSPLLE/DSPHost.cpp
+++ Source/Core/Core/HW/DSPLLE/DSPHost.cpp
@@ -64,31 +64,10 @@ void CodeLoaded(const u8 *ptr, int size)
 
 	DSPSymbols::Clear();
 
-	// Auto load text file - if none just disassemble.
-
 	NOTICE_LOG(DSPLLE, "g_dsp.iram_crc: %08x", g_dsp.iram_crc);
 
 	DSPSymbols::Clear();
-	bool success = false;
-	switch (g_dsp.iram_crc)
-	{
-		case 0x86840740: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_Zelda.txt"); break;
-		case 0x42f64ac4: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_Luigi.txt"); break;
-		case 0x07f88145: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_AX_07F88145.txt"); break;
-		case 0x3ad3b7ac: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_AX_3AD3B7AC.txt"); break;
-		case 0x3daf59b9: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_AX_3DAF59B9.txt"); break;
-		case 0x4e8a8b21: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_AX_4E8A8B21.txt"); break;
-		case 0xe2136399: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_AX_E2136399.txt"); break;
-		case 0xdd7e72d5: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_GBA.txt"); break;
-		case 0x347112BA: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_AXWii.txt"); break;
-		case 0xD643001F: success = DSPSymbols::ReadAnnotatedAssembly("../../docs/DSP/DSP_UC_SuperMarioGalaxy.txt"); break;
-		default: success = false; break;
-	}
-
-	if (!success)
-	{
-		DSPSymbols::AutoDisassembly(0x0, 0x1000);
-	}
+	DSPSymbols::AutoDisassembly(0x0, 0x1000);
 
 	// Always add the ROM.
 	DSPSymbols::AutoDisassembly(0x8000, 0x9000);
