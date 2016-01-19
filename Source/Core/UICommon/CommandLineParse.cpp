5d4aeaff008435c8a96ab2a28c4ea91515b1a93f
Ryan Houdek <Sonicadvance1@gmail.com>
2016-01-19 08:15:48 -0600

    Implement command line option setting

--- Source/Core/UICommon/CommandLineParse.cpp
+++ Source/Core/UICommon/CommandLineParse.cpp
@@ -2,10 +2,60 @@
 // Licensed under GPLv2+
 // Refer to the license.txt file included.
 
+#include <sstream>
+
+#include "Common/OnionConfig.h"
+
 #include "UICommon/CommandLineParse.h"
 
 namespace CommandLineParse
 {
+class CommandLineConfigLayerLoader : public OnionConfig::ConfigLayerLoader
+{
+public:
+  CommandLineConfigLayerLoader(const std::list<std::string>& args, const std::string& video_backend,
+                               const std::string& audio_backend)
+      : ConfigLayerLoader(OnionConfig::OnionLayerType::LAYER_COMMANDLINE)
+  {
+    if (video_backend.size())
+      m_values.emplace_back(std::make_tuple("Dolphin", "Core", "GFXBackend", video_backend));
+
+    if (audio_backend.size())
+      m_values.emplace_back(
+          std::make_tuple("Dolphin", "Core", "DSPHLE", audio_backend == "HLE" ? "True" : "False"));
+
+    // Arguments are in the format of <System>.<Petal>.<Key>=Value
+    for (const auto& arg : args)
+    {
+      std::istringstream buffer(arg);
+      std::string system, petal, key, value;
+      std::getline(buffer, system, '.');
+      std::getline(buffer, petal, '.');
+      std::getline(buffer, key, '=');
+      std::getline(buffer, value, '=');
+      m_values.emplace_back(std::make_tuple(system, petal, key, value));
+    }
+  }
+
+  void Load(OnionConfig::BloomLayer* config_layer) override
+  {
+    for (auto& value : m_values)
+    {
+      OnionConfig::OnionPetal* petal = config_layer->GetOrCreatePetal(
+          OnionConfig::GetSystemFromName(std::get<0>(value)), std::get<1>(value));
+      petal->Set(std::get<2>(value), std::get<3>(value));
+    }
+  }
+
+  void Save(OnionConfig::BloomLayer* config_layer) override
+  {
+    // Save Nothing
+  }
+
+private:
+  std::list<std::tuple<std::string, std::string, std::string, std::string>> m_values;
+};
+
 std::unique_ptr<optparse::OptionParser> CreateParser(bool gui)
 {
   std::unique_ptr<optparse::OptionParser> parser(new optparse::OptionParser());
@@ -20,6 +70,11 @@ std::unique_ptr<optparse::OptionParser> CreateParser(bool gui)
       .metavar("<file>")
       .type("string")
       .help("Load the specified file");
+  parser->add_option("-C", "--config")
+      .action("append")
+      .metavar("<System>.<Petal>.<Key>=<Value>")
+      .type("string")
+      .help("Set a configuration option");
 
   if (gui)
   {
@@ -29,7 +84,8 @@ std::unique_ptr<optparse::OptionParser> CreateParser(bool gui)
     parser->add_option("-c", "--confirm").action("store_true").help("Set Confirm on Stop");
   }
 
-  // XXX: These two are setting configuration options
+  parser->set_defaults("video_backend", "");
+  parser->set_defaults("audio_emulation", "");
   parser->add_option("-v", "--video_backend").action("store").help("Specify a video backend");
   parser->add_option("-a", "--audio_emulation")
       .choices({"HLE", "LLE"})
@@ -40,6 +96,14 @@ std::unique_ptr<optparse::OptionParser> CreateParser(bool gui)
 
 optparse::Values& ParseArguments(optparse::OptionParser* parser, int argc, char** argv)
 {
-  return parser->parse_args(argc, argv);
+  optparse::Values& options = parser->parse_args(argc, argv);
+
+  const std::list<std::string>& config_args = options.all("config");
+  if (config_args.size())
+    OnionConfig::AddLayer(
+        new CommandLineConfigLayerLoader(config_args, (const char*)options.get("video_backend"),
+                                         (const char*)options.get("audio_emulation")));
+
+  return options;
 }
 }
