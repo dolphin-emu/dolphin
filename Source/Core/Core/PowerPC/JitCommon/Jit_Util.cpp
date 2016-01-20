a7a744d33ce042b3ed72775de393149bee63663f
Chris Burgener <christhecoolist@gmail.com>
2016-01-19 19:29:19 -0500

    Properly clear JIT cache on save states

--- Source/Core/Core/PowerPC/JitCommon/Jit_Util.cpp
+++ Source/Core/Core/PowerPC/JitCommon/Jit_Util.cpp
@@ -1014,3 +1014,11 @@ void EmuCodeBlock::JitClearCA()
 {
 	MOV(8, PPCSTATE(xer_ca), Imm8(0));
 }
+
+void EmuCodeBlock::Clear()
+{
+	registersInUseAtLoc.clear();
+	pcAtLoc.clear();
+	exceptionHandlerAtLoc.clear();
+}
+
