b93609e1ffaeaf3faf6f8fde4e9efb45efd21826
Ryan Houdek <Sonicadvance1@gmail.com>
2016-01-27 18:09:49 -0600

    Movie configuration loader

---
+++ Source/Core/Core/ConfigLoaders/MovieConfigLoader.h
@@ -0,0 +1,35 @@
+// Copyright 2016 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <memory>
+
+#include "Common/Config/Config.h"
+
+namespace Movie
+{
+struct DTMHeader;
+}
+
+namespace ConfigLoaders
+{
+class MovieConfigLayerLoader final : public Config::ConfigLayerLoader
+{
+public:
+  explicit MovieConfigLayerLoader(Movie::DTMHeader* header)
+      : ConfigLayerLoader(Config::LayerType::Movie), m_header(header)
+  {
+  }
+
+  void Load(Config::Layer* config_layer) override;
+  void Save(Config::Layer* config_layer) override;
+
+  void ChangeDTMHeader(Movie::DTMHeader* header) { m_header = header; }
+private:
+  Movie::DTMHeader* m_header;
+};
+
+std::unique_ptr<Config::ConfigLayerLoader> GenerateMovieConfigLoader(Movie::DTMHeader* header);
+}
