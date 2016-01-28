8d6dacad60cb6cf591c631fabbfb6cfb1a906c9a
Ryan Houdek <Sonicadvance1@gmail.com>
2016-01-27 18:08:58 -0600

    Base configuration loader

---
+++ Source/Core/Core/OnionCoreLoaders/BaseConfigLoader.h
@@ -0,0 +1,9 @@
+// Copyright 2016 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include "Common/OnionConfig.h"
+
+OnionConfig::ConfigLayerLoader* GenerateBaseConfigLoader();
