62168708eaf6284348e8e3180cabf2ce4fcd3de4
sigmabeta <sigmabeta@users.noreply.github.com>
2016-01-30 22:34:23 -0500

    Add some base UI classes

---
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/ui/BaseActivity.java
@@ -0,0 +1,33 @@
+package org.dolphinemu.dolphinemu.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.dolphinemu.dolphinemu.application.DolphinApplication;
+import org.dolphinemu.dolphinemu.utils.Log;
+
+
+public abstract class BaseActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		if (DolphinApplication.appComponent == null)
+		{
+			Log.error("[BaseActivity] AppComponent null.");
+		}
+		inject();
+	}
+
+	void showToastMessage(String message)
+	{
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+	}
+
+	protected abstract void inject();
+}
---
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/ui/FragmentContainer.java
@@ -0,0 +1,6 @@
+package org.dolphinemu.dolphinemu.ui;
+
+public interface FragmentContainer
+{
+	void setActivityTitle(String title);
+}
