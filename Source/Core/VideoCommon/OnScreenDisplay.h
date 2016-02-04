e3f160d1554eaf5933d868703c1e577611e33eb8
Aestek <thib.gilles@gmail.com>
2016-02-04 08:35:44 +0100

    Add AddTypedMessage to OSD namespace
    AddTypedMessage behaves like AddMessage but takes a MessageType as first argument.
    Invoking AddNamedMessage multiple time will replace then previous message of same
    type instead of appending it.
    Type is hardtyped to MessageType enums to prevent conflicts and suggest consistent
    naming.
    
    Also add message duration and color enums.

--- Source/Core/VideoCommon/OnScreenDisplay.h
+++ Source/Core/VideoCommon/OnScreenDisplay.h
@@ -11,8 +11,44 @@
 
 namespace OSD
 {
+
+struct Message
+{
+	Message() {}
+	Message(const std::string& s, u32 ts, u32 rgba) : m_str(s), m_timestamp(ts), m_rgba(rgba) {}
+
+	std::string m_str;
+	u32 m_timestamp;
+	u32 m_rgba;
+};
+
+enum class MessageType {
+	NetPlayPing,
+
+	// This entry must be kept last so that persistent typed messages are displayed before other messages
+	Typeless,
+};
+
+namespace Color
+{
+constexpr u32 CYAN   = 0xFF00FFFF;
+constexpr u32 GREEN  = 0xFF00FF00;
+constexpr u32 RED    = 0xFFFF0000;
+constexpr u32 YELLOW = 0xFFFFFF30;
+};
+
+namespace Duration
+{
+constexpr u32 SHORT     = 2000;
+constexpr u32 NORMAL    = 5000;
+constexpr u32 VERY_LONG = 10000;
+};
+
 // On-screen message display (colored yellow by default)
-void AddMessage(const std::string& str, u32 ms = 2000, u32 rgba = 0xFFFFFF30);
+void AddMessage(const std::string& message, u32 ms = Duration::SHORT, u32 rgba = Color::YELLOW);
+void AddTypedMessage(MessageType type, const std::string &message, u32 ms = Duration::SHORT,
+					 u32 rgba = Color::YELLOW);
+void DrawMessage(const Message& msg, int top, int left, int time_left); // draw one message
 void DrawMessages(); // draw the current messages on the screen. Only call once per frame.
 void ClearMessages();
 
