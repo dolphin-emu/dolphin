aceb943dd3d80cb26633c06fd7f62b369951e591
Aestek <thib.gilles@gmail.com>
2016-02-04 08:40:09 +0100

    Log netplay ping to OSD

--- Source/Core/VideoCommon/VideoConfig.cpp
+++ Source/Core/VideoCommon/VideoConfig.cpp
@@ -60,6 +60,7 @@ void VideoConfig::Load(const std::string& ini_file)
 	settings->Get("UseRealXFB", &bUseRealXFB, 0);
 	settings->Get("SafeTextureCacheColorSamples", &iSafeTextureCache_ColorSamples, 128);
 	settings->Get("ShowFPS", &bShowFPS, false);
+	settings->Get("ShowNetPlayPing", &bShowNetPlayPing, false);
 	settings->Get("LogRenderTimeToFile", &bLogRenderTimeToFile, false);
 	settings->Get("OverlayStats", &bOverlayStats, false);
 	settings->Get("OverlayProjStats", &bOverlayProjStats, false);
@@ -271,6 +272,7 @@ void VideoConfig::Save(const std::string& ini_file)
 	settings->Set("UseRealXFB", bUseRealXFB);
 	settings->Set("SafeTextureCacheColorSamples", iSafeTextureCache_ColorSamples);
 	settings->Set("ShowFPS", bShowFPS);
+	settings->Set("ShowNetPlayPing", bShowNetPlayPing);
 	settings->Set("LogRenderTimeToFile", bLogRenderTimeToFile);
 	settings->Set("OverlayStats", bOverlayStats);
 	settings->Set("OverlayProjStats", bOverlayProjStats);
