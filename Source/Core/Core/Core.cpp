22bfc23e6507d379645360c1d9347216ad8f1c96
Lioncash <mathew1800@gmail.com>
2016-02-04 23:05:58 -0500

    Core: Use the thread_local keyword
    Android and OSX are still twiddling their thumbs though,
    so we have to keep pthreads around for them.

--- Source/Core/Core/Core.cpp
+++ Source/Core/Core/Core.cpp
@@ -69,15 +69,11 @@
 #include "VideoCommon/RenderBase.h"
 #include "VideoCommon/VideoBackendBase.h"
 
-// This can mostly be removed when we move to VS2015
-// to use the thread_local keyword
-#ifdef _MSC_VER
-#define ThreadLocalStorage __declspec(thread)
-#elif defined __ANDROID__ || defined __APPLE__
+#if defined __ANDROID__ || defined __APPLE__
 // This will most likely have to stay, to support android
 #include <pthread.h>
-#else // Everything besides VS and Android
-#define ThreadLocalStorage __thread
+#else // Everything besides OSX and Android
+#define ThreadLocalStorage thread_local
 #endif
 
 namespace Core
