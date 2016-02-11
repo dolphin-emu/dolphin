0d9b80b181578e6ac194f454a4383defae0d052f
Ryan Houdek <Sonicadvance1@gmail.com>
2016-02-10 19:38:13 -0600

    Initialize base config in UICommon

--- Source/Core/UICommon/UICommon.cpp
+++ Source/Core/UICommon/UICommon.cpp
@@ -8,10 +8,12 @@
 
 #include "Common/CommonPaths.h"
 #include "Common/FileUtil.h"
+#include "Common/OnionConfig.h"
 #include "Common/Logging/LogManager.h"
 
 #include "Core/ConfigManager.h"
 #include "Core/HW/Wiimote.h"
+#include "Core/OnionCoreLoaders/BaseConfigLoader.h"
 
 #include "InputCommon/GCAdapter.h"
 
@@ -24,6 +26,9 @@ namespace UICommon
 
 void Init()
 {
+	OnionConfig::Init();
+	OnionConfig::AddLayer(GenerateBaseConfigLoader());
+
 	LogManager::Init();
 	SConfig::Init();
 	VideoBackendBase::PopulateList();
@@ -41,6 +46,7 @@ void Shutdown()
 	VideoBackendBase::ClearList();
 	SConfig::Shutdown();
 	LogManager::Shutdown();
+	OnionConfig::Shutdown();
 }
 
 void CreateDirectories()
