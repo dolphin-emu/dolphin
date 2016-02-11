7c7170a3b78c390794e061dbfc9979bc96f92454
Ryan Houdek <Sonicadvance1@gmail.com>
2016-02-10 20:11:24 -0600

    [WX] Change GeckoCodeDiag over to OnionConfig

--- Source/Core/DolphinWX/Cheats/GeckoCodeDiag.cpp
+++ Source/Core/DolphinWX/Cheats/GeckoCodeDiag.cpp
@@ -93,13 +93,15 @@ void CodeConfigPanel::UpdateCodeList(bool checkRunning)
 	UpdateInfoBox(evt);
 }
 
-void CodeConfigPanel::LoadCodes(const IniFile& globalIni, const IniFile& localIni, const std::string& gameid, bool checkRunning)
+void CodeConfigPanel::LoadCodes(OnionConfig::BloomLayer* global_config,
+                                OnionConfig::BloomLayer* local_config,
+                                const std::string& gameid, bool checkRunning)
 {
 	m_gameid = gameid;
 
 	m_gcodes.clear();
 	if (!checkRunning || Core::IsRunning())
-		Gecko::LoadCodes(globalIni, localIni, m_gcodes);
+		Gecko::LoadCodes(global_config, local_config, m_gcodes);
 
 	UpdateCodeList(checkRunning);
 }
