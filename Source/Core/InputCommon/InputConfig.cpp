e65fc7b1e292553356e42b74257637fc882945ab
Ryan Houdek <Sonicadvance1@gmail.com>
2016-02-10 20:12:54 -0600

    [WIP] Change InputConfig over to OnionConfig

--- Source/Core/InputCommon/InputConfig.cpp
+++ Source/Core/InputCommon/InputConfig.cpp
@@ -5,7 +5,8 @@
 #include <vector>
 
 #include "Common/FileUtil.h"
-#include "Common/IniFile.h"
+#include "Common/OnionConfig.h"
+
 #include "Core/ConfigManager.h"
 #include "Core/HW/Wiimote.h"
 #include "InputCommon/ControllerEmu.h"
@@ -14,93 +15,36 @@
 
 bool InputConfig::LoadConfig(bool isGC)
 {
-	IniFile inifile;
-	bool useProfile[MAX_BBMOTES] = {false, false, false, false, false};
-	std::string num[MAX_BBMOTES] = {"1", "2", "3", "4", "BB"};
-	std::string profile[MAX_BBMOTES];
-	std::string path;
+	m_controllers[0]->LoadDefaults(g_controller_interface);
+	m_controllers[0]->UpdateReferences(g_controller_interface);
 
-	if (SConfig::GetInstance().GetUniqueID() != "00000000")
+	for (auto& controller : m_controllers)
 	{
-		std::string type;
-		if (isGC)
+		OnionConfig::OnionSystem system[] =
 		{
-			type = "Pad";
-			path = "Profiles/GCPad/";
-		}
-		else
-		{
-			type = "Wiimote";
-			path = "Profiles/Wiimote/";
-		}
+			OnionConfig::OnionSystem::SYSTEM_GCPAD,
+			OnionConfig::OnionSystem::SYSTEM_WIIPAD,
+		};
+		controller->LoadConfig(OnionConfig::GetOrCreatePetal(system[isGC ? 1 : 0], controller->GetName()));
 
-		IniFile game_ini = SConfig::GetInstance().LoadGameIni();
-		IniFile::Section* control_section = game_ini.GetOrCreateSection("Controls");
-
-		for (int i = 0; i < 4; i++)
-		{
-			if (control_section->Exists(type + "Profile" + num[i]))
-			{
-				if (control_section->Get(type + "Profile" + num[i], &profile[i]))
-				{
-					if (File::Exists(File::GetUserPath(D_CONFIG_IDX) + path + profile[i] + ".ini"))
-					{
-						useProfile[i] = true;
-					}
-					else
-					{
-						// TODO: PanicAlert shouldn't be used for this.
-						PanicAlertT("Selected controller profile does not exist");
-					}
-				}
-			}
-		}
+		// Update refs
+		controller->UpdateReferences(g_controller_interface);
 	}
-
-	if (inifile.Load(File::GetUserPath(D_CONFIG_IDX) + m_ini_name + ".ini"))
-	{
-		int n = 0;
-		for (auto& controller : m_controllers)
-		{
-			// Load settings from ini
-			if (useProfile[n])
-			{
-				IniFile profile_ini;
-				profile_ini.Load(File::GetUserPath(D_CONFIG_IDX) + path + profile[n] + ".ini");
-				controller->LoadConfig(profile_ini.GetOrCreateSection("Profile"));
-			}
-			else
-			{
-				controller->LoadConfig(inifile.GetOrCreateSection(controller->GetName()));
-			}
-
-			// Update refs
-			controller->UpdateReferences(g_controller_interface);
-
-			// Next profile
-			n++;
-		}
-		return true;
-	}
-	else
-	{
-		m_controllers[0]->LoadDefaults(g_controller_interface);
-		m_controllers[0]->UpdateReferences(g_controller_interface);
-		return false;
-	}
+	return true;
 }
 
 void InputConfig::SaveConfig()
 {
-	std::string ini_filename = File::GetUserPath(D_CONFIG_IDX) + m_ini_name + ".ini";
-
-	IniFile inifile;
-	inifile.Load(ini_filename);
-
-	for (auto& controller : m_controllers)
-		controller->SaveConfig(inifile.GetOrCreateSection(controller->GetName()));
-
-	inifile.Save(ini_filename);
+	// XXX:
+//	std::string ini_filename = File::GetUserPath(D_CONFIG_IDX) + m_ini_name + ".ini";
+//
+//	IniFile inifile;
+//	inifile.Load(ini_filename);
+//
+//	for (auto& controller : m_controllers)
+//		controller->SaveConfig(inifile.GetOrCreateSection(controller->GetName()));
+//
+//	inifile.Save(ini_filename);
 }
 
 ControllerEmu* InputConfig::GetController(int index)
