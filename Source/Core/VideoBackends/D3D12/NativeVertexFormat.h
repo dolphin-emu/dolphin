1aa619a5bbf53e18f6c286c72b034f16531f0307
Lioncash <mathew1800@gmail.com>
2016-02-15 23:11:43 -0500

    NativeVertexFormat: Use in-class initialization

--- Source/Core/VideoBackends/D3D12/NativeVertexFormat.h
+++ Source/Core/VideoBackends/D3D12/NativeVertexFormat.h
@@ -12,11 +12,6 @@ namespace DX12
 
 class D3DVertexFormat final : public NativeVertexFormat
 {
-	std::array<D3D12_INPUT_ELEMENT_DESC, 15> m_elems;
-	UINT m_num_elems;
-
-	D3D12_INPUT_LAYOUT_DESC m_layout12;
-
 public:
 	D3DVertexFormat(const PortableVertexDeclaration& vtx_decl);
 	~D3DVertexFormat();
@@ -27,5 +22,10 @@ public:
 
 private:
 	void AddInputElementDescFromAttributeFormatIfValid(const AttributeFormat* format, const char* semantic_name, unsigned int semantic_index);
+
+	std::array<D3D12_INPUT_ELEMENT_DESC, 15> m_elems{};
+	UINT m_num_elems = 0;
+
+	D3D12_INPUT_LAYOUT_DESC m_layout12{};
 };
 }
