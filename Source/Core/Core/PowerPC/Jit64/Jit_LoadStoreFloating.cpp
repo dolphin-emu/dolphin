d8741cbc59dd2e088b2de0ea871dde445b0ae28e
Matt Mastracci <matthew@mastracci.com>
2016-02-28 14:33:54 -0700

    [C++ regcache] Jit_LoadStoreFloating

--- Source/Core/Core/PowerPC/Jit64/Jit_LoadStoreFloating.cpp
+++ Source/Core/Core/PowerPC/Jit64/Jit_LoadStoreFloating.cpp
@@ -24,67 +24,29 @@ void Jit64::lfXXX(UGeckoInstruction inst)
 	bool update = indexed ? !!(inst.SUBOP10 & 0x20) : !!(inst.OPCD & 1);
 	bool single = indexed ? !(inst.SUBOP10 & 0x40) : !(inst.OPCD & 2);
 	update &= indexed || inst.SIMM_16;
-
 	int d = inst.RD;
 	int a = inst.RA;
 	int b = inst.RB;
+	int accessSize = single ? 32 : 64;
 
-	FALLBACK_IF(!indexed && !a);
+	auto ra = a ? regs.gpr.Lock(a) : regs.gpr.Zero();
+	auto rb = indexed ? regs.gpr.Lock(b) : regs.gpr.Imm32((u32)(s32)(s16)inst.SIMM_16);
+	auto rd = regs.fpu.Lock(d);
 
-	gpr.BindToRegister(a, true, update);
+	auto val = regs.gpr.BorrowAnyBut(BitSet32{ RSCRATCH });
+	SafeLoad(val, ra, rb, accessSize, false, true, update);
 
-	s32 offset = 0;
-	OpArg addr = gpr.R(a);
-	if (update && jo.memcheck)
-	{
-		addr = R(RSCRATCH2);
-		MOV(32, addr, gpr.R(a));
-	}
-	if (indexed)
-	{
-		if (update)
-		{
-			ADD(32, addr, gpr.R(b));
-		}
-		else
-		{
-			addr = R(RSCRATCH2);
-			MOV_sum(32, RSCRATCH2, a ? gpr.R(a) : Imm32(0), gpr.R(b));
-		}
-	}
-	else
-	{
-		if (update)
-			ADD(32, addr, Imm32((s32)(s16)inst.SIMM_16));
-		else
-			offset = (s16)inst.SIMM_16;
-	}
-
-	fpr.Lock(d);
-	if (jo.memcheck && single)
-	{
-		fpr.StoreFromRegister(d);
-		js.revertFprLoad = d;
-	}
-	fpr.BindToRegister(d, !single);
-	BitSet32 registersInUse = CallerSavedRegistersInUse();
-	if (update && jo.memcheck)
-		registersInUse[RSCRATCH2] = true;
-	SafeLoadToReg(RSCRATCH, addr, single ? 32 : 64, offset, registersInUse, false, 0, RSCRATCH);
-
+	auto xd = rd.BindWriteAndReadIf(!single);
 	if (single)
 	{
-		ConvertSingleToDouble(fpr.RX(d), RSCRATCH, true);
+		ConvertSingleToDouble(xd, val, regs.gpr.Borrow(), true);
 	}
 	else
 	{
-		MOVQ_xmm(XMM0, R(RSCRATCH));
-		MOVSD(fpr.RX(d), R(XMM0));
+		auto fscratch = regs.fpu.Borrow();
+		MOVQ_xmm((X64Reg)fscratch, R(val));
+		MOVSD(xd, R(fscratch));
 	}
-	if (update && jo.memcheck)
-		MOV(32, gpr.R(a), addr);
-	fpr.UnlockAll();
-	gpr.UnlockAll();
 }
 
 void Jit64::stfXXX(UGeckoInstruction inst)
@@ -99,88 +61,42 @@ void Jit64::stfXXX(UGeckoInstruction inst)
 	int s = inst.RS;
 	int a = inst.RA;
 	int b = inst.RB;
-	s32 imm = (s16)inst.SIMM_16;
 	int accessSize = single ? 32 : 64;
 
-	FALLBACK_IF(update && jo.memcheck && a == b);
+	auto val = regs.gpr.Borrow();
+
+	auto ra = a ? regs.gpr.Lock(a) : regs.gpr.Zero();
+	auto rb = indexed ? regs.gpr.Lock(b) : regs.gpr.Imm32((u32)(s32)(s16)inst.SIMM_16);
+	auto rs = regs.fpu.Lock(s);
 
 	if (single)
 	{
+		auto fscratch = regs.fpu.Borrow();
 		if (jit->js.op->fprIsStoreSafe[s])
 		{
-			CVTSD2SS(XMM0, fpr.R(s));
+			CVTSD2SS(fscratch, rs);
 		}
 		else
 		{
-			fpr.BindToRegister(s, true, false);
-			ConvertDoubleToSingle(XMM0, fpr.RX(s));
+			auto xs = rs.Bind(BindMode::Read);
+			ConvertDoubleToSingle(fscratch, xs);
 		}
-		MOVD_xmm(R(RSCRATCH), XMM0);
+		MOVD_xmm(R(val), fscratch);
 	}
 	else
 	{
-		if (fpr.R(s).IsSimpleReg())
-			MOVQ_xmm(R(RSCRATCH), fpr.RX(s));
-		else
-			MOV(64, R(RSCRATCH), fpr.R(s));
-	}
-
-	if (!indexed && (!a || gpr.R(a).IsImm()))
-	{
-		u32 addr = (a ? gpr.R(a).Imm32() : 0) + imm;
-		bool exception = WriteToConstAddress(accessSize, R(RSCRATCH), addr, CallerSavedRegistersInUse());
-
-		if (update)
+		if (rs.IsRegBound())
 		{
-			if (!jo.memcheck || !exception)
-			{
-				gpr.SetImmediate32(a, addr);
-			}
-			else
-			{
-				gpr.KillImmediate(a, true, true);
-				MemoryExceptionCheck();
-				ADD(32, gpr.R(a), Imm32((u32)imm));
-			}
-		}
-		fpr.UnlockAll();
-		gpr.UnlockAll();
-		return;
-	}
-
-	s32 offset = 0;
-	if (update)
-		gpr.BindToRegister(a, true, true);
-	if (indexed)
-	{
-		MOV_sum(32, RSCRATCH2, a ? gpr.R(a) : Imm32(0), gpr.R(b));
-	}
-	else
-	{
-		if (update)
-		{
-			LEA(32, RSCRATCH2, MDisp(gpr.RX(a), imm));
+			auto xs = rs.Bind(BindMode::Reuse);
+			MOVQ_xmm(R(val), xs);
 		}
 		else
 		{
-			offset = imm;
-			MOV(32, R(RSCRATCH2), gpr.R(a));
+			MOV(64, val, rs);
 		}
 	}
 
-	BitSet32 registersInUse = CallerSavedRegistersInUse();
-	// We need to save the (usually scratch) address register for the update.
-	if (update)
-		registersInUse[RSCRATCH2] = true;
-
-	SafeWriteRegToReg(RSCRATCH, RSCRATCH2, accessSize, offset, registersInUse);
-
-	if (update)
-		MOV(32, gpr.R(a), R(RSCRATCH2));
-
-	fpr.UnlockAll();
-	gpr.UnlockAll();
-	gpr.UnlockAllX();
+	SafeWrite(val, ra, rb, accessSize, true, update);
 }
 
 // This one is a little bit weird; it stores the low 32 bits of a double without converting it
@@ -193,12 +109,25 @@ void Jit64::stfiwx(UGeckoInstruction inst)
 	int a = inst.RA;
 	int b = inst.RB;
 
-	MOV_sum(32, RSCRATCH2, a ? gpr.R(a) : Imm32(0), gpr.R(b));
+	auto ra = a ? regs.gpr.Lock(a) : regs.gpr.Zero();
+	auto rb = regs.gpr.Lock(b);
+	auto rs = regs.fpu.Lock(s);
 
-	if (fpr.R(s).IsSimpleReg())
-		MOVD_xmm(R(RSCRATCH), fpr.RX(s));
+	// We could optimize this to avoid using a temp
+	auto addr = regs.gpr.Borrow();
+	auto val = regs.gpr.Borrow();
+
+	MOV_sum(32, addr, ra, rb);
+
+	if (rs.IsRegBound())
+	{
+		auto xs = rs.Bind(BindMode::Reuse);
+		MOVD_xmm(R(val), xs);
+	}
 	else
-		MOV(32, R(RSCRATCH), fpr.R(s));
-	SafeWriteRegToReg(RSCRATCH, RSCRATCH2, 32, 0, CallerSavedRegistersInUse());
-	gpr.UnlockAllX();
+	{
+		MOV(32, val, rs);
+	}
+
+	SafeWriteRegToReg(R(val), addr, 32, 0, CallerSavedRegistersInUse());
 }
