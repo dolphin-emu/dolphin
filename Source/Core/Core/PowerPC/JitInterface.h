db431ad06ad1169d0dbfabac015e6c738c406373
Matt Mastracci <matthew@mastracci.com>
2016-02-28 14:33:54 -0700

    [C++ regcache] Jit_LoadStore

--- Source/Core/Core/PowerPC/JitInterface.h
+++ Source/Core/Core/PowerPC/JitInterface.h
@@ -44,6 +44,8 @@ void InvalidInstruction();
 // If "forced" is true, a recompile is being requested on code that hasn't been modified.
 void InvalidateICache(u32 address, u32 size, bool forced);
 
+void InvalidateICacheLine(u32 address);
+
 void CompileExceptionCheck(ExceptionType type);
 
 void Shutdown();
