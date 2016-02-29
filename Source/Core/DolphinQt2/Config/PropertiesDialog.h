5fb4f0c967426b0bd7e019b85be5d60eb620011e
Rukai <rubickent@gmail.com>
2016-02-29 19:52:15 +1100

    GCVolume: supports reading all opening.bnr information
    DQT2: Game properties dialog contains info tab giving information about the selected iso.

---
+++ Source/Core/DolphinQt2/Config/PropertiesDialog.h
@@ -0,0 +1,16 @@
+// Copyright 2016 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <QDialog>
+
+#include "DolphinQt2/GameList/GameFile.h"
+
+class PropertiesDialog final : public QDialog
+{
+	Q_OBJECT
+public:
+	explicit PropertiesDialog(QWidget* parent, const GameFile& game);
+};
