1b65adcd5677cad527b48e9fc58cd934d858aa8e
Rukai <rubickent@gmail.com>
2016-03-01 21:44:44 +1100

    Fully remove wii remote netplay UI code and remove reference to WiimoteReal in NetPlayClient.cpp

--- Source/Core/DolphinWX/NetPlay/PadMapDialog.h
+++ Source/Core/DolphinWX/NetPlay/PadMapDialog.h
@@ -20,13 +20,11 @@ public:
 	PadMapDialog(wxWindow* parent, NetPlayServer* server, NetPlayClient* client);
 
 	PadMappingArray GetModifiedPadMappings() const;
-	PadMappingArray GetModifiedWiimoteMappings() const;
 
 private:
 	void OnAdjust(wxCommandEvent& event);
 
-	wxChoice* m_map_cbox[8];
+	wxChoice* m_map_cbox[4];
 	PadMappingArray m_pad_mapping;
-	PadMappingArray m_wii_mapping;
 	std::vector<const Player*> m_player_list;
 };
