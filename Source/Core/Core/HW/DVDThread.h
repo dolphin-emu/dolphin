141bc9288c21bd52e43ab1c65f6ff9b9d7c276dc
Matt Mastracci <matthew@mastracci.com>
2016-03-02 17:36:11 -0700

    DVD read chunking Splits DVD reads up into smaller chunks so that data is available before the final interrupt is triggered. This better simulates the DMA that happens on a real device, which some games will take advantage of - by either playing back data as it is loading or by using data that is going to be overwritten shortly by an outstanding read.

--- Source/Core/Core/HW/DVDThread.h
+++ Source/Core/Core/HW/DVDThread.h
@@ -14,6 +14,9 @@ void Stop();
 void DoState(PointerWrap& p);
 
 void WaitUntilIdle();
-void StartRead(u64 dvd_offset, u32 output_address, u32 length, bool decrypt, bool reply_to_ios,
-               int ticks_until_completion);
+void StartRead(u64 dvd_offset, u32 length, bool decrypt);
+
+bool CompleteRead();
+void DMA(u32 dvd_offset, u32 output_address, u32 length);
+void Cleanup();
 }
