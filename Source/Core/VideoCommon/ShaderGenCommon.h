3ab7806e242d631f2fb034a23f354764ef6b9ee9
Ryan Houdek <Sonicadvance1@gmail.com>
2016-03-07 18:05:52 -0600

    Workaround OS X video driver bug #24983074
    OS X's shader compiler has a bug with interface blocks where interface block members don't properly inherit the layout qualifier from the interface
    block.
    Work around this limitation by explicitly stating the layout qualifier on both the interface block and every single member inside of that block.

--- Source/Core/VideoCommon/ShaderGenCommon.h
+++ Source/Core/VideoCommon/ShaderGenCommon.h
@@ -214,12 +214,12 @@ private:
 };
 
 template<class T>
-inline void DefineOutputMember(T& object, API_TYPE api_type, const char* qualifier, const char* type, const char* name, int var_index, const char* semantic = "", int semantic_index = -1)
+inline void DefineOutputMember(T& object, API_TYPE api_type, const char* qualifier, const char* in_out, const char* type, const char* name, int var_index, const char* semantic = "", int semantic_index = -1)
 {
 	if (qualifier != nullptr)
-		object.Write("\t%s %s %s", qualifier, type, name);
+		object.Write("\t%s %s %s %s", qualifier, in_out, type, name);
 	else
-		object.Write("\t%s %s", type, name);
+		object.Write("\t%s %s %s", in_out, type, name);
 
 	if (var_index != -1)
 		object.Write("%d", var_index);
@@ -236,21 +236,21 @@ inline void DefineOutputMember(T& object, API_TYPE api_type, const char* qualifi
 }
 
 template<class T>
-inline void GenerateVSOutputMembers(T& object, API_TYPE api_type, const char* qualifier = nullptr)
+inline void GenerateVSOutputMembers(T& object, API_TYPE api_type, const char* in_out, const char* qualifier = nullptr)
 {
-	DefineOutputMember(object, api_type, qualifier, "float4", "pos", -1, "POSITION");
-	DefineOutputMember(object, api_type, qualifier, "float4", "colors_", 0, "COLOR", 0);
-	DefineOutputMember(object, api_type, qualifier, "float4", "colors_", 1, "COLOR", 1);
+	DefineOutputMember(object, api_type, qualifier, in_out, "float4", "pos", -1, "POSITION");
+	DefineOutputMember(object, api_type, qualifier, in_out, "float4", "colors_", 0, "COLOR", 0);
+	DefineOutputMember(object, api_type, qualifier, in_out, "float4", "colors_", 1, "COLOR", 1);
 
 	for (unsigned int i = 0; i < xfmem.numTexGen.numTexGens; ++i)
-		DefineOutputMember(object, api_type, qualifier, "float3", "tex", i, "TEXCOORD", i);
+		DefineOutputMember(object, api_type, qualifier, in_out, "float3", "tex", i, "TEXCOORD", i);
 
-	DefineOutputMember(object, api_type, qualifier, "float4", "clipPos", -1, "TEXCOORD", xfmem.numTexGen.numTexGens);
+	DefineOutputMember(object, api_type, qualifier, in_out, "float4", "clipPos", -1, "TEXCOORD", xfmem.numTexGen.numTexGens);
 
 	if (g_ActiveConfig.bEnablePixelLighting)
 	{
-		DefineOutputMember(object, api_type, qualifier, "float3", "Normal", -1, "TEXCOORD", xfmem.numTexGen.numTexGens + 1);
-		DefineOutputMember(object, api_type, qualifier, "float3", "WorldPos", -1, "TEXCOORD", xfmem.numTexGen.numTexGens + 2);
+		DefineOutputMember(object, api_type, qualifier, in_out, "float3", "Normal", -1, "TEXCOORD", xfmem.numTexGen.numTexGens + 1);
+		DefineOutputMember(object, api_type, qualifier, in_out, "float3", "WorldPos", -1, "TEXCOORD", xfmem.numTexGen.numTexGens + 2);
 	}
 }
 
@@ -281,17 +281,13 @@ inline void AssignVSOutputMembers(T& object, const char* a, const char* b)
 // As a workaround, we interpolate at the centroid of the coveraged pixel, which
 // is always inside the primitive.
 // Without MSAA, this flag is defined to have no effect.
-inline const char* GetInterpolationQualifier(API_TYPE api_type, bool in = true, bool in_out = false)
+inline const char* GetInterpolationQualifier()
 {
 	if (g_ActiveConfig.iMultisamples <= 1)
 		return "";
 
 	if (!g_ActiveConfig.bSSAA)
-	{
-		if (in_out && api_type == API_OPENGL && !g_ActiveConfig.backend_info.bSupportsBindingLayout)
-			return in ? "centroid in" : "centroid out";
 		return "centroid";
-	}
 
 	return "sample";
 }
