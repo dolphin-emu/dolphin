573e93293ef15423be7f1f31b6ae01cf99799caf
Preston Smith <preston.s1993@gmail.com>
2016-03-26 23:48:10 -0400

    Post matrix q is also broken when 0, and it stacks weird

--- Source/Core/VideoBackends/Software/TransformUnit.cpp
+++ Source/Core/VideoBackends/Software/TransformUnit.cpp
@@ -146,29 +146,13 @@ static void TransformTexCoordRegular(const TexMtxInfo &texinfo, int coordNum, bo
 			MultiplyVec3Mat34(*src, mat, *dst);
 	}
 
-	// TODO: Write something here
-	if(dst->z == 0.0f)
-	{
-		if(mat[8] != 0.0f || mat[9] != 0.0f)
-		{
-			// TODO: Run more tests on this to make sure
-			dst->x = 0.0f;
-			dst->y = 0.0f;
-		}
-		else
-		{
-			dst->x = MathUtil::Clamp(dst->x / 2.0f, -1.0f, 1.0f);
-			dst->y = MathUtil::Clamp(dst->y / 2.0f, -1.0f, 1.0f);
-		}
-	}
+	// normalize
+	const PostMtxInfo &postInfo = xfmem.postMtxInfo[coordNum];
+	const float* postMat = &xfmem.postMatrices[postInfo.index * 4];
 
 	if (xfmem.dualTexTrans.enabled)
 	{
 		Vec3 tempCoord;
-
-		// normalize
-		const PostMtxInfo &postInfo = xfmem.postMtxInfo[coordNum];
-		const float* postMat = &xfmem.postMatrices[postInfo.index * 4];
 
 		if (specialCase)
 		{
@@ -191,6 +175,21 @@ static void TransformTexCoordRegular(const TexMtxInfo &texinfo, int coordNum, bo
 
 			MultiplyVec3Mat34(tempCoord, postMat, *dst);
 		}
+	}
+
+	if(dst->z == 0.0f)
+	{
+		if(mat[0] != 0.0f || (postMat[0] != 0.0f && xfmem.dualTexTrans.enabled))
+		{
+			dst->x = 0.0f;
+		}
+		if(mat[5] != 0.0f || (postMat[5] != 0.0f && xfmem.dualTexTrans.enabled))
+		{
+			dst->y = 0.0f;
+		}
+
+		dst->x = MathUtil::Clamp(dst->x / 2.0f, -1.0f, 1.0f);
+		dst->y = MathUtil::Clamp(dst->y / 2.0f, -1.0f, 1.0f);
 	}
 }
 
