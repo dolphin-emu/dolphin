5122b2304faa415426b4ab52173a658c762f5cbf
Scott Mansell <phiren@gmail.com>
2016-04-15 12:59:31 +1200

    Ubershaders: only DualSourceBlend when Alpha Writes are enabled
    Chibi-Robo leaves the Constant Alpha enabled while disabling alpha
    updates.

--- Source/Core/VideoCommon/PixelShaderManager.cpp
+++ Source/Core/VideoCommon/PixelShaderManager.cpp
@@ -167,6 +167,10 @@ void PixelShaderManager::SetConstants()
       }
     }
 
+    // Not really the right place.
+    // Destination alpha is only enabled if alpha writes are enabled.
+    more_constants.dstalpha = bpmem.blendmode.alphaupdate ? bpmem.dstalpha.hex : 0;
+
     dirty = true;
     s_bIndirectDirty = false;
   }
@@ -395,8 +399,7 @@ void PixelShaderManager::UpdateBP(u32 bp, u32 newValue)
   }
   else if (bp == 0x42)
   {
-    more_constants.dstalpha = newValue;
-    dirty = true;
+    s_bIndirectDirty = true;
   }
   else if (bp == 0x43)
   {
