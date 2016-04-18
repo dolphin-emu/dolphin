937caea1c92257cae7d6475d356e2cc3243d986a
Tyler Dunn <robotdude@att.net>
2016-04-18 19:30:50 -0400

    [Android] Drop commit() for apply()
    Apply is asynchronous, and we aren't expecting a return value so there
    is no reason not to use apply.

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/services/AssetCopyService.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/services/AssetCopyService.java
@@ -54,7 +54,7 @@ public final class AssetCopyService extends IntentService
 		SharedPreferences.Editor editor = preferences.edit();
 
 		editor.putBoolean("assetsCopied", true);
-		editor.commit();
+		editor.apply();
 	}
 
 	private void copyAsset(String asset, String output, Boolean overwrite)
