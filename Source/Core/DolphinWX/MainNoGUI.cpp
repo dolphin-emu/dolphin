2030ad45772742e8de34dbc94baa9eb49a0f7335
degasus <wickmarkus@web.de>
2016-05-04 23:47:23 +0200

    SCM: Use std::string.
    Those macros may be defined, or not. We should support both cases, so use std::string as it also defines the length of the string.

--- Source/Core/DolphinWX/MainNoGUI.cpp
+++ Source/Core/DolphinWX/MainNoGUI.cpp
@@ -320,14 +320,14 @@ int main(int argc, char* argv[])
 			help = 1;
 			break;
 		case 'v':
-			fprintf(stderr, "%s\n", scm_rev_str);
+			fprintf(stderr, "%s\n", scm_rev_str.c_str());
 			return 1;
 		}
 	}
 
 	if (help == 1 || argc == optind)
 	{
-		fprintf(stderr, "%s\n\n", scm_rev_str);
+		fprintf(stderr, "%s\n\n", scm_rev_str.c_str());
 		fprintf(stderr, "A multi-platform GameCube/Wii emulator\n\n");
 		fprintf(stderr, "Usage: %s [-e <file>] [-h] [-v]\n", argv[0]);
 		fprintf(stderr, "  -e, --exec     Load the specified file\n");
