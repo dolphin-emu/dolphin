21d7b38c49f36dd00fcf1afffb964aba57b141c6
mimimi085181 <mimimi085181@gmail.com>
2016-05-05 15:32:54 +0200

    Netplay: Fix gamecube controller mapping, 1st step
    This fixes issues with setups like:
    Player 1 uses port 1 and player 2 uses port 3
    
    Not fixed with this pr:
    - No player using port 1, like player 1 uses port 2 and player 2 uses port 3
    - If a player uses 2 controllers, the 1st 2 slots must be set, most likely it's the same issue as above. Example: Player 1 uses ports 1 and 3, and player 2 uses port 4

--- Source/Core/Core/NetPlayClient.cpp
+++ Source/Core/Core/NetPlayClient.cpp
@@ -740,35 +740,34 @@ bool NetPlayClient::ChangeGame(const std::string&)
 void NetPlayClient::UpdateDevices()
 {
 	u8 local_pad = 0;
-	// Add local pads first:
-	// As stated in the comment in NetPlayClient::GetNetPads, the pads pertaining
-	// to the local user are always locally mapped to the first gamecube ports,
-	// so they should be added first.
+	u8 pad = 0;
+
 	for (auto player_id : m_pad_map)
 	{
 		// Use local controller types for local controllers if they are compatible
+		// Only GCController-like controllers are supported, GBA and similar
+		// exotic devices are not supported on netplay.
 		if (player_id == m_local_player->pid)
 		{
 			if (SIDevice_IsGCController(SConfig::GetInstance().m_SIDevice[local_pad]))
 			{
-				SerialInterface::AddDevice(SConfig::GetInstance().m_SIDevice[local_pad], local_pad);
+				SerialInterface::AddDevice(SConfig::GetInstance().m_SIDevice[local_pad], pad);
 			}
 			else
 			{
-				SerialInterface::AddDevice(SIDEVICE_GC_CONTROLLER, local_pad);
+				SerialInterface::AddDevice(SIDEVICE_GC_CONTROLLER, pad);
 			}
 			local_pad++;
 		}
-	}
-	for (auto player_id : m_pad_map)
-	{
-		if (player_id != m_local_player->pid)
+		else if (player_id > 0)
 		{
-			// Only GCController-like controllers are supported, GBA and similar
-			// exotic devices are not supported on netplay.
-			SerialInterface::AddDevice(player_id > 0 ? SIDEVICE_GC_CONTROLLER : SIDEVICE_NONE, local_pad);
-			local_pad++;
+			SerialInterface::AddDevice(SIDEVICE_GC_CONTROLLER, pad);
 		}
+		else
+		{
+			SerialInterface::AddDevice(SIDEVICE_NONE, pad);
+		}
+		pad++;
 	}
 }
 
