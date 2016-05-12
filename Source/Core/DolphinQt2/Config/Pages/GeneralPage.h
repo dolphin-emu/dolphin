f86a2dbf6405fc203e15e6df6c4d056b01f5cb03
Corwin McKnight <lioneltabre@gmail.com>
2016-05-12 01:54:45 -0700

    [Qt] Add General Page to Settings
    Update VS Project files to include General Page

---
+++ Source/Core/DolphinQt2/Config/Pages/GeneralPage.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <QWidget>
+
+class QCheckBox;
+class QComboBox;
+class QVBoxLayout;
+
+class GeneralPage final : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit GeneralPage(QWidget* parent = nullptr);
+	void LoadConfig();
+
+private slots:
+	void SaveConfig();
+
+private:
+	void BuildBasicSettings();
+	void BuildAdvancedSettings();
+	void BuildOptions();
+	void ConnectOptions();
+	QVBoxLayout* m_main_layout;
+
+	// Basic Group
+	QComboBox* m_combobox_language;
+	QCheckBox* m_checkbox_enable_cheats;
+	QComboBox* m_checkbox_speed_limit;
+
+	// Advanced Group
+	QCheckBox* m_checkbox_force_ntsc;
+};
