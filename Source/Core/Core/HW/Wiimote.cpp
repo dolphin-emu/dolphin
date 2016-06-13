365ec9ee8ef0a06c02051f3b5341e5253fb62fd4
Léo Lam <leo@innovatetechnologi.es>
2016-06-13 11:11:47 +0200

    ControllerInterface: Add a way to register callbacks
    This adds RegisterDeviceCallback() to register a callback which will be
    invoked by the input backends when there is a new device, so that Core
    (GCKeyboard, GCPad, Wiimote, Hotkey) can reload the configuration
    without having to add a dependency to Core from InputCommon.

--- Source/Core/Core/HW/Wiimote.cpp
+++ Source/Core/Core/HW/Wiimote.cpp
@@ -39,6 +39,7 @@ void Initialize(void* const hwnd, bool wait)
 	}
 
 	g_controller_interface.Initialize(hwnd);
+	g_controller_interface.RegisterDeviceCallback(LoadConfig);
 
 	s_config.LoadConfig(false);
 
