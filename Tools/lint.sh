e1c65406dcb9c9a4bae117400381da833fe1e981
Pierre Bourdon <delroth@gmail.com>
2016-06-24 12:50:46 +0200

    linter: Stop checking for include ordering (inconsistent with clang-format)

--- Tools/lint.sh
+++ Tools/lint.sh
@@ -6,22 +6,7 @@ REPO_BASE=$(realpath $(dirname $0)/..)
 
 fail=0
 
-# Step 1: check for includes ordering.
-echo "[.] Checking #include statements ordering."
-res=$(
-  ${REPO_BASE}/Tools/check-includes.py \
-      $(find ${REPO_BASE}/Source/Core -name "*.cpp" -o -name "*.h") \
-      2>&1 \
-      >/dev/null
-)
-if [ -n "${res}" ]; then
-  echo "FAIL: ${res}"
-  fail=1
-else
-  echo "OK"
-fi
-
-# Step 2: check for trailing whitespaces.
+# Step 1: check for trailing whitespaces.
 echo "[.] Checking for trailing whitespaces."
 res=$(
     find ${REPO_BASE}/Source/Core -name "*.cpp" -o -name "*.h" \
