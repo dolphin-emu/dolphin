dea933c6c2926608387811b7699efd4e1c77151e
mimimi085181 <mimimi085181@gmail.com>
2016-06-26 10:44:15 +0200

    Revert pr#3691
    https://github.com/dolphin-emu/dolphin/pull/3691

--- Source/Core/DolphinWX/NetPlay/NetPlaySetupFrame.cpp
+++ Source/Core/DolphinWX/NetPlay/NetPlaySetupFrame.cpp
@@ -138,7 +138,7 @@ NetPlaySetupFrame::NetPlaySetupFrame(wxWindow* const parent, const CGameListCtrl
           "If DSP LLE is used, DSP ROMs must be identical between players.\n"
           "If connecting directly, the host must have the chosen UDP port open/forwarded!\n"
           "\n"
-          "Wiimote support is broken in netplay and therefore disabled.\n"));
+          "Wiimote netplay is experimental and should not be expected to work.\n"));
 
     wxBoxSizer* const top_szr = new wxBoxSizer(wxHORIZONTAL);
 
