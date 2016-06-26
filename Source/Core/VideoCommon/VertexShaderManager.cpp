1418aae717f97e7f92b49c88d1706ea2b7fa36a8
Joel Longanecker <joel.longanecker@gmail.com>
2016-06-25 20:56:06 -0500

    relocated control to stereoscopy

--- Source/Core/VideoCommon/VertexShaderManager.cpp
+++ Source/Core/VideoCommon/VertexShaderManager.cpp
@@ -523,7 +523,7 @@ void VertexShaderManager::SetConstants()
     PRIM_LOG("Projection: %f %f %f %f %f %f\n", rawProjection[0], rawProjection[1],
              rawProjection[2], rawProjection[3], rawProjection[4], rawProjection[5]);
 
-    if ((g_ActiveConfig.bFreeLook|| g_ActiveConfig.bMouselookAlwaysOn) && xfmem.projection.type == GX_PERSPECTIVE)
+    if ((g_ActiveConfig.bFreeLook|| g_ActiveConfig.bFreeLookAlwaysOn) && xfmem.projection.type == GX_PERSPECTIVE)
     {
       Matrix44 mtxA;
       Matrix44 mtxB;
