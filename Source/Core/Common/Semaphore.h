82dd36a3d19fea52d4e24e963b4317bef668e00e
Stenzek <stenzek@gmail.com>
2016-07-03 20:56:33 +1000

    Vulkan: Apply clang-format

--- Source/Core/Common/Semaphore.h
+++ Source/Core/Common/Semaphore.h
@@ -7,7 +7,7 @@
 #ifdef _WIN32
 
 #ifndef WIN32_LEAN_AND_MEAN
-  #define WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 #endif
 #include <Windows.h>
 
@@ -21,27 +21,15 @@ public:
     m_handle = CreateSemaphoreA(nullptr, initial_count, maximum_count, nullptr);
   }
 
-  ~Semaphore()
-  {
-    CloseHandle(m_handle);
-  }
-
-  void Wait()
-  {
-    WaitForSingleObject(m_handle, INFINITE);
-  }
-
-  void Post()
-  {
-    ReleaseSemaphore(m_handle, 1, nullptr);
-  }
-
+  ~Semaphore() { CloseHandle(m_handle); }
+  void Wait() { WaitForSingleObject(m_handle, INFINITE); }
+  void Post() { ReleaseSemaphore(m_handle, 1, nullptr); }
 private:
   HANDLE m_handle;
 };
-}       // namespace Common
+}  // namespace Common
 
-#else   // _WIN32
+#else  // _WIN32
 
 #include <semaphore.h>
 
@@ -50,29 +38,13 @@ namespace Common
 class Semaphore
 {
 public:
-  Semaphore(int initial_count, int maximum_count)
-  {
-    sem_init(&m_handle, 1, initial_count);
-  }
-
-  ~Semaphore()
-  {
-    sem_destroy(&m_handle);
-  }
-
-  void Wait()
-  {
-    sem_wait(&m_handle);
-  }
-
-  void Post()
-  {
-    sem_post(&m_handle);
-  }
-
+  Semaphore(int initial_count, int maximum_count) { sem_init(&m_handle, 1, initial_count); }
+  ~Semaphore() { sem_destroy(&m_handle); }
+  void Wait() { sem_wait(&m_handle); }
+  void Post() { sem_post(&m_handle); }
 private:
   sem_t m_handle;
 };
-}       // namespace Common
+}  // namespace Common
 
 #endif  // _WIN32
