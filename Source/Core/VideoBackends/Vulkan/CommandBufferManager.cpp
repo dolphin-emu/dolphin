9c2878a43c606c211574727d6144fb28797cab53
Stenzek <stenzek@gmail.com>
2016-07-12 20:40:24 +1000

    Vulkan: Use two command buffers per-frame
    This means that we can do texture uploads, conversion, etc in one
    command buffer, and our normal draws in the other, removing the
    need to have to exit/re-enter render passes all the time.

--- Source/Core/VideoBackends/Vulkan/CommandBufferManager.cpp
+++ Source/Core/VideoBackends/Vulkan/CommandBufferManager.cpp
@@ -80,23 +80,25 @@ void CommandBufferManager::DestroyCommandPool()
 
 bool CommandBufferManager::CreateCommandBuffers()
 {
-  VkCommandBufferAllocateInfo allocate_info = {
-      VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO, nullptr, m_command_pool,
-      VK_COMMAND_BUFFER_LEVEL_PRIMARY, static_cast<uint32_t>(m_command_buffers.size())};
-
-  VkFenceCreateInfo fence_info = {VK_STRUCTURE_TYPE_FENCE_CREATE_INFO, nullptr,
-                                  VK_FENCE_CREATE_SIGNALED_BIT};
-
-  VkResult res = vkAllocateCommandBuffers(m_device, &allocate_info, m_command_buffers.data());
-  if (res != VK_SUCCESS)
+  for (size_t i = 0; i < m_frame_resources.size(); i++)
   {
-    LOG_VULKAN_ERROR(res, "vkAllocateCommandBuffers failed: ");
-    return false;
-  }
+    FrameResources& resources = m_frame_resources[i];
+    VkCommandBufferAllocateInfo allocate_info = {
+        VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO, nullptr, m_command_pool,
+        VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+        static_cast<uint32_t>(ARRAYSIZE(resources.command_buffers))};
 
-  for (size_t i = 0; i < NUM_COMMAND_BUFFERS; i++)
-  {
-    res = vkCreateFence(m_device, &fence_info, nullptr, &m_fences[i]);
+    VkResult res = vkAllocateCommandBuffers(m_device, &allocate_info, resources.command_buffers);
+    if (res != VK_SUCCESS)
+    {
+      LOG_VULKAN_ERROR(res, "vkAllocateCommandBuffers failed: ");
+      return false;
+    }
+
+    VkFenceCreateInfo fence_info = {VK_STRUCTURE_TYPE_FENCE_CREATE_INFO, nullptr,
+                                    VK_FENCE_CREATE_SIGNALED_BIT};
+
+    res = vkCreateFence(m_device, &fence_info, nullptr, &resources.fence);
     if (res != VK_SUCCESS)
     {
       LOG_VULKAN_ERROR(res, "vkCreateFence failed: ");
@@ -116,7 +118,7 @@ bool CommandBufferManager::CreateCommandBuffers()
                                                    ARRAYSIZE(pool_sizes),
                                                    pool_sizes};
 
-    res = vkCreateDescriptorPool(m_device, &pool_create_info, nullptr, &m_descriptor_pools[i]);
+    res = vkCreateDescriptorPool(m_device, &pool_create_info, nullptr, &resources.descriptor_pool);
     if (res != VK_SUCCESS)
     {
       LOG_VULKAN_ERROR(res, "vkCreateDescriptorPool failed: ");
@@ -124,53 +126,38 @@ bool CommandBufferManager::CreateCommandBuffers()
     }
   }
 
-  // Activate the first command buffer, so that setup commands, uploads, etc can occur before the
-  // frame starts.
-  VkCommandBufferBeginInfo begin_info = {VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO, nullptr,
-                                         VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT, nullptr};
-  m_current_command_buffer_index = 0;
-  res = vkResetFences(m_device, 1, &m_fences[m_current_command_buffer_index]);
-  if (res != VK_SUCCESS)
-  {
-    LOG_VULKAN_ERROR(res, "vkResetFences failed: ");
-    return false;
-  }
-  res = vkBeginCommandBuffer(m_command_buffers[m_current_command_buffer_index], &begin_info);
-  if (res != VK_SUCCESS)
-  {
-    LOG_VULKAN_ERROR(res, "vkBeginCommandBuffer failed: ");
-    return false;
-  }
-
+  // Activate the first command buffer. ActivateCommandBuffer moves forward, so start with the last
+  m_current_frame = m_frame_resources.size() - 1;
+  ActivateCommandBuffer();
   return true;
 }
 
 void CommandBufferManager::DestroyCommandBuffers()
 {
-  for (size_t i = 0; i < m_command_buffers.size(); i++)
+  for (size_t i = 0; i < m_frame_resources.size(); i++)
   {
-    for (const auto& it : m_pending_destructions[m_current_command_buffer_index])
+    FrameResources& resources = m_frame_resources[i];
+    for (const auto& it : resources.cleanup_resources)
       it.destroy_callback(m_device, it.object);
-    m_pending_destructions[m_current_command_buffer_index].clear();
+    resources.cleanup_resources.clear();
 
-    if (m_fences[i])
+    if (resources.fence != VK_NULL_HANDLE)
     {
-      vkDestroyFence(m_device, m_fences[i], nullptr);
-      m_fences[i] = nullptr;
+      vkDestroyFence(m_device, resources.fence, nullptr);
+      resources.fence = VK_NULL_HANDLE;
     }
-    if (m_descriptor_pools[i])
+    if (resources.descriptor_pool != VK_NULL_HANDLE)
     {
-      vkDestroyDescriptorPool(m_device, m_descriptor_pools[i], nullptr);
-      m_descriptor_pools[i] = nullptr;
+      vkDestroyDescriptorPool(m_device, resources.descriptor_pool, nullptr);
+      resources.descriptor_pool = VK_NULL_HANDLE;
     }
-  }
+    if (resources.init_command_buffer != VK_NULL_HANDLE)
+    {
+      vkFreeCommandBuffers(m_device, m_command_pool, ARRAYSIZE(resources.command_buffers),
+                           resources.command_buffers);
 
-  if (m_command_buffers[0])
-  {
-    vkFreeCommandBuffers(m_device, m_command_pool, static_cast<uint32_t>(m_command_buffers.size()),
-                         m_command_buffers.data());
-    for (size_t i = 0; i < m_command_buffers.size(); i++)
-      m_command_buffers[i] = nullptr;
+      resources.init_command_buffer = VK_NULL_HANDLE;
+    }
   }
 }
 
@@ -178,7 +165,7 @@ VkDescriptorSet CommandBufferManager::AllocateDescriptorSet(VkDescriptorSetLayou
 {
   VkDescriptorSetAllocateInfo allocate_info = {
       VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO, nullptr,
-      m_descriptor_pools[m_current_command_buffer_index], 1, &set_layout};
+      m_frame_resources[m_current_frame].descriptor_pool, 1, &set_layout};
 
   VkDescriptorSet descriptor_set;
   VkResult res = vkAllocateDescriptorSets(m_device, &allocate_info, &descriptor_set);
@@ -243,7 +230,13 @@ void CommandBufferManager::SubmitCommandBuffer(bool submit_off_thread)
 {
   // End the current command buffer.
   // TODO: Can we move this off-thread?
-  VkResult res = vkEndCommandBuffer(m_command_buffers[m_current_command_buffer_index]);
+  VkResult res = vkEndCommandBuffer(m_frame_resources[m_current_frame].init_command_buffer);
+  if (res != VK_SUCCESS)
+  {
+    LOG_VULKAN_ERROR(res, "vkEndCommandBuffer failed: ");
+    PanicAlert("Failed to end command buffer");
+  }
+  res = vkEndCommandBuffer(m_frame_resources[m_current_frame].draw_command_buffer);
   if (res != VK_SUCCESS)
   {
     LOG_VULKAN_ERROR(res, "vkEndCommandBuffer failed: ");
@@ -257,7 +250,7 @@ void CommandBufferManager::SubmitCommandBuffer(bool submit_off_thread)
     {
       std::lock_guard<std::mutex> guard(m_pending_submit_lock);
       m_pending_submits.push_back(
-          {m_current_command_buffer_index, VK_NULL_HANDLE, VK_NULL_HANDLE, VK_NULL_HANDLE, 0});
+          {m_current_frame, VK_NULL_HANDLE, VK_NULL_HANDLE, VK_NULL_HANDLE, 0});
     }
 
     // Wake up the worker thread for a single iteration.
@@ -266,13 +259,12 @@ void CommandBufferManager::SubmitCommandBuffer(bool submit_off_thread)
   else
   {
     // Pass through to normal submission path.
-    SubmitCommandBuffer(m_current_command_buffer_index, VK_NULL_HANDLE, VK_NULL_HANDLE,
-                        VK_NULL_HANDLE, 0);
+    SubmitCommandBuffer(m_current_frame, VK_NULL_HANDLE, VK_NULL_HANDLE, VK_NULL_HANDLE, 0);
   }
 
   // Fire fence tracking callbacks. This can't happen on the worker thread.
   for (const auto& iter : m_fence_point_callbacks)
-    iter.second.first(m_fences[m_current_command_buffer_index]);
+    iter.second.first(m_frame_resources[m_current_frame].fence);
 }
 
 void CommandBufferManager::SubmitCommandBufferAndPresent(VkSemaphore wait_semaphore,
@@ -283,7 +275,13 @@ void CommandBufferManager::SubmitCommandBufferAndPresent(VkSemaphore wait_semaph
 {
   // End the current command buffer.
   // TODO: Can we move this off-thread?
-  VkResult res = vkEndCommandBuffer(m_command_buffers[m_current_command_buffer_index]);
+  VkResult res = vkEndCommandBuffer(m_frame_resources[m_current_frame].init_command_buffer);
+  if (res != VK_SUCCESS)
+  {
+    LOG_VULKAN_ERROR(res, "vkEndCommandBuffer failed: ");
+    PanicAlert("Failed to end command buffer");
+  }
+  res = vkEndCommandBuffer(m_frame_resources[m_current_frame].draw_command_buffer);
   if (res != VK_SUCCESS)
   {
     LOG_VULKAN_ERROR(res, "vkEndCommandBuffer failed: ");
@@ -296,7 +294,7 @@ void CommandBufferManager::SubmitCommandBufferAndPresent(VkSemaphore wait_semaph
     // Push to the pending submit queue.
     {
       std::lock_guard<std::mutex> guard(m_pending_submit_lock);
-      m_pending_submits.push_back({m_current_command_buffer_index, wait_semaphore, signal_semaphore,
+      m_pending_submits.push_back({m_current_frame, wait_semaphore, signal_semaphore,
                                    present_swap_chain, present_image_index});
     }
 
@@ -306,13 +304,13 @@ void CommandBufferManager::SubmitCommandBufferAndPresent(VkSemaphore wait_semaph
   else
   {
     // Pass through to normal submission path.
-    SubmitCommandBuffer(m_current_command_buffer_index, wait_semaphore, signal_semaphore,
-                        present_swap_chain, present_image_index);
+    SubmitCommandBuffer(m_current_frame, wait_semaphore, signal_semaphore, present_swap_chain,
+                        present_image_index);
   }
 
   // Fire fence tracking callbacks. This can't happen on the worker thread.
   for (const auto& iter : m_fence_point_callbacks)
-    iter.second.first(m_fences[m_current_command_buffer_index]);
+    iter.second.first(m_frame_resources[m_current_frame].fence);
 }
 
 void CommandBufferManager::SubmitCommandBuffer(size_t index, VkSemaphore wait_semaphore,
@@ -321,10 +319,16 @@ void CommandBufferManager::SubmitCommandBuffer(size_t index, VkSemaphore wait_se
                                                uint32_t present_image_index)
 {
   // This may be executed on the worker thread, so don't modify any state of the manager class.
-  uint32_t wait_bits = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
-  VkSubmitInfo submit_info = {
-      VK_STRUCTURE_TYPE_SUBMIT_INFO, nullptr, 0,      nullptr, &wait_bits, 1,
-      &m_command_buffers[index],     0,       nullptr};
+  uint32_t wait_bits = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+  VkSubmitInfo submit_info = {VK_STRUCTURE_TYPE_SUBMIT_INFO,
+                              nullptr,
+                              0,
+                              nullptr,
+                              &wait_bits,
+                              ARRAYSIZE(m_frame_resources[index].command_buffers),
+                              m_frame_resources[index].command_buffers,
+                              0,
+                              nullptr};
 
   if (wait_semaphore != VK_NULL_HANDLE)
   {
@@ -338,7 +342,7 @@ void CommandBufferManager::SubmitCommandBuffer(size_t index, VkSemaphore wait_se
     submit_info.pSignalSemaphores = &signal_semaphore;
   }
 
-  VkResult res = vkQueueSubmit(m_graphics_queue, 1, &submit_info, m_fences[index]);
+  VkResult res = vkQueueSubmit(m_graphics_queue, 1, &submit_info, m_frame_resources[index].fence);
   if (res != VK_SUCCESS)
   {
     LOG_VULKAN_ERROR(res, "vkQueueSubmit failed: ");
@@ -371,39 +375,46 @@ void CommandBufferManager::SubmitCommandBuffer(size_t index, VkSemaphore wait_se
 void CommandBufferManager::ActivateCommandBuffer()
 {
   // Move to the next command buffer.
-  m_current_command_buffer_index = (m_current_command_buffer_index + 1) % NUM_COMMAND_BUFFERS;
-  VkResult res =
-      vkWaitForFences(m_device, 1, &m_fences[m_current_command_buffer_index], true, UINT64_MAX);
+  m_current_frame = (m_current_frame + 1) % NUM_COMMAND_BUFFERS;
+  FrameResources& resources = m_frame_resources[m_current_frame];
+
+  VkResult res = vkWaitForFences(m_device, 1, &resources.fence, true, UINT64_MAX);
   if (res != VK_SUCCESS)
     LOG_VULKAN_ERROR(res, "vkWaitForFences failed: ");
 
-  res = vkResetFences(m_device, 1, &m_fences[m_current_command_buffer_index]);
+  res = vkResetFences(m_device, 1, &resources.fence);
   if (res != VK_SUCCESS)
     LOG_VULKAN_ERROR(res, "vkResetFences failed: ");
 
   // Fire fence tracking callbacks.
   for (const auto& iter : m_fence_point_callbacks)
-    iter.second.second(m_fences[m_current_command_buffer_index]);
+    iter.second.second(resources.fence);
 
   // Clean up all objects pending destruction on this command buffer
-  for (const auto& it : m_pending_destructions[m_current_command_buffer_index])
+  for (const auto& it : resources.cleanup_resources)
     it.destroy_callback(m_device, it.object);
-  m_pending_destructions[m_current_command_buffer_index].clear();
+  resources.cleanup_resources.clear();
 
   // Reset command buffer to beginning since we can re-use the memory now
-  res = vkResetCommandBuffer(m_command_buffers[m_current_command_buffer_index], 0);
+  res = vkResetCommandBuffer(resources.init_command_buffer, 0);
+  if (res != VK_SUCCESS)
+    LOG_VULKAN_ERROR(res, "vkResetCommandBuffer failed: ");
+  res = vkResetCommandBuffer(resources.draw_command_buffer, 0);
   if (res != VK_SUCCESS)
     LOG_VULKAN_ERROR(res, "vkResetCommandBuffer failed: ");
 
   // Also can do the same for the descriptor pools
-  res = vkResetDescriptorPool(m_device, m_descriptor_pools[m_current_command_buffer_index], 0);
+  res = vkResetDescriptorPool(m_device, resources.descriptor_pool, 0);
   if (res != VK_SUCCESS)
     LOG_VULKAN_ERROR(res, "vkResetDescriptorPool failed: ");
 
   VkCommandBufferBeginInfo begin_info = {VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO, nullptr,
                                          VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT, nullptr};
 
-  res = vkBeginCommandBuffer(m_command_buffers[m_current_command_buffer_index], &begin_info);
+  res = vkBeginCommandBuffer(resources.init_command_buffer, &begin_info);
+  if (res != VK_SUCCESS)
+    LOG_VULKAN_ERROR(res, "vkBeginCommandBuffer failed: ");
+  res = vkBeginCommandBuffer(resources.draw_command_buffer, &begin_info);
   if (res != VK_SUCCESS)
     LOG_VULKAN_ERROR(res, "vkBeginCommandBuffer failed: ");
 }
@@ -417,7 +428,7 @@ void CommandBufferManager::ExecuteCommandBuffer(bool submit_off_thread, bool wai
 
   if (wait_for_completion)
   {
-    VkResult res = vkWaitForFences(m_device, 1, &m_fences[m_current_command_buffer_index], VK_TRUE,
+    VkResult res = vkWaitForFences(m_device, 1, &m_frame_resources[m_current_frame].fence, VK_TRUE,
                                    UINT64_MAX);
     if (res != VK_SUCCESS)
       LOG_VULKAN_ERROR(res, "vkWaitForFences failed: ");
