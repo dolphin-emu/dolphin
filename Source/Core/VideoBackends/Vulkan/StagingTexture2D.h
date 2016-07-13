888116a22bed860b3b5a3789568adb47b14993cc
Stenzek <stenzek@gmail.com>
2016-07-13 18:14:13 +1000

    Vulkan: Add StagingTexture2D class

---
+++ Source/Core/VideoBackends/Vulkan/StagingTexture2D.h
@@ -0,0 +1,114 @@
+// Copyright 2016 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <memory>
+
+#include "VideoBackends/Vulkan/VulkanImports.h"
+
+namespace Vulkan
+{
+class StagingTexture2D
+{
+public:
+  StagingTexture2D(u32 width, u32 height, VkFormat format, u32 stride);
+  virtual ~StagingTexture2D();
+
+  u32 GetWidth() const { return m_width; }
+  u32 GetHeight() const { return m_height; }
+  VkFormat GetFormat() const { return m_format; }
+  u32 GetRowStride() const { return m_row_stride; }
+  u32 GetTexelSize() const { return m_texel_size; }
+  const char* GetMapPointer() const { return m_map_pointer; }
+  char* GetMapPointer() { return m_map_pointer; }
+  VkDeviceSize GetMapOffset() const { return m_map_offset; }
+  VkDeviceSize GetMapSize() const { return m_map_size; }
+  const char* GetRowPointer(u32 row) const { return m_map_pointer + row * m_row_stride; }
+  char* GetRowPointer(u32 row) { return m_map_pointer + row * m_row_stride; }
+  // Requires Map() to be called first.
+  void ReadTexel(u32 x, u32 y, void* data, size_t data_size);
+  void WriteTexel(u32 x, u32 y, const void* data, size_t data_size);
+
+  // Assumes that image is in TRANSFER_SRC layout.
+  // Results are not ready until command_buffer has been executed.
+  virtual void CopyFromImage(VkCommandBuffer command_buffer, VkImage image,
+                             VkImageAspectFlags aspect, u32 x, u32 y, u32 width, u32 height,
+                             u32 level, u32 layer) = 0;
+
+  // Assumes that image is in TRANSFER_DST layout.
+  // Buffer is not safe for re-use until after command_buffer has been executed.
+  virtual void CopyToImage(VkCommandBuffer command_buffer, VkImage image, VkImageAspectFlags aspect,
+                           u32 x, u32 y, u32 width, u32 height, u32 level, u32 layer) = 0;
+  virtual bool Map(VkDeviceSize offset = 0, VkDeviceSize size = VK_WHOLE_SIZE) = 0;
+  virtual void Unmap() = 0;
+
+  // Creates the optimal format of image copy.
+  static std::unique_ptr<StagingTexture2D> Create(u32 width, u32 height, VkFormat format);
+
+protected:
+  u32 m_width;
+  u32 m_height;
+  VkFormat m_format;
+  u32 m_texel_size;
+  u32 m_row_stride;
+
+  char* m_map_pointer;
+  VkDeviceSize m_map_offset;
+  VkDeviceSize m_map_size;
+};
+
+class StagingTexture2DLinear : public StagingTexture2D
+{
+public:
+  StagingTexture2DLinear(u32 width, u32 height, VkFormat format, u32 stride, VkImage image,
+                         VkDeviceMemory memory, VkDeviceSize size);
+
+  ~StagingTexture2DLinear();
+
+  virtual void CopyFromImage(VkCommandBuffer command_buffer, VkImage image,
+                             VkImageAspectFlags aspect, u32 x, u32 y, u32 width, u32 height,
+                             u32 level, u32 layer) override;
+
+  virtual void CopyToImage(VkCommandBuffer command_buffer, VkImage image, VkImageAspectFlags aspect,
+                           u32 x, u32 y, u32 width, u32 height, u32 level, u32 layer) override;
+
+  virtual bool Map(VkDeviceSize offset = 0, VkDeviceSize size = VK_WHOLE_SIZE) override;
+  virtual void Unmap() override;
+
+  static std::unique_ptr<StagingTexture2D> Create(u32 width, u32 height, VkFormat format);
+
+private:
+  VkImage m_image;
+  VkDeviceMemory m_memory;
+  VkDeviceSize m_size;
+  VkImageLayout m_layout;
+};
+
+class StagingTexture2DBuffer : public StagingTexture2D
+{
+public:
+  StagingTexture2DBuffer(u32 width, u32 height, VkFormat format, u32 stride, VkBuffer buffer,
+                         VkDeviceMemory memory, VkDeviceSize size);
+
+  ~StagingTexture2DBuffer();
+
+  virtual void CopyFromImage(VkCommandBuffer command_buffer, VkImage image,
+                             VkImageAspectFlags aspect, u32 x, u32 y, u32 width, u32 height,
+                             u32 level, u32 layer) override;
+
+  virtual void CopyToImage(VkCommandBuffer command_buffer, VkImage image, VkImageAspectFlags aspect,
+                           u32 x, u32 y, u32 width, u32 height, u32 level, u32 layer) override;
+
+  virtual bool Map(VkDeviceSize offset = 0, VkDeviceSize size = VK_WHOLE_SIZE) override;
+  virtual void Unmap() override;
+
+  static std::unique_ptr<StagingTexture2D> Create(u32 width, u32 height, VkFormat format);
+
+private:
+  VkBuffer m_buffer;
+  VkDeviceMemory m_memory;
+  VkDeviceSize m_size;
+};
+}
