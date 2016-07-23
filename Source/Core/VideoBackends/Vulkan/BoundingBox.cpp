1c42185b94d27e8876d7902f4843c158194a9c84
Stenzek <stenzek@gmail.com>
2016-07-23 17:18:34 +1000

    Vulkan: Use StagingBuffer for bounding box readbacks

--- Source/Core/VideoBackends/Vulkan/BoundingBox.cpp
+++ Source/Core/VideoBackends/Vulkan/BoundingBox.cpp
@@ -9,6 +9,7 @@
 #include "VideoBackends/Vulkan/BoundingBox.h"
 #include "VideoBackends/Vulkan/CommandBufferManager.h"
 #include "VideoBackends/Vulkan/ObjectCache.h"
+#include "VideoBackends/Vulkan/StagingBuffer.h"
 #include "VideoBackends/Vulkan/StateTracker.h"
 #include "VideoBackends/Vulkan/Util.h"
 
@@ -24,15 +25,6 @@ BoundingBox::~BoundingBox()
   {
     vkDestroyBuffer(g_object_cache->GetDevice(), m_gpu_buffer, nullptr);
     vkFreeMemory(g_object_cache->GetDevice(), m_gpu_memory, nullptr);
-  }
-
-  if (m_readback_buffer != VK_NULL_HANDLE)
-  {
-    if (m_readback_map)
-      vkUnmapMemory(g_object_cache->GetDevice(), m_readback_memory);
-
-    vkDestroyBuffer(g_object_cache->GetDevice(), m_readback_buffer, nullptr);
-    vkFreeMemory(g_object_cache->GetDevice(), m_readback_memory, nullptr);
   }
 }
 
@@ -66,13 +58,14 @@ void BoundingBox::Flush(StateTracker* state_tracker)
       continue;
 
     size_t count = 0;
-    std::array<int, 4> write_values;
+    std::array<s32, 4> write_values;
     for (; (start + count) < 4; count++)
     {
       if (!m_values_dirty[start + count])
         break;
 
-      write_values[count] = static_cast<int>(m_values[start + count]);
+      m_readback_buffer->Read((start + count) * sizeof(s32), &write_values[count], sizeof(s32),
+                              false);
       m_values_dirty[start + count] = false;
     }
 
@@ -92,8 +85,8 @@ void BoundingBox::Flush(StateTracker* state_tracker)
     }
 
     vkCmdUpdateBuffer(g_command_buffer_mgr->GetCurrentCommandBuffer(), m_gpu_buffer,
-                      start * sizeof(int), count * sizeof(int),
-                      reinterpret_cast<const uint32_t*>(write_values.data()));
+                      start * sizeof(s32), count * sizeof(s32),
+                      reinterpret_cast<const u32*>(write_values.data()));
   }
 
   // Restore fragment shader access to the buffer.
@@ -119,27 +112,33 @@ void BoundingBox::Invalidate(StateTracker* state_tracker)
 
 s32 BoundingBox::Get(StateTracker* state_tracker, size_t index)
 {
-  _assert_(index < m_values.size());
+  _assert_(index < NUM_VALUES);
 
   if (!m_valid)
     Readback(state_tracker);
 
-  return m_values[index];
+  s32 value;
+  m_readback_buffer->Read(index * sizeof(s32), &value, sizeof(value), false);
+  return static_cast<u16>(value);
 }
 
 void BoundingBox::Set(StateTracker* state_tracker, size_t index, s32 value)
 {
+  _assert_(index < NUM_VALUES);
+
   // If we're currently valid, update the stored value in both our cache and the GPU buffer.
   if (m_valid)
   {
     // Skip when it hasn't changed.
-    if (m_values[index] == value)
+    s32 current_value;
+    m_readback_buffer->Read(index * sizeof(s32), &current_value, sizeof(current_value), false);
+    if (static_cast<u16>(current_value) == value)
       return;
   }
 
   // Flag as dirty, and update values.
+  m_readback_buffer->Write(index * sizeof(s32), &value, sizeof(value), true);
   m_values_dirty[index] = true;
-  m_values[index] = value;
 }
 
 bool BoundingBox::CreateGPUBuffer()
@@ -202,66 +201,11 @@ bool BoundingBox::CreateGPUBuffer()
 
 bool BoundingBox::CreateReadbackBuffer()
 {
-  VkBufferUsageFlags buffer_usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT;
-  VkBufferCreateInfo info = {
-      VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,  // VkStructureType        sType
-      nullptr,                               // const void*            pNext
-      0,                                     // VkBufferCreateFlags    flags
-      BUFFER_SIZE,                           // VkDeviceSize           size
-      buffer_usage,                          // VkBufferUsageFlags     usage
-      VK_SHARING_MODE_EXCLUSIVE,             // VkSharingMode          sharingMode
-      0,                                     // uint32_t               queueFamilyIndexCount
-      nullptr                                // const uint32_t*        pQueueFamilyIndices
-  };
+  m_readback_buffer = StagingBuffer::Create(STAGING_BUFFER_TYPE_READBACK, BUFFER_SIZE,
+                                            VK_BUFFER_USAGE_TRANSFER_DST_BIT);
 
-  VkBuffer buffer;
-  VkResult res = vkCreateBuffer(g_object_cache->GetDevice(), &info, nullptr, &buffer);
-  if (res != VK_SUCCESS)
-  {
-    LOG_VULKAN_ERROR(res, "vkCreateBuffer failed: ");
+  if (!m_readback_buffer || !m_readback_buffer->Map())
     return false;
-  }
-
-  VkMemoryRequirements memory_requirements;
-  vkGetBufferMemoryRequirements(g_object_cache->GetDevice(), buffer, &memory_requirements);
-
-  uint32_t memory_type_index = g_object_cache->GetMemoryType(memory_requirements.memoryTypeBits,
-                                                             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
-  VkMemoryAllocateInfo memory_allocate_info = {
-      VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,  // VkStructureType    sType
-      nullptr,                                 // const void*        pNext
-      memory_requirements.size,                // VkDeviceSize       allocationSize
-      memory_type_index                        // uint32_t           memoryTypeIndex
-  };
-  VkDeviceMemory memory;
-  res = vkAllocateMemory(g_object_cache->GetDevice(), &memory_allocate_info, nullptr, &memory);
-  if (res != VK_SUCCESS)
-  {
-    LOG_VULKAN_ERROR(res, "vkAllocateMemory failed: ");
-    vkDestroyBuffer(g_object_cache->GetDevice(), buffer, nullptr);
-    return false;
-  }
-
-  res = vkBindBufferMemory(g_object_cache->GetDevice(), buffer, memory, 0);
-  if (res != VK_SUCCESS)
-  {
-    LOG_VULKAN_ERROR(res, "vkBindBufferMemory failed: ");
-    vkDestroyBuffer(g_object_cache->GetDevice(), buffer, nullptr);
-    vkFreeMemory(g_object_cache->GetDevice(), memory, nullptr);
-    return false;
-  }
-
-  m_readback_buffer = buffer;
-  m_readback_memory = memory;
-
-  // Map the buffer and keep it mapped. We use invalidate calls in case access is not coherent.
-  res = vkMapMemory(g_object_cache->GetDevice(), m_readback_memory, 0, BUFFER_SIZE, 0,
-                    &m_readback_map);
-  if (res != VK_SUCCESS)
-  {
-    LOG_VULKAN_ERROR(res, "vkMapMemory failed: ");
-    return false;
-  }
 
   return true;
 }
@@ -276,38 +220,28 @@ void BoundingBox::Readback(StateTracker* state_tracker)
       g_command_buffer_mgr->GetCurrentCommandBuffer(), m_gpu_buffer,
       VK_ACCESS_SHADER_READ_BIT | VK_ACCESS_SHADER_WRITE_BIT, VK_ACCESS_TRANSFER_READ_BIT, 0,
       BUFFER_SIZE, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT);
-  Util::BufferMemoryBarrier(g_command_buffer_mgr->GetCurrentCommandBuffer(), m_readback_buffer,
-                            VK_ACCESS_HOST_READ_BIT, VK_ACCESS_TRANSFER_WRITE_BIT, 0, BUFFER_SIZE,
-                            VK_PIPELINE_STAGE_HOST_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT);
+  m_readback_buffer->PrepareForGPUWrite(g_command_buffer_mgr->GetCurrentCommandBuffer(),
+                                        VK_ACCESS_TRANSFER_WRITE_BIT,
+                                        VK_PIPELINE_STAGE_TRANSFER_BIT);
 
   // Copy from GPU -> readback buffer.
   VkBufferCopy region = {0, 0, BUFFER_SIZE};
-  vkCmdCopyBuffer(g_command_buffer_mgr->GetCurrentCommandBuffer(), m_gpu_buffer, m_readback_buffer,
-                  1, &region);
+  vkCmdCopyBuffer(g_command_buffer_mgr->GetCurrentCommandBuffer(), m_gpu_buffer,
+                  m_readback_buffer->GetBuffer(), 1, &region);
 
   // Restore GPU buffer access.
   Util::BufferMemoryBarrier(g_command_buffer_mgr->GetCurrentCommandBuffer(), m_gpu_buffer,
                             VK_ACCESS_TRANSFER_READ_BIT,
                             VK_ACCESS_SHADER_READ_BIT | VK_ACCESS_SHADER_WRITE_BIT, 0, BUFFER_SIZE,
                             VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT);
-
-  // Ensure writes are visible to the host.
-  Util::BufferMemoryBarrier(g_command_buffer_mgr->GetCurrentCommandBuffer(), m_readback_buffer,
-                            VK_ACCESS_TRANSFER_WRITE_BIT, VK_ACCESS_HOST_READ_BIT, 0, BUFFER_SIZE,
-                            VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_HOST_BIT);
+  m_readback_buffer->FlushGPUCache(g_command_buffer_mgr->GetCurrentCommandBuffer(),
+                                   VK_ACCESS_TRANSFER_WRITE_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT);
 
   // Wait until these commands complete.
-  g_command_buffer_mgr->ExecuteCommandBuffer(false, true);
-  state_tracker->InvalidateDescriptorSets();
-  state_tracker->SetPendingRebind();
-
-  // Invalidate host caches.
-  VkMappedMemoryRange range = {VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE, nullptr, m_readback_memory, 0,
-                               BUFFER_SIZE};
-  vkInvalidateMappedMemoryRanges(g_object_cache->GetDevice(), 1, &range);
+  Util::ExecuteCurrentCommandsAndRestoreState(state_tracker, false, true);
 
   // Cache is now valid.
-  memcpy(m_values.data(), m_readback_map, sizeof(s32) * m_values.size());
+  m_readback_buffer->InvalidateCPUCache();
   m_valid = true;
 }
 
