86f77a9d768ccf42df681724a76550b5ae906efa
EmptyChaos <EmptyChaos@users.noreply.github.com>
2016-08-02 06:23:02 +0000

    WX: HiDPI: NetPlay
    Several refactors of GUI creation into separate functions where the
    function was too large or intermixed different concerns making it hard
    to modify.

--- Source/Core/DolphinWX/NetPlay/NetPlaySetupFrame.h
+++ Source/Core/DolphinWX/NetPlay/NetPlaySetupFrame.h
@@ -12,6 +12,7 @@ class CGameListCtrl;
 class wxCheckBox;
 class wxChoice;
 class wxListBox;
+class wxNotebook;
 class wxSpinCtrl;
 class wxStaticText;
 class wxTextCtrl;
@@ -29,6 +30,9 @@ private:
   static constexpr int DIRECT_CHOICE = 0;
   static constexpr int TRAVERSAL_CHOICE = 1;
 
+  void CreateGUI();
+  wxNotebook* CreateNotebookGUI(wxWindow* parent);
+
   void OnJoin(wxCommandEvent& event);
   void OnHost(wxCommandEvent& event);
   void DoJoin();
@@ -39,7 +43,6 @@ private:
   void OnTraversalListenPortChanged(wxCommandEvent& event);
   void OnKeyDown(wxKeyEvent& event);
   void OnTabChanged(wxCommandEvent& event);
-  void OnAfterTabChange(wxIdleEvent& event);
   void DispatchFocus();
 
   void MakeNetPlayDiag(int port, const std::string& game, bool is_hosting);
@@ -64,5 +67,6 @@ private:
   wxCheckBox* m_upnp_chk;
 #endif
 
+  wxString m_traversal_string;
   const CGameListCtrl* const m_game_list;
 };
