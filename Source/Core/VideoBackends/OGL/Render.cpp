b1ed7e80fb689da5a4679e5c178f91391e55a6ff
Jules Blok <jules.blok@gmail.com>
2016-08-04 14:09:35 +0200

    VertexShaderGen: Clip z using user-defined clipping planes.

--- Source/Core/VideoBackends/OGL/Render.cpp
+++ Source/Core/VideoBackends/OGL/Render.cpp
@@ -724,6 +724,7 @@ Renderer::Renderer()
   glClearDepthf(1.0f);
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LEQUAL);
+  glEnable(GL_CLIP_DISTANCE0);
 
   glPixelStorei(GL_UNPACK_ALIGNMENT, 4);  // 4-byte pixel alignment
 
