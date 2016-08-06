1ae7eab6701a06631a9f9d18ba89a986fa340781
Jules Blok <jules.blok@gmail.com>
2016-08-06 03:40:45 +0200

    VertexShaderGen: Use reversed depth range.

--- Source/Core/VideoBackends/OGL/Render.cpp
+++ Source/Core/VideoBackends/OGL/Render.cpp
@@ -1144,7 +1144,7 @@ void Renderer::SetViewport()
     auto iceilf = [](float f) { return static_cast<GLint>(ceilf(f)); };
     glViewport(iceilf(X), iceilf(Y), iceilf(Width), iceilf(Height));
   }
-  glDepthRangef(0.0f, 16777215.0f / 16777216.0f);
+  glDepthRangef(16777215.0f / 16777216.0f, 0.0f);
 }
 
 void Renderer::ClearScreen(const EFBRectangle& rc, bool colorEnable, bool alphaEnable, bool zEnable,
