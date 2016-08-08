a051db979231cb8440aefbac9f8baf9e5a627916
degasus <wickmarkus@web.de>
2016-08-08 22:35:01 +0200

    PixelEngine: Cleanup PE Token.
    This also affects the behavior a bit, it should now fit better to the one of the commonly more used PE Finish flag.

--- Source/Core/VideoCommon/VideoBackendBase.h
+++ Source/Core/VideoCommon/VideoBackendBase.h
@@ -43,11 +43,6 @@ struct SCPFifoStruct
   volatile u32 CPReadPointer;
   volatile u32 CPBreakpoint;
   volatile u32 SafeCPReadPointer;
-  // Super Monkey Ball Adventure require this.
-  // Because the read&check-PEToken-loop stays in its JITed block I suppose.
-  // So no possiblity to ack the Token irq by the scheduler until some sort of PPC watchdog do its
-  // mess.
-  volatile u16 PEToken;
 
   volatile u32 bFF_GPLinkEnable;
   volatile u32 bFF_GPReadEnable;
