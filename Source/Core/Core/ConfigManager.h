241c85bd33d64042b701aee41aa6b1dcaffe12be
Léo Lam <leo@innovatetechnologi.es>
2016-08-23 16:19:30 +0200

    Add ability to passthrough a Bluetooth adapter
    This adds the ability to passthrough a whole Bluetooth adapter and skip
    the majority of the Bluetooth emulation code. We use libusb to send HCI
    commands, receive HCI events and transfer ACL data directly to the
    first adapter that is found or to a specific adapter (if configured to)
    
    This is possible because the Wii's Bluetooth module is actually just
    a pretty standard Bluetooth adapter…
    
    …except for two vendor-specific commands, for which replies are faked,
    and also for the sync button. This adds a hotkey that works in the
    exact same way as the sync button would on a Wii: it triggers an HCI
    event, which emulated software interpret as a command to perform
    a BT inquiry.
    
    In this mode, the current emulated Wiimote will inevitably not work
    at all, so this commit also disables the "connect Wiimote" menu.

--- Source/Core/Core/ConfigManager.h
+++ Source/Core/Core/ConfigManager.h
@@ -136,6 +136,11 @@ struct SConfig : NonCopyable
   bool m_analytics_enabled;
   bool m_analytics_permission_asked;
 
+  // Bluetooth passthrough mode settings
+  bool m_bt_passthrough_enabled = false;
+  int m_bt_passthrough_pid = -1;
+  int m_bt_passthrough_vid = -1;
+
   // Fifo Player related settings
   bool bLoopFifoReplay;
 
@@ -310,6 +315,7 @@ private:
   void SaveMovieSettings(IniFile& ini);
   void SaveFifoPlayerSettings(IniFile& ini);
   void SaveAnalyticsSettings(IniFile& ini);
+  void SaveBluetoothPassthroughSettings(IniFile& ini);
 
   void LoadGeneralSettings(IniFile& ini);
   void LoadInterfaceSettings(IniFile& ini);
@@ -321,6 +327,7 @@ private:
   void LoadMovieSettings(IniFile& ini);
   void LoadFifoPlayerSettings(IniFile& ini);
   void LoadAnalyticsSettings(IniFile& ini);
+  void LoadBluetoothPassthroughSettings(IniFile& ini);
 
   static SConfig* m_Instance;
 };
