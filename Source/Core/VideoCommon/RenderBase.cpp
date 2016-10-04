c11aba296c383043671c6f627edaf61c6b4769a1
Shawn Hoffman <godisgovernment@gmail.com>
2016-10-03 20:42:04 -0700

    Follow-up for OSD cleanup: people probably want to see decimal instead of hex...

--- Source/Core/VideoCommon/RenderBase.cpp
+++ Source/Core/VideoCommon/RenderBase.cpp
@@ -313,12 +313,12 @@ void Renderer::DrawDebugText()
   if (SConfig::GetInstance().m_ShowFrameCount)
   {
     draw_text(OSD::MessageType::FrameCount,
-              StringFromFormat("Frame: %" PRIx64, Movie::GetCurrentFrame()));
+              StringFromFormat("Frame: %" PRIu64, Movie::GetCurrentFrame()));
 
     if (Movie::IsPlayingInput())
     {
       draw_text(OSD::MessageType::MovieInputCount,
-                StringFromFormat("Input: %" PRIx64 " / %" PRIx64, Movie::GetCurrentInputCount(),
+                StringFromFormat("Input: %" PRIu64 " / %" PRIu64, Movie::GetCurrentInputCount(),
                                  Movie::GetTotalInputCount()));
     }
   }
