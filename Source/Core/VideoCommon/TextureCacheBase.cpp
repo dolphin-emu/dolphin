661f5236c7959c91f56932a8621246ed397f65c9
Stenzek <stenzek@gmail.com>
2016-10-06 00:27:02 +1000

    TextureCache: Don't re-use pooled textures within the same frame
    This is an issue because a driver may have to maintain two copies of a
    texture if it batches all uploads together at the start of a frame.
    
    In the Vulkan backend, we do something similar to avoid breaking out of a
    render pass to copy a texture from the streaming buffer to the destination
    image.
    
    This was causing issues in the sms-bubbles fifolog, where an EFB copy to
    the same address of a previously-used texture caused the previous texture
    to be re-used again for a different image later on in the frame, causing
    the original contents to be discarded.

--- Source/Core/VideoCommon/TextureCacheBase.cpp
+++ Source/Core/VideoCommon/TextureCacheBase.cpp
@@ -1353,7 +1353,7 @@ void TextureCacheBase::CopyRenderTargetToTexture(u32 dstAddr, unsigned int dstFo
 TextureCacheBase::TCacheEntryBase*
 TextureCacheBase::AllocateTexture(const TCacheEntryConfig& config)
 {
-  TexPool::iterator iter = texture_pool.find(config);
+  TexPool::iterator iter = FindMatchingTextureFromPool(config);
   TextureCacheBase::TCacheEntryBase* entry;
   if (iter != texture_pool.end())
   {
@@ -1371,6 +1371,24 @@ TextureCacheBase::AllocateTexture(const TCacheEntryConfig& config)
 
   entry->textures_by_hash_iter = textures_by_hash.end();
   return entry;
+}
+
+TextureCacheBase::TexPool::iterator
+TextureCacheBase::FindMatchingTextureFromPool(const TCacheEntryConfig& config)
+{
+  // Find a texture from the pool that does not have a frameCount of FRAMECOUNT_INVALID.
+  // This prevents a texture from being used twice in a single frame with different data,
+  // which potentially means that a driver has to maintain two copies of the texture anyway.
+  auto range = texture_pool.equal_range(config);
+  for (auto iter = range.first; iter != range.second; iter++)
+  {
+    if (iter->second->frameCount == FRAMECOUNT_INVALID && !iter->second->IsEfbCopy())
+      continue;
+
+    return iter;
+  }
+
+  return texture_pool.end();
 }
 
 TextureCacheBase::TexCache::iterator
