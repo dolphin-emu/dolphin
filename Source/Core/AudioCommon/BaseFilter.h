229ff9961687cfdbf1176f89965261ac80bb7c21
kamiyo <kamiyo@gmail.com>
2016-10-06 12:53:53 -0500

    FIR Resampling, dither, wiimote-emu speaker fix

---
+++ Source/Core/AudioCommon/BaseFilter.h
@@ -0,0 +1,21 @@
+// Copyright 2016 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include "Common/RingBuffer.h"
+
+class BaseFilter
+{
+public:
+  BaseFilter(u32 taps = 0) : m_num_taps(taps) {}
+  ~BaseFilter() {}
+
+  // ratio is out_rate / in_rate
+  virtual void ConvolveStereo(const RingBuffer<float>& input, u32 index,
+                              float* output_l, float* output_r,
+                              const float fraction, const float ratio) const = 0;
+
+  const u32 m_num_taps; // one-sided
+};
--- Source/Core/AudioCommon/Mixer.h
+++ Source/Core/AudioCommon/Mixer.h
@@ -7,28 +7,30 @@
 #include <array>
 #include <atomic>
 
+#include "AudioCommon/BaseFilter.h"
+#include "AudioCommon/Dither.h"
 #include "AudioCommon/WaveFile.h"
 #include "Common/CommonTypes.h"
+#include "Common/RingBuffer.h"
 
 class CMixer final
 {
 public:
-  explicit CMixer(unsigned int BackendSampleRate);
-  ~CMixer();
+  explicit CMixer(u32 BackendSampleRate);
+  ~CMixer() {}
 
   // Called from audio threads
-  unsigned int Mix(short* samples, unsigned int numSamples, bool consider_framelimit = true);
+  u32 Mix(s16* samples, u32 numSamples, bool consider_framelimit = true);
 
   // Called from main thread
-  void PushSamples(const short* samples, unsigned int num_samples);
-  void PushStreamingSamples(const short* samples, unsigned int num_samples);
-  void PushWiimoteSpeakerSamples(const short* samples, unsigned int num_samples,
-                                 unsigned int sample_rate);
-  unsigned int GetSampleRate() const { return m_sampleRate; }
-  void SetDMAInputSampleRate(unsigned int rate);
-  void SetStreamInputSampleRate(unsigned int rate);
-  void SetStreamingVolume(unsigned int lvolume, unsigned int rvolume);
-  void SetWiimoteSpeakerVolume(unsigned int lvolume, unsigned int rvolume);
+  void PushSamples(const s16* samples, u32 num_samples);
+  void PushStreamingSamples(const s16* samples, u32 num_samples);
+  void PushWiimoteSpeakerSamples(u32 wiimote_index, const s16* samples, u32 num_samples, u32 sample_rate);
+  u32 GetSampleRate() const { return m_output_sample_rate; }
+  void SetDMAInputSampleRate(u32 rate);
+  void SetStreamInputSampleRate(u32 rate);
+  void SetStreamingVolume(u32 lvolume, u32 rvolume);
+  void SetWiimoteSpeakerVolume(u32 wiimote_index, u32 lvolume, u32 rvolume);
 
   void StartLogDTKAudio(const std::string& filename);
   void StopLogDTKAudio();
@@ -38,6 +40,7 @@ public:
 
   float GetCurrentSpeed() const { return m_speed.load(); }
   void UpdateSpeed(float val) { m_speed.store(val); }
+
 private:
   static constexpr u32 MAX_SAMPLES = 1024 * 4;  // 128 ms
   static constexpr u32 INDEX_MASK = MAX_SAMPLES * 2 - 1;
@@ -45,42 +48,55 @@ private:
   static constexpr float CONTROL_FACTOR = 0.2f;
   static constexpr u32 CONTROL_AVG = 32;  // In freq_shift per FIFO size offset
 
-  class MixerFifo final
+  class MixerFifo
   {
   public:
-    MixerFifo(CMixer* mixer, unsigned sample_rate)
-        : m_mixer(mixer), m_input_sample_rate(sample_rate)
+    MixerFifo(CMixer* mixer, unsigned sample_rate, std::shared_ptr<BaseFilter> filter = nullptr)
+      : m_mixer(mixer)
+      , m_filter(std::move(filter))
+      , m_input_sample_rate(sample_rate)
     {
+      m_floats.Resize(MAX_SAMPLES * 2);
+      m_shorts.Resize(MAX_SAMPLES * 2);
     }
-    void PushSamples(const short* samples, unsigned int num_samples);
-    unsigned int Mix(short* samples, unsigned int numSamples, bool consider_framelimit = true);
-    void SetInputSampleRate(unsigned int rate);
-    unsigned int GetInputSampleRate() const;
-    void SetVolume(unsigned int lvolume, unsigned int rvolume);
+
+    void PushSamples(const s16* samples, u32 num_samples);
+    u32 Mix(std::array<float, MAX_SAMPLES * 2>& samples, u32 numSamples, bool consider_framelimit = true);
+    void SetInputSampleRate(u32 rate);
+    u32 GetInputSampleRate() const;
+    void SetVolume(u32 lvolume, u32 rvolume);
 
   private:
     CMixer* m_mixer;
+    std::shared_ptr<BaseFilter> m_filter;
+    RingBuffer<float> m_floats;
+    RingBuffer<s16> m_shorts;
+
     unsigned m_input_sample_rate;
-    std::array<short, MAX_SAMPLES * 2> m_buffer{};
-    std::atomic<u32> m_indexW{0};
-    std::atomic<u32> m_indexR{0};
-    // Volume ranges from 0-256
-    std::atomic<s32> m_LVolume{256};
-    std::atomic<s32> m_RVolume{256};
+    
     float m_numLeftI = 0.0f;
-    u32 m_frac = 0;
+    float m_frac = 0;
+    // Volume ranges from 0-256
+    std::atomic<s32> m_LVolume{ 256 };
+    std::atomic<s32> m_RVolume{ 256 };
   };
-  MixerFifo m_dma_mixer{this, 32000};
-  MixerFifo m_streaming_mixer{this, 48000};
-  MixerFifo m_wiimote_speaker_mixer{this, 3000};
-  unsigned int m_sampleRate;
+
+  std::array<float, MAX_SAMPLES * 2> m_accumulator{};
+
+  std::unique_ptr<MixerFifo> m_dma_mixer;
+  std::unique_ptr<MixerFifo> m_streaming_mixer;
+  std::array<std::unique_ptr<MixerFifo>, 4> m_wiimote_speaker_mixers; // max # wiimotes = 4, one mixer per wiimote
+  std::unique_ptr<Dither> m_dither;
 
   WaveFileWriter m_wave_writer_dtk;
   WaveFileWriter m_wave_writer_dsp;
+  WaveFileWriter m_wave_writer_debug;
 
   bool m_log_dtk_audio = false;
   bool m_log_dsp_audio = false;
 
+  u32 m_output_sample_rate;
   // Current rate of emulation (1.0 = 100% speed)
-  std::atomic<float> m_speed{0.0f};
+  std::atomic<float> m_speed{ 0.0f };
+
 };
