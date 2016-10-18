b4744fc39c4572ed0acc29d4b59058e9ad69a2b0
Michael Maltese <michaeljosephmaltese@gmail.com>
2016-10-17 23:00:11 -0700

    ControllerInterface: enable hotplugging on macOS

--- Source/Core/InputCommon/ControllerInterface/OSX/OSXJoystick.mm
+++ Source/Core/InputCommon/ControllerInterface/OSX/OSXJoystick.mm
@@ -270,5 +270,10 @@ std::string Joystick::Hat::GetName() const
 {
   return m_name;
 }
+
+bool Joystick::IsSameDevice(const IOHIDDeviceRef other_device) const
+{
+  return m_device == other_device;
+}
 }
 }
