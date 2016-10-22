20f2955ad965f27938a0e9556ace6b56322d0cd0
Léo Lam <leo@innovatetechnologi.es>
2016-10-22 17:16:33 +0200

    IPC_HLE: Remove dead code (Wiimote and emulated BT)
    Neither needed or used in the emulated Bluetooth code.

--- Source/Core/Core/IPC_HLE/WiiMote_HID_Attr.h
+++ Source/Core/Core/IPC_HLE/WiiMote_HID_Attr.h
@@ -6,23 +6,4 @@
 
 #include "Common/CommonTypes.h"
 
-#if 0
-struct SAttrib
-{
-	u16 ID;
-	u8* pData;
-	u16 size;
-
-	SAttrib(u16 _ID, u8* _Data, u16 _size)
-		: ID(_ID)
-		, pData(_Data)
-		, size(_size)
-	{ }
-};
-
-typedef std::vector<SAttrib> CAttribTable;
-
-const CAttribTable& GetAttribTable();
-#endif
-
 const u8* GetAttribPacket(u32 serviceHandle, u32 cont, u32& _size);
