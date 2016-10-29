21aca8013631afedd4b0efd947665a813e45cec2
Jules Blok <jules.blok@gmail.com>
2016-10-29 17:25:04 +0200

    DriverDetails: Add OS X to the dual-source blending bug.

--- Source/Core/VideoCommon/DriverDetails.h
+++ Source/Core/VideoCommon/DriverDetails.h
@@ -231,12 +231,12 @@ enum Bug
   // index as a MRT location instead, or omit the binding completely.
   BUG_BROKEN_FRAGMENT_SHADER_INDEX_DECORATION,
 
-  // Bug: Dual-source outputs from fragment shaders are broken on AMD OpenGL drivers
+  // Bug: Dual-source outputs from fragment shaders will break some drivers.
   // Started Version: -1
   // Ended Version: -1
-  // Fragment shaders that specify dual-source outputs, cause the driver to crash
-  // sometimes this happens in the kernel mode part of the driver resulting in a BSOD.
-  // Disable dual-source blending support for now.
+  // Fragment shaders that specify dual-source outputs, but don't use them in the blending mode
+  // will cause graphical errors and crashes in some drivers. The workaround is to only use
+  // dual-source blending when absolutely required and not just for blending precision.
   BUG_BROKEN_DUAL_SOURCE_BLENDING,
 };
 
