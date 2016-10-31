f176fcad574f299612d9487760a8e7935d8b91c3
Léo Lam <leo@innovatetechnologi.es>
2016-10-31 21:51:35 +0100

    WiiConfigPane: Add USB passthrough settings
    Allows adding/removing devices from USB passthrough.

--- Source/Core/DolphinWX/Config/WiiConfigPane.h
+++ Source/Core/DolphinWX/Config/WiiConfigPane.h
@@ -4,14 +4,28 @@
 
 #pragma once
 
+#include <utility>
+
 #include <wx/arrstr.h>
+#include <wx/clntdata.h>
 #include <wx/panel.h>
 #include "Common/CommonTypes.h"
 
 class DolphinSlider;
+class wxButton;
 class wxCheckBox;
 class wxChoice;
+class wxListBox;
 class wxSlider;
+class wxTextCtrl;
+
+class USBPassthroughDeviceEntry final : public wxClientData
+{
+public:
+  USBPassthroughDeviceEntry(u16 vid, u16 pid) : m_vid(vid), m_pid(pid) {}
+  const u16 m_vid;
+  const u16 m_pid;
+};
 
 class WiiConfigPane final : public wxPanel
 {
@@ -23,6 +37,8 @@ private:
   void LoadGUIValues();
   void BindEvents();
 
+  void AddUSBPassthroughDeviceToList(const std::pair<u16, u16>& device);
+
   void OnScreenSaverCheckBoxChanged(wxCommandEvent&);
   void OnPAL60CheckBoxChanged(wxCommandEvent&);
   void OnSDCardCheckBoxChanged(wxCommandEvent&);
@@ -30,6 +46,9 @@ private:
   void OnSystemLanguageChoiceChanged(wxCommandEvent&);
   void OnAspectRatioChoiceChanged(wxCommandEvent&);
 
+  void OnUSBPassthroughDevicesChanged(wxCommandEvent&);
+  void OnUSBPassthroughDeviceAdd(wxCommandEvent&);
+  void OnUSBPassthroughDeviceRemove(wxCommandEvent&);
   void OnSensorBarPosChanged(wxCommandEvent&);
   void OnSensorBarSensChanged(wxCommandEvent&);
   void OnSpeakerVolumeChanged(wxCommandEvent&);
@@ -45,6 +64,12 @@ private:
   wxCheckBox* m_connect_keyboard_checkbox;
   wxChoice* m_system_language_choice;
   wxChoice* m_aspect_ratio_choice;
+
+  wxListBox* m_usb_passthrough_devices_listbox;
+  wxButton* m_usb_passthrough_add_device_btn;
+  wxButton* m_usb_passthrough_rem_device_btn;
+  wxTextCtrl* m_usb_passthrough_new_device_vid;
+  wxTextCtrl* m_usb_passthrough_new_device_pid;
 
   wxChoice* m_bt_sensor_bar_pos;
   DolphinSlider* m_bt_sensor_bar_sens;
