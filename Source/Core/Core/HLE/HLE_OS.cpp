ddb0c0e4a10d42f4e75592b218fe88db04b79de7
Sepalani <sepalani@hotmail.fr>
2016-11-26 17:29:43 +0000

    [HLE] Prevent GetStringVA to strip newlines

--- Source/Core/Core/HLE/HLE_OS.cpp
+++ Source/Core/Core/HLE/HLE_OS.cpp
@@ -55,6 +55,8 @@ void HLE_GeneralDebugPrint()
   rGPR[3] = static_cast<u32>(report_message.length());
   NPC = LR;
 
+  if (!report_message.empty() && report_message.back() == '\n')
+    report_message.pop_back();
   NOTICE_LOG(OSREPORT, "%08x->%08x| %s", LR, PC, SHIFTJISToUTF8(report_message).c_str());
 }
 
@@ -159,9 +161,6 @@ std::string GetStringVA(u32 strReg)
       result += string[i];
     }
   }
-
-  if (!result.empty() && result.back() == '\n')
-    result.pop_back();
 
   return result;
 }
