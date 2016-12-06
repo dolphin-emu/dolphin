a6114bad34f85eae346d2c7eec8d2f35c1728cf9
Sepalani <sepalani@hotmail.fr>
2016-12-06 15:43:41 +0000

    Import/Export signature files as CSV

---
+++ Source/Core/Core/PowerPC/SignatureDB/CSVSignatureDB.cpp
@@ -0,0 +1,75 @@
+#include <cstdio>
+#include <fstream>
+#include <sstream>
+
+#include "Common/FileUtil.h"
+#include "Common/Logging/Log.h"
+
+#include "Core/PowerPC/SignatureDB/CSVSignatureDB.h"
+
+// CSV separated with tabs
+// Checksum | Size | Symbol | [Object Location |] Object Name
+bool CSVSignatureDB::Load(const std::string& file_path, SignatureDB::FuncDB& database) const
+{
+  std::string line;
+  std::ifstream ifs;
+  OpenFStream(ifs, file_path, std::ios_base::in);
+
+  if (!ifs)
+    return false;
+  for (size_t i = 1; std::getline(ifs, line); i += 1)
+  {
+    std::istringstream iss(line);
+    u32 checksum, size;
+    std::string tab, symbol, object_location, object_name;
+
+    iss >> std::hex >> checksum >> std::hex >> size;
+    if (iss && std::getline(iss, tab, '\t'))
+    {
+      if (std::getline(iss, symbol, '\t') && std::getline(iss, object_location, '\t'))
+        std::getline(iss, object_name);
+      SignatureDB::DBFunc func;
+      func.name = symbol;
+      func.size = size;
+      // Doesn't have an object location
+      if (object_name.empty())
+      {
+        func.object_name = object_location;
+      }
+      else
+      {
+        func.object_location = object_location;
+        func.object_name = object_name;
+      }
+      database[checksum] = func;
+    }
+    else
+    {
+      WARN_LOG(OSHLE, "CSV database failed to parse line %zu", i);
+    }
+  }
+
+  return true;
+}
+
+bool CSVSignatureDB::Save(const std::string& file_path, const SignatureDB::FuncDB& database) const
+{
+  File::IOFile f(file_path, "w");
+
+  if (!f)
+  {
+    ERROR_LOG(OSHLE, "CSV database save failed");
+    return false;
+  }
+  for (const auto& func : database)
+  {
+    // The object name/location are unused for the time being.
+    // To be implemented.
+    fprintf(f.GetHandle(), "%08x\t%08x\t%s\t%s\t%s\n", func.first, func.second.size,
+            func.second.name.c_str(), func.second.object_location.c_str(),
+            func.second.object_name.c_str());
+  }
+
+  INFO_LOG(OSHLE, "CSV database save successful");
+  return true;
+}
---
+++ Source/Core/Core/PowerPC/SignatureDB/DSYSignatureDB.cpp
@@ -0,0 +1,69 @@
+#include <cstddef>
+#include <cstring>
+
+#include "Common/CommonTypes.h"
+#include "Common/FileUtil.h"
+#include "Common/Logging/Log.h"
+
+#include "Core/PowerPC/SignatureDB/DSYSignatureDB.h"
+
+namespace
+{
+// On-disk format for SignatureDB entries.
+struct FuncDesc
+{
+  u32 checksum;
+  u32 size;
+  char name[128];
+};
+}  // namespace
+
+bool DSYSignatureDB::Load(const std::string& file_path, SignatureDB::FuncDB& database) const
+{
+  File::IOFile f(file_path, "rb");
+
+  if (!f)
+    return false;
+  u32 fcount = 0;
+  f.ReadArray(&fcount, 1);
+  for (size_t i = 0; i < fcount; i++)
+  {
+    FuncDesc temp;
+    memset(&temp, 0, sizeof(temp));
+
+    f.ReadArray(&temp, 1);
+    temp.name[sizeof(temp.name) - 1] = 0;
+
+    SignatureDB::DBFunc func;
+    func.name = temp.name;
+    func.size = temp.size;
+    database[temp.checksum] = func;
+  }
+
+  return true;
+}
+
+bool DSYSignatureDB::Save(const std::string& file_path, const SignatureDB::FuncDB& database) const
+{
+  File::IOFile f(file_path, "wb");
+
+  if (!f)
+  {
+    ERROR_LOG(OSHLE, "Database save failed");
+    return false;
+  }
+  u32 fcount = static_cast<u32>(database.size());
+  f.WriteArray(&fcount, 1);
+  for (const auto& entry : database)
+  {
+    FuncDesc temp;
+    memset(&temp, 0, sizeof(temp));
+    temp.checksum = entry.first;
+    temp.size = entry.second.size;
+    strncpy(temp.name, entry.second.name.c_str(), 127);
+    f.WriteArray(&temp, 1);
+  }
+
+  INFO_LOG(OSHLE, "Database save successful");
+  return true;
+}
