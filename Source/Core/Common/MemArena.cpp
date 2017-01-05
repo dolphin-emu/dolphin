6e0ae077206273a6be57b46039996cccf4487013
sagb <sagb@users.noreply.github.com>
2017-01-05 08:17:48 -0500

    Prevent mmap failure, closes #10016

--- Source/Core/Common/MemArena.cpp
+++ Source/Core/Common/MemArena.cpp
@@ -100,7 +100,7 @@ void* MemArena::CreateView(s64 offset, size_t size, void* base)
   return MapViewOfFileEx(hMemoryMapping, FILE_MAP_ALL_ACCESS, 0, (DWORD)((u64)offset), size, base);
 #else
   void* retval = mmap(base, size, PROT_READ | PROT_WRITE,
-                      MAP_SHARED | ((base == nullptr) ? 0 : MAP_FIXED), fd, offset);
+                      MAP_SHARED | ((base == nullptr) ? 0 : MAP_FIXED) | MAP_ANONYMOUS, fd, offset);
 
   if (retval == MAP_FAILED)
   {
