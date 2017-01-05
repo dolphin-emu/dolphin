5d456fafc6ceb24a522fbda1296728ef33432775
System25 <amp_21004@yahoo.es>
2017-01-05 15:43:06 +0100

    Added tests for C8 formats

--- Source/UnitTests/VideoCommon/TextureDecoderTest.cpp
+++ Source/UnitTests/VideoCommon/TextureDecoderTest.cpp
@@ -26,6 +26,8 @@ void printTestImage(u32* img);
 
 int testDecoder_DecodeC4(u32 *example, u32 *dst, u8 *src, int width, int height, int texformat, u16 *tlut, TlutFormat tlutfmt);
 
+int testDecoder_DecodeC8(u32 *example, u32 *dst, u8 *src, int width, int height, int texformat, u16 *tlut, TlutFormat tlutfmt);
+
 /* Print a test image in the console */
 void printTestImage(u32* img) {
 	int i, j;
@@ -71,6 +73,36 @@ int testDecoder_DecodeC4(u32 *example, u32 *dst, u8 *src, int width, int height,
 	return comp;
 }
 
+
+/* Test a C8 (8 bit color index, 8x8 tiles) texture decoder */
+int testDecoder_DecodeC8(u32 *example, u32 *dst, u8 *src, int width, int height, int texformat, u16 *tlut, TlutFormat tlutfmt) {
+	int comp = 0;
+	int i, j, ti, tj, val, index;
+
+	// Initialize texture (8x8 tiles)
+	index = 0;
+	for (j=0; j<TEST_HEIGHT; j+=4) {
+		for (i=0; i<TEST_WIDTH; i+=8) {
+			for (tj=0; tj < 4; tj++) {
+				val = 0;
+				if ((i/8)%2 == 1) {
+					val = 8;
+				}
+				for (ti=0; ti < 8; ti++) {
+					src[index++] = val++;
+				}
+			}
+		}
+	}
+
+	// Run test
+	TexDecoder_Decode((u8*) dst, (u8*) src, width, height, texformat, (u8*) tlut, tlutfmt);
+
+	// Compare the results
+	comp = memcmp ( &example[0], &dst[0], TEST_WIDTH*TEST_HEIGHT*4);
+
+	return comp;
+}
 
 
 /* CI4 (4 bit color index, 8x8 tiles) in IA8 format */
@@ -328,4 +360,275 @@ TEST (TextureDecoderTest, DecodeBytes_I4) {
 
 }
 
+
+
+/* C8 (8 bit color index, 8x8 tiles) in IA8 format */
+TEST (TextureDecoderTest, DecodeBytes_C8_IA8) { 
+	int comp = 0;
+	int width = TEST_WIDTH;
+    int height = TEST_HEIGHT;
+	u32 dst[TEST_WIDTH*TEST_HEIGHT];
+	u32 example[TEST_WIDTH*TEST_HEIGHT];
+	u8 src[TEST_HEIGHT*(TEST_WIDTH)];
+    int texformat = GX_TF_C8;
+
+	// 8 bit color index = 256 colors
+    u16 tlut[256];
+    TlutFormat tlutfmt = GX_TL_IA8;
+
+	int i, j, val;
+
+	// Initialize the color index
+	for (i = 0; i < 256; i++) {
+		tlut[i] = 0;
+	}
+
+    tlut[0] = 0x00FF; // Black
+	tlut[1] = 0x10FF;
+	tlut[2] = 0x20FF;
+	tlut[3] = 0x30FF;
+	tlut[4] = 0x40FF;
+	tlut[5] = 0x50FF;
+	tlut[6] = 0x60FF;
+	tlut[7] = 0x70FF;
+	tlut[8] = 0x80FF; // Gray
+	tlut[9] = 0x90FF;
+	tlut[10] = 0xA0FF;
+	tlut[11] = 0xB0FF;
+	tlut[12] = 0xC0FF; // Silver
+	tlut[13] = 0xD0FF;
+	tlut[14] = 0xE0FF;
+    tlut[15] = 0xFFFF; // White
+
+	// Initizatize the example
+	for (i=0; i < TEST_WIDTH*TEST_HEIGHT; i+=16) {
+		for (j=0; j<16; j++) {
+			val = (tlut[j] >> 8) & 0xFF;
+			example[i+j] = (0xFF << 24) | (val << 16) | (val << 8) | val;
+		}
+	}
+
+	comp = testDecoder_DecodeC8(example, dst, src, width, height, texformat, tlut, tlutfmt);
+
+	if (comp != 0) {
+		printf("Decoding error=\n");
+		printf("EXAMPLE=\n");
+		printTestImage(example);
+
+		printf("DECODED=\n");
+		printTestImage(dst);
+	}
+
+
+    EXPECT_EQ (0, comp);
+}
+
+
+/* C8 (8 bit color index, 8x8 tiles) in RGB565 format */
+TEST (TextureDecoderTest, DecodeBytes_C8_RGB565) { 
+	int r,g,b;
+	int comp = 0;
+	int width = TEST_WIDTH;
+    int height = TEST_HEIGHT;
+	u32 dst[TEST_WIDTH*TEST_HEIGHT];
+	u32 example[TEST_WIDTH*TEST_HEIGHT];
+	u8 src[TEST_HEIGHT*TEST_WIDTH];
+    int texformat = GX_TF_C8;
+
+	// 8 bit color index = 256 colors
+    u16 tlut[256];
+    TlutFormat tlutfmt = GX_TL_RGB565;
+
+	int i, j, val;
+
+	// Initialize the color index
+	for (i = 0; i < 256; i++) {
+		tlut[i] = 0;
+	}
+
+    tlut[0] = 0x0000; // Black
+	tlut[1] = 0xF800; // Red
+	tlut[2] = 0x07E0; // Green
+	tlut[3] = 0x001F; // Blue
+
+	tlut[4] = 0x000F;
+	tlut[5] = 0x00F0;
+	tlut[6] = 0x0F00;
+	tlut[7] = 0xF000;
+
+	tlut[8] = 0x1111; // Gray
+	tlut[9] = 0x2222;
+	tlut[10] = 0x3333;
+	tlut[11] = 0x4444;
+
+	tlut[12] = 0x5555; 
+	tlut[13] = 0x6666;
+	tlut[14] = 0x7777;
+    tlut[15] = 0xFFFF; // White
+
+
+	// Swap the color bytes to make them BIG ENDIAN
+	for (i=0; i<16; i++) {
+		tlut[i] = Common::swap16(tlut[i]);
+	}
+	
+
+	// Initizatize the example
+	for (i=0; i < TEST_WIDTH*TEST_HEIGHT; i+=16) {
+		for (j=0; j<16; j++) {
+			val = Common::swap16(tlut[j]) & 0xFFFF;
+
+			r = ((val >> 11) & 0x1f) << 3;
+			g = ((val >> 5) & 0x3f) << 2;
+			b = ((val)&0x1f) << 3;
+			example[i+j] = (0xFF << 24) | (b << 16) | (g << 8) | r;
+		}
+	}
+
+	comp = testDecoder_DecodeC8(example, dst, src, width, height, texformat, tlut, tlutfmt);
+
+	if (comp != 0) {
+		printf("Decoding error=\n");
+		printf("EXAMPLE=\n");
+		printTestImage(example);
+
+		printf("DECODED=\n");
+		printTestImage(dst);
+	}
+
+
+    EXPECT_EQ (0, comp);
+
+}
+
+
+
+/* CI8 (8 bit color index, 8x8 tiles) in RGB5A3 format */
+TEST (TextureDecoderTest, DecodeBytes_C8_RGB5A3) { 
+	int r,g,b, a;
+	int comp = 0;
+	int width = TEST_WIDTH;
+    int height = TEST_HEIGHT;
+	u32 dst[TEST_WIDTH*TEST_HEIGHT];
+	u32 example[TEST_WIDTH*TEST_HEIGHT];
+	u8 src[TEST_HEIGHT*TEST_WIDTH];
+    int texformat = GX_TF_C8;
+
+	// 8 bit color index = 256 colors
+    u16 tlut[256];
+
+    TlutFormat tlutfmt = GX_TL_RGB5A3;
+
+	int i, j, val;
+
+	// Initialize the color index
+	for (i = 0; i < 256; i++) {
+		tlut[i] = 0;
+	}
+
+    tlut[0] = 0x7000; // Black
+	tlut[1] = 0x7F00; // Red
+	tlut[2] = 0x70F0; // Green
+	tlut[3] = 0x700F; // Blue
+
+	tlut[4] = 0x0000; // Transparent
+	tlut[5] = 0x5F00;
+	tlut[6] = 0x50F0;
+	tlut[7] = 0x500F;
+
+	tlut[8] = 0xFC00; // Red
+	tlut[9] = 0x83E0; // Green
+	tlut[10] = 0x801F; // Blue
+	tlut[11] = 0x4444;
+
+	tlut[12] = 0x5555; 
+	tlut[13] = 0x6666;
+	tlut[14] = 0x7777;
+    tlut[15] = 0xFFFF; // White
+
+
+	// Swap the color bytes to make them BIG ENDIAN
+	for (i=0; i<16; i++) {
+		tlut[i] = Common::swap16(tlut[i]);
+	}
+	
+
+	// Initizatize the example
+	for (i=0; i < TEST_WIDTH*TEST_HEIGHT; i+=16) {
+		for (j=0; j<16; j++) {
+			val = Common::swap16(tlut[j]) & 0xFFFF;
+
+		  if ((val & 0x8000))
+		  {
+			r = ((val >> 10) & 0x1f) << 3;
+			g = ((val >> 5) & 0x1f) << 3;
+			b = ((val)&0x1f) << 3;
+			a = 0xFF;
+		  }
+		  else
+		  {
+			a = ((val >> 12) & 0x7) << 5;
+			r = ((val >> 8) & 0xf) << 4;
+			g = ((val >> 4) & 0xf) << 4;
+			b = ((val)&0xf) << 4;
+		  }
+		  example[i+j] = ( r | (g << 8) | (b << 16) | (a << 24) );
+		}
+	}
+
+	comp = testDecoder_DecodeC8(example, dst, src, width, height, texformat, tlut, tlutfmt);
+
+	if (comp != 0) {
+		printf("Decoding error=\n");
+		printf("EXAMPLE=\n");
+		printTestImage(example);
+
+		printf("DECODED=\n");
+		printTestImage(dst);
+	}
+
+
+    EXPECT_EQ (0, comp);
+
+}
+
+
+/* I8 (8 bit gray scale color, 8x8 tiles) */
+TEST (TextureDecoderTest, DecodeBytes_I8) { 
+	int comp = 0;
+	int width = TEST_WIDTH;
+    int height = TEST_HEIGHT;
+	u32 dst[TEST_WIDTH*TEST_HEIGHT];
+	u32 example[TEST_WIDTH*TEST_HEIGHT];
+	u8 src[TEST_HEIGHT*TEST_WIDTH];
+    int texformat = GX_TF_I8;
+
+    u16* tlut = NULL;
+    TlutFormat tlutfmt = (TlutFormat) 0;
+
+	int i, j, val;
+
+	// Initizatize the example
+	for (i=0; i < TEST_WIDTH*TEST_HEIGHT; i+=16) {
+		for (j=0; j<16; j++) {
+		  val = j;
+		  example[i+j] = ( val | (val << 8) | (val << 16) | (0xFF << 24) );
+		}
+	}
+
+	comp = testDecoder_DecodeC8(example, dst, src, width, height, texformat, tlut, tlutfmt);
+
+	if (comp != 0) {
+		printf("Decoding error=\n");
+		printf("EXAMPLE=\n");
+		printTestImage(example);
+
+		printf("DECODED=\n");
+		printTestImage(dst);
+	}
+
+
+    EXPECT_EQ (0, comp);
+
+}
 
