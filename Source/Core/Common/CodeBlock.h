9d8e8652fb17a802a74369f46a5033970fb92a89
Lioncash <mathew1800@gmail.com>
2017-01-07 18:10:39 -0500

    CodeBlock: Get rid of implicit sign-conversion in AllocCodeSpace
    Size is internally stored as a size_t, so having an int parameter
    would cause implicit sign-conversion from a signed value to an
    unsigned value to occur.

--- Source/Core/Common/CodeBlock.h
+++ Source/Core/Common/CodeBlock.h
@@ -4,7 +4,10 @@
 
 #pragma once
 
+#include <cstddef>
+
 #include "Common/Assert.h"
+#include "Common/CommonTypes.h"
 #include "Common/MemoryUtil.h"
 #include "Common/NonCopyable.h"
 
@@ -39,7 +42,7 @@ public:
   }
 
   // Call this before you generate any code.
-  void AllocCodeSpace(int size, bool need_low = true)
+  void AllocCodeSpace(size_t size, bool need_low = true)
   {
     region_size = size;
     region = static_cast<u8*>(Common::AllocateExecutableMemory(region_size, need_low));
--- Source/Core/Core/PowerPC/Jit64Common/FarCodeCache.h
+++ Source/Core/Core/PowerPC/Jit64Common/FarCodeCache.h
@@ -4,19 +4,20 @@
 
 #pragma once
 
+#include <cstddef>
 #include "Common/x64Emitter.h"
 
 // a bit of a hack; the MMU results in a vast amount more code ending up in the far cache,
 // mostly exception handling, so give it a whole bunch more space if the MMU is on.
-constexpr int FARCODE_SIZE = 1024 * 1024 * 8;
-constexpr int FARCODE_SIZE_MMU = 1024 * 1024 * 48;
+constexpr size_t FARCODE_SIZE = 1024 * 1024 * 8;
+constexpr size_t FARCODE_SIZE_MMU = 1024 * 1024 * 48;
 
 // A place to throw blocks of code we don't want polluting the cache, e.g. rarely taken
 // exception branches.
 class FarCodeCache : public Gen::X64CodeBlock
 {
 public:
-  void Init(int size);
+  void Init(size_t size);
   void Shutdown();
 
   bool Enabled() const;
