47b3a9c68ed1cc2879a4af3938c40022d36fa79b
Chris Burgener <christhecoolist@gmail.com>
2017-01-08 13:51:00 -0500

    Add Silent Audio Dumping INI Option

--- Source/Core/Core/ConfigManager.cpp
+++ Source/Core/Core/ConfigManager.cpp
@@ -322,6 +322,7 @@ void SConfig::SaveDSPSettings(IniFile& ini)
 
   dsp->Set("EnableJIT", m_DSPEnableJIT);
   dsp->Set("DumpAudio", m_DumpAudio);
+  dsp->Set("DumpAudioSilent", m_DumpAudioSilent);
   dsp->Set("DumpUCode", m_DumpUCode);
   dsp->Set("Backend", sBackend);
   dsp->Set("Volume", m_Volume);
@@ -643,6 +644,7 @@ void SConfig::LoadDSPSettings(IniFile& ini)
 
   dsp->Get("EnableJIT", &m_DSPEnableJIT, true);
   dsp->Get("DumpAudio", &m_DumpAudio, false);
+  dsp->Get("DumpAudioSilent", &m_DumpAudioSilent, false);
   dsp->Get("DumpUCode", &m_DumpUCode, false);
 #if defined __linux__ && HAVE_ALSA
   dsp->Get("Backend", &sBackend, BACKEND_ALSA);
