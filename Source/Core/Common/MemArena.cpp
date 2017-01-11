e304ac12397fd898d270b58f33d9224af14fd976
sa GB <sa-dev@rainbow.by>
2017-01-11 13:21:34 +0300

    lint cosmetics

--- Source/Core/Common/MemArena.cpp
+++ Source/Core/Common/MemArena.cpp
@@ -99,9 +99,10 @@ void* MemArena::CreateView(s64 offset, size_t size, void* base)
 #ifdef _WIN32
   return MapViewOfFileEx(hMemoryMapping, FILE_MAP_ALL_ACCESS, 0, (DWORD)((u64)offset), size, base);
 #else
-  void* retval = mmap(base, size, PROT_READ | PROT_WRITE,
-                      MAP_SHARED | ((base == nullptr) ? 0 : MAP_FIXED) |
-                      ((fd == -1) ? MAP_ANONYMOUS : 0), fd, offset);
+  void* retval =
+      mmap(base, size, PROT_READ | PROT_WRITE,
+           MAP_SHARED | ((base == nullptr) ? 0 : MAP_FIXED) | ((fd == -1) ? MAP_ANONYMOUS : 0), fd,
+           offset);
 
   if (retval == MAP_FAILED)
   {
