d266be5b568596a6a2e48734bd79e281a8193a9b
Lioncash <mathew1800@gmail.com>
2017-01-18 22:52:56 -0500

    PowerPC: Explicitly savestate PowerPCState members
    Makes it more obvious which data is going into the savestate.
    It also allows PowerPCState and InstructionCache to potentially
    contain members that don't necessarily need to be saved to the save state.
    
    It also gets rid of any potential padding data being put into the save
    state.

--- Source/Core/Core/State.cpp
+++ Source/Core/Core/State.cpp
@@ -71,7 +71,7 @@ static Common::Event g_compressAndDumpStateSyncEvent;
 static std::thread g_save_thread;
 
 // Don't forget to increase this after doing changes on the savestate system
-static const u32 STATE_VERSION = 70;  // Last changed in PR 4679
+static const u32 STATE_VERSION = 71;  // Last changed in PR 4687
 
 // Maps savestate versions to Dolphin versions.
 // Versions after 42 don't need to be added to this list,
