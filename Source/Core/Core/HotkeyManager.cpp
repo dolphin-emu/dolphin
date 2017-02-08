8adad0729e2aaf56158894fefffa633f5d888fcc
Michael Maltese <michaeljosephmaltese@gmail.com>
2017-02-07 18:26:06 -0800

    Implement hotkey options group in HotkeyInputConfigDiag

--- Source/Core/Core/HotkeyManager.cpp
+++ Source/Core/Core/HotkeyManager.cpp
@@ -14,7 +14,6 @@
 #include "InputCommon/ControllerEmu/Control/Input.h"
 #include "InputCommon/ControllerEmu/ControlGroup/Buttons.h"
 #include "InputCommon/ControllerEmu/ControlGroup/ControlGroup.h"
-#include "InputCommon/ControllerEmu/Setting/BackgroundInputSetting.h"
 #include "InputCommon/ControllerEmu/Setting/BooleanSetting.h"
 #include "InputCommon/GCPadStatus.h"
 
@@ -228,11 +227,6 @@ ControllerEmu::ControlGroup* GetHotkeyGroup(HotkeyGroup group)
   return static_cast<HotkeyManager*>(s_config.GetController(0))->GetHotkeyGroup(group);
 }
 
-ControllerEmu::ControlGroup* GetOptionsGroup()
-{
-  return static_cast<HotkeyManager*>(s_config.GetController(0))->GetOptionsGroup();
-}
-
 void Shutdown()
 {
   s_config.ClearControllers();
@@ -272,12 +266,6 @@ HotkeyManager::HotkeyManager()
       m_keys[group]->controls.emplace_back(new ControllerEmu::Input(hotkey_labels[key]));
     }
   }
-
-  groups.emplace_back(m_options = new ControllerEmu::ControlGroup(_trans("Options")));
-  m_options->boolean_settings.emplace_back(
-      std::make_unique<ControllerEmu::BackgroundInputSetting>(_trans("Background Input")));
-  m_options->boolean_settings.emplace_back(std::make_unique<ControllerEmu::BooleanSetting>(
-      _trans("Iterative Input"), false, ControllerEmu::SettingType::VIRTUAL));
 }
 
 HotkeyManager::~HotkeyManager()
@@ -307,11 +295,6 @@ void HotkeyManager::GetInput(HotkeyStatus* const kb)
 ControllerEmu::ControlGroup* HotkeyManager::GetHotkeyGroup(HotkeyGroup group) const
 {
   return m_hotkey_groups[group];
-}
-
-ControllerEmu::ControlGroup* HotkeyManager::GetOptionsGroup() const
-{
-  return m_options;
 }
 
 int HotkeyManager::FindGroupByID(int id) const
