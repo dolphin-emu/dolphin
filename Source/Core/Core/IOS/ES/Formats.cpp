bc1990ce5d38397b25233333ff65bc2895d3f659
Sepalani <sepalani@hotmail.fr>
2017-03-06 01:41:42 +0000

    Fix a vector assertion introduced by PR#5002

--- Source/Core/Core/IOS/ES/Formats.cpp
+++ Source/Core/Core/IOS/ES/Formats.cpp
@@ -258,7 +258,7 @@ std::vector<u8> TicketReader::GetRawTicketView(u32 ticket_num) const
   std::memcpy(view.data(), &view_id, sizeof(view_id));
 
   // Copy the rest of the ticket view structure from the ticket.
-  view.insert(view.end(), view_start, view_start + sizeof(TicketView) - sizeof(view_id));
+  view.insert(view.end(), view_start, view_start + (sizeof(TicketView) - sizeof(view_id)));
   _assert_(view.size() == sizeof(TicketView));
 
   return view;
