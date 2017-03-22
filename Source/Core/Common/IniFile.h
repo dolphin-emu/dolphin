99adc7338360eb5723b3325e5829b417e4d3cf08
Lioncash <mathew1800@gmail.com>
2017-03-22 18:07:23 -0400

    IniFile: Make Section constructor explicit

--- Source/Core/Common/IniFile.h
+++ Source/Core/Common/IniFile.h
@@ -30,7 +30,7 @@ public:
 
   public:
     Section() {}
-    Section(const std::string& _name) : name(_name) {}
+    explicit Section(const std::string& name_) : name(name_) {}
     bool Exists(const std::string& key) const;
     bool Delete(const std::string& key);
 
