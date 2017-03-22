189b6368a6d49bc7be8d35c9f0b44ad1e2060cd6
Anthony <aserna3@gmail.com>
2017-03-21 17:26:01 -0700

    Revert "Merge pull request #4916 from leoetlino/cpp-optparse"
    This reverts commit 06428c3f14485e644006dcbb6329a4591b1d82ec, reversing
    changes made to 722ff4c020b07ca0a285c70e26923cdab5623589.
    
    # Conflicts:
    #	Source/Core/DolphinNoGUI/MainNoGUI.cpp
    #	Source/Core/DolphinWX/Main.cpp
    #	Source/Core/DolphinWX/Main.h
    #	Source/Core/UICommon/CMakeLists.txt
    #	Source/Core/UICommon/CommandLineParse.cpp

--- Source/Core/DolphinWX/Main.h
+++ Source/Core/DolphinWX/Main.h
@@ -8,7 +8,6 @@
 #include <wx/app.h>
 
 class CFrame;
-class wxCmdLineParser;
 class wxLocale;
 
 extern CFrame* main_frame;
@@ -22,9 +21,9 @@ public:
 
 private:
   bool OnInit() override;
+  int OnExit() override;
   void OnInitCmdLine(wxCmdLineParser& parser) override;
   bool OnCmdLineParsed(wxCmdLineParser& parser) override;
-  int OnExit() override;
   void OnFatalException() override;
   bool Initialize(int& c, wxChar** v) override;
 
@@ -38,8 +37,6 @@ private:
   void OnActivate(wxActivateEvent& ev);
   void OnIdle(wxIdleEvent&);
 
-  void ParseCommandLine();
-
   bool m_batch_mode = false;
   bool m_confirm_stop = false;
   bool m_is_active = true;
@@ -49,7 +46,7 @@ private:
   bool m_use_logger = false;
   bool m_select_video_backend = false;
   bool m_select_audio_emulation = false;
-  bool m_confirm_setting;
+  wxString m_confirm_setting;
   wxString m_video_backend_name;
   wxString m_audio_emulation_name;
   wxString m_user_path;
