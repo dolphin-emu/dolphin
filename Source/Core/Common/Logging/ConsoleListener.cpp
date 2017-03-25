b0e6c015c31dcd658a7d30145df670d6b6a02516
Michael Maltese <michaeljosephmaltese@gmail.com>
2017-03-24 22:52:58 -0700

    combine console listeners

--- Source/Core/Common/Logging/ConsoleListener.cpp
+++ Source/Core/Common/Logging/ConsoleListener.cpp
@@ -2,28 +2,63 @@
 // Licensed under GPLv2+
 // Refer to the license.txt file included.
 
+#ifdef _WIN32
+#include <windows.h>
+#elif defined(__ANDROID__)
+#include <android/log.h>
+#else
 #include <cstdio>
 #include <cstring>
-
-#ifndef _WIN32
 #include <unistd.h>
+
+#include "Common/Logging/Log.h"
 #endif
 
 #include "Common/Logging/ConsoleListener.h"
-#include "Common/Logging/Log.h"
 
 ConsoleListener::ConsoleListener()
 {
+#if !defined(_WIN32) && !defined(__ANDROID__)
   m_use_color = !!isatty(fileno(stdout));
+#endif
 }
 
 ConsoleListener::~ConsoleListener()
 {
+#if !defined(_WIN32) && !defined(__ANDROID__)
   fflush(nullptr);
+#endif
 }
 
 void ConsoleListener::Log(LogTypes::LOG_LEVELS level, const char* text)
 {
+#ifdef _WIN32
+  ::OutputDebugStringA(text);
+#elif defined(__ANDROID__)
+  android_LogPriority logLevel = ANDROID_LOG_UNKNOWN;
+
+  // Map dolphin's log levels to android's
+  switch (level)
+  {
+  case LogTypes::LOG_LEVELS::LDEBUG:
+    logLevel = ANDROID_LOG_DEBUG;
+    break;
+  case LogTypes::LOG_LEVELS::LINFO:
+    logLevel = ANDROID_LOG_INFO;
+    break;
+  case LogTypes::LOG_LEVELS::LWARNING:
+    logLevel = ANDROID_LOG_WARN;
+    break;
+  case LogTypes::LOG_LEVELS::LERROR:
+    logLevel = ANDROID_LOG_ERROR;
+    break;
+  case LogTypes::LOG_LEVELS::LNOTICE:
+    logLevel = ANDROID_LOG_INFO;
+    break;
+  }
+
+  __android_log_write(logLevel, "Dolphinemu", text);
+#else
   char color_attr[16] = "";
   char reset_attr[16] = "";
 
@@ -49,4 +84,5 @@ void ConsoleListener::Log(LogTypes::LOG_LEVELS level, const char* text)
     }
   }
   fprintf(stderr, "%s%s%s", color_attr, text, reset_attr);
+#endif
 }
