6a6bfa0dbb15b73cdf6c50ae3888f98d85f489a7
Michael Maltese <michaeljosephmaltese@gmail.com>
2017-04-25 17:32:54 -0700

    FifoPlayer: subsume FifoFileStruct.h into FifoDataFile.cpp
    There's no reason for it to be in its own file, and it's confusing.

--- Source/Core/Core/FifoPlayer/FifoDataFile.cpp
+++ Source/Core/Core/FifoPlayer/FifoDataFile.cpp
@@ -9,9 +9,59 @@
 #include "Common/FileUtil.h"
 
 #include "Core/FifoPlayer/FifoDataFile.h"
-#include "Core/FifoPlayer/FifoFileStruct.h"
 
-using namespace FifoFileStruct;
+enum
+{
+  FILE_ID = 0x0d01f1f0,
+  VERSION_NUMBER = 4,
+  MIN_LOADER_VERSION = 1,
+};
+
+#pragma pack(push, 1)
+
+struct FileHeader
+{
+  u32 fileId;
+  u32 file_version;
+  u32 min_loader_version;
+  u64 bpMemOffset;
+  u32 bpMemSize;
+  u64 cpMemOffset;
+  u32 cpMemSize;
+  u64 xfMemOffset;
+  u32 xfMemSize;
+  u64 xfRegsOffset;
+  u32 xfRegsSize;
+  u64 frameListOffset;
+  u32 frameCount;
+  u32 flags;
+  u64 texMemOffset;
+  u32 texMemSize;
+  u8 reserved[40];
+};
+
+struct FileFrameInfo
+{
+  u64 fifoDataOffset;
+  u32 fifoDataSize;
+  u32 fifoStart;
+  u32 fifoEnd;
+  u64 memoryUpdatesOffset;
+  u32 numMemoryUpdates;
+  u8 reserved[32];
+};
+
+struct FileMemoryUpdate
+{
+  u32 fifoPosition;
+  u32 address;
+  u64 dataOffset;
+  u32 dataSize;
+  u8 type;
+  u8 reserved[3];
+};
+
+#pragma pack(pop)
 
 FifoDataFile::FifoDataFile() = default;
 
