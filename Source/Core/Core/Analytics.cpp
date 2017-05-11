0ba995e68dbcd91d3a3c09dacb7baad00fcbd652
Augustin Cavalier <waddlesplash@gmail.com>
2017-05-11 19:51:19 -0400

    Analytics: Add missing os-type cases, and make an unset os-type an error.

--- Source/Core/Core/Analytics.cpp
+++ Source/Core/Core/Analytics.cpp
@@ -171,8 +171,14 @@ void DolphinAnalytics::MakeBaseBuilder()
   builder.AddData("os-type", "linux");
 #elif defined(__FreeBSD__)
   builder.AddData("os-type", "freebsd");
+#elif defined(__OpenBSD__)
+  builder.AddData("os-type", "openbsd");
+#elif defined(__NetBSD__)
+  builder.AddData("os-type", "netbsd");
+#elif defined(__HAIKU__)
+  builder.AddData("os-type", "haiku");
 #else
-  builder.AddData("os-type", "unknown");
+#error os-type not set
 #endif
 
   m_base_builder = builder;
