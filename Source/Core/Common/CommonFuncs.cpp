5b7f99e57a09c8e026af5893e5c2b7aa2b1622d6
JosJuice <josjuice@gmail.com>
2017-05-13 11:22:30 +0200

    Try to make sure that we have the XSI version of strerror_r

--- Source/Core/Common/CommonFuncs.cpp
+++ Source/Core/Common/CommonFuncs.cpp
@@ -2,8 +2,20 @@
 // Licensed under GPLv2+
 // Refer to the license.txt file included.
 
-#include <cstddef>
+// The code in GetErrorMessage can't handle some systems having the
+// GNU version of strerror_r and other systems having the XSI version,
+// so we undefine _GNU_SOURCE here in an attempt to always get the XSI version.
+// We include cstring before all other headers in case cstring is included
+// indirectly (without undefining _GNU_SOURCE) by some other header.
+#ifdef _GNU_SOURCE
+#undef _GNU_SOURCE
 #include <cstring>
+#define _GNU_SOURCE
+#else
+#include <cstring>
+#endif
+
+#include <cstddef>
 #include <errno.h>
 
 #include "Common/CommonFuncs.h"
@@ -24,8 +36,11 @@ std::string GetLastErrorMsg()
   FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, GetLastError(),
                  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), err_str, buff_size, nullptr);
 #else
-  // Thread safe (XSI-compliant)
-  if (strerror_r(errno, err_str, buff_size))
+  // We assume that the XSI-compliant version of strerror_r (returns int) is used
+  // rather than the GNU version (returns char*). The returned value is stored to
+  // an int variable to get a compile-time check that the return type is not char*.
+  const int result = strerror_r(errno, err_str, buff_size);
+  if (result != 0)
     return "";
 #endif
 
