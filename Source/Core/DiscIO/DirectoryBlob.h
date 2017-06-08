bf46096ad6e02cc701b90a3d6cb5596e2f11ee3e
JosJuice <josjuice@gmail.com>
2017-06-08 22:11:31 +0200

    DirectoryBlob: Support tickets and TMDs

--- Source/Core/DiscIO/DirectoryBlob.h
+++ Source/Core/DiscIO/DirectoryBlob.h
@@ -118,6 +118,13 @@ private:
   std::vector<u8> m_disk_header;
 
 #pragma pack(push, 1)
+  struct TMDHeader
+  {
+    u32 tmd_size;
+    u32 tmd_offset;
+  } m_tmd_header;
+  static_assert(sizeof(TMDHeader) == 8, "Wrong size for TMDHeader");
+
   struct SDiskHeaderInfo
   {
     u32 debug_monitor_size;
@@ -144,6 +151,7 @@ private:
       unknown2 = 0;
     }
   };
+  static_assert(sizeof(SDiskHeaderInfo) == 36, "Wrong size for SDiskHeaderInfo");
 #pragma pack(pop)
   std::unique_ptr<SDiskHeaderInfo> m_disk_header_info;
 
