6ae8d371e18df6f0d66fe4bd78f65f0da5ca2b5e
JosJuice <josjuice@gmail.com>
2017-06-11 14:53:53 +0200

    DolphinWX: Increment game list cache version
    This is so that DirectoryBlobs won't look like valid discs
    in old versions that don't support the DirectoryBlob format.

--- Source/Core/DolphinWX/ISOFile.cpp
+++ Source/Core/DolphinWX/ISOFile.cpp
@@ -38,7 +38,7 @@
 #include "DolphinWX/ISOFile.h"
 #include "DolphinWX/WxUtils.h"
 
-static const u32 CACHE_REVISION = 0x129;  // Last changed in PR 5102
+static const u32 CACHE_REVISION = 0x12A;  // Last changed in PR 5573
 
 static std::string GetLanguageString(DiscIO::Language language,
                                      std::map<DiscIO::Language, std::string> strings)
