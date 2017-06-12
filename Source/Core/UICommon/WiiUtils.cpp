ffd3580cbc7096139ad5cc987b652404512d047d
Léo Lam <leo@innovatetechnologi.es>
2017-06-12 02:01:10 +0200

    Allow the user to ignore signature issues during WAD import
    Improves usability with signature checks.

--- Source/Core/UICommon/WiiUtils.cpp
+++ Source/Core/UICommon/WiiUtils.cpp
@@ -5,6 +5,7 @@
 #include "UICommon/WiiUtils.h"
 #include "Common/CommonTypes.h"
 #include "Common/MsgHandler.h"
+#include "Core/ConfigManager.h"
 #include "Core/IOS/ES/ES.h"
 #include "Core/IOS/ES/Formats.h"
 #include "Core/IOS/IOS.h"
@@ -27,12 +28,23 @@ bool InstallWAD(const std::string& wad_path)
   const auto es = ios.GetES();
 
   IOS::HLE::Device::ES::Context context;
-  if (es->ImportTicket(wad.GetTicket().GetBytes(), wad.GetCertificateChain()) < 0 ||
-      es->ImportTitleInit(context, tmd.GetBytes(), wad.GetCertificateChain()) < 0)
+  IOS::HLE::ReturnCode ret;
+  const bool checks_disabled = SConfig::GetInstance().m_disable_signature_checks;
+  while ((ret = es->ImportTicket(wad.GetTicket().GetBytes(), wad.GetCertificateChain())) < 0 ||
+         (ret = es->ImportTitleInit(context, tmd.GetBytes(), wad.GetCertificateChain())) < 0)
   {
+    if (!checks_disabled && ret == IOS::HLE::IOSC_FAIL_CHECKVALUE &&
+        AskYesNoT("This WAD does not have a valid signature. Continue to import?"))
+    {
+      SConfig::GetInstance().m_disable_signature_checks = true;
+      continue;
+    }
+
+    SConfig::GetInstance().m_disable_signature_checks = checks_disabled;
     PanicAlertT("WAD installation failed: Could not initialise title import.");
     return false;
   }
+  SConfig::GetInstance().m_disable_signature_checks = checks_disabled;
 
   const bool contents_imported = [&]() {
     const u64 title_id = tmd.GetTitleId();
