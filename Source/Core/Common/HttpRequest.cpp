8f87433719c58c60df4cb514e9e0eb215fb65158
Léo Lam <leo@innovatetechnologi.es>
2017-06-13 12:23:08 +0200

    HttpRequest: Log response body on failure

--- Source/Core/Common/HttpRequest.cpp
+++ Source/Core/Common/HttpRequest.cpp
@@ -111,7 +111,8 @@ HttpRequest::Response HttpRequest::Impl::Fetch(const std::string& url, Method me
   curl_easy_getinfo(m_curl.get(), CURLINFO_RESPONSE_CODE, &response_code);
   if (response_code != 200)
   {
-    ERROR_LOG(COMMON, "Failed to %s %s: response code was %li", type, url.c_str(), response_code);
+    ERROR_LOG(COMMON, "Failed to %s %s: server replied with code %li and body\n\x1b[0m%s", type,
+              url.c_str(), response_code, buffer.data());
     return {};
   }
 
