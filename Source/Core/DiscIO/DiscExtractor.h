1a2adcab0b2c36de1d05ed49cebf44e14601d268
JosJuice <josjuice@gmail.com>
2017-06-17 13:37:33 +0200

    DiscScrubber: Deduplicate code for writing to file

--- Source/Core/DiscIO/DiscExtractor.h
+++ Source/Core/DiscIO/DiscExtractor.h
@@ -16,6 +16,8 @@ class Volume;
 
 u64 ReadFile(const Volume& volume, const Partition& partition, const FileInfo* file_info,
              u8* buffer, u64 max_buffer_size, u64 offset_in_file = 0);
+bool ExportData(const Volume& volume, const Partition& partition, u64 offset, u64 size,
+                const std::string& export_filename);
 bool ExportFile(const Volume& volume, const Partition& partition, const FileInfo* file_info,
                 const std::string& export_filename);
 bool ExportApploader(const Volume& volume, const Partition& partition,
