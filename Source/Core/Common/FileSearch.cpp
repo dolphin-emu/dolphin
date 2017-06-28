98b0d8a1197c6402b030258685b2eb6e1cd74e39
JosJuice <josjuice@gmail.com>
2017-06-28 09:50:02 +0200

    Fix DoFileSearch for non-ASCII extensions on Windows
    We don't use non-ASCII extensions for anything right now,
    but we might as well fix this.

--- Source/Core/Common/FileSearch.cpp
+++ Source/Core/Common/FileSearch.cpp
@@ -74,7 +74,7 @@ std::vector<std::string> DoFileSearch(const std::vector<std::string>& directorie
 
   std::vector<fs::path> native_exts;
   for (const auto& ext : exts)
-    native_exts.push_back(ext);
+    native_exts.push_back(fs::u8path(ext));
 
   // N.B. This avoids doing any copies
   auto ext_matches = [&native_exts](const fs::path& path) {
