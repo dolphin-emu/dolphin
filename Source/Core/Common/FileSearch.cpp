1d063ad7675a6d9bb18dff86ce06cf13da14d09c
JosJuice <josjuice@gmail.com>
2017-06-29 14:46:27 +0200

    Optimize DoFileSearch on all platforms using DirectoryIterator
    My main reason for doing this was to avoid the slow function
    File::ScanDirectoryTree which previously was used on all OSes
    other than Windows, but it seems like this improved the speed
    a bit on Windows too; the DoFileSearch call that happens when
    pressing the game list refresh button took about 50-80 ms
    before this and 25-50 ms after this. (The directories that I
    tested with mostly contain files with non-matching extensions.)
    The performance improvement on other platforms should be much
    larger because of how slow File::ScanDirectoryTree was.

--- Source/Core/Common/FileSearch.cpp
+++ Source/Core/Common/FileSearch.cpp
@@ -2,131 +2,97 @@
 // Licensed under GPLv2+
 // Refer to the license.txt file included.
 
+#include "Common/FileSearch.h"
+
 #include <algorithm>
+#include <cstring>
 #include <functional>
 
 #include "Common/CommonPaths.h"
-#include "Common/FileSearch.h"
+#include "Common/DirectoryIterator.h"
 
-#ifdef _MSC_VER
+#ifdef _WIN32
 #include <Windows.h>
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#define HAS_STD_FILESYSTEM
-#else
-#include <cstring>
-#include "Common/CommonFuncs.h"
-#include "Common/FileUtil.h"
+#include "Common/StringUtil.h"
 #endif
 
 namespace Common
 {
-#ifndef HAS_STD_FILESYSTEM
-
-static std::vector<std::string>
-FileSearchWithTest(const std::vector<std::string>& directories, bool recursive,
-                   std::function<bool(const File::FSTEntry&)> callback)
-{
-  std::vector<std::string> result;
-  for (const std::string& directory : directories)
-  {
-    File::FSTEntry top = File::ScanDirectoryTree(directory, recursive);
-
-    std::function<void(File::FSTEntry&)> DoEntry;
-    DoEntry = [&](File::FSTEntry& entry) {
-      if (callback(entry))
-        result.push_back(entry.physicalName);
-      for (auto& child : entry.children)
-        DoEntry(child);
-    };
-    for (auto& child : top.children)
-      DoEntry(child);
-  }
-  // remove duplicates
-  std::sort(result.begin(), result.end());
-  result.erase(std::unique(result.begin(), result.end()), result.end());
-  return result;
-}
-
-std::vector<std::string> DoFileSearch(const std::vector<std::string>& directories,
-                                      const std::vector<std::string>& exts, bool recursive)
-{
-  bool accept_all = exts.empty();
-  return FileSearchWithTest(directories, recursive, [&](const File::FSTEntry& entry) {
-    if (accept_all)
-      return true;
-    if (entry.isDirectory)
-      return false;
-    return std::any_of(exts.begin(), exts.end(), [&](const std::string& ext) {
-      const std::string& name = entry.virtualName;
-      return name.length() >= ext.length() &&
-             strcasecmp(name.c_str() + name.length() - ext.length(), ext.c_str()) == 0;
-    });
-  });
-}
-
-#else
-
 std::vector<std::string> DoFileSearch(const std::vector<std::string>& directories,
                                       const std::vector<std::string>& exts, bool recursive)
 {
   bool accept_all = exts.empty();
 
-  std::vector<fs::path> native_exts;
-  for (const auto& ext : exts)
-    native_exts.push_back(fs::u8path(ext));
+#ifdef _WIN32
+  std::vector<std::wstring> native_exts;
+  for (const std::string& ext : exts)
+    native_exts.push_back(UTF8ToTStr(ext));
 
   // N.B. This avoids doing any copies
-  auto ext_matches = [&native_exts](const fs::path& path) {
-    const auto& native_path = path.native();
-    return std::any_of(native_exts.cbegin(), native_exts.cend(), [&native_path](const auto& ext) {
-      // TODO provide cross-platform compat for the comparison function, once more platforms
-      // support std::filesystem
-      int compare_len = static_cast<int>(ext.native().length());
-      return native_path.length() >= compare_len &&
-             CompareStringOrdinal(&native_path.c_str()[native_path.length() - compare_len],
-                                  compare_len, ext.c_str(), compare_len, TRUE) == CSTR_EQUAL;
+  auto ext_matches = [&native_exts](const wchar_t* name) {
+    return std::any_of(native_exts.cbegin(), native_exts.cend(), [&name](const std::wstring& ext) {
+      const int compare_length = static_cast<int>(ext.length());
+      const size_t name_length = wcslen(name);
+      return name_length >= compare_length &&
+             CompareStringOrdinal(&name[name_length - compare_length], compare_length, ext.c_str(),
+                                  compare_length, TRUE) == CSTR_EQUAL;
     });
   };
+#else
+  // N.B. This avoids doing any copies
+  auto ext_matches = [&exts](const char* name) {
+    return std::any_of(exts.cbegin(), exts.cend(), [&name](const std::string& ext) {
+      const size_t name_length = strlen(name);
+      return name_length >= ext.length() &&
+             strcasecmp(name + name_length - ext.length(), ext.c_str()) == 0;
+    });
+  };
+#endif
 
   std::vector<std::string> result;
-  auto add_filtered = [&](const fs::directory_entry& entry) {
-    auto& path = entry.path();
-    if (accept_all || (ext_matches(path) && !fs::is_directory(path)))
-      result.emplace_back(path.u8string());
+
+#ifdef _WIN32
+  constexpr wchar_t directory_separator = L'/';
+#else
+  constexpr char directory_separator = '/';
+#endif
+
+  std::function<void(const DirectoryEntry::StringType& path)> do_directory;
+  do_directory = [&](const DirectoryEntry::StringType& path) {
+    for (const DirectoryEntry& entry : Directory<DirectoryEntry::StringType>(path))
+    {
+      // For performance, skip calling IsDirectory if we won't be using the value
+      const bool is_directory = (!accept_all || recursive) ? entry.IsDirectory() : false;
+      const DirectoryEntry::CharType* const child_name = entry.GetName();
+
+      if (accept_all || (!is_directory && ext_matches(child_name)))
+        result.emplace_back(ToUTF8(path + directory_separator + child_name));
+
+      if (recursive && is_directory)
+        do_directory(path + directory_separator + child_name);
+
+      // TODO: We could get better performance in the (currently unused?) case
+      // (accept_all && recursive) by not computing (path + directory_separator + child_name)
+      // twice. We shouldn't compute it if the result won't be used, though.
+    }
   };
+
   for (const auto& directory : directories)
-  {
-    if (recursive)
-    {
-      // TODO use fs::directory_options::follow_directory_symlink ?
-      for (auto& entry : fs::recursive_directory_iterator(fs::u8path(directory)))
-        add_filtered(entry);
-    }
-    else
-    {
-      for (auto& entry : fs::directory_iterator(fs::u8path(directory)))
-        add_filtered(entry);
-    }
-  }
+    do_directory(ToOSEncoding(directory));
 
   // Remove duplicates (occurring because caller gave e.g. duplicate or overlapping directories -
-  // not because std::filesystem returns duplicates). Also note that this pathname-based uniqueness
-  // isn't as thorough as std::filesystem::equivalent.
+  // not because the scanning code returns duplicates). Also note that this pathname-based
+  // uniqueness isn't as thorough as std::filesystem::equivalent.
   std::sort(result.begin(), result.end());
   result.erase(std::unique(result.begin(), result.end()), result.end());
 
-  // Dolphin expects to be able to use "/" (DIR_SEP) everywhere.
-  // std::filesystem uses the OS separator.
-  constexpr fs::path::value_type os_separator = fs::path::preferred_separator;
-  static_assert(os_separator == DIR_SEP_CHR || os_separator == '\\', "Unsupported path separator");
-  if (os_separator != DIR_SEP_CHR)
-    for (auto& path : result)
-      std::replace(path.begin(), path.end(), '\\', DIR_SEP_CHR);
+#ifdef _WIN32
+  // Dolphin expects to be able to use / (DIR_SEP_CHR) everywhere, but Windows uses \.
+  for (std::string& path : result)
+    std::replace(path.begin(), path.end(), '\\', DIR_SEP_CHR);
+#endif
 
   return result;
 }
-
-#endif
 
 }  // namespace Common
