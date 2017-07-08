4b8e2cb2b3509046157744350f2e2c3a3f97ea88
ZexaronS <ZexaronS@users.noreply.github.com>
2017-07-08 22:10:38 +0200

    Custom Textures Prefetcher Overhaul Various

--- Source/Core/Core/ConfigLoaders/IsSettingSaveable.cpp
+++ Source/Core/Core/ConfigLoaders/IsSettingSaveable.cpp
@@ -34,6 +34,7 @@ bool IsSettingSaveable(const Config::ConfigLocation& config_location)
       Config::GFX_LOG_RENDER_TIME_TO_FILE.location, Config::GFX_OVERLAY_STATS.location,
       Config::GFX_OVERLAY_PROJ_STATS.location, Config::GFX_DUMP_TEXTURES.location,
       Config::GFX_HIRES_TEXTURES.location, Config::GFX_CONVERT_HIRES_TEXTURES.location,
+      Config::GFX_HIRESTEX_OVERRIDE_PREFETCH_CULLED_PERCENT.location,
       Config::GFX_CACHE_HIRES_TEXTURES.location, Config::GFX_DUMP_EFB_TARGET.location,
       Config::GFX_DUMP_FRAMES_AS_IMAGES.location, Config::GFX_FREE_LOOK.location,
       Config::GFX_USE_FFV1.location, Config::GFX_DUMP_FORMAT.location,
