19dc580a4eed0e48198b1cb5bb453cccbb8e25ee
Michael Maltese <mchtly@gmail.com>
2017-07-16 14:25:36 -0700

    Qt: extract ListTabWidget from SettingsWindow

--- Source/Core/DolphinQt2/Config/SettingsWindow.h
+++ Source/Core/DolphinQt2/Config/SettingsWindow.h
@@ -4,15 +4,9 @@
 
 #pragma once
 
-#include <string>
-
 #include <QDialog>
 
-class MainWindow;
-class QGroupBox;
-class QListWidget;
-class QListWidgetItem;
-class QStackedWidget;
+class ListTabWidget;
 
 class SettingsWindow final : public QDialog
 {
@@ -23,13 +17,6 @@ signals:
   void EmulationStarted();
   void EmulationStopped();
 
-public slots:
-  void changePage(QListWidgetItem* current, QListWidgetItem* previous);
-
 private:
-  void MakeCategoryList();
-  void AddCategoryToList(const QString& title, const std::string& icon_name);
-  void SetupSettingsWidget();
-  QStackedWidget* m_settings_outer;
-  QListWidget* m_categories;
+  ListTabWidget* m_tabs;
 };
