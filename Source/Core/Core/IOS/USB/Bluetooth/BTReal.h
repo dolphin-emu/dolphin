e05ef01e112da892f0624380d68c021f34c1214f
ATMD <macman31@free.fr>
2017-07-18 19:23:18 +0200

    Revert the modification to BTReal's TIMEOUT value
    As per leoetlino's comment here:
    https://github.com/dolphin-emu/dolphin/pull/5799#discussion_r128030473

--- Source/Core/Core/IOS/USB/Bluetooth/BTReal.h
+++ Source/Core/Core/IOS/USB/Bluetooth/BTReal.h
@@ -65,10 +65,10 @@ public:
 
 private:
   static constexpr u8 INTERFACE = 0x00;
-  // Arbitrarily chosen value that allows emulated software to not block if libusb transfers
-  // hang for some reason.
+  // Arbitrarily chosen value that allows emulated software to send commands often enough
+  // so that the sync button event is triggered at least every 200ms.
   // Ideally this should be equal to 0, so we don't trigger unnecessary libusb transfers.
-  static constexpr int TIMEOUT = 5000;
+  static constexpr int TIMEOUT = 200;
   static constexpr int SYNC_BUTTON_HOLD_MS_TO_RESET = 10000;
 
   std::atomic<SyncButtonState> m_sync_button_state{SyncButtonState::Unpressed};
