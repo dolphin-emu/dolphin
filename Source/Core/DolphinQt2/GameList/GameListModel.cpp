623026f42066be41790b2625b334f9a3b3912f04
Michael M <mchtly@gmail.com>
2017-08-19 15:47:48 -0700

    Connect to Settings::PathAdded/Removed directly in GameListModel
    It no longer makes sense to connect these signals in GameList, since
    GameListModel is now owned by Settings.

--- Source/Core/DolphinQt2/GameList/GameListModel.cpp
+++ Source/Core/DolphinQt2/GameList/GameListModel.cpp
@@ -14,8 +14,8 @@ GameListModel::GameListModel(QObject* parent) : QAbstractTableModel(parent)
 {
   connect(&m_tracker, &GameTracker::GameLoaded, this, &GameListModel::UpdateGame);
   connect(&m_tracker, &GameTracker::GameRemoved, this, &GameListModel::RemoveGame);
-  connect(this, &GameListModel::DirectoryAdded, &m_tracker, &GameTracker::AddDirectory);
-  connect(this, &GameListModel::DirectoryRemoved, &m_tracker, &GameTracker::RemoveDirectory);
+  connect(&Settings::Instance(), &Settings::PathAdded, &m_tracker, &GameTracker::AddDirectory);
+  connect(&Settings::Instance(), &Settings::PathRemoved, &m_tracker, &GameTracker::RemoveDirectory);
 
   for (const QString& dir : Settings::Instance().GetPaths())
     m_tracker.AddDirectory(dir);
