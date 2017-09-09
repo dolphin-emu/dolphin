dd8b41326c7b953f62fdfac85040bb9b2013ff1a
Lioncash <mathew1800@gmail.com>
2017-09-09 15:23:16 -0400

    SWVertexLoader: Make SetFormat() and ResetBuffer() private
    SetFormat() is only ever used internally. ResetBuffer() is only
    used to implement the VertexManagerBase class interface, so
    there's no need to make it protected.

--- Source/Core/VideoBackends/Software/SWVertexLoader.h
+++ Source/Core/VideoBackends/Software/SWVertexLoader.h
@@ -23,22 +23,18 @@ public:
   std::unique_ptr<NativeVertexFormat>
   CreateNativeVertexFormat(const PortableVertexDeclaration& vdec) override;
 
-protected:
-  void ResetBuffer(u32 stride) override;
-
 private:
+  void ResetBuffer(u32 stride) override;
   void vFlush() override;
+
+  void SetFormat(u8 attributeIndex, u8 primitiveType);
+  void ParseVertex(const PortableVertexDeclaration& vdec, int index);
+
   std::vector<u8> m_local_vertex_buffer;
   std::vector<u16> m_local_index_buffer;
 
   InputVertexData m_vertex;
-
-  void ParseVertex(const PortableVertexDeclaration& vdec, int index);
-
   SetupUnit m_setup_unit;
 
   bool m_tex_gen_special_case;
-
-public:
-  void SetFormat(u8 attributeIndex, u8 primitiveType);
 };
