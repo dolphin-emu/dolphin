8dcd5cdd1de6d9bd90437a62cd5c9795586301b1
Léo Lam <leo@innovatetechnologi.es>
2017-09-19 22:26:03 +0200

    Qt: Reduce spacing between toolbar actions

--- Source/Core/DolphinQt2/ToolBar.cpp
+++ Source/Core/DolphinQt2/ToolBar.cpp
@@ -75,7 +75,7 @@ void ToolBar::MakeActions()
   std::transform(items.begin(), items.end(), std::back_inserter(widths),
                  [](QWidget* item) { return item->sizeHint().width(); });
 
-  const int min_width = *std::max_element(widths.begin(), widths.end());
+  const int min_width = *std::max_element(widths.begin(), widths.end()) * 0.85;
   for (QWidget* widget : items)
     widget->setMinimumWidth(min_width);
 }
