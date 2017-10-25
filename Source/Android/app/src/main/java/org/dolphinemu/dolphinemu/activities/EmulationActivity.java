18cb68eb3cfe656a4eb22229a5441c665a54bf3d
Mike <7153163+hackbar@users.noreply.github.com>
2017-10-25 12:45:03 -0700

    Android: Destroy the surface in EmulationFragment onStop.
    Emulation needs to be running when the surface is destroyed, but we want
    to pause in onStop. So call the surfaceDestroyed callback, as this
    accomplished both.

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/EmulationActivity.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/EmulationActivity.java
@@ -275,8 +275,6 @@ public final class EmulationActivity extends AppCompatActivity
 							mImageView.setVisibility(View.GONE);
 						}
 					});
-
-			mEmulationFragment.startEmulation();
 		}
 		else
 		{
