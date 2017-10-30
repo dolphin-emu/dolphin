e331a761762b23810df8c399c1f62d694fe48e4d
MerryMage <MerryMage@users.noreply.github.com>
2017-10-30 17:09:05 +0000

    ConfigInfo: Switch to doing case-insensitive comparison

--- Source/Core/Common/Config/ConfigInfo.cpp
+++ Source/Core/Common/Config/ConfigInfo.cpp
@@ -2,15 +2,17 @@
 // Licensed under GPLv2+
 // Refer to the license.txt file included.
 
-#include <strings.h>
+#include <cstring>
 
+#include "Common/CommonFuncs.h"
 #include "Common/Config/ConfigInfo.h"
 
 namespace Config
 {
 bool ConfigLocation::operator==(const ConfigLocation& other) const
 {
-  return std::tie(system, section, key) == std::tie(other.system, other.section, other.key);
+  return system == other.system && strcasecmp(section.c_str(), other.section.c_str()) == 0 &&
+         strcasecmp(key.c_str(), other.key.c_str()) == 0;
 }
 
 bool ConfigLocation::operator!=(const ConfigLocation& other) const
@@ -20,6 +22,14 @@ bool ConfigLocation::operator!=(const ConfigLocation& other) const
 
 bool ConfigLocation::operator<(const ConfigLocation& other) const
 {
-  return std::tie(system, section, key) < std::tie(other.system, other.section, other.key);
+  if (system != other.system)
+    return system < other.system;
+
+  const int section_compare = strcasecmp(section.c_str(), other.section.c_str());
+  if (section_compare != 0)
+    return section_compare < 0;
+
+  const int key_compare = strcasecmp(key.c_str(), other.key.c_str());
+  return key_compare < 0;
 }
 }
