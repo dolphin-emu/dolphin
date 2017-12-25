cb168f22d66edf0216fadc5bdc602394d5e9b9a7
Léo Lam <leo@innovatetechnologi.es>
2017-12-25 22:56:36 +0100

    Replace custom UNUSED macro with [[maybe_unused]]

--- Source/Core/Common/Compiler.h
+++ Source/Core/Common/Compiler.h
@@ -4,15 +4,6 @@
 
 #pragma once
 
-// TODO: Replace this with [[maybe_unused]] directly when GCC 7 and clang 3.9
-//       are hard requirements.
-#if defined(__GNUC__) || __clang__
-// Disable "unused function" warnings for the ones manually marked as such.
-#define DOLPHIN_UNUSED __attribute__((unused))
-#else
-#define DOLPHIN_UNUSED [[maybe_unused]]
-#endif
-
 #ifdef _WIN32
 #define DOLPHIN_FORCE_INLINE __forceinline
 #else
