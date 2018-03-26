9f13048e1edcfc7ef6e38f3c579c9e95a8aa9212
spycrab <spycrab@users.noreply.github.com>
2018-03-26 04:25:36 +0200

    Qt/GraphicsWindow: Decrease description box size

--- Source/Core/DolphinQt2/Config/Graphics/GraphicsWindow.cpp
+++ Source/Core/DolphinQt2/Config/Graphics/GraphicsWindow.cpp
@@ -43,7 +43,7 @@ void GraphicsWindow::CreateMainLayout()
   m_button_box = new QDialogButtonBox(QDialogButtonBox::Ok);
 
   description_box->setLayout(description_layout);
-  description_box->setFixedHeight(230);
+  description_box->setFixedHeight(200);
 
   m_description->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
   m_description->setWordWrap(true);
@@ -68,15 +68,16 @@ void GraphicsWindow::CreateMainLayout()
 
   if (SConfig::GetInstance().m_strVideoBackend != "Software Renderer")
   {
-    m_tab_widget->addTab(GetWrappedWidget(m_general_widget, this, 50, 325), tr("General"));
-    m_tab_widget->addTab(GetWrappedWidget(m_enhancements_widget, this, 50, 325),
+    m_tab_widget->addTab(GetWrappedWidget(m_general_widget, this, 50, 305), tr("General"));
+    m_tab_widget->addTab(GetWrappedWidget(m_enhancements_widget, this, 50, 305),
                          tr("Enhancements"));
-    m_tab_widget->addTab(GetWrappedWidget(m_hacks_widget, this, 50, 325), tr("Hacks"));
-    m_tab_widget->addTab(GetWrappedWidget(m_advanced_widget, this, 50, 325), tr("Advanced"));
+    m_tab_widget->addTab(GetWrappedWidget(m_hacks_widget, this, 50, 305), tr("Hacks"));
+    m_tab_widget->addTab(GetWrappedWidget(m_advanced_widget, this, 50, 305), tr("Advanced"));
   }
   else
   {
-    m_tab_widget->addTab(GetWrappedWidget(m_software_renderer, this, 325), tr("Software Renderer"));
+    m_tab_widget->addTab(GetWrappedWidget(m_software_renderer, this, 50, 305),
+                         tr("Software Renderer"));
   }
 
   setLayout(main_layout);
