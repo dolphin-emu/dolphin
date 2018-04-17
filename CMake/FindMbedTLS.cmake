5157d3511e181392fe28a40b53a3ef3e0daaa9c6
Lioncash <mathew1800@gmail.com>
2018-04-17 10:32:25 -0400

    CMakeLists/FindMbedTLS: Amend indentation
    Amends it to follow the common pattern we already use in other CMake files.

--- CMake/FindMbedTLS.cmake
+++ CMake/FindMbedTLS.cmake
@@ -9,15 +9,21 @@ set(MBEDTLS_LIBRARIES ${MBEDTLS_LIBRARY} ${MBEDX509_LIBRARY} ${MBEDCRYPTO_LIBRAR
 
 set(CMAKE_REQUIRED_INCLUDES ${MBEDTLS_INCLUDE_DIRS})
 check_cxx_source_compiles("
-	#include <mbedtls/version.h>
-	#if MBEDTLS_VERSION_NUMBER < 0x02040000
-	#error \"Your mbed TLS version is too old.\"
-	#endif
-	int main() {}"
-	MBEDTLS_VERSION_OK)
+  #include <mbedtls/version.h>
+  #if MBEDTLS_VERSION_NUMBER < 0x02040000
+  #error \"Your mbed TLS version is too old.\"
+  #endif
+  int main() {}"
+  MBEDTLS_VERSION_OK
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MBEDTLS DEFAULT_MSG
-	MBEDTLS_INCLUDE_DIR MBEDTLS_LIBRARY MBEDX509_LIBRARY MBEDCRYPTO_LIBRARY MBEDTLS_VERSION_OK)
+  MBEDTLS_INCLUDE_DIR
+  MBEDTLS_LIBRARY
+  MBEDX509_LIBRARY
+  MBEDCRYPTO_LIBRARY
+  MBEDTLS_VERSION_OK
+)
 
 mark_as_advanced(MBEDTLS_INCLUDE_DIR MBEDTLS_LIBRARY MBEDX509_LIBRARY MBEDCRYPTO_LIBRARY)
