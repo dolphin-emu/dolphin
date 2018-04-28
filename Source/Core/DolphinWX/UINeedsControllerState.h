44b22c90df9c05d18a34667dc41e8d05af24683f
spycrab <spycrab@users.noreply.github.com>
2018-04-28 12:50:47 +0200

    Remove DolphinWX

--- Source/Core/DolphinWX/UINeedsControllerState.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2017 Dolphin Emulator Project
-// Licensed under GPLv2+
-// Refer to the license.txt file included.
-
-#pragma once
-
-void SetUINeedsControllerState(bool needs_controller_state);
-bool GetUINeedsControllerState();
