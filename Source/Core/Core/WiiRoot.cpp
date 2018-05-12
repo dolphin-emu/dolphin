5f567b38fe2a8ac067d051fd3669bb2c43f50f86
Léo Lam <leo@innovatetechnologi.es>
2018-05-12 16:52:33 +0200

    WiiRoot: Move content manipulation to separate functions
    {Initialize,Shutdown}WiiRoot should only be responsible for setting the
    SESSION_WII_ROOT or managing the temporary NAND directory.
    
    Move all the content manipulation out of these functions to ensure
    separation of concerns and call them after/before WiiRoot init/shutdown
    to make sure they operate on the correct root.

--- Source/Core/Core/WiiRoot.cpp
+++ Source/Core/Core/WiiRoot.cpp
@@ -61,14 +61,8 @@ void InitializeWiiRoot(bool use_temporary)
       ERROR_LOG(IOS_FS, "Could not create temporary directory");
       return;
     }
-    File::CopyDir(File::GetSysDirectory() + WII_USER_DIR, s_temp_wii_root);
     WARN_LOG(IOS_FS, "Using temporary directory %s for minimal Wii FS", s_temp_wii_root.c_str());
     File::SetUserPath(D_SESSION_WIIROOT_IDX, s_temp_wii_root);
-    // Generate a SYSCONF with default settings for the temporary Wii NAND.
-    SysConf sysconf{IOS::HLE::Kernel{}.GetFS()};
-    sysconf.Save();
-
-    InitializeDeterministicWiiSaves();
   }
   else
   {
@@ -80,27 +74,48 @@ void ShutdownWiiRoot()
 {
   if (!s_temp_wii_root.empty())
   {
-    const u64 title_id = SConfig::GetInstance().GetTitleID();
-    std::string save_path = Common::GetTitleDataPath(title_id, Common::FROM_SESSION_ROOT);
-    std::string user_save_path = Common::GetTitleDataPath(title_id, Common::FROM_CONFIGURED_ROOT);
-    std::string user_backup_path = File::GetUserPath(D_BACKUP_IDX) +
-                                   StringFromFormat("%08x/%08x/", static_cast<u32>(title_id >> 32),
-                                                    static_cast<u32>(title_id));
-    if (File::Exists(save_path + "/banner.bin") && SConfig::GetInstance().bEnableMemcardSdWriting)
-    {
-      // Backup the existing save just in case it's still needed.
-      if (File::Exists(user_save_path + "/banner.bin"))
-      {
-        if (File::Exists(user_backup_path))
-          File::DeleteDirRecursively(user_backup_path);
-        File::CopyDir(user_save_path, user_backup_path);
-        File::DeleteDirRecursively(user_save_path);
-      }
-      File::CopyDir(save_path, user_save_path);
-      File::DeleteDirRecursively(save_path);
-    }
     File::DeleteDirRecursively(s_temp_wii_root);
     s_temp_wii_root.clear();
   }
 }
+
+void InitializeWiiFileSystemContents()
+{
+  if (s_temp_wii_root.empty())
+    return;
+
+  File::CopyDir(File::GetSysDirectory() + WII_USER_DIR, s_temp_wii_root);
+
+  // Generate a SYSCONF with default settings for the temporary Wii NAND.
+  SysConf sysconf{IOS::HLE::GetIOS()->GetFS()};
+  sysconf.Save();
+
+  InitializeDeterministicWiiSaves();
 }
+
+void CleanUpWiiFileSystemContents()
+{
+  if (s_temp_wii_root.empty())
+    return;
+
+  const u64 title_id = SConfig::GetInstance().GetTitleID();
+  std::string save_path = Common::GetTitleDataPath(title_id, Common::FROM_SESSION_ROOT);
+  std::string user_save_path = Common::GetTitleDataPath(title_id, Common::FROM_CONFIGURED_ROOT);
+  std::string user_backup_path =
+      File::GetUserPath(D_BACKUP_IDX) +
+      StringFromFormat("%08x/%08x/", static_cast<u32>(title_id >> 32), static_cast<u32>(title_id));
+  if (File::Exists(save_path + "/banner.bin") && SConfig::GetInstance().bEnableMemcardSdWriting)
+  {
+    // Backup the existing save just in case it's still needed.
+    if (File::Exists(user_save_path + "/banner.bin"))
+    {
+      if (File::Exists(user_backup_path))
+        File::DeleteDirRecursively(user_backup_path);
+      File::CopyDir(user_save_path, user_backup_path);
+      File::DeleteDirRecursively(user_save_path);
+    }
+    File::CopyDir(save_path, user_save_path);
+    File::DeleteDirRecursively(save_path);
+  }
+}
+}  // namespace Core
