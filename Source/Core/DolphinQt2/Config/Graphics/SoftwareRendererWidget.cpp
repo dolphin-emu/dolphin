5fcb5097021c4dfafb142f2c150544e7ba36e7e7
Ryan Meredith <rwm@udel.edu>
2018-06-06 11:59:29 -0400

    Give "Dump TEV Stages" and "Dump Texture Fetches" descriptions

--- Source/Core/DolphinQt2/Config/Graphics/SoftwareRendererWidget.cpp
+++ Source/Core/DolphinQt2/Config/Graphics/SoftwareRendererWidget.cpp
@@ -165,8 +165,18 @@ void SoftwareRendererWidget::AddDescriptions()
       QT_TR_NOOP("Dump objects to User/Dump/Objects/.\n\nIf unsure, leave "
                  "this unchecked.");
 
+  static const char TR_DUMP_TEV_STAGES_DESCRIPTION[] =
+      QT_TR_NOOP("Dump TEV Stages to User/Dump/Objects/.\n\nIf unsure, leave "
+                 "this unchecked.");
+
+  static const char TR_DUMP_TEV_FETCHES_DESCRIPTION[] =
+      QT_TR_NOOP("Dump Texture Fetches to User/Dump/Objects/.\n\nIf unsure, leave "
+                 "this unchecked.");
+
   AddDescription(m_backend_combo, TR_BACKEND_DESCRIPTION);
   AddDescription(m_show_statistics, TR_SHOW_STATISTICS_DESCRIPTION);
   AddDescription(m_dump_textures, TR_DUMP_TEXTURES_DESCRIPTION);
   AddDescription(m_dump_objects, TR_DUMP_OBJECTS_DESCRIPTION);
+  AddDescription(m_dump_tev_stages, TR_DUMP_TEV_STAGES_DESCRIPTION);
+  AddDescription(m_dump_tev_fetches, TR_DUMP_TEV_FETCHES_DESCRIPTION);
 }
