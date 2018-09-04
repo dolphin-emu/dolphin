393b0fa5c8bc9cf1c57e2b5f5fb41f01618dc3f8
weihuoya <zwdreams@gmail.com>
2018-09-04 12:06:14 +0800

    remove primitive restart

--- Source/Core/VideoBackends/OGL/main.cpp
+++ Source/Core/VideoBackends/OGL/main.cpp
@@ -95,7 +95,6 @@ void VideoBackend::InitBackendInfo()
 
   // Overwritten in Render.cpp later
   g_Config.backend_info.bSupportsDualSourceBlend = true;
-  g_Config.backend_info.bSupportsPrimitiveRestart = true;
   g_Config.backend_info.bSupportsPaletteConversion = true;
   g_Config.backend_info.bSupportsClipControl = true;
   g_Config.backend_info.bSupportsDepthClamp = true;
