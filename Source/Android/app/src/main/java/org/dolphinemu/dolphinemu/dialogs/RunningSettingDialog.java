d4561fbc2c031b1b4a4556581cb2ca4ad5a7f12e
weihuoya <zwdreams@gmail.com>
2018-09-18 22:07:40 +0800

    running

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/dialogs/RunningSettingDialog.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/dialogs/RunningSettingDialog.java
@@ -4,24 +4,62 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.nononsenseapps.filepicker.DividerItemDecoration;
-
 import org.dolphinemu.dolphinemu.R;
-import org.dolphinemu.dolphinemu.features.settings.model.Setting;
-import org.dolphinemu.dolphinemu.features.settings.model.Settings;
-import org.dolphinemu.dolphinemu.features.settings.model.view.SettingsItem;
-import org.dolphinemu.dolphinemu.features.settings.ui.MenuTag;
-import org.dolphinemu.dolphinemu.features.settings.ui.SettingsAdapter;
-import org.dolphinemu.dolphinemu.features.settings.ui.SettingsFragmentView;
-
 import java.util.ArrayList;
 
-public class RunningSettingDialog extends DialogFragment implements SettingsFragmentView {
+public class RunningSettingDialog extends DialogFragment {
+
+	public class SettingsItem {
+		public static final int TYPE_CHECKBOX = 1;
+		public static final int TYPE_SINGLE_CHOICE = 2;
+		public static final int TYPE_SLIDER = 3;
+	}
+
+	public class SettingViewHolder extends RecyclerView.ViewHolder {
+		public SettingViewHolder(View itemView) {
+			super(itemView);
+		}
+	}
+
+	public class SettingsAdapter extends RecyclerView.Adapter<SettingViewHolder> {
+		private ArrayList<SettingsItem> mSettings;
+
+		public SettingsAdapter() {
+		}
+
+		@Override
+		public SettingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			return null;
+		}
+
+		@Override
+		public int getItemCount() {
+			return 0;
+		}
+
+		@Override
+		public int getItemViewType(int position) {
+			return position;
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull SettingViewHolder holder, int position) {
+
+		}
+
+		public void setSettings(ArrayList<SettingsItem> settings) {
+			mSettings = settings;
+			notifyDataSetChanged();
+		}
+	}
 
 	public static RunningSettingDialog newInstance() {
 		return new RunningSettingDialog();
@@ -33,58 +71,23 @@ public class RunningSettingDialog extends DialogFragment implements SettingsFrag
 		ViewGroup contents = (ViewGroup) getActivity().getLayoutInflater().inflate(R.layout.dialog_running_settings, null);
 
 		int columns = 2;
+		SettingsAdapter adapter = new SettingsAdapter();
 		Drawable lineDivider = getContext().getDrawable(R.drawable.line_divider);
 		RecyclerView recyclerView = contents.findViewById(R.id.list_settings);
 		RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), columns);
 		recyclerView.setLayoutManager(layoutManager);
-		recyclerView.setAdapter(new SettingsAdapter(this, getContext()));
+		recyclerView.setAdapter(adapter);
 		recyclerView.addItemDecoration(new DividerItemDecoration(lineDivider));
+
+		// show settings
+		adapter.setSettings(getSettings());
 
 		builder.setView(contents);
 		return builder.create();
 	}
 
-	public void onSettingsFileLoaded(Settings settings) {
-
+	private ArrayList<SettingsItem> getSettings() {
+		return null;
 	}
 
-	public void passSettingsToActivity(Settings settings) {
-
-	}
-
-	public void showSettingsList(ArrayList<SettingsItem> settingsList) {
-
-	}
-
-	public void loadDefaultSettings() {
-
-	}
-
-	public void loadSubMenu(MenuTag menuKey) {
-
-	}
-
-	public void showToastMessage(String message) {
-
-	}
-
-	public void putSetting(Setting setting) {
-
-	}
-
-	public void onSettingChanged() {
-
-	}
-
-	public void onGcPadSettingChanged(MenuTag menuTag, int value) {
-
-	}
-
-	public void onWiimoteSettingChanged(MenuTag menuTag, int value) {
-
-	}
-
-	public void onExtensionSettingChanged(MenuTag menuTag, int value) {
-
-	}
 }
