dfe8305ae7ca95edc14aba0df04398b825c8e413
Tillmann Karras <tilkax@gmail.com>
2018-10-09 23:47:18 +0100

    PowerPC: fix a cast warning

--- Source/Core/Core/PowerPC/PowerPC.cpp
+++ Source/Core/Core/PowerPC/PowerPC.cpp
@@ -191,7 +191,8 @@ static void InitializeCPUCore(CPUCore cpu_core)
     s_cpu_core_base = JitInterface::InitJitCore(cpu_core);
     if (!s_cpu_core_base)  // Handle Situations where JIT core isn't available
     {
-      WARN_LOG(POWERPC, "CPU core %d not available. Falling back to default.", cpu_core);
+      WARN_LOG(POWERPC, "CPU core %d not available. Falling back to default.",
+               static_cast<int>(cpu_core));
       s_cpu_core_base = JitInterface::InitJitCore(DefaultCPUCore());
     }
     break;
