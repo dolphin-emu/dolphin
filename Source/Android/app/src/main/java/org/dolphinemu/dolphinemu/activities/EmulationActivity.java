ce640e129dbd9b0077c65c9990cd0d664dba422a
JosJuice <josjuice@gmail.com>
2018-10-27 15:49:17 +0200

    Android: Don't create a savestate when rotating

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/EmulationActivity.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/EmulationActivity.java
@@ -340,7 +340,10 @@ public final class EmulationActivity extends AppCompatActivity
   @Override
   protected void onSaveInstanceState(Bundle outState)
   {
-    mEmulationFragment.saveTemporaryState();
+    if (!isChangingConfigurations())
+    {
+      mEmulationFragment.saveTemporaryState();
+    }
     outState.putString(EXTRA_SELECTED_GAME, mPath);
     outState.putString(EXTRA_SELECTED_TITLE, mSelectedTitle);
     outState.putInt(EXTRA_PLATFORM, mPlatform);
