6392be61ebf00188cd048b42c99fff60f0a56139
Stenzek <stenzek@gmail.com>
2018-10-27 21:22:55 +1000

    Host: Also call ControllerInterface::ChangeWindow when window changes
    This should prevent us from trying to poll against destroyed native
    windows.

--- Source/Core/DolphinQt/Host.cpp
+++ Source/Core/DolphinQt/Host.cpp
@@ -19,6 +19,8 @@
 #include "DolphinQt/QtUtils/QueueOnObject.h"
 #include "DolphinQt/Settings.h"
 
+#include "InputCommon/ControllerInterface/ControllerInterface.h"
+
 #include "VideoCommon/RenderBase.h"
 #include "VideoCommon/VideoConfig.h"
 
@@ -37,7 +39,11 @@ void Host::SetRenderHandle(void* handle)
 
   m_render_handle = handle;
   if (g_renderer)
+  {
     g_renderer->ChangeSurface(handle);
+    if (g_controller_interface.IsInit())
+      g_controller_interface.ChangeWindow(handle);
+  }
 }
 
 bool Host::GetRenderFocus()
