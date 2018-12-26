e6b859a9238fd4f35c1b12a5bf92a8b50c031988
JMC47 <JMC4789@gmail.com>
2018-12-26 14:01:20 -0500

    Update Netplay Main Page Warnings
    We now support automatically syncing most things.  Add Dual Core Netplay
    Warning.  Update Wii Remote Netplay string since it now kinda works.

--- Source/Core/DolphinQt/NetPlay/NetPlaySetupDialog.cpp
+++ Source/Core/DolphinQt/NetPlay/NetPlaySetupDialog.cpp
@@ -87,14 +87,17 @@ void NetPlaySetupDialog::CreateMainLayout()
   connection_layout->addWidget(m_connect_port_label, 0, 2);
   connection_layout->addWidget(m_connect_port_box, 0, 3);
   connection_layout->addWidget(
-      new QLabel(tr(
-          "ALERT:\n\n"
-          "All players must use the same Dolphin version.\n"
-          "All memory cards, SD cards and cheats must be identical between players or disabled.\n"
-          "If DSP LLE is used, DSP ROMs must be identical between players.\n"
-          "If connecting directly, the host must have the chosen UDP port open/forwarded!\n"
-          "\n"
-          "Wii Remote support in netplay is experimental and should not be expected to work.\n")),
+      new QLabel(
+          tr("ALERT:\n\n"
+             "All players must use the same Dolphin version.\n"
+             "If enabled, SD cards must be identical between players.\n"
+             "If DSP LLE is used, DSP ROMs must be identical between players.\n"
+             "If a game is hanging on boot, it may not support Dual Core Netplay."
+             " Disable Dual Core.\n"
+             "If connecting directly, the host must have the chosen UDP port open/forwarded!\n"
+             "\n"
+             "Wii Remote support in netplay is experimental and may not work correctly.\n"
+             "Use at your own risk.\n")),
       1, 0, -1, -1);
   connection_layout->addWidget(m_connect_button, 3, 3, Qt::AlignRight);
 
