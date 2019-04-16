cd4e69d365bd56a5ebf082d5b9c60da7a542d5d5
altimumdelta <48530820+altimumdelta@users.noreply.github.com>
2019-04-16 07:53:57 +0200

    Ext: Update FFmpeg to 4.2.1 with new features, Add x264 encoder

---
+++ Externals/x264-win64/include/x264_config.h
@@ -0,0 +1,8 @@
+#define X264_GPL           1
+#define X264_INTERLACED    1
+#define X264_BIT_DEPTH     0
+#define X264_CHROMA_FORMAT 0
+#define X264_REV 2980
+#define X264_REV_DIFF 0
+#define X264_VERSION " r2980 34c06d1"
+#define X264_POINTVER "0.157.2980 34c06d1"
