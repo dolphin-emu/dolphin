ec8d57d882067e1cd933f7efaed9947e8a444b3c
MerryMage <MerryMage@users.noreply.github.com>
2019-05-25 22:43:04 +0100

    UnitTests/Jit64Common: Test GenConvertDoubleToSingle

--- Source/UnitTests/Core/PowerPC/Jit64Common/ConvertDoubleToSingle.cpp
+++ Source/UnitTests/Core/PowerPC/Jit64Common/ConvertDoubleToSingle.cpp
@@ -1,15 +1,15 @@
-// Copyright 2018 Dolphin Emulator Project
+// Copyright 2019 Dolphin Emulator Project
 // Licensed under GPLv2+
 // Refer to the license.txt file included.
 
 #include <cstring>
+#include <tuple>
 #include <vector>
 
-#include "Common/BitUtils.h"
 #include "Common/CommonTypes.h"
-#include "Common/FloatUtils.h"
 #include "Common/x64ABI.h"
 #include "Core/PowerPC/Gekko.h"
+#include "Core/PowerPC/Interpreter/Interpreter_FPUtils.h"
 #include "Core/PowerPC/Jit64/Jit.h"
 #include "Core/PowerPC/Jit64Common/Jit64AsmCommon.h"
 #include "Core/PowerPC/Jit64Common/Jit64PowerPCState.h"
@@ -28,38 +28,32 @@ public:
     AllocCodeSpace(4096);
     m_const_pool.Init(AllocChildCodeSpace(1024), 1024);
 
-    const auto raw_frsqrte = reinterpret_cast<double (*)(double)>(AlignCode4());
-    GenFrsqrte();
+    const auto raw_cdts = reinterpret_cast<double (*)(double)>(AlignCode4());
+    GenConvertDoubleToSingle();
 
-    wrapped_frsqrte = reinterpret_cast<u64 (*)(u64, UReg_FPSCR&)>(AlignCode4());
+    wrapped_cdts = reinterpret_cast<u32 (*)(u64)>(AlignCode4());
     ABI_PushRegistersAndAdjustStack(ABI_ALL_CALLEE_SAVED, 8, 16);
-
-    // We know the frsqrte implementation only accesses the fpscr. We manufacture a
-    // PPCSTATE pointer so we read/write to our provided fpscr argument instead.
-    XOR(32, R(RPPCSTATE), R(RPPCSTATE));
-    LEA(64, RSCRATCH, PPCSTATE(fpscr));
-    SUB(64, R(ABI_PARAM2), R(RSCRATCH));
-    MOV(64, R(RPPCSTATE), R(ABI_PARAM2));
 
     // Call
     MOVQ_xmm(XMM0, R(ABI_PARAM1));
-    ABI_CallFunction(raw_frsqrte);
+    ABI_CallFunction(raw_cdts);
     MOVQ_xmm(R(ABI_RETURN), XMM0);
 
     ABI_PopRegistersAndAdjustStack(ABI_ALL_CALLEE_SAVED, 8, 16);
     RET();
   }
 
-  u64 (*wrapped_frsqrte)(u64, UReg_FPSCR&);
+  u32 (*wrapped_cdts)(u64);
   Jit64 jit;
 };
 }  // namespace
 
-TEST(Jit64, Frsqrte)
+TEST(Jit64, ConvertDoubleToSingle)
 {
   TestCommonAsmRoutines routines;
 
-  const std::vector<u64> special_values{
+  const std::vector<u64> input_values{
+      // Special values
       0x0000'0000'0000'0000,  // positive zero
       0x0000'0000'0000'0001,  // smallest positive denormal
       0x0000'0000'0100'0000,
@@ -86,19 +80,36 @@ TEST(Jit64, Frsqrte)
       0xFFF7'FFFF'FFFF'FFFF,  // last negative SNaN
       0xFFF8'0000'0000'0000,  // first negative QNaN
       0xFFFF'FFFF'FFFF'FFFF,  // last negative QNaN
+
+      // (exp > 896) Boundary Case
+      0x3800'0000'0000'0000,  // 2^(-127) = Denormal in single-prec
+      0x3810'0000'0000'0000,  // 2^(-126) = Smallest single-prec normal
+      0xB800'0000'0000'0000,  // -2^(-127) = Denormal in single-prec
+      0xB810'0000'0000'0000,  // -2^(-126) = Smallest single-prec normal
+      0x3800'1234'5678'9ABC, 0x3810'1234'5678'9ABC, 0xB800'1234'5678'9ABC, 0xB810'1234'5678'9ABC,
+
+      // (exp >= 874) Boundary Case
+      0x3680'0000'0000'0000,  // 2^(-150) = Unrepresentable in single-prec
+      0x36A0'0000'0000'0000,  // 2^(-149) = Smallest single-prec denormal
+      0x36B0'0000'0000'0000,  // 2^(-148) = Single-prec denormal
+      0xB680'0000'0000'0000,  // -2^(-150) = Unrepresentable in single-prec
+      0xB6A0'0000'0000'0000,  // -2^(-149) = Smallest single-prec denormal
+      0xB6B0'0000'0000'0000,  // -2^(-148) = Single-prec denormal
+      0x3680'1234'5678'9ABC, 0x36A0'1234'5678'9ABC, 0x36B0'1234'5678'9ABC, 0xB680'1234'5678'9ABC,
+      0xB6A0'1234'5678'9ABC, 0xB6B0'1234'5678'9ABC,
+
+      // Some typical numbers
+      0x3FF8'0000'0000'0000,  // 1.5
+      0x408F'4000'0000'0000,  // 1000
+      0xC008'0000'0000'0000,  // -3
   };
 
-  UReg_FPSCR fpscr;
-
-  for (u64 ivalue : special_values)
+  for (const u64 input : input_values)
   {
-    double dvalue = Common::BitCast<double>(ivalue);
+    const u32 expected = ConvertToSingle(input);
+    const u32 actual = routines.wrapped_cdts(input);
 
-    u64 expected = Common::BitCast<u64>(Common::ApproximateReciprocalSquareRoot(dvalue));
-
-    u64 actual = routines.wrapped_frsqrte(ivalue, fpscr);
-
-    printf("%016llx -> %016llx == %016llx\n", ivalue, actual, expected);
+    printf("%016llx -> %08x == %08x\n", input, actual, expected);
 
     EXPECT_EQ(expected, actual);
   }
