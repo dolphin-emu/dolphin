fd8bc021e1c635e344fdb1c409a717a3edbca860
Scott Mansell <phiren@gmail.com>
2019-09-06 21:22:47 +1200

    Extend Minimal TMEM cache implementation
    Now works with games that deliberately avoid invalidating TMEM because
    they know textures are too large to fit:
    
     * Sonic Riders
     * Metal Arms: Glitch in the System
     * Godzilla: Destroy All Monsters Melee
     * NHL Slapshot
     * Tak and the Power of Juju
     * Night at the Museum: Battle of the Smithsonian
     * 428: Fūsa Sareta Shibuya de
    
    This is just a first pass, while it should work, I'm not happy with
    how the code is structured.

--- Source/Core/VideoCommon/BPStructs.cpp
+++ Source/Core/VideoCommon/BPStructs.cpp
@@ -32,6 +32,7 @@
 #include "VideoCommon/RenderBase.h"
 #include "VideoCommon/TextureCacheBase.h"
 #include "VideoCommon/TextureDecoder.h"
+#include "VideoCommon/TmemBase.h"
 #include "VideoCommon/VertexShaderManager.h"
 #include "VideoCommon/VideoBackendBase.h"
 #include "VideoCommon/VideoCommon.h"
@@ -458,8 +459,7 @@ static void BPWritten(const BPCmd& bp)
   }
     return;
   case BPMEM_TEXINVALIDATE:
-    // TODO: Needs some restructuring in TextureCacheBase.
-    TextureCacheBase::InvalidateAllBindPoints();
+    TmemBase::Invalidate(bp.newvalue);
     return;
 
   case BPMEM_ZCOMPARE:  // Set the Z-Compare and EFB pixel format
@@ -668,14 +668,17 @@ static void BPWritten(const BPCmd& bp)
   // --------------------------------------------
   case BPMEM_TX_SETIMAGE0:
   case BPMEM_TX_SETIMAGE0_4:
-  case BPMEM_TX_SETIMAGE1:
-  case BPMEM_TX_SETIMAGE1_4:
-  case BPMEM_TX_SETIMAGE2:
-  case BPMEM_TX_SETIMAGE2_4:
   case BPMEM_TX_SETIMAGE3:
   case BPMEM_TX_SETIMAGE3_4:
     TextureCacheBase::InvalidateAllBindPoints();
     return;
+  case BPMEM_TX_SETIMAGE1:
+  case BPMEM_TX_SETIMAGE1_4:
+  case BPMEM_TX_SETIMAGE2:
+  case BPMEM_TX_SETIMAGE2_4:
+    TmemBase::ConfigurationChanged(bp.address, bp.newvalue);
+    return;
+
   // -------------------------------
   // Set a TLUT
   // BPMEM_TX_SETTLUT - Format, TMEM Offset (offset of TLUT from start of TMEM high bank > > 5)
