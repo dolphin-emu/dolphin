bbeb25de48fa11103ed131eacf7ecf02bc9a1dd3
Admiral H. Curtiss <pikachu025@gmail.com>
2019-10-14 21:47:27 +0200

    Qt/Debugger/CodeWidget: Allow pressing 'enter' in address search box.

--- Source/Core/DolphinQt/Debugger/CodeWidget.cpp
+++ Source/Core/DolphinQt/Debugger/CodeWidget.cpp
@@ -156,6 +156,7 @@ void CodeWidget::CreateWidgets()
 void CodeWidget::ConnectWidgets()
 {
   connect(m_search_address, &QLineEdit::textChanged, this, &CodeWidget::OnSearchAddress);
+  connect(m_search_address, &QLineEdit::returnPressed, this, &CodeWidget::OnSearchAddress);
   connect(m_search_symbols, &QLineEdit::textChanged, this, &CodeWidget::OnSearchSymbols);
 
   connect(m_symbols_list, &QListWidget::itemPressed, this, &CodeWidget::OnSelectSymbol);
