b947c6a2315f75c7cd378af252efdabb6f2952c0
SirMangler <thecatsmangler@gmail.com>
2019-10-18 16:19:27 +0100

    Removed redundant hack_config code.

--- Source/Core/Core/PrimeHack/HackConfig.cpp
+++ Source/Core/Core/PrimeHack/HackConfig.cpp
@@ -22,9 +22,6 @@ static bool inverted_y = false;
 static bool inverted_x = false;
 static HackManager hack_mgr;
 
-static std::array<std::string, 4> beam_binds = {"`1` & !E", "`2` & !E", "`3` & !E", "`4` & !E"};
-static std::array<std::string, 4> visor_binds = {"", "E & `1`", "E & `2`", "E & `3`"};
-
 void InitializeHack(std::string const& mkb_device_name, std::string const& mkb_device_source)
 {
   // Create mods for all games/regions.
@@ -49,57 +46,6 @@ void RefreshControlDevices()
         g_controller_interface,
         ciface::Core::DeviceQualifier(device_source.c_str(), 0, device_name.c_str()));
   }
-}
-
-void SaveStateToIni()
-{
-  IniFile cfg_file;
-  cfg_file.Load(config_path, true);
-
-  auto* sens_section = cfg_file.GetOrCreateSection("mouse");
-  sens_section->Set("sensitivity", sensitivity);
-  sens_section->Set("cursor_sensitivity", cursor_sensitivity);
-  auto* misc_section = cfg_file.GetOrCreateSection("misc");
-  misc_section->Set("fov", camera_fov);
-  misc_section->Set("inverted_y", inverted_y);
-  misc_section->Set("inverted_x", inverted_x);
-  auto* beam_section = cfg_file.GetOrCreateSection("beam");
-  auto* visor_section = cfg_file.GetOrCreateSection("visor");
-
-  for (int i = 0; i < 4; i++)
-  {
-    std::string control = std::string("index_") + std::to_string(i);
-    beam_section->Set(control, control_list[i]->GetExpression());
-    visor_section->Set(control, control_list[i + 4]->GetExpression());
-  }
-  cfg_file.Save(config_path);
-}
-
-void LoadStateFromIni()
-{
-  IniFile cfg_file;
-  cfg_file.Load(config_path, true);
-
-  auto* sens_section = cfg_file.GetOrCreateSection("mouse");
-  sens_section->Get("sensitivity", &sensitivity);
-  sens_section->Get("cursor_sensitivity", &cursor_sensitivity);
-  auto* misc_section = cfg_file.GetOrCreateSection("misc");
-  misc_section->Get("fov", &camera_fov);
-  misc_section->Get("inverted_y", &inverted_y);
-  misc_section->Get("inverted_x", &inverted_x);
-  auto* beam_section = cfg_file.GetOrCreateSection("beam");
-  auto* visor_section = cfg_file.GetOrCreateSection("visor");
-
-  for (int i = 0; i < 4; i++)
-  {
-    std::string control = std::string("index_") + std::to_string(i);
-    std::string expr;
-    beam_section->Get(control, &expr);
-    control_list[i]->SetExpression(expr);
-    visor_section->Get(control, &expr);
-    control_list[i + 4]->SetExpression(expr);
-  }
-  cfg_file.Save(config_path);
 }
 
 bool CheckBeamCtl(int beam_num)
