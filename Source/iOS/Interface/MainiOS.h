66c5a56d7f59baa0a7e41903d4b341f612a82602
OatmealDome <julian@oatmealdome.me>
2019-10-24 20:17:49 -0400

    iOS: Add barebones Host interface file

---
+++ Source/iOS/Interface/MainiOS.h
@@ -0,0 +1,11 @@
+// Copyright 2019 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#pragma once
+
+#import <Foundation/Foundation.h>
+
+@interface MainiOS : NSObject
+
+@end
