d8bc142ee9d0799cc0b29652a70a88f55cef05d7
OatmealDome <julian@oatmealdome.me>
2019-10-24 20:17:49 -0400

    iOS: Add barebones Host interface file

---
+++ Source/iOS/Interface/MainiOS.mm
@@ -0,0 +1,72 @@
+// Copyright 2019 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#include <string>
+
+#include "Core/Host.h"
+
+#include "MainiOS.h"
+
+void Host_NotifyMapLoaded()
+{
+}
+void Host_RefreshDSPDebuggerWindow()
+{
+}
+
+bool Host_UIBlocksControllerState()
+{
+  return false;
+}
+
+void Host_Message(HostMessageID id)
+{
+}
+
+void Host_UpdateTitle(const std::string& title)
+{
+}
+
+void Host_UpdateDisasmDialog()
+{
+}
+
+void Host_UpdateMainFrame()
+{
+}
+
+void Host_RequestRenderWindowSize(int width, int height)
+{
+}
+
+bool Host_UINeedsControllerState()
+{
+  return false;
+}
+
+bool Host_RendererHasFocus()
+{
+  return true;
+}
+
+bool Host_RendererIsFullscreen()
+{
+  return true;
+}
+
+void Host_YieldToUI()
+{
+}
+
+void Host_UpdateProgressDialog(const char* caption, int position, int total)
+{
+}
+
+void Host_TitleChanged()
+{
+}
+
+@implementation MainiOS
+
+@end
