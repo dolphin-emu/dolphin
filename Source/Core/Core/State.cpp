371e3f10210feb42189da5d3379ad67c814b7227
booto <remornicus@gmail.com>
2019-10-31 20:34:28 -0400

    VI: Adjust start/end of ACV region for fields
    This wasn't changed when halfline counts were changed to 0-based. Also
    included is a diagram showing an understanding of how the values in the VI
    timing registers maps to the video signal.

--- Source/Core/Core/State.cpp
+++ Source/Core/Core/State.cpp
@@ -72,7 +72,7 @@ static Common::Event g_compressAndDumpStateSyncEvent;
 static std::thread g_save_thread;
 
 // Don't forget to increase this after doing changes on the savestate system
-static const u32 STATE_VERSION = 111;  // Last changed in PR 6321
+static const u32 STATE_VERSION = 112;  // Last changed in PR 8444
 
 // Maps savestate versions to Dolphin versions.
 // Versions after 42 don't need to be added to this list,
