7d8393bf7894f49c5dbae7219f55dfc6b6b782a9
SirMangler <thecatsmangler@gmail.com>
2019-11-26 17:13:33 +0000

    Fixed MP3 Springball bug

--- Source/Core/Core/PrimeHack/AimMods.cpp
+++ Source/Core/Core/PrimeHack/AimMods.cpp
@@ -659,7 +659,7 @@ void MP3::run_mod()
     return;
   }
 
-  springball_check(base_address + 0xE884, base_address + 0x29c);
+  springball_check(base_address + 0x358, base_address + 0x29c);
 
   int dx = g_mouse_input->GetDeltaHorizontalAxis(), dy = g_mouse_input->GetDeltaVerticalAxis();
   const float compensated_sens = GetSensitivity() * TURNRATE_RATIO / 60.0f;
