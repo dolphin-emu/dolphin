515363f83f996c03c8de35e0561f2cb3aeaa09d8
OatmealDome <julian@oatmealdome.me>
2019-11-29 23:12:45 -0500

    JitArm64: Use WriteCode and WriteCodeAtRegion for W^X compliance

--- Source/Core/Core/PowerPC/JitArm64/JitArm64Cache.cpp
+++ Source/Core/Core/PowerPC/JitArm64/JitArm64Cache.cpp
@@ -16,45 +16,50 @@ JitArm64BlockCache::JitArm64BlockCache(JitBase& jit) : JitBaseBlockCache{jit}
 void JitArm64BlockCache::WriteLinkBlock(Arm64Gen::ARM64XEmitter& emit,
                                         const JitBlock::LinkData& source, const JitBlock* dest)
 {
-  if (!dest)
-  {
-    // Use a fixed amount of instructions, so we can assume to use 3 instructions on patching.
-    emit.MOVZ(DISPATCHER_PC, source.exitAddress & 0xFFFF, SHIFT_0);
-    emit.MOVK(DISPATCHER_PC, source.exitAddress >> 16, SHIFT_16);
+  JitArm64& armJit = static_cast<JitArm64&>(m_jit);
+  armJit.WriteCodeAtRegion(
+      [&] {
+        if (!dest)
+        {
+          // Use a fixed amount of instructions, so we can assume to use 3 instructions on patching.
+          emit.MOVZ(DISPATCHER_PC, source.exitAddress & 0xFFFF, SHIFT_0);
+          emit.MOVK(DISPATCHER_PC, source.exitAddress >> 16, SHIFT_16);
 
-    if (source.call)
-      emit.BL(m_jit.GetAsmRoutines()->dispatcher);
-    else
-      emit.B(m_jit.GetAsmRoutines()->dispatcher);
-    return;
-  }
+          if (source.call)
+            emit.BL(m_jit.GetAsmRoutines()->dispatcher);
+          else
+            emit.B(m_jit.GetAsmRoutines()->dispatcher);
+          return;
+        }
 
-  if (source.call)
-  {
-    // The "fast" BL must be the third instruction. So just use the former two to inline the
-    // downcount check here. It's better to do this near jump before the long jump to the other
-    // block.
-    FixupBranch fast_link = emit.B(CC_PL);
-    emit.BL(dest->checkedEntry);
-    emit.SetJumpTarget(fast_link);
-    emit.BL(dest->normalEntry);
-    return;
-  }
+        if (source.call)
+        {
+          // The "fast" BL must be the third instruction. So just use the former two to inline the
+          // downcount check here. It's better to do this near jump before the long jump to the
+          // other block.
+          FixupBranch fast_link = emit.B(CC_PL);
+          emit.BL(dest->checkedEntry);
+          emit.SetJumpTarget(fast_link);
+          emit.BL(dest->normalEntry);
+          return;
+        }
 
-  // Are we able to jump directly to the normal entry?
-  s64 distance = ((s64)dest->normalEntry - (s64)emit.GetCodePtr()) >> 2;
-  if (distance >= -0x40000 && distance <= 0x3FFFF)
-  {
-    emit.B(CC_PL, dest->normalEntry);
-    emit.B(dest->checkedEntry);
-    emit.BRK(101);
-    return;
-  }
+        // Are we able to jump directly to the normal entry?
+        s64 distance = ((s64)dest->normalEntry - (s64)emit.GetCodePtr()) >> 2;
+        if (distance >= -0x40000 && distance <= 0x3FFFF)
+        {
+          emit.B(CC_PL, dest->normalEntry);
+          emit.B(dest->checkedEntry);
+          emit.BRK(101);
+          return;
+        }
 
-  FixupBranch fast_link = emit.B(CC_PL);
-  emit.B(dest->checkedEntry);
-  emit.SetJumpTarget(fast_link);
-  emit.B(dest->normalEntry);
+        FixupBranch fast_link = emit.B(CC_PL);
+        emit.B(dest->checkedEntry);
+        emit.SetJumpTarget(fast_link);
+        emit.B(dest->normalEntry);
+      },
+      source.exitPtrs, 32);
 }
 
 void JitArm64BlockCache::WriteLinkBlock(const JitBlock::LinkData& source, const JitBlock* dest)
@@ -72,8 +77,13 @@ void JitArm64BlockCache::WriteDestroyBlock(const JitBlock& block)
   // Only clear the entry points as we might still be within this block.
   ARM64XEmitter emit(block.checkedEntry);
 
-  while (emit.GetWritableCodePtr() <= block.normalEntry)
-    emit.BRK(0x123);
+  JitArm64& armJit = static_cast<JitArm64&>(m_jit);
+  armJit.WriteCodeAtRegion(
+      [&] {
+        while (emit.GetWritableCodePtr() <= block.normalEntry)
+          emit.BRK(0x123);
+      },
+      emit.GetWritableCodePtr(), block.normalEntry - emit.GetWritableCodePtr());
 
   emit.FlushIcache();
 }
