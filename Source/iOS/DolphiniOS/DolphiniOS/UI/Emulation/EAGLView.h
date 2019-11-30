5d0bf9698e9730b9e6cb4396323521e03a457547
OatmealDome <julian@oatmealdome.me>
2019-11-30 17:00:46 -0500

    iOS: Silence OpenGL ES deprecation warnings

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Emulation/EAGLView.h
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Emulation/EAGLView.h
@@ -4,6 +4,10 @@
 
 #pragma once
 
+// Apple started to mark all OpenGL ES APIs as deprecated in the iOS 12 SDK.
+// We can define this to silence all of the deprecation warnings.
+#define GLES_SILENCE_DEPRECATION
+
 #import <UIKit/UIKit.h>
 
 @interface EAGLView : UIView
