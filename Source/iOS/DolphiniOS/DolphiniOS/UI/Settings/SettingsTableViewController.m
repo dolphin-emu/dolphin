f486bd45a484de445a60677c9eddd397b9d6b9cb
OatmealDome <julian@oatmealdome.me>
2019-12-10 18:41:23 -0500

    iOS: Implement barebones Settings view Also, the Games tab has been wrapped in a navigation controller.

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/SettingsTableViewController.m
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/SettingsTableViewController.m
@@ -13,86 +13,30 @@
 - (void)viewDidLoad
 {
   [super viewDidLoad];
-
-  // Uncomment the following line to preserve selection between presentations.
-  // self.clearsSelectionOnViewWillAppear = NO;
-
-  // Uncomment the following line to display an Edit button in the navigation bar for this view
-  // controller. self.navigationItem.rightBarButtonItem = self.editButtonItem;
+  
+  // Set the version from Info.plist
+  NSDictionary* info = [[NSBundle mainBundle] infoDictionary];
+  [self.m_version_label setText:[info objectForKey:@"CFBundleVersion"]];
 }
 
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView*)tableView
+- (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
 {
-#warning Incomplete implementation, return the number of sections
-  return 0;
-}
-
-- (NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section
-{
-#warning Incomplete implementation, return the number of rows
-  return 0;
-}
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath
-*)indexPath { UITableViewCell *cell = [tableView
-dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-
-    // Configure the cell...
-
-    return cell;
-}
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView
-commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath
-*)indexPath { if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath]
-withRowAnimation:UITableViewRowAnimationFade]; } else if (editingStyle ==
-UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new
-row to the table view
+  if (indexPath.section == 1)
+  {
+    NSString* url;
+    if (indexPath.row == 0)
+    {
+      url = @"https://oatmealdome.me";
     }
+    else
+    {
+      url = @"https://patreon.com/oatmealdome";
+    }
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url] options:@{} completionHandler:nil];
+  }
+  
+  [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath
-toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before
-navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
