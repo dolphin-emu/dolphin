a8b69b2f3ec32a06abfa7fd85e0fc1db05e6382e
OatmealDome <julian@oatmealdome.me>
2019-12-12 03:28:46 -0500

    iOS: Add navigation bar and quit emulation button

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Emulation/EmulationVIewController.swift
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Emulation/EmulationVIewController.swift
@@ -8,58 +8,89 @@ import UIKit
 
 class EmulationViewController: UIViewController
 {
-  var softwareFile: String = ""
-  var isWii: Bool = false
+  @objc public var softwareFile: String = ""
+  @objc public var isWii: Bool = false
   
-  @objc init(file: String, wii: Bool)
-  {
-    super.init(nibName: nil, bundle: nil)
-    
-    self.softwareFile = file
-    self.isWii = wii
-    
-    self.modalPresentationStyle = .fullScreen
-  }
+  @IBOutlet weak var m_metal_view: MTKView!
+  @IBOutlet weak var m_gc_pad_view: TCGameCubePad!
+  @IBOutlet weak var m_wii_pad_view: TCWiiPad!
   
   required init?(coder: NSCoder)
   {
     super.init(coder: coder)
   }
   
-  override func loadView()
+  override func viewDidLoad()
   {
-    let videoBackend = MainiOS.getGfxBackend()
-    if (videoBackend == "Vulkan")
+    let has_seen_alert = UserDefaults.standard.bool(forKey: "seen_double_tap_alert")
+    if (!has_seen_alert)
     {
-      self.view = MTKView(frame: UIScreen.main.bounds)
+      let alert = UIAlertController(title: "Note", message: "Double tap the screen to reveal the top bar.", preferredStyle: .alert)
+      
+      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+        self.navigationController!.setNavigationBarHidden(true, animated: true)
+        
+        UserDefaults.standard.set(true, forKey: "seen_double_tap_alert")
+      }))
+      
+      self.present(alert, animated: true, completion: nil)
     }
     else
     {
-      // Everything else (OpenGL ES, Software, Null) should use the EAGLView
-      self.view = EAGLView(frame: UIScreen.main.bounds)
+      self.navigationController!.setNavigationBarHidden(true, animated: true)
     }
     
-    let controllerNibName: String = (self.isWii) ? "TCWiiPad" : "TCGameCubePad"
+    if (self.isWii)
+    {
+      m_wii_pad_view.isUserInteractionEnabled = true
+      m_wii_pad_view.isHidden = false
+    }
+    else
+    {
+      m_gc_pad_view.isUserInteractionEnabled = true
+      m_gc_pad_view.isHidden = false
+    }
     
-    let padView = Bundle(for: type(of: self)).loadNibNamed(controllerNibName, owner: self, options: nil)![0] as! UIView
-    padView.frame = UIScreen.main.bounds
-    self.view.addSubview(padView)
-  }
-  
-  override func viewDidLoad()
-  {
+    // TODO: EAGL
+    
     let queue = DispatchQueue(label: "org.dolphin-emu.ios.emulation-queue")
-    let view = self.view
-    
     queue.async
     {
-      MainiOS.startEmulation(withFile: self.softwareFile, view: view)
+      MainiOS.startEmulation(withFile: self.softwareFile, view: self.m_metal_view)
+      
+      DispatchQueue.main.async {
+        self.performSegue(withIdentifier: "toSoftwareTable", sender: nil)
+      }
     }
+  }
+  
+  @IBAction func tapInController(_ sender: UITapGestureRecognizer)
+  {
+    // Ignore double taps on things that can be tapped
+    let hit_view = sender.view?.window?.hitTest(sender.location(in: nil), with: nil)
+    if (hit_view is TCButton || hit_view is TCJoystick || hit_view is TCDirectionalPad)
+    {
+      return
+    }
+    
+    self.navigationController!.setNavigationBarHidden(!self.navigationController!.isNavigationBarHidden, animated: true)
   }
   
   override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator)
   {
     MainiOS.windowResized()
+  }
+  
+  @IBAction func exitButtonPressed(_ sender: Any)
+  {
+    let alert = UIAlertController(title: "Stop Emulation", message: "Do you really want to stop the emulation? All unsaved data will be lost.", preferredStyle: .alert)
+    
+    alert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
+    alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { action in
+      MainiOS.stopEmulation()
+    }))
+    
+    self.present(alert, animated: true, completion: nil)
   }
   
 }
