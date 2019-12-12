6690a589c3d9aa5c189089d8faed0ea4e9d56540
OatmealDome <julian@oatmealdome.me>
2019-12-12 18:04:40 -0500

    iOS: Add special thanks Includes patrons and Dolphin Team.

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/SettingsTableViewController.m
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/SettingsTableViewController.m
@@ -28,9 +28,29 @@
     {
       url = @"https://oatmealdome.me";
     }
-    else
+    else if (indexPath.row == 1)
     {
       url = @"https://patreon.com/oatmealdome";
+    }
+    else
+    {
+      // Load the special thanks
+      NSString* thanks_path = [[NSBundle mainBundle] pathForResource:@"SpecialThanks" ofType:@"txt"];
+      NSString* thanks_str = [NSString stringWithContentsOfFile:thanks_path encoding:NSUTF8StringEncoding error:nil];
+      
+      UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Special Thanks"
+                                     message:[NSString stringWithFormat:@"Thank you to the following awesome people!\n\n%@\nFinally, thanks to the Dolphin Team for making a great emulator!", thanks_str]
+                                     preferredStyle:UIAlertControllerStyleAlert];
+       
+      UIAlertAction* ok_action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+         handler:nil];
+      [alert addAction:ok_action];
+      
+      [self presentViewController:alert animated:true completion:nil];
+      
+      [self.tableView deselectRowAtIndexPath:indexPath animated:true];
+      
+      return;
     }
     
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url] options:@{} completionHandler:nil];
