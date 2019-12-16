1bd3af82c8138929441ba8e14b23544a78a8d4df
OatmealDome <julian@oatmealdome.me>
2019-12-16 17:18:45 -0500

    iOS: Don't fail if DolphiniOS folder already exists

--- Source/iOS/DolphiniOS/DolphiniOS/postinst.sh
+++ Source/iOS/DolphiniOS/DolphiniOS/postinst.sh
@@ -5,7 +5,7 @@ set -e
 chown root:wheel /Library/LaunchDaemons/me.oatmealdome.csdbgd.plist
 launchctl load -w /Library/LaunchDaemons/me.oatmealdome.csdbgd.plist
 
-mkdir /private/var/mobile/Documents/DolphiniOS
+mkdir /private/var/mobile/Documents/DolphiniOS || true
 chown -R mobile:mobile /private/var/mobile/Documents/DolphiniOS
 
 uicache
