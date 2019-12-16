1417c1332c87093a1110ac62724d6a1af0395de4
OatmealDome <julian@oatmealdome.me>
2019-12-15 23:10:01 -0500

    iOS: Make user directory and chown it on installation

--- Source/iOS/DolphiniOS/DolphiniOS/postinst.sh
+++ Source/iOS/DolphiniOS/DolphiniOS/postinst.sh
@@ -4,3 +4,6 @@ set -e
 
 chown root:wheel /Library/LaunchDaemons/me.oatmealdome.csdbgd.plist
 launchctl load -w /Library/LaunchDaemons/me.oatmealdome.csdbgd.plist
+
+mkdir /private/var/mobile/Documents/DolphiniOS
+chown -R mobile:mobile /private/var/mobile/Documents/DolphiniOS
