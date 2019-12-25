d88757a006daccab52311b3db7eb66d685806343
OatmealDome <julian@oatmealdome.me>
2019-12-25 03:14:27 -0500

    iOS: Prevent opening the motion controls menu during a GC game

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Emulation/InGameSettings/InGameConfigViewController.swift
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Emulation/InGameSettings/InGameConfigViewController.swift
@@ -15,6 +15,20 @@ class InGameConfigViewController: UITableViewController
     self.m_emulation_controller = self.parent?.presentingViewController?.children[0] as? EmulationViewController
   }
   
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath?
+  {
+    if (indexPath.section == 0 && indexPath.row == 1 && !m_emulation_controller!.isWii)
+    {
+      let alert = UIAlertController(title: "GameCube Game", message: "You cannot do this in a GameCube game.", preferredStyle: .alert)
+      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+      self.present(alert, animated: true, completion: nil)
+      
+      return nil
+    }
+    
+    return indexPath
+  }
+  
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
   {
     if (indexPath.section == 0 && indexPath.row == 0)
