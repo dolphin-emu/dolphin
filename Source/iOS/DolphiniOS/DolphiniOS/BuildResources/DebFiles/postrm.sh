7c2fa0c8ac80b5f733d70aa6e3994eca6e60771b
OatmealDome <julian@oatmealdome.me>
2019-12-25 03:29:17 -0500

    iOS: Add postrm script to say that user data was not deleted

---
+++ Source/iOS/DolphiniOS/DolphiniOS/BuildResources/DebFiles/postrm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e
+
+echo "DolphiniOS has been uninstalled. However, your user data and ROMs have not been deleted."
+echo "If you want to delete these, remove the folder /private/var/mobile/Documents/DolphiniOS."
