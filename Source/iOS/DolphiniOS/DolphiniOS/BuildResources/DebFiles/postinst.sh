1c544e83063be63a0c0bb6b15c0417a4624df729
OatmealDome <julian@oatmealdome.me>
2019-12-25 20:22:45 -0500

    iOS: Make a Documents folder if the user doesn't have one

--- Source/iOS/DolphiniOS/DolphiniOS/BuildResources/DebFiles/postinst.sh
+++ Source/iOS/DolphiniOS/DolphiniOS/BuildResources/DebFiles/postinst.sh
@@ -9,7 +9,7 @@ echo "Loading csdbgd"
 launchctl load -w /Library/LaunchDaemons/me.oatmealdome.csdbgd.plist
 
 echo "Creating user directory if needed"
-mkdir /private/var/mobile/Documents/DolphiniOS || true
+mkdir -p /private/var/mobile/Documents/DolphiniOS || true
 
 echo "Setting user directory permissions"
 chown -R mobile:mobile /private/var/mobile/Documents/DolphiniOS
