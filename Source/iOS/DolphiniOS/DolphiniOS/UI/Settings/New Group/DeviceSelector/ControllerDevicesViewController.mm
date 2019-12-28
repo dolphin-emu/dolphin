1091f19d1740da1b101f46c315aead49e486981b
OatmealDome <julian@oatmealdome.me>
2019-12-28 17:52:30 -0500

    iOS: Load default profiles when changing the device

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/New Group/DeviceSelector/ControllerDevicesViewController.mm
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/New Group/DeviceSelector/ControllerDevicesViewController.mm
@@ -32,6 +32,8 @@
 {
   NSMutableArray* array = [NSMutableArray array];
   
+  self.m_last_selected = -1;
+  
   const std::string current_device = self.m_controller->GetDefaultDevice().ToString();
   std::vector<std::string> devices = g_controller_interface.GetAllDeviceStrings();
   
@@ -47,6 +49,19 @@
   }
   
   self.m_devices = [array copy];
+  
+  if (self.m_last_selected == -1)
+  {
+    NSString* alert_str = [NSString stringWithFormat:@"The selected physical controller \"%@\" for this emulated controller is not connected to your device.", [NSString stringWithUTF8String:current_device.c_str()]];
+    
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Warning"
+                                   message:alert_str
+                                   preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                            handler:nil]];
+    
+    [self presentViewController:alert animated:true completion:nil];
+  }
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -76,18 +91,102 @@
 
 - (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
 {
+  if (indexPath.row == self.m_last_selected)
+  {
+    return;
+  }
+  
+  std::string last_device = self.m_controller->GetDefaultDevice().ToString();
+  std::string device_name = std::string([[self.m_devices objectAtIndex:indexPath.row] cStringUsingEncoding:NSUTF8StringEncoding]);
+  std::string android_str("Android");
+  std::string mfi_str("MFi");
+  
+  if (device_name.compare(0, android_str.size(), android_str) == 0)
+  {
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Configuration"
+                                   message:@"Your configuration will be overwritten by the Touchscreen defaults. If you wish to keep your button rebinds and settings, save it as a profile on the previous screen before changing devices.\n\nDo you wish to proceed?"
+                                   preferredStyle:UIAlertControllerStyleAlert];
+     
+    UIAlertAction* stop_action = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleDefault
+       handler:^(UIAlertAction * action) {
+      [self.tableView deselectRowAtIndexPath:indexPath animated:true];
+    }];
+    
+    UIAlertAction* overwrite_action = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDestructive
+       handler:^(UIAlertAction * action) {
+      [self LoadDefaultProfileForType:@"Touch"];
+      [self ChangeDevice:device_name];
+     }];
+    
+    [alert addAction:stop_action];
+    [alert addAction:overwrite_action];
+    
+    [self presentViewController:alert animated:true completion:nil];
+  }
+  else if (device_name.compare(0, mfi_str.size(), mfi_str) == 0 && last_device.compare(0, android_str.size(), android_str) == 0)
+  {
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Configuration"
+                                   message:@"Do you want to load the default configuration for MFi controllers? You may customize the button rebinds and settings after if you wish."
+                                   preferredStyle:UIAlertControllerStyleAlert];
+     
+    UIAlertAction* stop_action = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleDefault
+       handler:^(UIAlertAction * action) {
+      [self ChangeDevice:device_name];
+    }];
+    
+    UIAlertAction* continue_action = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+      [self LoadDefaultProfileForType:@"MFi"];
+      [self ChangeDevice:device_name];
+     }];
+    
+    [alert addAction:stop_action];
+    [alert addAction:continue_action];
+    
+    [self presentViewController:alert animated:true completion:nil];
+  }
+  else
+  {
+    [self ChangeDevice:device_name];
+  }
+}
+
+- (void)ChangeDevice:(std::string)new_device
+{
+  NSIndexPath* indexPath = [self.tableView indexPathForSelectedRow];
   UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:indexPath];
   [cell setAccessoryType:UITableViewCellAccessoryCheckmark];
   
-  UITableViewCell* old_cell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:self.m_last_selected inSection:0]];
-  [old_cell setAccessoryType:UITableViewCellAccessoryNone];
+  if (self.m_last_selected != -1)
+  {
+    UITableViewCell* old_cell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:self.m_last_selected inSection:0]];
+    [old_cell setAccessoryType:UITableViewCellAccessoryNone];
+  }
   
   self.m_last_selected = indexPath.row;
   
-  std::string device_name = std::string([[self.m_devices objectAtIndex:indexPath.row] cStringUsingEncoding:NSUTF8StringEncoding]);
-  self.m_controller->SetDefaultDevice(device_name);
+  self.m_controller->SetDefaultDevice(new_device);
   
   [self.tableView deselectRowAtIndexPath:indexPath animated:true];
+}
+
+- (void)LoadDefaultProfileForType:(NSString*)type
+{
+  NSString* ini_path;
+  if (self.m_is_wii)
+  {
+    ini_path = [NSString stringWithFormat:@"%@WiimoteProfile", type];
+  }
+  else
+  {
+    ini_path = [NSString stringWithFormat:@"%@GCPadProfile", type];
+  }
+  
+  NSString* bundle_path = [[NSBundle mainBundle] pathForResource:ini_path ofType:@"ini"];
+  
+  IniFile ini;
+  ini.Load(std::string([bundle_path cStringUsingEncoding:NSUTF8StringEncoding]));
+  self.m_controller->LoadConfig(ini.GetOrCreateSection("Profile"));
+  self.m_controller->UpdateReferences(g_controller_interface);
 }
 
 @end
