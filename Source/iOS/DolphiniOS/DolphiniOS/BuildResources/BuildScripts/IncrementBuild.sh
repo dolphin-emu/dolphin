44847c7e93fdc35cec13ec7ab7aadc4c56b31c65
OatmealDome <julian@oatmealdome.me>
2019-12-30 15:15:09 -0500

    iOS: Update build system to increment CFBundleVersion instead of using agvtool

--- Source/iOS/DolphiniOS/DolphiniOS/BuildResources/BuildScripts/IncrementBuild.sh
+++ Source/iOS/DolphiniOS/DolphiniOS/BuildResources/BuildScripts/IncrementBuild.sh
@@ -4,4 +4,6 @@ set -e
 
 cd $PROJECT_DIR
 
-xcrun agvtool next-version -all
+INFO_FILE=$PROJECT_DIR/DolphiniOS/Info.plist
+BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$INFO_FILE")
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $(($BUILD_NUMBER + 1))" "$INFO_FILE"
