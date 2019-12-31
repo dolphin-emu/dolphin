763b7d6a5fbe3fcb3282d5e8e57a29a54850322e
Cranky Supertoon <48419040+CrankySupertoon@users.noreply.github.com>
2019-12-31 11:52:32 -0500

    fudge linux

--- Source/Core/Common/make_scmrev.h.js
+++ Source/Core/Common/make_scmrev.h.js
@@ -92,7 +92,7 @@ describe = describe.replace(/(-0)?-[^-]+(-dirty)?$/, '$2');
 var out_contents =
 	"#define SCM_REV_STR \"" + revision + "\"\n" +
 	"#define SCM_DESC_STR \"" + describe + "\"\n" +
-	"#define SCM_BRANCH_STR \"" + "\"\n" +
+	"#define SCM_BRANCH_STR \"" + branch + "\"\n" +
 	"#define SCM_IS_MASTER " + isStable + "\n" +
 	"#define SCM_DISTRIBUTOR_STR \"" + distributor + "\"\n" +
     "#define SCM_UPDATE_TRACK_STR \"" + default_update_track + "\"\n";
