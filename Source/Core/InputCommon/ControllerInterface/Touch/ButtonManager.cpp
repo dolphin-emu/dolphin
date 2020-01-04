eca73aec35256854f90f3ffc87e713594bd32cd0
OatmealDome <julian@oatmealdome.me>
2020-01-04 03:53:36 -0500

    InputCommon: Hack ButtonManager to add one "controller" per touchscreen

--- Source/Core/InputCommon/ControllerInterface/Touch/ButtonManager.cpp
+++ Source/Core/InputCommon/ControllerInterface/Touch/ButtonManager.cpp
@@ -365,18 +365,18 @@ constexpr std::array<ButtonType, 155> CONFIG_TYPES{{
     RUMBLE,
 }};
 
-std::unordered_map<std::string, InputDevice*> m_controllers;
+std::unordered_map<int, InputDevice*> m_controllers;
 
 void AddBind(const std::string& dev, sBind* bind)
 {
-  auto it = m_controllers.find(dev);
+  auto it = m_controllers.find(bind->m_pad_id);
   if (it != m_controllers.end())
   {
     it->second->AddBind(bind);
     return;
   }
-  m_controllers[dev] = new InputDevice(dev);
-  m_controllers[dev]->AddBind(bind);
+  m_controllers[bind->m_pad_id] = new InputDevice(dev);
+  m_controllers[bind->m_pad_id]->AddBind(bind);
 }
 }  // Anonymous namespace
 
@@ -676,18 +676,17 @@ void Init(const std::string& game_id)
 
 bool GetButtonPressed(int pad_id, ButtonType button)
 {
-  bool pressed = m_controllers[TOUCHSCREEN_KEY]->ButtonValue(pad_id, button);
-
-  for (const auto& ctrl : m_controllers)
-    pressed |= ctrl.second->ButtonValue(pad_id, button);
+  bool pressed = m_controllers[pad_id]->ButtonValue(pad_id, button);
+  // for (const auto& ctrl : m_controllers)
+  //  pressed |= ctrl.second->ButtonValue(pad_id, button);
 
   return pressed;
 }
 
 float GetAxisValue(int pad_id, ButtonType axis)
 {
-  float value = m_controllers[TOUCHSCREEN_KEY]->AxisValue(pad_id, axis);
-  if (value == 0.0f)
+  float value = m_controllers[pad_id]->AxisValue(pad_id, axis);
+  /*if (value == 0.0f)
   {
     for (const auto& ctrl : m_controllers)
     {
@@ -695,7 +694,7 @@ float GetAxisValue(int pad_id, ButtonType axis)
       if (value != 0.0f)
         return value;
     }
-  }
+  }*/
   return value;
 }
 
@@ -734,7 +733,7 @@ double GetInputRadiusAtAngle(int pad_id, ButtonType stick, double angle)
 
 bool GamepadEvent(const std::string& dev, int pad_id, int button, int action)
 {
-  auto it = m_controllers.find(dev);
+  auto it = m_controllers.find(pad_id);
   if (it != m_controllers.end())
     return it->second->PressEvent(pad_id, button, action);
   return false;
@@ -742,7 +741,7 @@ bool GamepadEvent(const std::string& dev, int pad_id, int button, int action)
 
 void GamepadAxisEvent(const std::string& dev, int pad_id, int axis, float value)
 {
-  auto it = m_controllers.find(dev);
+  auto it = m_controllers.find(pad_id);
   if (it != m_controllers.end())
     it->second->AxisEvent(pad_id, axis, value);
 }
