99ac7f06c04ca4e7d0dfdd7224f926616a124419
OatmealDome <julian@oatmealdome.me>
2020-01-04 23:18:40 -0500

    iOS: Add touchscreen settings menu

---
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/New Group/Interface/ControllerTouchscreenSettingsViewController.h
@@ -0,0 +1,17 @@
+// Copyright 2020 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ControllerTouchscreenSettingsViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UISwitch* m_haptic_switch;
+@property (weak, nonatomic) IBOutlet UISwitch* m_motion_switch;
+@property (weak, nonatomic) IBOutlet UISlider* m_button_opacity_slider;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/New Group/Interface/ControllerTouchscreenSettingsViewController.m
@@ -0,0 +1,57 @@
+// Copyright 2020 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#import "ControllerTouchscreenSettingsViewController.h"
+
+#import "DolphiniOS-Swift.h"
+
+@interface ControllerTouchscreenSettingsViewController ()
+
+@end
+
+@implementation ControllerTouchscreenSettingsViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+  [super viewWillAppear:animated];
+  
+  [self.m_haptic_switch setOn:[[NSUserDefaults standardUserDefaults] boolForKey:@"haptic_feedback_enabled"]];
+  
+  [self.m_motion_switch setOn:[[TCDeviceMotion shared] getMotionMode] == 0];
+  
+  [self.m_button_opacity_slider setValue:[[NSUserDefaults standardUserDefaults] floatForKey:@"pad_opacity_value"]];
+}
+
+- (IBAction)HapticChanged:(id)sender
+{
+  [[NSUserDefaults standardUserDefaults] setBool:self.m_haptic_switch.on forKey:@"haptic_feedback_enabled"];
+}
+
+- (IBAction)MotionChanged:(id)sender
+{
+  // Done this way in case a new motion mode is introduced
+  int mode = -1;
+  if (self.m_motion_switch.on)
+  {
+    mode = 0;
+  }
+  else
+  {
+    mode = 1;
+  }
+  
+  [[TCDeviceMotion shared] setMotionMode:mode];
+}
+
+- (IBAction)OpacityChanged:(id)sender
+{
+  [[NSUserDefaults standardUserDefaults] setFloat:self.m_button_opacity_slider.value forKey:@"pad_opacity_value"];
+}
+
+@end
