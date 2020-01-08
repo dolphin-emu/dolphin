129f2288ac0887d0ffcced4a247e083fc14de9da
OatmealDome <julian@oatmealdome.me>
2020-01-08 14:23:25 -0500

    iOS: Remove SceneDelegate

--- Source/iOS/DolphiniOS/DolphiniOS/AppDelegate.m
+++ Source/iOS/DolphiniOS/DolphiniOS/AppDelegate.m
@@ -35,25 +35,4 @@
   return YES;
 }
 
-#pragma mark - UISceneSession lifecycle
-
-- (UISceneConfiguration*)application:(UIApplication*)application
-    configurationForConnectingSceneSession:(UISceneSession*)connectingSceneSession
-                                   options:(UISceneConnectionOptions*)options API_AVAILABLE(ios(13))
-{
-  // Called when a new scene session is being created.
-  // Use this method to select a configuration to create the new scene with.
-  return [[UISceneConfiguration alloc] initWithName:@"Default Configuration"
-                                        sessionRole:connectingSceneSession.role];
-}
-
-- (void)application:(UIApplication*)application
-    didDiscardSceneSessions:(NSSet<UISceneSession*>*)sceneSessions API_AVAILABLE(ios(13))
-{
-  // Called when the user discards a scene session.
-  // If any sessions were discarded while the application was not running, this will be called
-  // shortly after application:didFinishLaunchingWithOptions. Use this method to release any
-  // resources that were specific to the discarded scenes, as they will not return.
-}
-
 @end
