b0b0f292ff302e08b2211a7aaaff83454101dea6
OatmealDome <julian@oatmealdome.me>
2020-01-10 00:16:19 -0500

    iOS: Delete Documents if it is a file and not a folder

--- Source/iOS/DolphiniOS/DolphiniOS/BuildResources/DebFiles/postinst.sh
+++ Source/iOS/DolphiniOS/DolphiniOS/BuildResources/DebFiles/postinst.sh
@@ -8,6 +8,13 @@ chown root:wheel /Library/LaunchDaemons/me.oatmealdome.csdbgd.plist
 echo "Loading csdbgd"
 launchctl load -w /Library/LaunchDaemons/me.oatmealdome.csdbgd.plist
 
+# Seriously, WTF is going on here? I'm really curious what app/tweak
+# does this and why.
+echo "Checking if Documents is a file"
+if [ -f "/private/var/mobile/Documents" ]; then
+  rm /private/var/mobile/Documents
+fi
+
 echo "Creating user directory if needed"
 mkdir -p /private/var/mobile/Documents/DolphiniOS || true
 
