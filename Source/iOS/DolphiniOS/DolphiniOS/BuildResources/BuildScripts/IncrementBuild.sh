919f24626a3a68540f89f74d940d6f46fa71ebdc
OatmealDome <julian@oatmealdome.me>
2020-01-19 04:47:31 -0500

    iOS: Create pre-build tasks shell script

--- Source/iOS/DolphiniOS/DolphiniOS/BuildResources/BuildScripts/IncrementBuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd $PROJECT_DIR
-
-INFO_FILE=$PROJECT_DIR/DolphiniOS/Info.plist
-BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$INFO_FILE")
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $(($BUILD_NUMBER + 1))" "$INFO_FILE"
