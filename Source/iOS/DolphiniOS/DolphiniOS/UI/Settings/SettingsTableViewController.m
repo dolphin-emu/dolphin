a1a7cece1d51a140aa546dcf42a165caf02160d8
OatmealDome <julian@oatmealdome.me>
2020-01-23 20:49:22 -0500

    iOS: Add dedicated about view

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/SettingsTableViewController.m
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/SettingsTableViewController.m
@@ -26,46 +26,28 @@
 
 - (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
 {
-  if (indexPath.section == 1)
+  [tableView deselectRowAtIndexPath:indexPath animated:YES];
+  
+  if (indexPath.section == 0)
   {
     NSString* url;
-    if (indexPath.row == 0)
+    if (indexPath.row == 2)
     {
       url = @"https://oatmealdome.me/dolphinios/";
     }
-    else if (indexPath.row == 1)
+    else if (indexPath.row == 3)
     {
       url = @"https://patreon.com/oatmealdome";
     }
-    else if (indexPath.row == 2)
-    {
-      url = @"https://github.com/oatmealdome/dolphin/tree/ios-jb/";
-    }
     else
     {
-      // Load the special thanks
-      NSString* thanks_path = [[NSBundle mainBundle] pathForResource:@"SpecialThanks" ofType:@"txt"];
-      NSString* thanks_str = [NSString stringWithContentsOfFile:thanks_path encoding:NSUTF8StringEncoding error:nil];
-      
-      UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Special Thanks"
-                                     message:[NSString stringWithFormat:@"(c) 2003 - 2015+ Dolphin Team\n(c) 2019-2020 OatmealDome, goob47, and Magnetar\n\nShoutouts to Shadów for their help.\n\nThank you to the following generous Patrons!\n\n%@", thanks_str]
-                                     preferredStyle:UIAlertControllerStyleAlert];
-       
-      UIAlertAction* ok_action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
-         handler:nil];
-      [alert addAction:ok_action];
-      
-      [self presentViewController:alert animated:true completion:nil];
-      
-      [self.tableView deselectRowAtIndexPath:indexPath animated:true];
-      
       return;
     }
     
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url] options:@{} completionHandler:nil];
   }
-  
-  [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
+
+- (IBAction)UnwindToSettings:(UIStoryboardSegue*)segue {}
 
 @end
