3ebf5feb717159336fa87617b1dc74429b258dc7
OatmealDome <julian@oatmealdome.me>
2020-01-26 17:49:08 -0500

    iOS: Move update check into a fancy custom view controller

---
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Notices/Updates/UpdateNoticeViewController.m
@@ -0,0 +1,44 @@
+// Copyright 2020 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#import "UpdateNoticeViewController.h"
+
+@interface UpdateNoticeViewController ()
+
+@end
+
+@implementation UpdateNoticeViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  
+  // Set the version label
+  NSString* message = [NSString stringWithFormat:@"DolphiniOS version %@ is now available.", self.m_update_json[@"version"]];
+  [self.m_version_label setText:message];
+  
+  // Set the changes label
+  [self.m_changes_label setText:self.m_update_json[@"changes"]];
+  
+  // TODO: save states incompatibility label
+}
+
+- (IBAction)UpdateNowTouched:(id)sender
+{
+  NSURL* url = [NSURL URLWithString:@"cydia://url/https://cydia.saurik.com/api/share#?source=http://cydia.oatmealdome.me/&package=me.oatmealdome.dolphinios"];
+  [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+}
+
+- (IBAction)SeeChangesTouched:(id)sender
+{
+  NSURL* url = [NSURL URLWithString:self.m_update_json[@"url"]];
+  [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+}
+
+- (IBAction)NotNowTouched:(id)sender
+{
+  [self dismissViewControllerAnimated:true completion:nil];
+}
+
+@end
