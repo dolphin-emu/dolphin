9bd99423f1401dd127fef3eb07f017fa53e5ce83
OatmealDome <julian@oatmealdome.me>
2020-01-29 02:34:36 -0500

    iOS: Implement General tab in Config

---
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Config/General/SpeedCell.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SpeedCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel* m_speed_label;
+
+@end
+
+NS_ASSUME_NONNULL_END
