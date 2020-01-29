37d2aff38cb4d3b6e281eec651d812933eb0ed99
OatmealDome <julian@oatmealdome.me>
2020-01-29 09:23:15 -0500

    iOS: Port some Interface options from DolphinQt

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Config/Interface/ConfigInterfaceViewController.mm
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Config/Interface/ConfigInterfaceViewController.mm
@@ -4,11 +4,22 @@
 
 #import "ConfigInterfaceViewController.h"
 
+#import "Common/MsgHandler.h"
+
+#import "Core/ConfigManager.h"
+
 @interface ConfigInterfaceViewController ()
 
 @end
 
 @implementation ConfigInterfaceViewController
+
+// Skipped settings:
+// "User Interface" (all) - none needed from here
+// "Keep Window on Top" - no concept of layered windows on iOS
+// "Show Active Title in Window Title" - no window title in iOS
+// "Pause on Focus Loss" - default behaviour for all iOS apps
+// "Always Hide Mouse Cursor" - no concept of a mouse cursor on iOS
 
 - (void)viewDidLoad
 {
@@ -19,8 +30,27 @@
 {
   [super viewWillAppear:animated];
   
+  [self.m_confirm_stop_switch setOn:SConfig::GetInstance().bConfirmStop];
+  [self.m_panic_handlers_switch setOn:SConfig::GetInstance().bUsePanicHandlers];
+  [self.m_osd_switch setOn:SConfig::GetInstance().bOnScreenDisplayMessages];
   [self.m_top_bar_switch setOn:[[NSUserDefaults standardUserDefaults] boolForKey:@"always_show_top_bar"]];
   [self.m_center_image_switch setOn:[[NSUserDefaults standardUserDefaults] boolForKey:@"do_not_raise_rendering_view"]];
+}
+
+- (IBAction)ConfirmStopChanged:(id)sender
+{
+  SConfig::GetInstance().bConfirmStop = [self.m_confirm_stop_switch isOn];
+}
+
+- (IBAction)UsePanicHandlersChanged:(id)sender
+{
+  SConfig::GetInstance().bUsePanicHandlers = [self.m_panic_handlers_switch isOn];
+  Common::SetEnableAlert([self.m_panic_handlers_switch isOn]);
+}
+
+- (IBAction)ShowOsdChanged:(id)sender
+{
+  SConfig::GetInstance().bOnScreenDisplayMessages = [self.m_osd_switch isOn];
 }
 
 - (IBAction)ShowTopBarChanged:(id)sender
