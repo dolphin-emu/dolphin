14fa81b2683e7efbcb7a99c94df87f47f82b9003
OatmealDome <julian@oatmealdome.me>
2020-02-19 13:18:18 -0500

    iOS: Set enhancements transition cell labels as bold if overridden

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Graphics/GraphicsSettingsUtils.h
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Graphics/GraphicsSettingsUtils.h
@@ -10,6 +10,7 @@
 
 #define GSUSetInitialForBool(x, i, y, z) [GraphicsSettingsUtils SetInitialForBoolSetting:x isInverted:i forSwitch:y label:z]
 #define GSUSetInitialForTransitionCell(x, y) [GraphicsSettingsUtils SetInitialForTransitionCell:x forLabel:y]
+#define GSUSetInitialForTransitionCellU32(x, y) [GraphicsSettingsUtils SetInitialForTransitionCellU32:x forLabel:y]
 #define GSUShowHelpWithString(x) [GraphicsSettingsUtils ShowInfoAlertForLocalizable:x onController:self]
 
 NS_ASSUME_NONNULL_BEGIN
@@ -18,6 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (void)SetInitialForBoolSetting:(const Config::ConfigInfo<bool>&)setting isInverted:(bool)inverted forSwitch:(UISwitch*)sw label:(UILabel*)label;
 + (void)SetInitialForTransitionCell:(const Config::ConfigInfo<int>&)setting forLabel:(UILabel*)label;
++ (void)SetInitialForTransitionCellU32:(const Config::ConfigInfo<u32>&)setting forLabel:(UILabel*)label;
 + (void)ShowInfoAlertForLocalizable:(NSString*)localizable onController:(UIViewController*)target_controller;
 
 @end
