043a84d0168d6dd8e778c703246319f60b6e21ec
OatmealDome <julian@oatmealdome.me>
2020-02-20 01:34:58 -0500

    iOS: Fix order of help for enhancements

--- Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Graphics/Enhancements/GraphicsEnhancementsViewController.mm
+++ Source/iOS/DolphiniOS/DolphiniOS/UI/Settings/Graphics/Enhancements/GraphicsEnhancementsViewController.mm
@@ -86,57 +86,57 @@
                             "certain games. Generally speaking, the lower the internal resolution, the "
                             "better performance will be.\n\nIf unsure, select Native.");
       break;
-    case 1:
+    /*case 1:
       GSUShowHelpWithString(@"Reduces the amount of aliasing caused by rasterizing 3D graphics, resulting "
                             "in smoother edges on objects. Increases GPU load and sometimes causes graphical "
                             "issues.\n\nSSAA is significantly more demanding than MSAA, but provides top quality "
                             "geometry anti-aliasing and also applies anti-aliasing to lighting, shader "
                             "effects, and textures.\n\nIf unsure, select None.");
-      break;
-    case 2:
+      break;*/
+    case 1:
       GSUShowHelpWithString(@"Enables anisotropic filtering, which enhances the visual quality of textures that "
                             "are at oblique viewing angles.\n\nMight cause issues in a small "
                             "number of games.\n\nIf unsure, select 1x.");
       break;
-    case 3:
+    case 2:
       GSUShowHelpWithString(@"Greatly increases the quality of textures generated using render-to-texture "
                             "effects.\n\nSlightly increases GPU load and causes relatively few graphical "
                             "issues. Raising the internal resolution will improve the effect of this setting. "
                             "\n\nIf unsure, leave this checked.");
       break;
-    case 4:
+    case 3:
       GSUShowHelpWithString(@"Filters all textures, including any that the game explicitly set as "
                             "unfiltered.\n\nMay improve quality of certain textures in some games, but "
                             "will cause issues in others.\n\nIf unsure, leave this unchecked.");
       break;
-    case 5:
+    case 4:
       GSUShowHelpWithString(@"Makes distant objects more visible by removing fog, thus increasing the overall "
                             "detail.\n\nDisabling fog will break some games which rely on proper fog "
                             "emulation.\n\nIf unsure, leave this unchecked.");
       break;
-    case 6:
+    case 5:
       GSUShowHelpWithString(@"Disables the blending of adjacent rows when copying the EFB. This is known in "
                             "some games as \"deflickering\" or \"smoothing\". \n\nDisabling the filter has no "
                             "effect on performance, but may result in a sharper image. Causes few "
                             "graphical issues.\n\nIf unsure, leave this checked.");
       break;
-    case 7:
+    case 6:
       GSUShowHelpWithString(@"Calculates lighting of 3D objects per-pixel rather than per-vertex, smoothing out the "
       "appearance of lit polygons and making individual triangles less noticeable.\n\nRarely "
                             "causes slowdowns or graphical issues.\n\nIf unsure, leave this unchecked.");
       break;
-    case 8:
+    case 7:
       GSUShowHelpWithString(@"Forces the game to output graphics for any aspect ratio. Use with \"Aspect Ratio\" set to "
                             "\"Force 16:9\" to force 4:3-only games to run at 16:9.\n\nRarely produces good results and "
                             "often partially breaks graphics and game UIs. Unnecessary (and detrimental) if using any "
                             "AR/Gecko-code widescreen patches.\n\nIf unsure, leave this unchecked.");
       break;
-    case 9:
+    case 8:
       GSUShowHelpWithString(@"Forces the game to render the RGB color channels in 24-bit, thereby increasing "
                             "quality by reducing color banding.\n\nHas no impact on performance and causes "
                             "few graphical issues.\n\nIf unsure, leave this checked.");
       break;
-    case 10:
+    case 9:
       GSUShowHelpWithString(@"Enables detection of arbitrary mipmaps, which some games use for special distance-based "
                             "effects.\n\nMay have false positives that result in blurry textures at increased internal "
                             "resolution, such as in games that use very low resolution mipmaps. Disabling this can also "
