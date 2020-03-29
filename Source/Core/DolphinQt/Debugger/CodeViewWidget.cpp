3751e25ca869795737beb8dd98a4739b9b658cdf
spycrab <spycrab@users.noreply.github.com>
2020-03-29 17:02:07 +0200

    Qt/CodeViewWidget: Don't use deprecated QFontMetrics methods

--- Source/Core/DolphinQt/Debugger/CodeViewWidget.cpp
+++ Source/Core/DolphinQt/Debugger/CodeViewWidget.cpp
@@ -193,7 +193,8 @@ void CodeViewWidget::FontBasedSizing()
   verticalHeader()->setMaximumSectionSize(rowh);
   horizontalHeader()->setMinimumSectionSize(rowh + 5);
   setColumnWidth(CODE_VIEW_COLUMN_BREAKPOINT, rowh + 5);
-  setColumnWidth(CODE_VIEW_COLUMN_ADDRESS, fm.width(QStringLiteral("80000000")) + extra_text_width);
+  setColumnWidth(CODE_VIEW_COLUMN_ADDRESS,
+                 fm.horizontalAdvance(QStringLiteral("80000000")) + extra_text_width);
 
   // The longest instruction is technically 'ps_merge00' (0x10000420u), but those instructions are
   // very rare and would needlessly increase the column size, so let's go with 'rlwinm.' instead.
@@ -205,11 +206,11 @@ void CodeViewWidget::FontBasedSizing()
   const std::string ins = (split == std::string::npos ? disas : disas.substr(0, split));
   const std::string param = (split == std::string::npos ? "" : disas.substr(split + 1));
   setColumnWidth(CODE_VIEW_COLUMN_INSTRUCTION,
-                 fm.width(QString::fromStdString(ins)) + extra_text_width);
+                 fm.horizontalAdvance(QString::fromStdString(ins)) + extra_text_width);
   setColumnWidth(CODE_VIEW_COLUMN_PARAMETERS,
-                 fm.width(QString::fromStdString(param)) + extra_text_width);
+                 fm.horizontalAdvance(QString::fromStdString(param)) + extra_text_width);
   setColumnWidth(CODE_VIEW_COLUMN_DESCRIPTION,
-                 fm.width(QStringLiteral("0")) * 25 + extra_text_width);
+                 fm.horizontalAdvance(QStringLiteral("0")) * 25 + extra_text_width);
 
   Update();
 }
