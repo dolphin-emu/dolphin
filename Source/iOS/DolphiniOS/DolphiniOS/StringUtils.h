a58c871dabaab8e5a366a0f0d901dc8cc506bb6f
OatmealDome <julian@oatmealdome.me>
2020-04-13 04:19:31 -0400

    iOS: Add string utils for conversion between NSString and std::string

---
+++ Source/iOS/DolphiniOS/DolphiniOS/StringUtils.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#import <Foundation/Foundation.h>
+
+#ifdef __cplusplus
+
+#import <string>
+
+#define FoundationToCppString(x) std::string([x UTF8String])
+
+#define CppToFoundationString(x) [NSString stringWithUTF8String:x.c_str()]
+
+#endif
