9a6d9ead10032a83b2a9959667752a6e8c44404a
OatmealDome <julian@oatmealdome.me>
2020-05-02 06:18:02 -0400

    iOS: Add script to set entitlements using PsychicPaper

--- Source/iOS/DolphiniOS/DolphiniOS/BuildResources/BuildScripts/UpdateEntitlements.sh
+++ Source/iOS/DolphiniOS/DolphiniOS/BuildResources/BuildScripts/UpdateEntitlements.sh
@@ -8,4 +8,15 @@ XCENT_FILE=$DERIVED_SOURCES_DIR/../DolphiniOS.app.xcent
 if [ $BUILD_FOR_JAILBROKEN_DEVICE == "YES" ]
 then
   /usr/libexec/PlistBuddy -x -c "Merge $PROJECT_DIR/DolphiniOS/BuildResources/Entitlements_JB.plist" "$XCENT_FILE"
+else
+  grep -q "get-task-allow" "$XCENT_FILE"
+  if [[ $? != 0 ]];
+  then
+    echo "        <key>get-task-allow</key>" >> "$XCENT_FILE"
+    echo "        <true/>" >> "$XCENT_FILE"
+  fi
+  
+  sed -i '' -e '$d' "$XCENT_FILE"
+  sed -i '' -e '$d' "$XCENT_FILE"
+  echo "`cat $PROJECT_DIR/DolphiniOS/BuildResources/Entitlements_PsychicPaper.txt`" >> "$XCENT_FILE"
 fi
