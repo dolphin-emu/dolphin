467354a83b24a9d08c4e61771c06045b5399655e
OatmealDome <OatmealDome@users.noreply.github.com>
2020-06-21 02:53:18 -0400

    iOS: Update BuildDebs script to support CI

--- Source/iOS/DolphiniOS/DolphiniOS/BuildResources/BuildScripts/BuildDebs.sh
+++ Source/iOS/DolphiniOS/DolphiniOS/BuildResources/BuildScripts/BuildDebs.sh
@@ -9,8 +9,14 @@ DOLPHIN_EXPORT_PATH="$EXPORT_PATH/dolphin_deb_root/"
 CSDBGD_EXPORT_PATH="$EXPORT_PATH/csdbgd_deb_root/"
 CONTROL_FILE=$ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/DebFiles/control.in
 APPLICATION_DESTINATION_PATH=$DOLPHIN_EXPORT_PATH/Applications/DolphiniOS.app
+CODESIGN_ARGS='-f -s OatmealDome'
 BUNDLE_ID="me.oatmealdome.DolphiniOS"
+VERSION_STRING=$(cat $PROJECT_DIR/DolphiniOS.xcodeproj/project.pbxproj | grep -m1 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ')
 BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$PROJECT_DIR/DolphiniOS/Info.plist")
+
+if [ -n "$IS_CI" ]; then
+  CODESIGN_ARGS+=" --keychain $1"
+fi
 
 if [ $BUILD_FOR_PATREON == "YES" ]; then
   CONTROL_FILE=$ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/DebFiles/control-patreon.in
@@ -32,14 +38,16 @@ mkdir -p $CSDBGD_EXPORT_PATH/Library/LaunchDaemons
 mkdir -p $CSDBGD_EXPORT_PATH/usr/libexec
 mkdir $CSDBGD_EXPORT_PATH/DEBIAN
 
-exec > $EXPORT_PATH/archive.log 2>&1
+if [ -z "$IS_CI" ]; then
+  exec > $EXPORT_PATH/archive.log 2>&1
+fi
 
 # Copy resources
 cp -r "$ARCHIVE_PATH/Products/Applications/DolphiniOS.app" $APPLICATION_DESTINATION_PATH
 cp $ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/DebFiles/postinst.sh $DOLPHIN_EXPORT_PATH/DEBIAN/postinst
 cp $ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/DebFiles/postrm.sh $DOLPHIN_EXPORT_PATH/DEBIAN/postrm
 chmod -R 755 $DOLPHIN_EXPORT_PATH/DEBIAN/*
-sed "s/VERSION_NUMBER/$MARKETING_VERSION-$BUILD_NUMBER/g" $CONTROL_FILE > $DOLPHIN_EXPORT_PATH/DEBIAN/control
+sed "s/VERSION_NUMBER/$VERSION_STRING-$BUILD_NUMBER/g" $CONTROL_FILE > $DOLPHIN_EXPORT_PATH/DEBIAN/control
 
 cp $ROOT_SRC_DIR/csdbgd/csdbgd $CSDBGD_EXPORT_PATH/usr/libexec
 cp $ROOT_SRC_DIR/csdbgd/me.oatmealdome.csdbgd.plist $CSDBGD_EXPORT_PATH/Library/LaunchDaemons
@@ -47,18 +55,18 @@ cp $ROOT_SRC_DIR/csdbgd/DebFiles/preinst.sh $CSDBGD_EXPORT_PATH/DEBIAN/preinst
 cp $ROOT_SRC_DIR/csdbgd/DebFiles/postinst.sh $CSDBGD_EXPORT_PATH/DEBIAN/postinst
 cp $ROOT_SRC_DIR/csdbgd/DebFiles/prerm.sh $CSDBGD_EXPORT_PATH/DEBIAN/prerm
 chmod -R 755 $CSDBGD_EXPORT_PATH/DEBIAN/*
-sed "s/VERSION_NUMBER/$MARKETING_VERSION-$BUILD_NUMBER/g" $ROOT_SRC_DIR/csdbgd/DebFiles/control > $CSDBGD_EXPORT_PATH/DEBIAN/control
+sed "s/VERSION_NUMBER/$VERSION_STRING-$BUILD_NUMBER/g" $ROOT_SRC_DIR/csdbgd/DebFiles/control > $CSDBGD_EXPORT_PATH/DEBIAN/control
 
 # Hack bundle ID
 plutil -replace "CFBundleIdentifier" -string "$BUNDLE_ID" $APPLICATION_DESTINATION_PATH/Info.plist
 
 # Resign the application
-codesign -f -s "OatmealDome Software" --entitlements $ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/Entitlements_JB.plist $APPLICATION_DESTINATION_PATH
-codesign -f -s "OatmealDome Software" --entitlements $ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/Entitlements_JB.plist $APPLICATION_DESTINATION_PATH/Frameworks/*
-codesign -f -s "OatmealDome Software" --entitlements $ROOT_SRC_DIR/csdbgd/Entitlements.plist $CSDBGD_EXPORT_PATH/usr/libexec/csdbgd
+codesign $CODESIGN_ARGS --entitlements $ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/Entitlements_JB.plist $APPLICATION_DESTINATION_PATH
+codesign $CODESIGN_ARGS --entitlements $ROOT_SRC_DIR/DolphiniOS/DolphiniOS/BuildResources/Entitlements_JB.plist $APPLICATION_DESTINATION_PATH/Frameworks/*
+codesign $CODESIGN_ARGS --entitlements $ROOT_SRC_DIR/csdbgd/Entitlements.plist $CSDBGD_EXPORT_PATH/usr/libexec/csdbgd
 
 # Remove the mobileprovision file
-rm $APPLICATION_DESTINATION_PATH/embedded.mobileprovision
+rm $APPLICATION_DESTINATION_PATH/embedded.mobileprovision || true
 
 # Create the deb file
 dpkg-deb -b $DOLPHIN_EXPORT_PATH
@@ -67,4 +75,8 @@ mv $EXPORT_PATH/dolphin_deb_root.deb $EXPORT_PATH/DolphiniOS.deb
 dpkg-deb -b $CSDBGD_EXPORT_PATH
 mv $EXPORT_PATH/csdbgd_deb_root.deb $EXPORT_PATH/csdbgd.deb
 
-open $EXPORT_PATH
+if [ -n "$IS_CI" ]; then
+  echo ::set-env name=$2::$EXPORT_PATH
+else
+  open $EXPORT_PATH
+fi
