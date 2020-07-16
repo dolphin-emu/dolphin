1d30f94cf15d3cf534cb325e2d9b090f6b62e314
R2DLiu <zliu3301@gmail.com>
2020-07-16 18:32:31 -0400

    move fonts around

--- Source/Core/VideoCommon/RenderBase.cpp
+++ Source/Core/VideoCommon/RenderBase.cpp
@@ -922,10 +922,10 @@ bool Renderer::InitializeImGui()
 #ifdef IS_PLAYBACK
   ImFontConfig config;
   config.MergeMode = true;
-  ImGui::GetIO().Fonts->AddFontFromFileTTF("Roboto-Medium.ttf", 14.0f, 0, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
+  ImGui::GetIO().Fonts->AddFontFromFileTTF((File::GetSysDirectory() + "Resources/Roboto-Medium.ttf").c_str(), 14.0f, 0, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
   static const ImWchar icons_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
   ImFontConfig icons_config; icons_config.MergeMode = true; icons_config.PixelSnapH = true;
-  ImGui::GetIO().Fonts->AddFontFromFileTTF(FONT_ICON_FILE_NAME_FA, 16.0f, &icons_config, icons_ranges);
+  ImGui::GetIO().Fonts->AddFontFromFileTTF((File::GetSysDirectory() + "Resources/" + FONT_ICON_FILE_NAME_FA).c_str(), 16.0f, &icons_config, icons_ranges);
 #endif
 
   // Don't create an ini file. TODO: Do we want this in the future?
