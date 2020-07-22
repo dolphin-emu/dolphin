172c8fe9883d7737256b74499827c2f0cadba7f2
weihuoya <zwdreams@gmail.com>
2020-07-22 12:42:46 +0800

    filepicker

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/CustomFilePickerActivity.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/CustomFilePickerActivity.java
@@ -3,16 +3,12 @@ package org.dolphinemu.dolphinemu.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-
 import androidx.annotation.Nullable;
-
 import com.nononsenseapps.filepicker.AbstractFilePickerFragment;
 import com.nononsenseapps.filepicker.FilePickerActivity;
-
-import org.dolphinemu.dolphinemu.fragments.CustomFilePickerFragment;
-
 import java.io.File;
 import java.util.HashSet;
+import org.dolphinemu.dolphinemu.fragments.CustomFilePickerFragment;
 
 public class CustomFilePickerActivity extends FilePickerActivity
 {
@@ -28,21 +24,20 @@ public class CustomFilePickerActivity extends FilePickerActivity
     Intent intent = getIntent();
     if (intent != null)
     {
-      mExtensions = (HashSet<String>) intent.getSerializableExtra(EXTRA_EXTENSIONS);
+      mExtensions = (HashSet<String>)intent.getSerializableExtra(EXTRA_EXTENSIONS);
     }
   }
 
   @Override
-  protected AbstractFilePickerFragment<File> getFragment(
-          @Nullable final String startPath, final int mode, final boolean allowMultiple,
-          final boolean allowCreateDir, final boolean allowExistingFile,
-          final boolean singleClick)
+  protected AbstractFilePickerFragment<File>
+  getFragment(@Nullable final String startPath, final int mode, final boolean allowMultiple,
+              final boolean allowExistingFile, final boolean singleClick)
   {
     CustomFilePickerFragment fragment = new CustomFilePickerFragment();
     // startPath is allowed to be null. In that case, default folder should be SD-card and not "/"
-    fragment.setArgs(
-            startPath != null ? startPath : Environment.getExternalStorageDirectory().getPath(),
-            mode, allowMultiple, allowCreateDir, allowExistingFile, singleClick);
+    fragment.setArgs(startPath != null ? startPath :
+                                         Environment.getExternalStorageDirectory().getPath(),
+                     mode, allowMultiple, allowExistingFile, singleClick);
     fragment.setExtensions(mExtensions);
     return fragment;
   }
--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/utils/FileBrowserHelper.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/utils/FileBrowserHelper.java
@@ -3,57 +3,51 @@ package org.dolphinemu.dolphinemu.utils;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
-
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
-
 import com.nononsenseapps.filepicker.FilePickerActivity;
 import com.nononsenseapps.filepicker.Utils;
-
-import org.dolphinemu.dolphinemu.activities.CustomFilePickerActivity;
-import org.dolphinemu.dolphinemu.ui.main.MainPresenter;
-
 import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import org.dolphinemu.dolphinemu.activities.CustomFilePickerActivity;
+import org.dolphinemu.dolphinemu.ui.main.MainPresenter;
 
 public final class FileBrowserHelper
 {
   public static final HashSet<String> GAME_EXTENSIONS = new HashSet<>(Arrays.asList(
-          "gcm", "tgc", "iso", "ciso", "gcz", "wbfs", "wia", "rvz", "wad", "dol", "elf", "dff"));
+      "gcm", "tgc", "iso", "ciso", "gcz", "wbfs", "wia", "rvz", "wad", "dol", "elf", "dff"));
 
-  public static final HashSet<String> RAW_EXTENSION = new HashSet<>(Collections.singletonList(
-          "raw"));
+  public static final HashSet<String> RAW_EXTENSION =
+      new HashSet<>(Collections.singletonList("raw"));
 
-  public static final HashSet<String> WAD_EXTENSION = new HashSet<>(Collections.singletonList(
-          "wad"));
+  public static final HashSet<String> WAD_EXTENSION =
+      new HashSet<>(Collections.singletonList("wad"));
 
   public static void openDirectoryPicker(FragmentActivity activity, HashSet<String> extensions)
   {
     Intent i = new Intent(activity, CustomFilePickerActivity.class);
 
     i.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
-    i.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false);
     i.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR);
     i.putExtra(FilePickerActivity.EXTRA_START_PATH,
-            Environment.getExternalStorageDirectory().getPath());
+               Environment.getExternalStorageDirectory().getPath());
     i.putExtra(CustomFilePickerActivity.EXTRA_EXTENSIONS, extensions);
 
     activity.startActivityForResult(i, MainPresenter.REQUEST_DIRECTORY);
   }
 
   public static void openFilePicker(FragmentActivity activity, int requestCode, boolean allowMulti,
-          HashSet<String> extensions)
+                                    HashSet<String> extensions)
   {
     Intent i = new Intent(activity, CustomFilePickerActivity.class);
 
     i.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, allowMulti);
-    i.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false);
     i.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
     i.putExtra(FilePickerActivity.EXTRA_START_PATH,
-            Environment.getExternalStorageDirectory().getPath());
+               Environment.getExternalStorageDirectory().getPath());
     i.putExtra(CustomFilePickerActivity.EXTRA_EXTENSIONS, extensions);
 
     activity.startActivityForResult(i, requestCode);
