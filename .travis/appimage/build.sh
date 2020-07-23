31fe79034653ab7531b4e8d5a8cc3f38ee53b341
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-23 15:31:02 -0700

    Delete build.sh

--- .travis/appimage/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ex
-
-mkdir -p "$HOME/.ccache"
-docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/dolphin-emu -v "$HOME/.ccache":/root/.ccache quriouspixel/yuzu:latest /bin/bash /yuzu/.travis/appimage/docker.sh
