25bcd93443d650fc6d3f44af87cc1b931cfcfc1e
Tibladar <52620063+Tibladar@users.noreply.github.com>
2020-07-28 08:32:27 +0000

    NetPlay: Set Wiimotes at join

--- Source/Core/Core/NetPlayServer.cpp
+++ Source/Core/Core/NetPlayServer.cpp
@@ -399,6 +399,16 @@ unsigned int NetPlayServer::OnConnect(ENetPeer* socket, sf::Packet& rpac)
       break;
     }
   }
+  
+  // try to automatically assign new user a wiimote
+  for (PlayerId& mapping : m_wiimote_map)
+  {
+    if (mapping == 0)
+    {
+      mapping = player.pid;
+      break;
+    }
+  }
 
   // send join message to already connected clients
   sf::Packet spac;
