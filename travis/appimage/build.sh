5beda8dc6065021dd8622c2695cb2dfe78d93a7f
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-30 16:10:32 -0700

    Update build.sh

--- travis/appimage/build.sh
+++ travis/appimage/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
 mkdir -p "$HOME/.ccache"
-docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file travis/common/travis-ci.env -v $(pwd):/dolphin-emu -v "$HOME/.ccache":/root/.ccache -u root quriouspixel/yuzu:latest /bin/bash /yuzu/travis/appimage/docker.sh
+docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file travis/common/travis-ci.env -v $(pwd):/dolphin -v "$HOME/.ccache":/root/.ccache -u root quriouspixel/yuzu:latest /bin/bash /yuzu/travis/appimage/docker.sh
