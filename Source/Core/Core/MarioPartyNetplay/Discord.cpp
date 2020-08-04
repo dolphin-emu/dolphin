8add9e0a044b64ee0c0dcf29a55d835afcdb543c
CrankySupertoon <crankysupertoon@gmail.com>
2020-08-04 10:54:41 -0400

    DiscordRPC: Add Player Count to RPC. TODO: Detect Current Players

--- Source/Core/Core/MarioPartyNetplay/Discord.cpp
+++ Source/Core/Core/MarioPartyNetplay/Discord.cpp
@@ -1,14 +1,13 @@
 #include "Discord.h"
-
+#include "UICommon/DiscordPresence.h"
 bool mpn_update_discord()
 {
   if (!Memory::IsInitialized())
     return false;
-
   DiscordRichPresence RichPresence = {};
-
   RichPresence.largeImageKey = CurrentState.Image ? CurrentState.Image : "default";
   RichPresence.largeImageText = CurrentState.Title ? CurrentState.Title : "In-Game";
+  RichPresence.partyMax = 4;
 
   if (CurrentState.Scenes != NULL && CurrentState.Scene != NULL)
     RichPresence.state = CurrentState.Scene->Name.c_str();
@@ -19,9 +18,11 @@ bool mpn_update_discord()
 
     if (CurrentState.Boards && CurrentState.Board)
     {
-      snprintf(Details, sizeof(Details), "Turn: %d/%d",
-               mpn_read_value(CurrentState.Addresses->CurrentTurn, 1),
-               mpn_read_value(CurrentState.Addresses->TotalTurns, 1));
+      snprintf(Details, sizeof(Details), "Players: %d/%d Turn: %d/%d",
+          RichPresence.partySize,
+          RichPresence.partyMax,
+          mpn_read_value(CurrentState.Addresses->CurrentTurn, 1),
+          mpn_read_value(CurrentState.Addresses->TotalTurns, 1));
       RichPresence.smallImageKey = CurrentState.Board->Icon.c_str();
       RichPresence.smallImageText = CurrentState.Board->Name.c_str();
     }
@@ -32,7 +33,7 @@ bool mpn_update_discord()
     }
     RichPresence.details = Details;
   }
+  RichPresence.startTimestamp = time(0);
   Discord_UpdatePresence(&RichPresence);
-
   return true;
 }
