be471fed2a0c8617ea4077defacda0be4610ea2a
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-08 20:52:33 -0700

    Update docker.sh

--- travis/appimage/docker.sh
+++ travis/appimage/docker.sh
@@ -7,3 +7,8 @@ cd build
 cmake .. -G Ninja -DLINUX_LOCAL_DEV=true
 ninja
 ln -s ../../Data/Sys Binaries/
+
+cd /tmp
+#curl -sLO "https://raw.githubusercontent.com/qurious-pixel/dolphin/zap/travis/appimage/appimage.sh"
+#chmod a+x appimage.sh
+./appimage.sh
