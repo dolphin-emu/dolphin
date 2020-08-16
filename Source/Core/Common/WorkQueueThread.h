213610e95de9aeadf891f50c18897299c8322aea
Christian Aguilera <cristian64@gmail.com>
2020-08-16 01:33:30 +0100

    Extended WorkQueueThread class with Clear(), Cancel() and IsCancelled().

--- Source/Core/Common/WorkQueueThread.h
+++ Source/Core/Common/WorkQueueThread.h
@@ -27,6 +27,7 @@ public:
   {
     Shutdown();
     m_shutdown.Clear();
+    m_cancelled.Clear();
     m_function = std::move(function);
     m_thread = std::thread(&WorkQueueThread::ThreadLoop, this);
   }
@@ -34,12 +35,31 @@ public:
   template <typename... Args>
   void EmplaceItem(Args&&... args)
   {
+    if (!m_cancelled.IsSet())
     {
       std::lock_guard lg(m_lock);
       m_items.emplace(std::forward<Args>(args)...);
     }
     m_wakeup.Set();
   }
+
+  void Clear()
+  {
+    {
+      std::lock_guard lg(m_lock);
+      m_items = std::queue<T>();
+    }
+    m_wakeup.Set();
+  }
+
+  void Cancel()
+  {
+    m_cancelled.Set();
+    Clear();
+    Shutdown();
+  }
+
+  bool IsCancelled() const { return m_cancelled.IsSet(); }
 
 private:
   void Shutdown()
@@ -81,6 +101,7 @@ private:
   std::thread m_thread;
   Common::Event m_wakeup;
   Common::Flag m_shutdown;
+  Common::Flag m_cancelled;
   std::mutex m_lock;
   std::queue<T> m_items;
 };
