Clever-ops/Dolphin-SFS-RG35xx

Support for real Wiimotes and DolphinBar

Source/Core/DolphinLibretro/Boot.cpp
@@ -114,6 +114,7 @@ bool retro_load_game(const struct retro_game_info* game)
   Config::SetBase(Config::GFX_SHADER_COMPILER_THREADS, 1);
   Config::SetBase(Config::GFX_SHADER_PRECOMPILER_THREADS, 1);
 #endif
+  SConfig::GetInstance().m_WiimoteContinuousScanning = Libretro::Options::bluetoothContinuousScan;
 
   Libretro::Video::Init();
   NOTICE_LOG(VIDEO, "Using GFX backend: %s", SConfig::GetInstance().m_strVideoBackend.c_str());

dolphin-emu/dolphin

Source/Core/DolphinLibretro/Boot.cpp
@@ -92,6 +92,7 @@ bool retro_load_game(const struct retro_game_info* game)
   SConfig::GetInstance().bDPL2Decoder = false;
   SConfig::GetInstance().iLatency = 0;
   SConfig::GetInstance().m_audio_stretch = false;
+  SConfig::GetInstance().m_WiimoteContinuousScanning = Libretro::Options::WiimoteContinuousScanning;
 
   Config::SetBase(Config::SYSCONF_LANGUAGE, (u32)(DiscIO::Language)Libretro::Options::Language);
   Config::SetBase(Config::SYSCONF_WIDESCREEN, Libretro::Options::Widescreen);