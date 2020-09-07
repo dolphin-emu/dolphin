43ec859219cb63f0ff1ca6506623d68817eafd50
Pokechu22 <Pokechu022@gmail.com>
2020-09-07 11:12:58 -0700

    Handle GoIdleState and SendInterfaceCond exceptions

--- Source/Core/Core/HW/EXI/EXI_DeviceSD.cpp
+++ Source/Core/Core/HW/EXI/EXI_DeviceSD.cpp
@@ -180,7 +180,10 @@ void CEXISD::WriteByte(u8 byte)
         u8 hash = (Common::HashCrc7(command_buffer.data(), 5) << 1) | 1;
         if (byte != hash)
         {
-          if (byte != 0xff)
+          Command command = static_cast<Command>(command_buffer[0] & 0x3f);
+
+          if (byte != 0xff || command == Command::GoIdleState ||
+              command == Command::SendInterfaceCond)
           {
             WARN_LOG_FMT(EXPANSIONINTERFACE,
                          "EXI SD command invalid, incorrect CRC7: got {:02x}, should be {:02x}",
