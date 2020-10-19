12628c8c45f7350240305dfb1fba53796ddef2b5
JosJuice <josjuice@gmail.com>
2020-10-19 15:58:51 +0200

    Fix predefined Qt strings not getting translated
    https://bugs.dolphin-emu.org/issues/11507

--- Languages/update-source-strings.sh
+++ Languages/update-source-strings.sh
@@ -4,6 +4,8 @@
 # the source code.
 
 cd "$(dirname "$0")/.."
+
+# Scan the source code for strings and put them in dolphin-emu.pot
 SRCDIR=Source
 find $SRCDIR -name '*.cpp' -o -name '*.h' -o -name '*.c' | \
 	xgettext -d dolphin-emu -s --keyword=_ --keyword=wxTRANSLATE --keyword=SuccessAlertT \
@@ -12,6 +14,10 @@ find $SRCDIR -name '*.cpp' -o -name '*.h' -o -name '*.c' | \
 	--keyword=FmtFormatT \
 	--add-comments=i18n -p ./Languages/po -o dolphin-emu.pot -f - --package-name="Dolphin Emulator" \
 	--from-code=utf-8
+
+# Copy strings from qt-strings.pot to dolphin-emu.pot
+xgettext -d dolphin-emu -s -p ./Languages/po -o dolphin-emu.pot --package-name="Dolphin Emulator" \
+  -j ./Languages/po/qt-strings.pot
 
 sed -i "s/SOME DESCRIPTIVE TITLE\./Translation of dolphin-emu.pot to LANGUAGE/" Languages/po/dolphin-emu.pot
 sed -i "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/2003-2013/" Languages/po/dolphin-emu.pot
