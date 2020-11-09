23a8baa6057c67298f68c6c5d65c177642125212
Lioncash <mathew1800@gmail.com>
2020-11-09 03:13:59 -0500

    Software: Migrate logging over to fmt
    Migrates the software backend over to the fmt-capable logger.

--- Source/Core/VideoBackends/Software/SetupUnit.cpp
+++ Source/Core/VideoBackends/Software/SetupUnit.cpp
@@ -37,7 +37,7 @@ void SetupUnit::SetupVertex()
     SetupQuad();
     break;
   case OpcodeDecoder::GX_DRAW_QUADS_2:
-    WARN_LOG(VIDEO, "Non-standard primitive drawing command GL_DRAW_QUADS_2");
+    WARN_LOG_FMT(VIDEO, "Non-standard primitive drawing command GL_DRAW_QUADS_2");
     SetupQuad();
     break;
   case OpcodeDecoder::GX_DRAW_TRIANGLES:
