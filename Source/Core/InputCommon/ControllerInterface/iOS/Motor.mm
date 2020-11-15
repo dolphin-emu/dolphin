71e29ee8e8e54cbfb5b61499bc387810546fb582
OatmealDome <julian@oatmealdome.me>
2020-11-15 03:21:25 -0500

    iOS: Log errors when stopping haptics on Motor destruction

--- Source/Core/InputCommon/ControllerInterface/iOS/Motor.mm
+++ Source/Core/InputCommon/ControllerInterface/iOS/Motor.mm
@@ -62,7 +62,10 @@ Motor::~Motor() API_AVAILABLE(ios(13.0))
 {
   if (this->m_haptic_player)
   {
+    NSError* error;
     [this->m_haptic_player stopAtTime:CHHapticTimeImmediate error:&error];
+
+    LOG_NSERROR("Failed to stop haptics on Motor destruction: %s", error);
   }
 
   this->m_haptic_engine = nil;
