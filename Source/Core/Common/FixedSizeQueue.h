b71ecb7a381e31f7cbc9ad287c4330ad23e1626d
Filippo Tarpini <filippotarpini@hotmail.it>
2020-12-30 18:08:49 +0200

    Add safety checking and features to FixedSizeQueue
    -Added safety checking.
    -Added optimize copy from and to.
    -Changed from int to size_t (more in line with std and it was giving annoying warnings).
    -Added [] index access operator.
    -Added optimized erase of multiple items.
    -Added a few helper functions.
    
    Note that there is a test for this class already (FixedSizeQueueTest.cpp). These changes are needed for the new DPLII implementation and serve as optimizations there.
    FixedSizeQueue is only used for logs in the whole project so the changes don't affect anything.

--- Source/Core/Common/FixedSizeQueue.h
+++ Source/Core/Common/FixedSizeQueue.h
@@ -4,7 +4,9 @@
 
 #pragma once
 
+#include <algorithm>
 #include <array>
+#include <cassert>
 #include <cstddef>
 #include <type_traits>
 #include <utility>
@@ -12,9 +14,9 @@
 // STL-look-a-like interface, but name is mixed case to distinguish it clearly from the
 // real STL classes.
 //
-// Not fully featured, no safety checking yet. Add features as needed.
+// Not fully featured. Add features as needed.
 
-template <class T, int N>
+template <class T, size_t N>
 class FixedSizeQueue
 {
 public:
@@ -45,6 +47,46 @@ public:
     tail = (tail + 1) % N;
   }
 
+  // Copies over an array, loops over of num is greater than max_size
+  void push_array(const T* t, size_t num)
+  {
+    size_t back_writable_num = std::min(tail_to_end(), num);
+    memcpy(&back(), t, sizeof(T) * back_writable_num);
+    t += back_writable_num;
+    size_t readable_num = num - back_writable_num;
+    while (readable_num > 0)
+    {
+      size_t beginning_writable_num = std::min(readable_num, N);
+      memcpy(&beginning(), t, sizeof(T) * beginning_writable_num);
+      t += beginning_writable_num;
+      readable_num -= beginning_writable_num;
+    }
+
+    while (num > 0)
+    {
+      if (count == N)
+        head = (head + 1) % N;
+      else
+        count++;
+      tail = (tail + 1) % N;
+      --num;
+    }
+  }
+
+  // Takes a max num to copy (from head to tail), returns the actual copied number.
+  // Doesn't loop over if num is greater than our size
+  size_t copy_to_array(T* t, size_t num)
+  {
+    num = std::min(num, count);
+    const size_t front_readable_num = std::min(head_to_end(), num);
+    const size_t beginning_readable_num = num - front_readable_num;
+
+    memcpy(&t[0], &front(), sizeof(T) * front_readable_num);
+    if (beginning_readable_num > 0)
+      memcpy(&t[front_readable_num], &beginning(), sizeof(T) * beginning_readable_num);
+    return num;
+  }
+
   template <class... Args>
   void emplace(Args&&... args)
   {
@@ -57,8 +99,10 @@ public:
     tail = (tail + 1) % N;
   }
 
+  // From front
   void pop()
   {
+    assert(count > 0);
     if constexpr (!std::is_trivial_v<T>)
       storage[head] = {};
 
@@ -73,15 +117,57 @@ public:
     return temp;
   }
 
+  // From front
+  void erase(size_t num)
+  {
+    assert(count >= num);
+    if constexpr (!std::is_trivial_v<T>)
+    {
+      while (num > 0)
+      {
+        storage[head] = {};
+        head = (head + 1) % N;
+        count--;
+        num--;
+      }
+    }
+    else
+    {
+      head = (head + num) % N;
+      count -= num;
+    }
+  }
+
+  T& operator[](size_t index)
+  {
+    assert(index < count);
+    return storage[(head + index) % N];
+  }
+  const T& operator[](size_t index) const
+  {
+    assert(index < count);
+    return storage[(head + index) % N];
+  }
+
   T& front() noexcept { return storage[head]; }
   const T& front() const noexcept { return storage[head]; }
+  T& back() noexcept { return storage[tail]; }
+  const T& back() const noexcept { return storage[tail]; }
+  // Only use if head_to_end() > size()
+  T& beginning() noexcept { return storage[0]; }
+  const T& beginning() const noexcept { return storage[0]; }
   size_t size() const noexcept { return count; }
+  size_t max_size() const noexcept { return N; }
+  // Helper to know how many more samples we could read before needing to loop over
+  size_t head_to_end() const noexcept { return N - head; }
+  // Helper to know how many more samples we could write before needing to loop over
+  size_t tail_to_end() const noexcept { return N - tail; }
   bool empty() const noexcept { return size() == 0; }
 
 private:
   std::array<T, N> storage;
-  int head = 0;
-  int tail = 0;
-  // Sacrifice 4 bytes for a simpler implementation. may optimize away in the future.
-  int count = 0;
+  size_t head = 0;
+  size_t tail = 0;
+  // Not necessary but avoids a lot of calculations
+  size_t count = 0;
 };
