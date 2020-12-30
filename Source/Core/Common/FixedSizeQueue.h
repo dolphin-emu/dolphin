6d723dc10bf47850b7fa5824c6b8ae84e121a2d0
Filippo Tarpini <filippotarpini@hotmail.it>
2020-12-30 18:12:24 +0200

    FixedSizeQueue: Add features and safety checks
    -Add safety checking.
    -Change from int to size_t (more in line with std and it was giving annoying warnings).
    -Add [] index access operator.
    -Add optimized erase of multiple items.
    -Add optimize copy from and to array.
    -Add a few helper functions.
    -Add new features to the UnitTes and improve the old tests.
    
    These changes are needed for the new DPLII implementation
    and serve as optimizations there. The new copy is from ~18% to ~40% faster
    on the number of elements the DPLII can copy per audio frame.
    FixedSizeQueue is only used for logs so the changes don't affect anything.

--- Source/Core/Common/FixedSizeQueue.h
+++ Source/Core/Common/FixedSizeQueue.h
@@ -4,34 +4,32 @@
 
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <cstddef>
 #include <type_traits>
 #include <utility>
 
+#include "Common/Assert.h"
+
 // STL-look-a-like interface, but name is mixed case to distinguish it clearly from the
 // real STL classes.
 //
-// Not fully featured, no safety checking yet. Add features as needed.
+// Not fully featured. Add features as needed.
 
-template <class T, int N>
+template <class T, size_t N>
 class FixedSizeQueue
 {
 public:
   void clear()
   {
-    if constexpr (!std::is_trivial_v<T>)
-    {
-      // The clear of non-trivial objects previously used "storage = {}". However, this causes GCC
-      // to take a very long time to compile the file/function, as well as generating huge amounts
-      // of debug information (~2GB object file, ~600MB of debug info).
-      while (count > 0)
-        pop();
-    }
+    // The clear of non-trivial objects previously used "storage = {}". However, this causes GCC
+    // to take a very long time to compile the file/function, as well as generating huge amounts
+    // of debug information (~2GB object file, ~600MB of debug info).
+    erase(count);
 
     head = 0;
     tail = 0;
-    count = 0;
   }
 
   void push(T t)
@@ -43,6 +41,50 @@ public:
 
     storage[tail] = std::move(t);
     tail = (tail + 1) % N;
+  }
+
+  // Copies over an array, loops over if num is greater than max_size.
+  // Only available if T is trivial, just use push() otherwise.
+  template <typename = std::enable_if_t<std::is_trivial_v<T>>>
+  void push_array(const T* t, size_t num)
+  {
+    const size_t back_writable_num = std::min(tail_to_end(), num);
+    std::memcpy(&back(), t, sizeof(T) * back_writable_num);
+    t += back_writable_num;
+    size_t readable_num = num - back_writable_num;
+    while (readable_num > 0)
+    {
+      const size_t beginning_writable_num = std::min(readable_num, N);
+      std::memcpy(&beginning(), t, sizeof(T) * beginning_writable_num);
+      t += beginning_writable_num;
+      readable_num -= beginning_writable_num;
+    }
+
+    while (num > 0)
+    {
+      if (count == N)
+        head = (head + 1) % N;
+      else
+        count++;
+      tail = (tail + 1) % N;
+      --num;
+    }
+  }
+
+  // Takes a max num to copy (from head to tail), returns the actual copied number.
+  // Doesn't loop over if num is greater than our size.
+  // Only available if T is trivial, just copy by index otherwise.
+  template <typename = std::enable_if_t<std::is_trivial_v<T>>>
+  size_t copy_to_array(T* t, size_t num)
+  {
+    num = std::min(num, count);
+    const size_t front_readable_num = std::min(head_to_end(), num);
+    const size_t beginning_readable_num = num - front_readable_num;
+
+    std::memcpy(&t[0], &front(), sizeof(T) * front_readable_num);
+    if (beginning_readable_num > 0)
+      std::memcpy(&t[front_readable_num], &beginning(), sizeof(T) * beginning_readable_num);
+    return num;
   }
 
   template <class... Args>
@@ -57,8 +99,10 @@ public:
     tail = (tail + 1) % N;
   }
 
+  // From front
   void pop()
   {
+    ASSERT(count > 0);
     if constexpr (!std::is_trivial_v<T>)
       storage[head] = {};
 
@@ -73,15 +117,59 @@ public:
     return temp;
   }
 
-  T& front() noexcept { return storage[head]; }
-  const T& front() const noexcept { return storage[head]; }
+  // From front
+  void erase(size_t num)
+  {
+    ASSERT(count >= num);
+    if constexpr (!std::is_trivial_v<T>)
+    {
+      while (num > 0)
+      {
+        storage[head] = {};
+        head = (head + 1) % N;
+        count--;
+        num--;
+      }
+    }
+    else
+    {
+      head = (head + num) % N;
+      count -= num;
+    }
+  }
+
+  // From head/front, not the absolute index
+  T& operator[](size_t index) noexcept
+  {
+    ASSERT(index < count);
+    return storage[(head + index) % N];
+  }
+  const T& operator[](size_t index) const noexcept
+  {
+    ASSERT(index < count);
+    return storage[(head + index) % N];
+  }
+
   size_t size() const noexcept { return count; }
+  size_t max_size() const noexcept { return N; }
   bool empty() const noexcept { return size() == 0; }
 
 private:
+  T& front() noexcept { return storage[head]; }
+  const T& front() const noexcept { return storage[head]; }
+  T& back() noexcept { return storage[tail]; }
+  const T& back() const noexcept { return storage[tail]; }
+  // Returns the first index of the internal array. Only read if head_to_end() > size()
+  T& beginning() noexcept { return storage[0]; }
+  const T& beginning() const noexcept { return storage[0]; }
+  // Helper to know how many more items we could read before needing to loop over
+  size_t head_to_end() const noexcept { return N - head; }
+  // Helper to know how many more items we could write before needing to loop over
+  size_t tail_to_end() const noexcept { return N - tail; }
+
   std::array<T, N> storage;
-  int head = 0;
-  int tail = 0;
-  // Sacrifice 4 bytes for a simpler implementation. may optimize away in the future.
-  int count = 0;
+  size_t head = 0;
+  size_t tail = 0;
+  // Not necessary but avoids a lot of calculations
+  size_t count = 0;
 };
--- Source/UnitTests/Common/FixedSizeQueueTest.cpp
+++ Source/UnitTests/Common/FixedSizeQueueTest.cpp
@@ -6,20 +6,21 @@
 
 #include "Common/FixedSizeQueue.h"
 
-TEST(FixedSizeQueue, Simple)
+TEST(FixedSizeQueue, PushEmplacePop)
 {
   FixedSizeQueue<int, 5> q;
 
   EXPECT_EQ(0u, q.size());
+  EXPECT_EQ(5u, q.max_size());
 
   q.push(0);
   q.push(1);
   q.push(2);
   q.push(3);
-  q.push(4);
+  q.emplace(4);
   for (int i = 0; i < 1000; ++i)
   {
-    EXPECT_EQ(i, q.front());
+    EXPECT_EQ(i, q[0]);
     EXPECT_EQ(i, q.pop_front());
     q.push(i + 5);
   }
@@ -29,6 +30,21 @@ TEST(FixedSizeQueue, Simple)
   EXPECT_EQ(1003, q.pop_front());
   EXPECT_EQ(1004, q.pop_front());
 
+  EXPECT_EQ(0u, q.size());
+  EXPECT_EQ(5u, q.max_size());
+}
+
+TEST(FixedSizeQueue, EraseClear)
+{
+  FixedSizeQueue<int, 2> q;
+
+  q.push(0);
+  q.push(0);
+  q.erase(2);
+  EXPECT_EQ(0u, q.size());
+  q.push(0);
+  q.push(0);
+  q.clear();
   EXPECT_EQ(0u, q.size());
 }
 
@@ -40,16 +56,46 @@ TEST(FixedSizeQueue, RingBuffer)
   EXPECT_EQ(0u, q.size());
 
   q.push(0);
+  EXPECT_EQ(0, q[0]);
   q.push(1);
+  EXPECT_EQ(1, q[1]);
   q.push(2);
   q.push(3);
   q.push(4);
-  q.push(5);
+  EXPECT_EQ(4, q[4]);  // q is 0 1 2 3 4. External index 0 is 0 internally
+  q.push(5);  // Loops to internal index 0
+  EXPECT_EQ(5, q[4]);  // q is 5 1 2 3 4. External index 0 is 1 internally
+  EXPECT_EQ(1, q[0]);
 
   EXPECT_EQ(5u, q.size());
-  EXPECT_EQ(1, q.pop_front());
 
+  EXPECT_EQ(1, q.pop_front());
   EXPECT_EQ(4u, q.size());
+  q.erase(2);
+  EXPECT_EQ(2u, q.size());
+}
+
+TEST(FixedSizeQueue, Copy)
+{
+  // Testing copy from and to array, which can only be used on trivial types
+  FixedSizeQueue<int, 5> q;
+
+  // Copy over an array bigger than us, which means the copy will loop over
+  std::array<int, 6> other_array = {1, 2, 3, 4, 5, 6};
+  q.push_array(other_array.data(), other_array.size());
+  // q is 6 2 3 4 5. External index 0 is 1 internally
+  EXPECT_EQ(6, q[4]);
+  EXPECT_EQ(2, q[0]);
+  EXPECT_EQ(5, q[3]);
+
+  // Copy it back (up to 5, our size). It will start from its head/front
+  for (size_t i = 0; i < other_array.size(); ++i)
+  {
+    other_array[i] = 0;
+  }
+  q.copy_to_array(other_array.data(), 5);
+  EXPECT_EQ(2, other_array[0]);
+  EXPECT_EQ(6, other_array[4]);
 }
 
 // Local classes cannot have static fields,
