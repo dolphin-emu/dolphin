c675f33fed8ca40dc9a063b4cce1a72bd8c46280
Florian Bach <Leseratte10@users.noreply.github.com>
2020-12-31 12:37:00 +0100

    mbedtls: Disable SNI support

--- Externals/mbedtls/include/mbedtls/config.h
+++ Externals/mbedtls/include/mbedtls/config.h
@@ -1587,7 +1587,7 @@
  *
  * Comment this macro to disable support for server name indication in SSL
  */
-#define MBEDTLS_SSL_SERVER_NAME_INDICATION
+// #define MBEDTLS_SSL_SERVER_NAME_INDICATION
 
 /**
  * \def MBEDTLS_SSL_TRUNCATED_HMAC
