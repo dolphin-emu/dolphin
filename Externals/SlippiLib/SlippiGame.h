e6f721f5c0b985a6e9aa76dd4ccf37330f9918c2
Nicholas Marcott <481161+bmarcott@users.noreply.github.com>
2021-01-04 16:43:41 +0900

    Fix formatting for External SlippiLib files

--- Externals/SlippiLib/SlippiGame.h
+++ Externals/SlippiLib/SlippiGame.h
@@ -8,8 +8,8 @@
 #include <unordered_map>
 #include <vector>
 
-namespace Slippi {
-
+namespace Slippi
+{
 const uint8_t EVENT_SPLIT_MESSAGE = 0x10;
 const uint8_t EVENT_PAYLOAD_SIZES = 0x35;
 const uint8_t EVENT_GAME_INIT = 0x36;
@@ -29,9 +29,10 @@ const uint8_t GAME_SHEIK_EXTERNAL_ID = 0x13;
 
 const uint32_t SPLIT_MESSAGE_INTERNAL_DATA_LEN = 512;
 
-static uint8_t *data;
+static uint8_t* data;
 
-typedef struct {
+typedef struct
+{
   // Every player update has its own rng seed because it might change in between
   // players
   uint32_t randomSeed;
@@ -54,19 +55,20 @@ typedef struct {
   float cstickX;
   float cstickY;
   float trigger;
-  uint32_t buttons; // This will include multiple "buttons" pressed on special
-                    // buttons. For example I think pressing z sets 3 bits
+  uint32_t buttons;  // This will include multiple "buttons" pressed on special
+                     // buttons. For example I think pressing z sets 3 bits
 
   // This is extra controller information
-  uint16_t physicalButtons; // A better representation of what a player is
-                            // actually pressing
+  uint16_t physicalButtons;  // A better representation of what a player is
+                             // actually pressing
   float lTrigger;
   float rTrigger;
 
   uint8_t joystickXRaw;
 } PlayerFrameData;
 
-typedef struct FrameData {
+typedef struct FrameData
+{
   int32_t frame;
   uint32_t numSinceStart;
   bool randomSeedExists = false;
@@ -76,7 +78,8 @@ typedef struct FrameData {
   std::unordered_map<uint8_t, PlayerFrameData> followers;
 } FrameData;
 
-typedef struct {
+typedef struct
+{
   // Static data
   uint8_t characterId;
   uint8_t characterColor;
@@ -85,8 +88,9 @@ typedef struct {
   std::array<uint16_t, NAMETAG_SIZE> nametag;
 } PlayerSettings;
 
-typedef struct {
-  uint16_t stage; // Stage ID
+typedef struct
+{
+  uint16_t stage;  // Stage ID
   uint32_t randomSeed;
   std::array<uint32_t, GAME_INFO_HEADER_SIZE> header;
   std::array<uint32_t, UCF_TOGGLE_SIZE> ucfToggles;
@@ -96,14 +100,15 @@ typedef struct {
   std::vector<uint8_t> geckoCodes;
 } GameSettings;
 
-typedef struct Game {
+typedef struct Game
+{
   std::array<uint8_t, 4> version;
-  std::unordered_map<int32_t, FrameData *> framesByIndex;
+  std::unordered_map<int32_t, FrameData*> framesByIndex;
   std::vector<std::unique_ptr<FrameData>> frames;
   GameSettings settings;
   bool areSettingsLoaded = false;
 
-  int32_t frameCount; // Current/last frame count
+  int32_t frameCount;  // Current/last frame count
 
   // From OnGameEnd event
   uint8_t winCondition;
@@ -111,23 +116,23 @@ typedef struct Game {
 
 // TODO: This shouldn't be static. Doesn't matter too much atm because we always
 // TODO: only read one file at a time
-static std::unordered_map<uint8_t, uint32_t> asmEvents = {
-    {EVENT_GAME_INIT, 320},
-    {EVENT_PRE_FRAME_UPDATE, 58},
-    {EVENT_POST_FRAME_UPDATE, 33},
-    {EVENT_GAME_END, 1},
-    {EVENT_FRAME_START, 8}};
+static std::unordered_map<uint8_t, uint32_t> asmEvents = {{EVENT_GAME_INIT, 320},
+                                                          {EVENT_PRE_FRAME_UPDATE, 58},
+                                                          {EVENT_POST_FRAME_UPDATE, 33},
+                                                          {EVENT_GAME_END, 1},
+                                                          {EVENT_FRAME_START, 8}};
 
-class SlippiGame {
+class SlippiGame
+{
 public:
   static std::unique_ptr<SlippiGame> FromFile(std::string path);
   bool AreSettingsLoaded();
   bool DoesFrameExist(int32_t frame);
   std::array<uint8_t, 4> GetVersion();
-  FrameData *GetFrame(int32_t frame);
-  FrameData *GetFrameAt(uint32_t pos);
+  FrameData* GetFrame(int32_t frame);
+  FrameData* GetFrameAt(uint32_t pos);
   int32_t GetLatestIndex();
-  GameSettings *GetSettings();
+  GameSettings* GetSettings();
   bool DoesPlayerExist(int8_t port);
   bool IsProcessingComplete();
 
@@ -143,4 +148,4 @@ private:
   bool isProcessingComplete = false;
   void processData();
 };
-} // namespace Slippi
+}  // namespace Slippi
