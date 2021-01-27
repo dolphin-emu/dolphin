8c1a667b8b058d5ff0fa39a908aa7551f855f5fa
Dr. Dystopia <jonis9898@hotmail.com>
2021-01-26 16:36:12 -0800

    DolphinAnalytics: Extract timestamp functions
    Moves functions that generate starting timestamps for performance
    sampling to PerformanceSampleAggregator.

--- Source/Core/Core/PerformanceSampleAggregator.h
+++ Source/Core/Core/PerformanceSampleAggregator.h
@@ -4,8 +4,14 @@
 
 #pragma once
 
+#include <chrono>
+
 class PerformanceSampleAggregator
 {
 public:
   PerformanceSampleAggregator() = default;
+
+  static std::chrono::microseconds GetInitialSamplingStartTimestamp();
+  static std::chrono::microseconds GetRepeatSamplingStartTimestamp();
+  static std::chrono::microseconds GetCurrentMicroseconds();
 };
