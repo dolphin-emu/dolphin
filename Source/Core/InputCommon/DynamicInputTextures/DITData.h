80f952966a960b57681e859596a1f5f8651bf110
iwubcode <iwubcode@users.noreply.github.com>
2021-02-22 00:26:20 -0600

    InputCommon: Update dynamic input textures data with v2 specification changes (tags, multi-binding)

--- Source/Core/InputCommon/DynamicInputTextures/DITData.h
+++ Source/Core/InputCommon/DynamicInputTextures/DITData.h
@@ -3,8 +3,10 @@
 
 #pragma once
 
+#include <optional>
 #include <string>
 #include <unordered_map>
+#include <variant>
 #include <vector>
 
 #include "InputCommon/ImageOperations.h"
@@ -17,11 +19,35 @@ struct Data
   std::string m_hires_texture_name;
   std::string m_generated_folder_name;
 
-  using EmulatedKeyToRegionsMap = std::unordered_map<std::string, std::vector<Rect>>;
-  std::unordered_map<std::string, EmulatedKeyToRegionsMap> m_emulated_controllers;
+  struct EmulatedSingleEntry;
+  struct EmulatedMultiEntry;
+  using EmulatedEntry = std::variant<EmulatedSingleEntry, EmulatedMultiEntry>;
 
-  using HostKeyToImagePath = std::unordered_map<std::string, std::string>;
-  std::unordered_map<std::string, HostKeyToImagePath> m_host_devices;
+  struct EmulatedSingleEntry
+  {
+    std::string m_key;
+    std::optional<std::string> m_tag;
+    Rect m_region;
+  };
+
+  struct EmulatedMultiEntry
+  {
+    std::string m_combined_tag;
+    Rect m_combined_region;
+
+    std::vector<EmulatedEntry> m_sub_entries;
+  };
+
+  std::unordered_map<std::string, std::vector<EmulatedEntry>> m_emulated_controllers;
+
+  struct HostEntry
+  {
+    std::vector<std::string> m_keys;
+    std::optional<std::string> m_tag;
+    std::string m_path;
+  };
+
+  std::unordered_map<std::string, std::vector<HostEntry>> m_host_devices;
   bool m_preserve_aspect_ratio = true;
 };
 }  // namespace InputCommon::DynamicInputTextures
