0d18e7a4c1cb712993d1b940682b1de2cb2c44f5
iwubcode <iwubcode@users.noreply.github.com>
2021-04-17 22:49:57 -0500

    DolphinQt: add new controls to manage FreeLook ortho camera controller

--- Source/Core/DolphinQt/Config/FreeLookWindow.cpp
+++ Source/Core/DolphinQt/Config/FreeLookWindow.cpp
@@ -9,6 +9,7 @@
 #include <QTabWidget>
 #include <QVBoxLayout>
 
+#include "DolphinQt/Config/FreeLook2DWidget.h"
 #include "DolphinQt/Config/FreeLookWidget.h"
 
 FreeLookWindow::FreeLookWindow(QWidget* parent) : QDialog(parent)
@@ -24,8 +25,12 @@ void FreeLookWindow::CreateMainLayout()
   m_button_box = new QDialogButtonBox(QDialogButtonBox::Close);
   connect(m_button_box, &QDialogButtonBox::rejected, this, &QDialog::reject);
 
+  auto* tab_widget = new QTabWidget();
+  tab_widget->addTab(new FreeLookWidget(this), tr("3D"));
+  tab_widget->addTab(new FreeLook2DWidget(this), tr("2D"));
+
   auto* main_layout = new QVBoxLayout();
-  main_layout->addWidget(new FreeLookWidget(this));
+  main_layout->addWidget(tab_widget);
   main_layout->addWidget(m_button_box);
   setLayout(main_layout);
 }
