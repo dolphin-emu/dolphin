4f53adc3312291e946d6f12b3140a21dacb36e1b
Filoppi <filippotarpini@hotmail.it>
2021-04-28 19:15:53 +0300

    Qt: make all tooltips look similar to balloon tips (and share parts of their code)

--- Source/Core/DolphinQt/Settings.h
+++ Source/Core/DolphinQt/Settings.h
@@ -54,6 +54,10 @@ public:
   void SetUserStylesEnabled(bool enabled);
   bool AreUserStylesEnabled() const;
 
+  void GetToolTipStyle(QColor& window_color, QColor& text_color, QColor& emphasis_text_color,
+                       QColor& border_color, const QPalette& palette,
+                       const QPalette& high_contrast_palette) const;
+
   bool IsLogVisible() const;
   void SetLogVisible(bool visible);
   bool IsLogConfigVisible() const;
