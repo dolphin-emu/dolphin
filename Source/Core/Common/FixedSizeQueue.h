9bfae40c40f1ad5b4ebb853266cb6fc63c8726a4
Filoppi <filippotarpini@hotmail.it>
2021-05-15 22:11:06 +0300

    Update FixedSizeQueue

--- Source/Core/Common/FixedSizeQueue.h
+++ Source/Core/Common/FixedSizeQueue.h
@@ -6,34 +6,31 @@
 
 #include <algorithm>
 #include <array>
-#include <cassert>
 #include <cstddef>
 #include <type_traits>
 #include <utility>
+
+#include "Common/Assert.h"
 
 // STL-look-a-like interface, but name is mixed case to distinguish it clearly from the
 // real STL classes.
 //
 // Not fully featured. Add features as needed.
 
+// A Circular/Ring buffer
 template <class T, size_t N>
 class FixedSizeQueue
 {
 public:
   void clear()
   {
-    if constexpr (!std::is_trivial_v<T>)
-    {
-      // The clear of non-trivial objects previously used "storage = {}". However, this causes GCC
-      // to take a very long time to compile the file/function, as well as generating huge amounts
-      // of debug information (~2GB object file, ~600MB of debug info).
-      while (count > 0)
-        pop();
-    }
+    // The clear of non-trivial objects previously used "storage = {}". However, this causes GCC
+    // to take a very long time to compile the file/function, as well as generating huge amounts
+    // of debug information (~2GB object file, ~600MB of debug info).
+    erase(count);
 
     head = 0;
     tail = 0;
-    count = 0;
   }
 
   void push(T t)
@@ -47,17 +44,19 @@ public:
     tail = (tail + 1) % N;
   }
 
-  // Copies over an array, loops over if num is greater than max_size
+  // Copies over an array, loops over if num is greater than max_size.
+  // Only available if T is trivial, just use push() otherwise.
+  template <typename = std::enable_if_t<std::is_trivial_v<T>>>
   void push_array(const T* t, size_t num)
   {
-    size_t back_writable_num = std::min(tail_to_end(), num);
-    memcpy(&back(), t, sizeof(T) * back_writable_num);
+    const size_t back_writable_num = std::min(tail_to_end(), num);
+    std::memcpy(&back(), t, sizeof(T) * back_writable_num);
     t += back_writable_num;
     size_t readable_num = num - back_writable_num;
     while (readable_num > 0)
     {
-      size_t beginning_writable_num = std::min(readable_num, N);
-      memcpy(&beginning(), t, sizeof(T) * beginning_writable_num);
+      const size_t beginning_writable_num = std::min(readable_num, N);
+      std::memcpy(&beginning(), t, sizeof(T) * beginning_writable_num);
       t += beginning_writable_num;
       readable_num -= beginning_writable_num;
     }
@@ -74,16 +73,18 @@ public:
   }
 
   // Takes a max num to copy (from head to tail), returns the actual copied number.
-  // Doesn't loop over if num is greater than our size
+  // Doesn't loop over if num is greater than our size.
+  // Only available if T is trivial, just copy by index otherwise.
+  template <typename = std::enable_if_t<std::is_trivial_v<T>>>
   size_t copy_to_array(T* t, size_t num)
   {
     num = std::min(num, count);
     const size_t front_readable_num = std::min(head_to_end(), num);
     const size_t beginning_readable_num = num - front_readable_num;
 
-    memcpy(&t[0], &front(), sizeof(T) * front_readable_num);
+    std::memcpy(&t[0], &front(), sizeof(T) * front_readable_num);
     if (beginning_readable_num > 0)
-      memcpy(&t[front_readable_num], &beginning(), sizeof(T) * beginning_readable_num);
+      std::memcpy(&t[front_readable_num], &beginning(), sizeof(T) * beginning_readable_num);
     return num;
   }
 
@@ -102,7 +103,7 @@ public:
   // From front
   void pop()
   {
-    assert(count > 0);
+    ASSERT(count > 0);
     if constexpr (!std::is_trivial_v<T>)
       storage[head] = {};
 
@@ -120,7 +121,7 @@ public:
   // From front
   void erase(size_t num)
   {
-    assert(count >= num);
+    ASSERT(count >= num);
     if constexpr (!std::is_trivial_v<T>)
     {
       while (num > 0)
@@ -138,31 +139,33 @@ public:
     }
   }
 
-  T& operator[](size_t index)
+  // From head/front, not the absolute index
+  T& operator[](size_t index) noexcept
   {
-    assert(index < count);
+    ASSERT(index < count);
     return storage[(head + index) % N];
   }
-  const T& operator[](size_t index) const
+  const T& operator[](size_t index) const noexcept
   {
-    assert(index < count);
+    ASSERT(index < count);
     return storage[(head + index) % N];
   }
+
+  size_t size() const noexcept { return count; }
+  size_t max_size() const noexcept { return N; }
+  bool empty() const noexcept { return size() == 0; }
 
   T& front() noexcept { return storage[head]; }
   const T& front() const noexcept { return storage[head]; }
   T& back() noexcept { return storage[tail]; }
   const T& back() const noexcept { return storage[tail]; }
-  // Only use if head_to_end() > size()
+  // Returns the first index of the internal array. Only read if head_to_end() > size()
   T& beginning() noexcept { return storage[0]; }
   const T& beginning() const noexcept { return storage[0]; }
-  size_t size() const noexcept { return count; }
-  size_t max_size() const noexcept { return N; }
   // Helper to know how many more items we could read before needing to loop over
   size_t head_to_end() const noexcept { return N - head; }
   // Helper to know how many more items we could write before needing to loop over
   size_t tail_to_end() const noexcept { return N - tail; }
-  bool empty() const noexcept { return size() == 0; }
 
 private:
   std::array<T, N> storage;
