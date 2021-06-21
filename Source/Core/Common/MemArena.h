7a016458cb229420b8bbed51feb58988c5d28eee
Admiral H. Curtiss <pikachu025@gmail.com>
2021-06-21 02:13:00 +0200

    MemArena: Prepare interface for proper memory reservation.

--- Source/Core/Common/MemArena.h
+++ Source/Core/Common/MemArena.h
@@ -16,18 +16,57 @@ namespace Common
 {
 // This class lets you create a block of anonymous RAM, and then arbitrarily map views into it.
 // Multiple views can mirror the same section of the block, which makes it very convenient for
-// emulating
-// memory mirrors.
+// emulating memory mirrors.
 class MemArena
 {
 public:
+  MemArena();
+  ~MemArena();
+  MemArena(const MemArena&) = delete;
+  MemArena(MemArena&&) = delete;
+  MemArena& operator=(const MemArena&) = delete;
+  MemArena& operator=(MemArena&&) = delete;
+
+  // Allocate a memory segment that can be then interacted with using CreateView() and
+  // ReleaseView(). This is used to make a mappable region for emulated memory.
+  // 'size' is the amount of bytes available in this region.
   void GrabSHMSegment(size_t size);
+
+  // Release the memory segment previously allocated with GrabSHMSegment().
   void ReleaseSHMSegment();
-  void* CreateView(s64 offset, size_t size, void* base = nullptr);
+
+  // Map a memory region in the memory segment previously allocated with GrabSHMSegment().
+  // 'offset': Offset within the memory segment to map at.
+  // 'size': Size of the region to map.
+  // Returns a pointer to the memory region, or nullptr on failure.
+  void* CreateView(s64 offset, size_t size);
+
+  // Unmap a memory region previously mapped with CreateView().
+  // 'view': Pointer returned by CreateView().
+  // 'size': Size passed to the corresponding CreateView() call.
   void ReleaseView(void* view, size_t size);
 
-  // This finds 1 GB in 32-bit, 16 GB in 64-bit.
-  static u8* FindMemoryBase();
+  // Reserve a memory region, but don't allocate anything within it.
+  // This is used to create our 'fastmem' memory area for the emulated game code to access directly.
+  // 'memory_size': Size in bytes of the memory region to reserve.
+  // Returns a pointer to the memory region, or nullptr on failure.
+  u8* ReserveMemoryRegion(size_t memory_size);
+
+  // Release a memory region previously reserved with ReserveMemoryRegion().
+  void ReleaseMemoryRegion();
+
+  // Map a section from the memory segment previously allocated with GrabSHMSegment()
+  // into the region previously reserved with ReserveMemoryRegion().
+  // 'offset': Offset within the memory segment to map from.
+  // 'size': Size of the region to map.
+  // 'base': Address where to map it.
+  // Returns the address we actually ended up mapping, which should be the given 'base'.
+  void* MapInMemoryRegion(s64 offset, size_t size, void* base);
+
+  // Unmap a memory region previously mapped with MapInMemoryRegion().
+  // 'view': Pointer returned by MapInMemoryRegion().
+  // 'size': Size passed to the corresponding MapInMemoryRegion() call.
+  void UnmapFromMemoryRegion(void* view, size_t size);
 
 private:
 #ifdef _WIN32
