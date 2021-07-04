9b80fb7deb676485fb780eadb56ea389aa4ec9d0
Bonta <40473493+Bonta0@users.noreply.github.com>
2021-07-04 13:17:32 +0200

    Qt: GBA Pad config

--- Source/Core/DolphinQt/Config/Mapping/MappingWindow.cpp
+++ Source/Core/DolphinQt/Config/Mapping/MappingWindow.cpp
@@ -23,6 +23,7 @@
 
 #include "DolphinQt/Config/Mapping/FreeLookGeneral.h"
 #include "DolphinQt/Config/Mapping/FreeLookRotation.h"
+#include "DolphinQt/Config/Mapping/GBAPadEmu.h"
 #include "DolphinQt/Config/Mapping/GCKeyboardEmu.h"
 #include "DolphinQt/Config/Mapping/GCMicrophone.h"
 #include "DolphinQt/Config/Mapping/GCPadEmu.h"
@@ -374,10 +375,15 @@ void MappingWindow::SetMappingType(MappingWindow::Type type)
 
   switch (type)
   {
+  case Type::MAPPING_GC_GBA:
+    widget = new GBAPadEmu(this);
+    setWindowTitle(tr("GameBoy Advance at Port %1").arg(GetPort() + 1));
+    AddWidget(tr("GameBoy Advance"), widget);
+    break;
   case Type::MAPPING_GC_KEYBOARD:
     widget = new GCKeyboardEmu(this);
-    AddWidget(tr("GameCube Keyboard"), widget);
     setWindowTitle(tr("GameCube Keyboard at Port %1").arg(GetPort() + 1));
+    AddWidget(tr("GameCube Keyboard"), widget);
     break;
   case Type::MAPPING_GC_BONGOS:
   case Type::MAPPING_GC_STEERINGWHEEL:
