797d0b7b1b1862d2fa2d57b7aa66cb06abf348a2
Techjar <tecknojar@gmail.com>
2021-08-03 18:55:38 -0400

    VI: Implement post-scanout XFB output
    This adds about a frame of latency, and since most games don't change
    VI registers during scanout, we can get away with outputting the XFB at
    the start of scanout. WWE Crush Hour is the (only currently known)
    exception, which has flickering problems when doing it this way.
    
    This adds a path to perform the output at the end of scanout, and gates
    it behind an option which defaults to using the latency-reducing
    pre-scanout path.

--- Source/Core/Core/Config/GraphicsSettings.h
+++ Source/Core/Core/Config/GraphicsSettings.h
@@ -118,6 +118,7 @@ extern const Info<bool> GFX_HACK_DISABLE_COPY_TO_VRAM;
 extern const Info<bool> GFX_HACK_DEFER_EFB_COPIES;
 extern const Info<bool> GFX_HACK_IMMEDIATE_XFB;
 extern const Info<bool> GFX_HACK_SKIP_DUPLICATE_XFBS;
+extern const Info<bool> GFX_HACK_EARLY_XFB_OUTPUT;
 extern const Info<bool> GFX_HACK_COPY_EFB_SCALED;
 extern const Info<bool> GFX_HACK_EFB_EMULATE_FORMAT_CHANGES;
 extern const Info<bool> GFX_HACK_VERTEX_ROUDING;
