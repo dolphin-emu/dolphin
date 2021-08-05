4d3cc18c230d141dda12ad6a35d66644993c1983
JosJuice <josjuice@gmail.com>
2021-08-05 16:23:13 +0200

    Android: Programmatically open/close cheat details
    Only has an effect when using a narrow screen.

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/features/cheats/ui/CheatsActivity.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/features/cheats/ui/CheatsActivity.java
@@ -8,9 +8,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.slidingpanelayout.widget.SlidingPaneLayout;
 
 import org.dolphinemu.dolphinemu.R;
+import org.dolphinemu.dolphinemu.features.cheats.model.Cheat;
 import org.dolphinemu.dolphinemu.features.cheats.model.CheatsViewModel;
+import org.dolphinemu.dolphinemu.ui.TwoPaneOnBackPressedCallback;
 import org.dolphinemu.dolphinemu.ui.main.MainPresenter;
 
 public class CheatsActivity extends AppCompatActivity
@@ -21,6 +24,8 @@ public class CheatsActivity extends AppCompatActivity
   private String mGameId;
   private int mRevision;
   private CheatsViewModel mViewModel;
+
+  private SlidingPaneLayout mSlidingPaneLayout;
 
   public static void launch(Context context, String gameId, int revision)
   {
@@ -47,6 +52,16 @@ public class CheatsActivity extends AppCompatActivity
     mViewModel.load(mGameId, mRevision);
 
     setContentView(R.layout.activity_cheats);
+
+    mSlidingPaneLayout = findViewById(R.id.sliding_pane_layout);
+
+    getOnBackPressedDispatcher().addCallback(this,
+            new TwoPaneOnBackPressedCallback(mSlidingPaneLayout));
+
+    mViewModel.getSelectedCheat().observe(this, this::onSelectedCheatChanged);
+    onSelectedCheatChanged(mViewModel.getSelectedCheat().getValue());
+
+    mViewModel.getOpenDetailsViewEvent().observe(this, this::openDetailsView);
   }
 
   @Override
@@ -55,5 +70,22 @@ public class CheatsActivity extends AppCompatActivity
     super.onStop();
 
     mViewModel.saveIfNeeded(mGameId, mRevision);
+  }
+
+  private void onSelectedCheatChanged(Cheat selectedCheat)
+  {
+    boolean cheatSelected = selectedCheat != null;
+
+    if (!cheatSelected && mSlidingPaneLayout.isOpen())
+      mSlidingPaneLayout.close();
+
+    mSlidingPaneLayout.setLockMode(cheatSelected ?
+            SlidingPaneLayout.LOCK_MODE_UNLOCKED : SlidingPaneLayout.LOCK_MODE_LOCKED_CLOSED);
+  }
+
+  private void openDetailsView(boolean open)
+  {
+    if (open)
+      mSlidingPaneLayout.open();
   }
 }
