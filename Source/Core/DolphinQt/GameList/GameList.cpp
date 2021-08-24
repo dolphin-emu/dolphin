ce3c1e236ea42dce48810c3550fbf57270456e01
LittleCoaks <LittleCoaks@users.noreply.github.com>
2021-08-24 06:59:58 -0400

    Added website link to game list
    When right-clicking on games, beneath the Wiki option, there's now a button "Project Rio Website" which redirects to www.projectrio.online.

--- Source/Core/DolphinQt/GameList/GameList.cpp
+++ Source/Core/DolphinQt/GameList/GameList.cpp
@@ -348,6 +348,8 @@ void GameList::ShowContextMenu(const QPoint&)
       menu->addAction(tr("&Wiki"), this, &GameList::OpenWiki);
     }
 
+    menu->addAction(tr("&Project Rio Website"), this, &GameList::OpenProjectRio);
+
     menu->addSeparator();
 
     if (DiscIO::IsDisc(platform))
@@ -528,6 +530,13 @@ void GameList::OpenWiki()
   QString game_id = QString::fromStdString(game->GetGameID());
   QString url =
       QStringLiteral("https://wiki.dolphin-emu.org/dolphin-redirect.php?gameid=").append(game_id);
+  QDesktopServices::openUrl(QUrl(url));
+}
+
+void GameList::OpenProjectRio()
+{
+  QString url =
+      QStringLiteral("https://projectrio.online");
   QDesktopServices::openUrl(QUrl(url));
 }
 
