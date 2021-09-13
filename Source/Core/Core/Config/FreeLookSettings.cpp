e90b0f6131294a753d8adf8fb555ec7f97f75818
iwubcode <iwubcode@users.noreply.github.com>
2021-09-13 00:37:01 -0500

    Core: add settings for a UDP server

--- Source/Core/Core/Config/FreeLookSettings.cpp
+++ Source/Core/Core/Config/FreeLookSettings.cpp
@@ -16,5 +16,7 @@ const Info<bool> FREE_LOOK_ENABLED{{System::FreeLook, "General", "Enabled"}, fal
 // FreeLook.Controller1
 const Info<FreeLook::ControlType> FL1_CONTROL_TYPE{{System::FreeLook, "Camera1", "ControlType"},
                                                    FreeLook::ControlType::SixAxis};
+const Info<std::string> FL1_UDP_ADDRESS{{System::FreeLook, "Camera1", "UDPAddress"}, "127.0.0.1"};
+const Info<u16> FL1_UDP_PORT{{System::FreeLook, "Camera1", "UDPPort"}, 36760};
 
 }  // namespace Config
--- Source/Core/Core/Config/FreeLookSettings.h
+++ Source/Core/Core/Config/FreeLookSettings.h
@@ -18,5 +18,7 @@ extern const Info<bool> FREE_LOOK_ENABLED;
 
 // FreeLook.Controller1
 extern const Info<FreeLook::ControlType> FL1_CONTROL_TYPE;
+extern const Info<std::string> FL1_UDP_ADDRESS;
+extern const Info<u16> FL1_UDP_PORT;
 
 }  // namespace Config
