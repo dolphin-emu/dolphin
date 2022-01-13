e99e78fb9c3ca4dd8d6ce3656418d2884b0f97ee
LittleCoaks <LittleCoaks@users.noreply.github.com>
2022-01-13 01:42:40 -0500

    Update Version Number

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -16,7 +16,7 @@ namespace Common
 #else
 #define BUILD_TYPE_STR ""
 #endif
-#define RIO_REV_STR "0.1.3(Alpha)"
+#define RIO_REV_STR "0.1.4"
 
 const std::string scm_rev_str = "Project Rio "
 #if !SCM_IS_MASTER
