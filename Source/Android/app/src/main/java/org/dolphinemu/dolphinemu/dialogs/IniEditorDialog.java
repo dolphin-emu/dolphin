98583da94cb71b694071cc1b0f4b826fe3641683
Gamer64ytb <76565986+Gamer64ytb@users.noreply.github.com>
2022-01-30 13:57:01 -0500

    fx lint 2 (java reformat)

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/dialogs/IniEditorDialog.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/dialogs/IniEditorDialog.java
@@ -3,6 +3,7 @@ package org.dolphinemu.dolphinemu.dialogs;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
@@ -21,13 +22,11 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-public class IniEditorDialog extends DialogFragment
-{
+public class IniEditorDialog extends DialogFragment {
   private static final String ARG_GAME_ID = "game_id";
   private ViewGroup mViewGroup;
 
-  public static IniEditorDialog newInstance(String gameId)
-  {
+  public static IniEditorDialog newInstance(String gameId) {
     IniEditorDialog fragment = new IniEditorDialog();
 
     Bundle arguments = new Bundle();
@@ -39,8 +38,7 @@ public class IniEditorDialog extends DialogFragment
 
   @NonNull
   @Override
-  public Dialog onCreateDialog(Bundle savedInstanceState)
-  {
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
     final String gameId = getArguments().getString(ARG_GAME_ID);
     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
     mViewGroup = (ViewGroup) getActivity().getLayoutInflater()
@@ -65,25 +63,20 @@ public class IniEditorDialog extends DialogFragment
     return builder.create();
   }
 
-  private void setGameSettings(String gameId, EditText editCode)
-  {
+  private void setGameSettings(String gameId, EditText editCode) {
     String filename = DirectoryInitialization.getLocalSettingFile(gameId);
     int count = 0;
     String[] targets = {"[ActionReplay]", "[ActionReplay_Enabled]", "[Gecko]", "[Gecko_Enabled]"};
     int[] indices = {-1, -1, -1, -1};
 
     StringBuilder sb = new StringBuilder();
-    try
-    {
+    try {
       BufferedReader reader = new BufferedReader(new FileReader(filename));
       String line = reader.readLine();
-      while (line != null)
-      {
-        for(int i = 0; i < targets.length; ++i)
-        {
+      while (line != null) {
+        for (int i = 0; i < targets.length; ++i) {
           int index = line.indexOf(targets[i]);
-          if(index != -1)
-          {
+          if (index != -1) {
             indices[i] = count + index;
           }
         }
@@ -93,50 +86,40 @@ public class IniEditorDialog extends DialogFragment
         sb.append(System.lineSeparator());
         line = reader.readLine();
       }
-    }
-    catch (Exception e)
-    {
+    } catch (Exception e) {
       Log.error(e.getMessage());
       onSaveError();
     }
 
     int cursorPos = 0;
-    for(int i = 0; i < targets.length; ++i)
-    {
-      if(indices[i] < 0)
-      {
+    for (int i = 0; i < targets.length; ++i) {
+      if (indices[i] < 0) {
         sb.append(System.lineSeparator());
         sb.append(targets[i]);
         sb.append(System.lineSeparator());
         count += targets[i].length() + 2;
         cursorPos = count;
-      }
-      else if(indices[i] > cursorPos)
-      {
+      } else if (indices[i] > cursorPos) {
         cursorPos = indices[i] + targets[i].length() + 1;
       }
     }
 
     String content = sb.toString();
-    if(cursorPos > content.length())
+    if (cursorPos > content.length())
       cursorPos = content.length();
     editCode.setText(content);
     editCode.setSelection(cursorPos);
   }
 
-  private void saveIniContent(String gameId, String content)
-  {
+  private void saveIniContent(String gameId, String content) {
     String filename = DirectoryInitialization.getLocalSettingFile(gameId);
     boolean saved = false;
-    try
-    {
+    try {
       BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
       writer.write(content);
       writer.close();
       saved = true;
-    }
-    catch (Exception e)
-    {
+    } catch (Exception e) {
       Log.error(e.getMessage());
       onSaveError();
     }
@@ -145,8 +128,7 @@ public class IniEditorDialog extends DialogFragment
       Toast.LENGTH_SHORT).show();
   }
 
-  public void onSaveError()
-  {
+  public void onSaveError() {
     TextView textError = mViewGroup.findViewById(R.id.ini_editor_error);
     textError.setText(R.string.error);
   }
