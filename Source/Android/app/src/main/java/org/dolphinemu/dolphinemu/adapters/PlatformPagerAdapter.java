8a89734fec0aec6049c7de4a629b5a91ae8536fa
Gamer64ytb <76565986+Gamer64ytb@users.noreply.github.com>
2022-02-01 21:44:27 +0100

    Android: Minor redesign with TAB.
    This replaces icons of tab to text (int -> String), looks better and more clean.
    /home/gamer64/Descargas/Screenshot_2022-02-01-21-21-10-041_org.dolphinemu.testpr.jpg

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/adapters/PlatformPagerAdapter.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/adapters/PlatformPagerAdapter.java
@@ -23,12 +23,7 @@ public class PlatformPagerAdapter extends FragmentPagerAdapter
   private Context mContext;
   private SwipeRefreshLayout.OnRefreshListener mOnRefreshListener;
 
-  private final static int[] TAB_ICONS =
-          {
-                  R.drawable.ic_gamecube,
-                  R.drawable.ic_wii,
-                  R.drawable.ic_folder
-          };
+  private final static String[] TAB_TEXTS = { "NGC", "Wii", "Ware" };
 
   public PlatformPagerAdapter(FragmentManager fm, Context context,
           SwipeRefreshLayout.OnRefreshListener onRefreshListener)
@@ -52,24 +47,12 @@ public class PlatformPagerAdapter extends FragmentPagerAdapter
   @Override
   public int getCount()
   {
-    return TAB_ICONS.length;
+    return TAB_TEXTS.length;
   }
 
   @Override
   public CharSequence getPageTitle(int position)
   {
-    // Hax from https://guides.codepath.com/android/Google-Play-Style-Tabs-using-TabLayout#design-support-library
-    // Apparently a workaround for TabLayout not supporting icons.
-    // TODO: This workaround will eventually not be necessary; switch to more legit methods when that is the case
-    // TODO: Also remove additional hax from styles.xml
-    Drawable drawable = mContext.getResources().getDrawable(TAB_ICONS[position]);
-    drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-
-    ImageSpan imageSpan = new ImageSpan(drawable, ImageSpan.ALIGN_BOTTOM);
-
-    SpannableString sb = new SpannableString(" ");
-    sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-    return sb;
+    return TAB_TEXTS[position];
   }
 }
