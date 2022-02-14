fd511a689f5acb9cb563f6f267da836e7ad4f37b
JosJuice <josjuice@gmail.com>
2022-02-14 19:03:34 +0100

    Jit: Skip redundant flushes
    This makes codegen faster (by perhaps 10-20% in the case of Jit64,
    I didn't measure too closely), which helps speed up NBA Live 2005
    a little. But the game still has serious performance issues.

--- Source/Core/Core/PowerPC/Jit64/Jit.cpp
+++ Source/Core/Core/PowerPC/Jit64/Jit.cpp
@@ -1184,8 +1184,8 @@ bool Jit64::DoJit(u32 em_address, JitBlock* b, u32 nextPC)
         gpr.Discard(op.gprDiscardable);
         fpr.Discard(op.fprDiscardable);
       }
-      gpr.Flush(~op.gprInUse);
-      fpr.Flush(~op.fprInUse);
+      gpr.Flush(~op.gprInUse & (op.regsIn | op.regsOut));
+      fpr.Flush(~op.fprInUse & (op.fregsIn | op.GetFregsOut()));
 
       if (opinfo->flags & FL_LOADSTORE)
         ++js.numLoadStoreInst;
