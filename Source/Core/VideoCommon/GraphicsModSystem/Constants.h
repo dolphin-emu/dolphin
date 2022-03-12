f92beb611f5e7effc9d1b8a90fbd4004233945ff
iwubcode <iwubcode@users.noreply.github.com>
2022-03-12 15:30:50 -0600

    VideoCommon: add game mod constants

---
+++ Source/Core/VideoCommon/GraphicsModSystem/Constants.h
@@ -0,0 +1,11 @@
+// Copyright 2022 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <string>
+
+#include "Common/CommonPaths.h"
+
+static const inline std::string DOLPHIN_SYSTEM_GRAPHICS_MOD_DIR =
+    LOAD_DIR DIR_SEP GRAPHICSMOD_DIR DIR_SEP;
