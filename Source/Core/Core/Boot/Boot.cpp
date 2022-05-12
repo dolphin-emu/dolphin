30b5849f70c3db1d43a348921dc5fa8b411ad927
Langerz82 <langley.joshua@gmail.com>
2022-05-12 18:47:46 +1000

    t17

--- Source/Core/Core/Boot/Boot.cpp
+++ Source/Core/Core/Boot/Boot.cpp
@@ -75,22 +75,23 @@ static std::vector<std::string> ReadGameFile(const std::string& game_path,
   fprintf(stdout, "game_path: %s\n", game_path.c_str());
   std::string game_filename = (std::string) game_path;
   if (game_path.find_last_of('/') != std::string::npos)
-    game_filename = game_path.substr(game_path.find_last_of('/'));
+    game_filename = game_path.substr(game_path.find_last_of('/')+1);
   fprintf(stdout, "game_filename: %s\n", game_filename.c_str());
   
 	std::regex str_expr (".*(disc\\s*\\d)[^\\d]{0,1}.*", std::regex_constants::icase);
   if (std::regex_match(game_filename,str_expr))
   {
+    fprintf(stdout, "regex_match\n");
     int disc_num = 0;
     std::string line = game_filename;
 
     std::smatch matches;
     std::regex_search(game_filename, matches, str_expr);
     std::string disc_ref;
-    for (auto x : matches) {
-      disc_ref = x;
-      fprintf(stdout, "matches: %s\n", disc_ref.c_str());
-    }
+    //for (auto x : matches) {
+    disc_ref = (std::string) matches.str(matches.size()-1);
+    fprintf(stdout, "matches: %s\n", disc_ref.c_str());
+    //}
 
     while(true)
     {      
