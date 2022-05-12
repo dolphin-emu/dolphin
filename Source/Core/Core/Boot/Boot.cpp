e50362b37f03c257ec49ca836a4cde3183540a9e
Langerz82 <langley.joshua@gmail.com>
2022-05-12 17:44:54 +1000

    t15

--- Source/Core/Core/Boot/Boot.cpp
+++ Source/Core/Core/Boot/Boot.cpp
@@ -72,11 +72,11 @@ static std::vector<std::string> ReadGameFile(const std::string& game_path,
 
   std::vector<std::string> result;
 
-  fprintf(stdout, "game_path: %s\n", game_path);
+  fprintf(stdout, "game_path: %s\n", game_path.c_str());
   std::string game_filename = (std::string) game_path;
   if (game_path.find_last_of('/') != std::string::npos)
     game_filename = game_path.substr(game_path.find_last_of('/'));
-  fprintf(stdout, "game_filename: %s\n", game_filename);
+  fprintf(stdout, "game_filename: %s\n", game_filename.c_str());
   
 	std::regex str_expr (".*(disc\\s*\\d)[^\\d]{0,1}.*", std::regex_constants::icase);
   if (std::regex_match(game_filename,str_expr))
@@ -89,7 +89,7 @@ static std::vector<std::string> ReadGameFile(const std::string& game_path,
     std::string disc_ref;
     for (auto x : matches) {
       disc_ref = x;
-      fprintf(stdout, "matches: %s\n", disc_ref);
+      fprintf(stdout, "matches: %s\n", disc_ref.c_str());
     }
 
     while(true)
@@ -103,7 +103,7 @@ static std::vector<std::string> ReadGameFile(const std::string& game_path,
       const std::string path_to_add = line.front() != '/' ? folder_path + line : line;
 #endif
 
-      fprintf(stdout, "path_to_add: %s\n", path_to_add);
+      fprintf(stdout, "path_to_add: %s\n", path_to_add.c_str());
       if (File::Exists(path_to_add))
         result.push_back(path_to_add);
       else
@@ -260,8 +260,8 @@ std::unique_ptr<BootParameters> BootParameters::GenerateFromFile(std::vector<std
   SplitPath(paths.front(), &folder_path, nullptr, &extension);
   Common::ToLower(&extension);
   
-  fprintf(stdout, "file: %s\n", paths.front());
-  fprintf(stdout, "extension: %s\n", extension);
+  fprintf(stdout, "file: %s\n", paths.front().c_str());
+  fprintf(stdout, "extension: %s\n", extension.c_str());
   
   if (extension == ".m3u" || extension == ".m3u8")
   {
@@ -277,14 +277,9 @@ std::unique_ptr<BootParameters> BootParameters::GenerateFromFile(std::vector<std
   {
     fprintf(stdout, "MAIN_AUTO_DISC_CHANGE\n");
     paths = ReadGameFile(paths.front(), folder_path);
-    if (!paths.empty())
-    {
-      path = paths.front();
-    }
-    if (paths.size() == 1)
-      paths.clear();
   }
-  fprintf(stdout, "path: %s\n", path);
+  if (!paths.empty())
+    fprintf(stdout, "path: %s\n", paths.front().c_str());
 #endif
 
   std::string path = paths.front();
@@ -304,7 +299,7 @@ std::unique_ptr<BootParameters> BootParameters::GenerateFromFile(std::vector<std
       {".gcm", ".iso", ".tgc", ".wbfs", ".ciso", ".gcz", ".wia", ".rvz", ".dol", ".elf"}};
   if (disc_image_extensions.find(extension) != disc_image_extensions.end() || is_drive)
   {
-    NOTICE_LOG_FMT(BOOT, "original file: {}", path);
+    fprintf(stdout, "path file: {}", path);
 
     std::unique_ptr<DiscIO::VolumeDisc> disc = DiscIO::CreateDisc(path);
     if (disc)
