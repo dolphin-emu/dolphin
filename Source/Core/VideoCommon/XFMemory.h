736466a5d91c77cdcd3966e48b071c4f52658159
Pokechu22 <Pokechu022@gmail.com>
2022-05-17 11:27:49 -0700

    XFMemory: Rename hostinfo to invtxspec

--- Source/Core/VideoCommon/XFMemory.h
+++ Source/Core/VideoCommon/XFMemory.h
@@ -416,23 +416,23 @@ struct Projection
 
 struct XFMemory
 {
-  float posMatrices[256];    // 0x0000 - 0x00ff
-  u32 unk0[768];             // 0x0100 - 0x03ff
-  float normalMatrices[96];  // 0x0400 - 0x045f
-  u32 unk1[160];             // 0x0460 - 0x04ff
-  float postMatrices[256];   // 0x0500 - 0x05ff
-  Light lights[8];           // 0x0600 - 0x067f
-  u32 unk2[2432];            // 0x0680 - 0x0fff
-  u32 error;                 // 0x1000
-  u32 diag;                  // 0x1001
-  u32 state0;                // 0x1002
-  u32 state1;                // 0x1003
-  u32 xfClock;               // 0x1004
-  ClipDisable clipDisable;   // 0x1005
-  u32 perf0;                 // 0x1006
-  u32 perf1;                 // 0x1007
-  INVTXSPEC hostinfo;        // 0x1008 number of textures,colors,normals from vertex input
-  NumColorChannel numChan;   // 0x1009
+  float posMatrices[256];                   // 0x0000 - 0x00ff
+  u32 unk0[768];                            // 0x0100 - 0x03ff
+  float normalMatrices[96];                 // 0x0400 - 0x045f
+  u32 unk1[160];                            // 0x0460 - 0x04ff
+  float postMatrices[256];                  // 0x0500 - 0x05ff
+  Light lights[8];                          // 0x0600 - 0x067f
+  u32 unk2[2432];                           // 0x0680 - 0x0fff
+  u32 error;                                // 0x1000
+  u32 diag;                                 // 0x1001
+  u32 state0;                               // 0x1002
+  u32 state1;                               // 0x1003
+  u32 xfClock;                              // 0x1004
+  ClipDisable clipDisable;                  // 0x1005
+  u32 perf0;                                // 0x1006
+  u32 perf1;                                // 0x1007
+  INVTXSPEC invtxspec;                      // 0x1008
+  NumColorChannel numChan;                  // 0x1009
   u32 ambColor[NUM_XF_COLOR_CHANNELS];      // 0x100a, 0x100b
   u32 matColor[NUM_XF_COLOR_CHANNELS];      // 0x100c, 0x100d
   LitChannel color[NUM_XF_COLOR_CHANNELS];  // 0x100e, 0x100f
