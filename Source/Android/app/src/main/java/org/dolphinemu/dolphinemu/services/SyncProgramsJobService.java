1d772176a24f25de7487b4f52883da4409079931
JosJuice <josjuice@gmail.com>
2022-06-26 09:24:19 +0200

    Revert "Partially revert "Android: Clean up hardcoded platform names""
    This reverts commit 98bdf3b1ceae94a35e3f47158966967d1c947d99.

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/services/SyncProgramsJobService.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/services/SyncProgramsJobService.java
@@ -98,7 +98,8 @@ public class SyncProgramsJobService extends JobService
           Channel channel = TvUtil.getChannelById(context, channelId);
           for (Platform platform : Platform.values())
           {
-            if (channel != null && channel.getDisplayName().equals(platform.getIdString()))
+            if (channel != null &&
+                    channel.getAppLinkIntentUri().equals(AppLinkHelper.buildBrowseUri(platform)))
             {
               getGamesByPlatform(platform);
               syncPrograms(channelId);
--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/utils/TvUtil.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/utils/TvUtil.java
@@ -254,21 +254,19 @@ public class TvUtil
   /**
    * Generates all subscriptions for homescreen channels.
    */
-  public static List<HomeScreenChannel> createUniversalSubscriptions()
+  public static List<HomeScreenChannel> createUniversalSubscriptions(Context context)
   {
-    return new ArrayList<>(createPlatformSubscriptions());
+    return new ArrayList<>(createPlatformSubscriptions(context));
   }
 
-  private static List<HomeScreenChannel> createPlatformSubscriptions()
+  private static List<HomeScreenChannel> createPlatformSubscriptions(Context context)
   {
     List<HomeScreenChannel> subs = new ArrayList<>();
     for (Platform platform : Platform.values())
     {
-      // TODO: Replace the getIdString calls with getHeaderName to get localized names.
-      // This would require SyncProgramsJobService to stop using the display name as a key
       subs.add(new HomeScreenChannel(
-              platform.getIdString(),
-              platform.getIdString(),
+              context.getString(platform.getHeaderName()),
+              context.getString(platform.getHeaderName()),
               AppLinkHelper.buildBrowseUri(platform)));
     }
     return subs;
