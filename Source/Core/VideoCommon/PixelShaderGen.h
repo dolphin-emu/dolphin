e704385fce2a6383891d50253aafb61a486e7981
iwubcode <iwubcode@users.noreply.github.com>
2022-07-30 00:33:29 -0500

    VideoCommon: pixel shader gen changes needed to support custom pixel shaders in graphics mods

--- Source/Core/VideoCommon/PixelShaderGen.h
+++ Source/Core/VideoCommon/PixelShaderGen.h
@@ -158,8 +158,12 @@ struct pixel_shader_uid_data
 
 using PixelShaderUid = ShaderUid<pixel_shader_uid_data>;
 
+void WriteCustomShaderStructImpl(ShaderCode* out, u32 num_stages, bool per_pixel_lighting,
+                                 const pixel_shader_uid_data* uid_data);
+
 ShaderCode GeneratePixelShaderCode(APIType api_type, const ShaderHostConfig& host_config,
-                                   const pixel_shader_uid_data* uid_data);
+                                   const pixel_shader_uid_data* uid_data,
+                                   const CustomPixelShaderContents& custom_details);
 void WritePixelShaderCommonHeader(ShaderCode& out, APIType api_type,
                                   const ShaderHostConfig& host_config, bool bounding_box);
 void ClearUnusedPixelShaderUidBits(APIType api_type, const ShaderHostConfig& host_config,
