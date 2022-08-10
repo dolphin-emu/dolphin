d45df51d95893d0a9d042dbcbf12daef99bda310
Zopolis4 <creatorsmithmdt@gmail.com>
2022-08-10 13:40:48 +1000

    si part 7 unbound discordance edirion

--- Source/Core/Core/HW/SI/SI_DeviceBaseboard.cpp
+++ Source/Core/Core/HW/SI/SI_DeviceBaseboard.cpp
@@ -7,6 +7,7 @@
 #include "Common/Logging/Log.h"
 #include "Common/Swap.h"
 #include "Core/HW/SI/SI_Device.h"
+#include "Core/HW/SI/SI_DeviceGCController.h"
 
 namespace SerialInterface
 {
@@ -37,10 +38,85 @@ int CSIDevice_Baseboard::RunBuffer(u8* buffer, int request_length)
     for (int i=0; i < request_length; ++i)
       csum += buffer[i];
 
+    unsigned char res[0x80];
+    int resp = 0;
+
     int real_len = buffer[1];
+    int p = 2;
+
+    static int d10_1 = 0xfe;
+
+    memset(res, 0, 0x80);
+    res[resp++] = 1;
+    res[resp++] = 1;
+
+
 #define ptr(x) buffer[(2 + x)]
     while (2 < real_len+2)
     {
+      switch (ptr(0))
+      {
+        case 0x10:
+        {
+          INFO_LOG_FMT(SERIALINTERFACE, "Command 10 {:02x} (READ STATUS&SWITCHES)", ptr(1));
+          // GCPadStatus PadStatus;
+          // memset(&PadStatus, 0 ,sizeof(PadStatus));
+          // Pad::GetStatus(ISIDevice::GetDeviceNumber());
+          res[resp++] = 0x10;
+          res[resp++] = 0x2;
+          int d10_0 = 0xFF;
+
+          res[resp++] = d10_0;
+          res[resp++] = d10_1;
+          break;
+        }
+        case 0x11:
+        {
+          INFO_LOG_FMT(SERIALINTERFACE, "Command 11, {:02x} (READ SERIAL NR)", ptr(1));
+          char string[] = "AADE-01A14964511";
+          res[resp++] = 0x11;
+          res[resp++] = 0x10;
+          memcpy(res + resp, string, 0x10);
+          resp += 0x10;
+          break;
+        }
+        case 0x15:
+        {
+          INFO_LOG_FMT(SERIALINTERFACE, "Command 15, {:02x} (READ FIRM VERSION)", ptr(1));
+          res[resp++] = 0x15;
+          res[resp++] = 0x02;
+           // FIRM VERSION
+          res[resp++] = 0x00;
+          res[resp++] = 0x44;
+          break;
+        }
+        case 0x16:
+        {
+          INFO_LOG_FMT(SERIALINTERFACE, "Command 16, {:02x} (READ FPGA VERSION)", ptr(1));
+          res[resp++] = 0x16;
+          res[resp++] = 0x02;
+           // FPGA VERSION
+          res[resp++] = 0x07;
+          res[resp++] = 0x09;
+          break;
+        }
+        case 0x1f:
+        {
+          INFO_LOG_FMT(SERIALINTERFACE, "Command 1f, {:02x} {:02x} {:02x} {:02x} {:02x} (REGION)", ptr(1), ptr(2), ptr(3), ptr(4), ptr(5));
+          unsigned char string[] =  
+            "\x00\x00\x30\x00"
+            "\x01\xfe\x00\x00" // JAPAN
+            //"\x02\xfd\x00\x00" // USA
+            //"\x03\xfc\x00\x00" // export
+            "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff";
+          res[resp++] = 0x1f;
+          res[resp++] = 0x14;
+
+          for( int i=0; i<0x14; ++i )
+            res[resp++] = string[i];
+          break;
+        }
+      }
       ERROR_LOG_FMT(SERIALINTERFACE, "Unhandled SI subcommand {:02x} {:02x} {:02x} {:02x} {:02x}",
                     ptr(0), ptr(1), ptr(2), ptr(3), ptr(4));
     }
