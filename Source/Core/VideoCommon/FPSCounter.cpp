c126ed762ce078f2feeb43c8c9359fde41689e18
Sam Belliveau <sam.belliveau@gmail.com>
2022-08-16 11:57:44 -0400

    Adjust Sample Time for Faster FPS Measurements

--- Source/Core/VideoCommon/FPSCounter.cpp
+++ Source/Core/VideoCommon/FPSCounter.cpp
@@ -16,7 +16,7 @@
 static constexpr double US_TO_MS = 1000.0;
 static constexpr double US_TO_S = 1000000.0;
 
-static constexpr s64 FPS_SAMPLE_TIME_US = 2000000;
+static constexpr s64 FPS_SAMPLE_TIME_US = 1000000;
 
 FPSCounter::FPSCounter(const char* log_name)
 {
@@ -58,7 +58,7 @@ void FPSCounter::Update()
     m_dt_total += diff;
     m_dt_queue.push(diff);
 
-    while (1 < m_dt_size && FPS_SAMPLE_TIME_US <= m_dt_total - m_dt_queue.front())
+    while (1 < m_dt_size && FPS_SAMPLE_TIME_US <= m_dt_total - m_dt_queue.front()) 
     {
       m_dt_size -= 1;
       m_dt_total -= m_dt_queue.front();
