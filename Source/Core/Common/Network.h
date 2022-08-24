e783a7f4090efa1251a0215756daaf6c4fb48930
Sepalani <sepalani@hotmail.fr>
2022-08-24 20:34:49 +0400

    Common/Network: Add StrNetworkError helper function

--- Source/Core/Common/Network.h
+++ Source/Core/Common/Network.h
@@ -266,4 +266,5 @@ u16 ComputeTCPNetworkChecksum(const IPAddress& from, const IPAddress& to, const 
 NetworkErrorState SaveNetworkErrorState();
 void RestoreNetworkErrorState(const NetworkErrorState& state);
 const char* DecodeNetworkError(s32 error_code);
+const char* StrNetworkError();
 }  // namespace Common
