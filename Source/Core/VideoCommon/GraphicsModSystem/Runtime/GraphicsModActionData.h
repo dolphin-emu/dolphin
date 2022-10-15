74ac3cef0614b4cf2d6263ebc72df371020c7376
iwubcode <iwubcode@users.noreply.github.com>
2022-10-15 14:54:21 -0500

    VideoCommon: add data needed to support custom pixel shaders to graphics mod actions

--- Source/Core/VideoCommon/GraphicsModSystem/Runtime/GraphicsModActionData.h
+++ Source/Core/VideoCommon/GraphicsModSystem/Runtime/GraphicsModActionData.h
@@ -3,16 +3,22 @@
 
 #pragma once
 
+#include <array>
+#include <optional>
 #include <string_view>
 
 #include "Common/CommonTypes.h"
 #include "Common/Matrix.h"
+#include "VideoCommon/GraphicsModSystem/Runtime/CustomTextureData.h"
+#include "VideoCommon/PixelShaderGen.h"
 
 namespace GraphicsModActionData
 {
 struct DrawStarted
 {
+  u32 texture_unit;
   bool* skip;
+  std::optional<CustomPixelShader>* custom_pixel_shader;
 };
 
 struct EFB
@@ -31,5 +37,8 @@ struct Projection
 struct TextureLoad
 {
   std::string_view texture_name;
+  u32 texture_width;
+  u32 texture_height;
+  std::vector<VideoCommon::CustomTextureData*>* custom_textures;
 };
 }  // namespace GraphicsModActionData
