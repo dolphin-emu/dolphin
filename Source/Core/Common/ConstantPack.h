d017404d06b842ea6b6616aa38a9fa4e3d733fbf
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2022-10-17 00:50:09 -0500

    Indexable ConstantPack

--- Source/Core/Common/ConstantPack.h
+++ Source/Core/Common/ConstantPack.h
@@ -4,10 +4,14 @@
 #pragma once
 
 #include <cstddef>
+#include <optional>
 #include <utility>
 
 // ConstantPack allows one to recursively iterate through a std::integer_sequence
-// e.g. Common::ConstantPack<int, 1, 2, 3, 4>::peel::peel::first == 3
+// e.g. Common::ConstantPack<int, 23, 15, 73, 26, 64>::peel::peel::first == 73
+
+// ConstantPack can also be directly indexed.  Negative or out-of-bounds indicies are std::nullopt.
+// e.g. Common::ConstantPack<int, 23, 15, 73, 26, 64>::at<2>.value() == 73
 
 namespace Common
 {
@@ -16,6 +20,8 @@ struct ConstantPack : std::integer_sequence<T, vals...>
 {
   using value_type = T;
   using peel = ConstantPack<T, vals...>;
+  template <std::ptrdiff_t>
+  static constexpr auto at = std::nullopt;
 };
 
 template <typename T, T val, T... vals>
@@ -23,6 +29,9 @@ struct ConstantPack<T, val, vals...> : std::integer_sequence<T, val, vals...>
 {
   using value_type = T;
   using peel = ConstantPack<T, vals...>;
+  template <std::ptrdiff_t idx>
+  static constexpr auto at = (idx == 0) ? std::optional(first) : peel::template at<idx - 1>;
+
   static constexpr T first = val;
 };
 
