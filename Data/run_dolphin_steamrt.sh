0f82148aa89d9ac02046144fcac3c39aab57aec3
OatmealDome <julian@oatmealdome.me>
2023-01-18 10:37:16 -0500

    Data: Update Steam runtime run script to add a workaround for a Qt bug

--- Data/run_dolphin_steamrt.sh
+++ Data/run_dolphin_steamrt.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-$(dirname "$0")/dolphin-emu "$@"
+# We define QT_XCB_NO_XI2 as a workaround for https://bugs.dolphin-emu.org/issues/12913.
+QT_XCB_NO_XI2=1 $(dirname "$0")/dolphin-emu "$@"
