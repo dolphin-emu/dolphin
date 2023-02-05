33ade07b8b5afe13d86247aef2a3f0d7f0ce11e7
Lobsterzelda <tersaderwaslink@gmail.com>
2023-02-04 19:40:38 -0500

    Added SFA test script

---
+++ Source/Core/DolphinQt/LuaExamplesAndTests/Star_Fox_Adventures_electric_tile_puzzle_brute_forcer.lua
@@ -0,0 +1,66 @@
+basePasswordAddress = 0x80329849
+earlySaveStatePath= "saveStateName.sav"
+movieNamePath = "movieName.dtm"
+
+function isPasswordSet()
+	if memory:readFrom(basePasswordAddress + 10, "u8") ~= 0 then
+		return true
+	else
+		return false
+	end
+end
+
+function getPassword()
+	returnTable = {}
+	returnTable[1] = memory:readFrom(basePasswordAddress, "u8")
+	returnTable[2] = memory:readFrom(basePasswordAddress + 2, "u8")
+	returnTable[3] = memory:readFrom(basePasswordAddress + 4, "u8")
+	returnTable[4] = memory:readFrom(basePasswordAddress + 6, "u8")
+	returnTable[5] = memory:readFrom(basePasswordAddress + 8, "u8")
+	returnTable[6] = memory:readFrom(basePasswordAddress + 10, "u8")
+	return returnTable
+end
+
+emu:frameAdvance()
+emu:frameAdvance()
+emu:frameAdvance()
+emu:playMovie(movieNamePath)
+totalMovieLength = statistics:getMovieLength()
+
+shouldContinueSearching = true
+
+while shouldContinueSearching do
+	emu:loadState(earlySaveStatePath)
+
+	while statistics:getCurrentFrame() <= totalMovieLength - 10 and not isPasswordSet() do
+		gc_controller:addButtonPressChance(1, 15, "dPadLeft")
+		gc_controller:addButtonPressChance(1, 10, "dPadRight")
+		gc_controller:addButtonPressChance(1, 0.005, "Y")
+		gc_controller:addButtonComboChance(1, 10, false, {cStickX = 255})
+		gc_controller:addButtonComboChance(1, 10, false, {cStickX = 0})
+		gc_controller:addButtonComboChance(1, 10, false, {cStickY = 0})
+		gc_controller:addButtonComboChance(1, 10, false, {cStickY = 255})
+		emu:frameAdvance()
+	end
+	
+	if statistics:getCurrentFrame() <= totalMovieLength - 10 then -- This if statement is true when we broke out of the loop because the password was set.
+		emu:frameAdvance()
+		emu:frameAdvance()
+		emu:frameAdvance()
+		emu:frameAdvance()
+		emu:frameAdvance()
+		passwordValue = getPassword()
+		print("bruteForcePattern_" .. tostring(passwordValue[1]) .. tostring(passwordValue[2]) .. tostring(passwordValue[3]) .. "-" .. tostring(passwordValue[4]) .. tostring(passwordValue[5]) .. tostring(passwordValue[6]))
+		if passwordValue[1] ~= 0 and passwordValue[1] <= 2 and passwordValue[2] <= 2 and passwordValue[3] <= 2 and passwordValue[4] <= 2 and passwordValue[5] <= 2 and passwordValue[6] <= 2 then
+			generatedMoviePath = "bruteForcePattern_" .. tostring(passwordValue[1]) .. tostring(passwordValue[2]) .. tostring(passwordValue[3]) .. "-" .. tostring(passwordValue[4]) .. tostring(passwordValue[5]) .. tostring(passwordValue[6]) .. ".dtm"
+			print("Writing to: " .. generatedMoviePath)
+			emu:saveMovie(generatedMoviePath)
+		end
+		
+		if passwordValue[1] == 1 and passwordValue[2] == 1 and passwordValue[3] == 1 and passwordValue[4] == 1 and passwordValue[5] == 1 and passwordValue[6] == 1 then
+			shouldContinueSearching = false
+		end
+	end
+end
+
+print("Finished generating a movie with a 111 111 electric tile puzzle!")
