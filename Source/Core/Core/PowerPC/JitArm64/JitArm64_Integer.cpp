dolphin-emu/dolphin

commit 592ba31e224c062fe558dad21d2f605196df0b31

JitArm64: cmp - Optimize a == 0 case
By explicitly handling this, we can avoid materializing zero in a
register and generate more efficient code altogether.

Before:
0x52800016   mov    w22, #0x0
0xb94093b5   ldr    w21, [x29, #0x90]
0x93407ed7   sxtw   x23, w22
0x93407eb9   sxtw   x25, w21
0xcb1902f9   sub    x25, x23, x25

After:
0xb94093b7   ldr    w23, [x29, #0x90]
0x4b1703f9   neg    w25, w23
0x93407f39   sxtw   x25, w25

Source/Core/Core/PowerPC/JitArm64/JitArm64_Integer.cpp
@@ -579,6 +579,11 @@ void JitArm64::cmp(UGeckoInstruction inst)
     s64 B = static_cast<s32>(gpr.GetImm(b));
     MOVI2R(CR, A - B);
   }
+  else if (gpr.IsImm(a) && !gpr.GetImm(a))
+  {
+    NEG(EncodeRegTo32(CR), gpr.R(b));
+    SXTW(CR, EncodeRegTo32(CR));
+  }
   else if (gpr.IsImm(b) && !gpr.GetImm(b))
   {
     SXTW(CR, gpr.R(a));

commit 0049a767751807c124f099a7ba7f9a3add7c18f7

JitArm64: Fix special cases of cmp
This fixes a regression from 592ba31. When `a` was a constant 0 and `b`
was a non-constant 0x80000000, the 32-bit negation operation would
overflow, causing an incorrect result. The sign extension needs to happen
before the negation to avoid overflow.

Note that I can't merge the SXTW and NEG into one instruction.
NEG is an alias for SUB with the first operand being set to ZR,
but "SUB (extended register)" treats register 31 as SP instead of ZR.

I've also changed the order for the case where `a` is a constant
0xFFFFFFFF. I don't think the order actually affects correctness here,
but let's use the same order for all the cases since it makes the code
easier to reason about.

Source/Core/Core/PowerPC/JitArm64/JitArm64_Integer.cpp
@@ -584,13 +584,13 @@ void JitArm64::cmp(UGeckoInstruction inst)
   }
   else if (gpr.IsImm(a) && !gpr.GetImm(a))
   {
-    NEG(EncodeRegTo32(CR), gpr.R(b));
-    SXTW(CR, EncodeRegTo32(CR));
+    SXTW(CR, gpr.R(b));
+    NEG(CR, CR);
   }
   else if (gpr.IsImm(a) && gpr.GetImm(a) == 0xFFFFFFFF)
   {
-    MVN(EncodeRegTo32(CR), gpr.R(b));
-    SXTW(CR, EncodeRegTo32(CR));
+    SXTW(CR, gpr.R(b));
+    MVN(CR, CR);
   }
   else if (gpr.IsImm(b) && !gpr.GetImm(b))
   {