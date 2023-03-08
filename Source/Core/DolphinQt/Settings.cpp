ec15ff1b72be23c3112069d4244f8868464453df
LillyJadeKatrin <lilly.kitty.1988@gmail.com>
2023-03-08 07:10:32 -0500

    Hardcore Mode in Dolphin Qt Settings
    Added set/get on Hardcore Mode in the Dolphin Qt Settings object

--- Source/Core/DolphinQt/Settings.cpp
+++ Source/Core/DolphinQt/Settings.cpp
@@ -29,6 +29,7 @@
 #include "Common/FileUtil.h"
 #include "Common/StringUtil.h"
 
+#include "Core/Config/AchievementSettings.h"
 #include "Core/Config/GraphicsSettings.h"
 #include "Core/Config/MainSettings.h"
 #include "Core/ConfigManager.h"
@@ -457,6 +458,20 @@ void Settings::SetCheatsEnabled(bool enabled)
     Config::SetBaseOrCurrent(Config::MAIN_ENABLE_CHEATS, enabled);
     emit EnableCheatsChanged(enabled);
   }
+}
+
+void Settings::SetHardcoreModeEnabled(bool enabled)
+{
+  if (IsHardcoreModeEnabled() != enabled)
+  {
+    Config::SetBaseOrCurrent(Config::RA_HARDCORE_ENABLED, enabled);
+    emit HardcoreModeToggled(enabled);
+  }
+}
+
+bool Settings::IsHardcoreModeEnabled() const
+{
+  return Config::Get(Config::RA_HARDCORE_ENABLED);
 }
 
 void Settings::SetDebugModeEnabled(bool enabled)
