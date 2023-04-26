f4751b1547423ee1e83d7b15955c4ab25d4c3c80
Dentomologist <dentomologist@gmail.com>
2023-04-26 16:44:33 -0700

    Qt/GraphicsWindow: Remove unnecessary member variables
    These variables are only used in CreateMainLayout and can thus be local
    instead.

--- Source/Core/DolphinQt/Config/Graphics/GraphicsWindow.h
+++ Source/Core/DolphinQt/Config/Graphics/GraphicsWindow.h
@@ -34,20 +34,5 @@ private:
   void CreateMainLayout();
   void OnBackendChanged(const QString& backend);
 
-  QTabWidget* m_tab_widget;
-  QDialogButtonBox* m_button_box;
-
-  AdvancedWidget* m_advanced_widget;
-  EnhancementsWidget* m_enhancements_widget;
-  HacksWidget* m_hacks_widget;
-  GeneralWidget* m_general_widget;
-  SoftwareRendererWidget* m_software_renderer;
-
-  QWidget* m_wrapped_advanced;
-  QWidget* m_wrapped_enhancements;
-  QWidget* m_wrapped_hacks;
-  QWidget* m_wrapped_general;
-  QWidget* m_wrapped_software;
-
   X11Utils::XRRConfiguration* m_xrr_config;
 };
