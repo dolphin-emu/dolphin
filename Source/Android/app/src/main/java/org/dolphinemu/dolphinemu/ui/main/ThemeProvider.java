ecd62b221113edcc790fcf05155c6c49087c35bd
Charles Lombardo <clombardo169@gmail.com>
2023-06-03 20:40:26 -0400

    Android: Convert ThemeProvider to Kotlin

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/ui/main/ThemeProvider.java
+++
@@ -1,9 +0,0 @@
-package org.dolphinemu.dolphinemu.ui.main;
-
-public interface ThemeProvider
-{
-  /**
-   * Provides theme ID by overriding an activity's 'setTheme' method and returning that result
-   */
-  int getThemeId();
-}
