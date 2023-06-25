694948db5d6f25b4d6ba84938d86adc49e5c1b7e
Lobsterzelda <tersaderwaslink@gmail.com>
2023-06-25 03:16:48 -0400

    Fixed formatting issue

--- Source/Core/Core/Scripting/HelperClasses/ModuleLists_API_Implementations.h
+++ Source/Core/Core/Scripting/HelperClasses/ModuleLists_API_Implementations.h
@@ -1,9 +1,9 @@
 
 namespace Scripting
 {
-  const void* GetListOfDefaultModules_impl();
-  const void* GetListOfNonDefaultModules_impl();
-  unsigned long long GetSizeOfList_impl(const void*);
-  const char* GetElementAtListIndex_impl(const void*, unsigned long long);
-  const char* GetImportModuleName_impl();
-  }
+const void* GetListOfDefaultModules_impl();
+const void* GetListOfNonDefaultModules_impl();
+unsigned long long GetSizeOfList_impl(const void*);
+const char* GetElementAtListIndex_impl(const void*, unsigned long long);
+const char* GetImportModuleName_impl();
+}  // namespace Scripting
