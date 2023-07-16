d7e79683cc4164d7246ef4974cf35f01b439d175
Mateus B. Cassiano <mbc07@live.com>
2023-07-16 16:48:08 -0400

    Qt: Color Correction window fixes

--- Source/Core/DolphinQt/Config/Graphics/ColorCorrectionConfigWindow.h
+++ Source/Core/DolphinQt/Config/Graphics/ColorCorrectionConfigWindow.h
@@ -1,16 +1,17 @@
-// Copyright 2018 Dolphin Emulator Project
+// Copyright 2023 Dolphin Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
 #include <QDialog>
 
-class QWidget;
-class QLabel;
 class ConfigBool;
 class ConfigChoice;
 class ConfigFloatSlider;
 class QDialogButtonBox;
+class QLabel;
+class QRadioButton;
+class QWidget;
 
 class ColorCorrectionConfigWindow final : public QDialog
 {
@@ -27,7 +28,8 @@ private:
   ConfigFloatSlider* m_game_gamma;
   QLabel* m_game_gamma_value;
   ConfigBool* m_correct_gamma;
-  ConfigBool* m_sdr_display_gamma_srgb;
+  QRadioButton* m_sdr_display_target_srgb;
+  QRadioButton* m_sdr_display_target_custom;
   ConfigFloatSlider* m_sdr_display_custom_gamma;
   QLabel* m_sdr_display_custom_gamma_value;
   ConfigFloatSlider* m_hdr_paper_white_nits;
