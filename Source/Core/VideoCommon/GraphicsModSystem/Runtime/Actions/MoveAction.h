8a8985b50b8f8fce9099894ce28f4d415e607b92
iwubcode <iwubcode@users.noreply.github.com>
2023-07-25 18:02:43 -0500

    VideoCommon: update graphics mod action interface with details needed for editor

--- Source/Core/VideoCommon/GraphicsModSystem/Runtime/Actions/MoveAction.h
+++ Source/Core/VideoCommon/GraphicsModSystem/Runtime/Actions/MoveAction.h
@@ -15,7 +15,13 @@ class MoveAction final : public GraphicsModAction
 public:
   static constexpr std::string_view factory_name = "move";
   static std::unique_ptr<MoveAction> Create(const picojson::value& json_data);
+  static std::unique_ptr<MoveAction> Create();
+
+  MoveAction() = default;
   explicit MoveAction(Common::Vec3 position_offset);
+
+  void DrawImGui() override;
+
   void OnProjection(GraphicsModActionData::Projection* projection) override;
   void OnProjectionAndTexture(GraphicsModActionData::Projection* projection) override;
 
