LillyJadeKatrin/dolphin-retroachievements

Added AchievementProgressWidget to AchievementsWindow
This widget is a tab in the AchievementsWindow that displays the player's current achievement progress: which achievements are locked or unlocked, and the progress of achievements that have progress metrics.

Source/Core/DolphinQt/Achievements/AchievementProgressWidget.cpp
@@ -1,0 +1,125 @@
+// Copyright 2023 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifdef USE_RETRO_ACHIEVEMENTS
+#include "DolphinQt/Achievements/AchievementProgressWidget.h"
+
+#include <QCheckBox>
+#include <QGroupBox>
+#include <QLabel>
+#include <QLineEdit>
+#include <QProgressBar>
+#include <QPushButton>
+#include <QString>
+#include <QVBoxLayout>
+
+#include <fmt/format.h>
+
+#include <rcheevos/include/rc_api_runtime.h>
+#include <rcheevos/include/rc_api_user.h>
+#include <rcheevos/include/rc_runtime.h>
+
+#include "Core/AchievementManager.h"
+#include "Core/Config/AchievementSettings.h"
+#include "Core/Config/MainSettings.h"
+#include "Core/Core.h"
+
+#include "DolphinQt/Config/ControllerInterface/ControllerInterfaceWindow.h"
+#include "DolphinQt/QtUtils/ModalMessageBox.h"
+#include "DolphinQt/QtUtils/NonDefaultQPushButton.h"
+#include "DolphinQt/QtUtils/SignalBlocking.h"
+#include "DolphinQt/Settings.h"
+
+AchievementProgressWidget::AchievementProgressWidget(QWidget* parent) : QWidget(parent)
+{
+  m_common_box = new QGroupBox();
+  m_common_layout = new QVBoxLayout();
+
+  UpdateData();
+
+  m_common_box->setLayout(m_common_layout);
+
+  auto* layout = new QVBoxLayout;
+  layout->setContentsMargins(0, 0, 0, 0);
+  layout->setAlignment(Qt::AlignTop);
+  layout->addWidget(m_common_box);
+  setLayout(layout);
+}
+
+QGroupBox*
+AchievementProgressWidget::CreateAchievementBox(const rc_api_achievement_definition_t* achievement)
+{
+  QLabel* a_title =
+      new QLabel(QString::fromLocal8Bit(achievement->title, strlen(achievement->title)));
+  QLabel* a_description = new QLabel(
+      QString::fromLocal8Bit(achievement->description, strlen(achievement->description)));
+  QLabel* a_points = new QLabel(tr("%1 points").arg(achievement->points));
+  QLabel* a_status = new QLabel(GetStatusString(achievement->id));
+  QProgressBar* a_progress_bar = new QProgressBar();
+  unsigned int value = 0;
+  unsigned int target = 0;
+  AchievementManager::GetInstance()->GetAchievementProgress(achievement->id, &value, &target);
+  if (target > 0)
+  {
+    a_progress_bar->setRange(0, target);
+    a_progress_bar->setValue(value);
+  }
+  else
+  {
+    a_progress_bar->setVisible(false);
+  }
+
+  QVBoxLayout* a_col_right = new QVBoxLayout();
+  a_col_right->addWidget(a_title);
+  a_col_right->addWidget(a_description);
+  a_col_right->addWidget(a_points);
+  a_col_right->addWidget(a_status);
+  a_col_right->addWidget(a_progress_bar);
+  QHBoxLayout* a_total = new QHBoxLayout();
+  // TODO lillyjade 02 Jun 2023 - achievement badge goes here
+  a_total->addLayout(a_col_right);
+  QGroupBox* a_group_box = new QGroupBox();
+  a_group_box->setLayout(a_total);
+  return a_group_box;
+}
+
+void AchievementProgressWidget::UpdateData()
+{
+  QLayoutItem* item;
+  while ((item = m_common_layout->layout()->takeAt(0)) != nullptr)
+  {
+    delete item->widget();
+    delete item;
+  }
+
+  const auto* game_data = AchievementManager::GetInstance()->GetGameData();
+  for (u32 ix = 0; ix < game_data->num_achievements; ix++)
+  {
+    m_common_layout->addWidget(CreateAchievementBox(game_data->achievements + ix));
+  }
+}
+
+QString AchievementProgressWidget::GetStatusString(u32 achievement_id) const
+{
+  const auto unlock_status = AchievementManager::GetInstance()->GetUnlockStatus(achievement_id);
+  if (unlock_status.session_unlock_count > 0)
+  {
+    if (Config::Get(Config::RA_ENCORE_ENABLED))
+    {
+      return tr("Unlocked %1 times this session").arg(unlock_status.session_unlock_count);
+    }
+    return tr("Unlocked this session");
+  }
+  switch (unlock_status.remote_unlock_status)
+  {
+  case AchievementManager::UnlockStatus::UnlockType::LOCKED:
+    return tr("Locked");
+  case AchievementManager::UnlockStatus::UnlockType::SOFTCORE:
+    return tr("Unlocked (Casual)");
+  case AchievementManager::UnlockStatus::UnlockType::HARDCORE:
+    return tr("Unlocked");
+  }
+  return {};
+}
+
+#endif  // USE_RETRO_ACHIEVEMENTS

Source/Core/DolphinQt/Achievements/AchievementsWindow.cpp
@@ -9,6 +9,7 @@
 #include <QVBoxLayout>
 
 #include "DolphinQt/Achievements/AchievementHeaderWidget.h"
+#include "DolphinQt/Achievements/AchievementProgressWidget.h"
 #include "DolphinQt/Achievements/AchievementSettingsWidget.h"
 #include "DolphinQt/QtUtils/WrapInScrollArea.h"
 
@@ -33,9 +34,12 @@ void AchievementsWindow::CreateMainLayout()
 
   m_header_widget = new AchievementHeaderWidget(this);
   m_tab_widget = new QTabWidget();
+  m_progress_widget = new AchievementProgressWidget(m_tab_widget);
   m_tab_widget->addTab(
       GetWrappedWidget(new AchievementSettingsWidget(m_tab_widget, this), this, 125, 100),
       tr("Settings"));
+  m_tab_widget->addTab(GetWrappedWidget(m_progress_widget, this, 125, 100), tr("Progress"));
+  m_tab_widget->setTabEnabled(1, AchievementManager::GetInstance()->IsGameLoaded());
 
   m_button_box = new QDialogButtonBox(QDialogButtonBox::Close);
 
@@ -55,6 +59,9 @@ void AchievementsWindow::UpdateData()
 {
   m_header_widget->UpdateData();
   m_header_widget->setVisible(AchievementManager::GetInstance()->IsLoggedIn());
+  // Settings tab handles its own updates ... indeed, that calls this
+  m_progress_widget->UpdateData();
+  m_tab_widget->setTabEnabled(1, AchievementManager::GetInstance()->IsGameLoaded());
   update();
 }
 

dolphin-emu/dolphin

Source/Core/DolphinQt/Achievements/AchievementProgressWidget.cpp
@@ -1,0 +1,124 @@
+// Copyright 2023 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifdef USE_RETRO_ACHIEVEMENTS
+#include "DolphinQt/Achievements/AchievementProgressWidget.h"
+
+#include <QCheckBox>
+#include <QGroupBox>
+#include <QLabel>
+#include <QLineEdit>
+#include <QProgressBar>
+#include <QPushButton>
+#include <QString>
+#include <QVBoxLayout>
+
+#include <fmt/format.h>
+
+#include <rcheevos/include/rc_api_runtime.h>
+#include <rcheevos/include/rc_api_user.h>
+#include <rcheevos/include/rc_runtime.h>
+
+#include "Core/AchievementManager.h"
+#include "Core/Config/AchievementSettings.h"
+#include "Core/Config/MainSettings.h"
+#include "Core/Core.h"
+
+#include "DolphinQt/Config/ControllerInterface/ControllerInterfaceWindow.h"
+#include "DolphinQt/QtUtils/ModalMessageBox.h"
+#include "DolphinQt/QtUtils/NonDefaultQPushButton.h"
+#include "DolphinQt/QtUtils/SignalBlocking.h"
+#include "DolphinQt/Settings.h"
+
+AchievementProgressWidget::AchievementProgressWidget(QWidget* parent) : QWidget(parent)
+{
+  m_common_box = new QGroupBox();
+  m_common_layout = new QVBoxLayout();
+
+  UpdateData();
+
+  m_common_box->setLayout(m_common_layout);
+
+  auto* layout = new QVBoxLayout;
+  layout->setContentsMargins(0, 0, 0, 0);
+  layout->setAlignment(Qt::AlignTop);
+  layout->addWidget(m_common_box);
+  setLayout(layout);
+}
+
+QGroupBox*
+AchievementProgressWidget::CreateAchievementBox(const rc_api_achievement_definition_t* achievement)
+{
+  QLabel* a_title = new QLabel(QString::fromUtf8(achievement->title, strlen(achievement->title)));
+  QLabel* a_description =
+      new QLabel(QString::fromUtf8(achievement->description, strlen(achievement->description)));
+  QLabel* a_points = new QLabel(tr("%1 points").arg(achievement->points));
+  QLabel* a_status = new QLabel(GetStatusString(achievement->id));
+  QProgressBar* a_progress_bar = new QProgressBar();
+  unsigned int value = 0;
+  unsigned int target = 0;
+  AchievementManager::GetInstance()->GetAchievementProgress(achievement->id, &value, &target);
+  if (target > 0)
+  {
+    a_progress_bar->setRange(0, target);
+    a_progress_bar->setValue(value);
+  }
+  else
+  {
+    a_progress_bar->setVisible(false);
+  }
+
+  QVBoxLayout* a_col_right = new QVBoxLayout();
+  a_col_right->addWidget(a_title);
+  a_col_right->addWidget(a_description);
+  a_col_right->addWidget(a_points);
+  a_col_right->addWidget(a_status);
+  a_col_right->addWidget(a_progress_bar);
+  QHBoxLayout* a_total = new QHBoxLayout();
+  // TODO: achievement badge goes here
+  a_total->addLayout(a_col_right);
+  QGroupBox* a_group_box = new QGroupBox();
+  a_group_box->setLayout(a_total);
+  return a_group_box;
+}
+
+void AchievementProgressWidget::UpdateData()
+{
+  QLayoutItem* item;
+  while ((item = m_common_layout->layout()->takeAt(0)) != nullptr)
+  {
+    delete item->widget();
+    delete item;
+  }
+
+  const auto* game_data = AchievementManager::GetInstance()->GetGameData();
+  for (u32 ix = 0; ix < game_data->num_achievements; ix++)
+  {
+    m_common_layout->addWidget(CreateAchievementBox(game_data->achievements + ix));
+  }
+}
+
+QString AchievementProgressWidget::GetStatusString(u32 achievement_id) const
+{
+  const auto unlock_status = AchievementManager::GetInstance()->GetUnlockStatus(achievement_id);
+  if (unlock_status.session_unlock_count > 0)
+  {
+    if (Config::Get(Config::RA_ENCORE_ENABLED))
+    {
+      return tr("Unlocked %1 times this session").arg(unlock_status.session_unlock_count);
+    }
+    return tr("Unlocked this session");
+  }
+  switch (unlock_status.remote_unlock_status)
+  {
+  case AchievementManager::UnlockStatus::UnlockType::LOCKED:
+    return tr("Locked");
+  case AchievementManager::UnlockStatus::UnlockType::SOFTCORE:
+    return tr("Unlocked (Casual)");
+  case AchievementManager::UnlockStatus::UnlockType::HARDCORE:
+    return tr("Unlocked");
+  }
+  return {};
+}
+
+#endif  // USE_RETRO_ACHIEVEMENTS

Source/Core/DolphinQt/Achievements/AchievementsWindow.cpp
@@ -9,6 +9,7 @@
 #include <QVBoxLayout>
 
 #include "DolphinQt/Achievements/AchievementHeaderWidget.h"
+#include "DolphinQt/Achievements/AchievementProgressWidget.h"
 #include "DolphinQt/Achievements/AchievementSettingsWidget.h"
 #include "DolphinQt/QtUtils/WrapInScrollArea.h"
 
@@ -33,9 +34,12 @@ void AchievementsWindow::CreateMainLayout()
 
   m_header_widget = new AchievementHeaderWidget(this);
   m_tab_widget = new QTabWidget();
+  m_progress_widget = new AchievementProgressWidget(m_tab_widget);
   m_tab_widget->addTab(
       GetWrappedWidget(new AchievementSettingsWidget(m_tab_widget, this), this, 125, 100),
       tr("Settings"));
+  m_tab_widget->addTab(GetWrappedWidget(m_progress_widget, this, 125, 100), tr("Progress"));
+  m_tab_widget->setTabVisible(1, AchievementManager::GetInstance()->IsGameLoaded());
 
   m_button_box = new QDialogButtonBox(QDialogButtonBox::Close);
 
@@ -55,6 +59,9 @@ void AchievementsWindow::UpdateData()
 {
   m_header_widget->UpdateData();
   m_header_widget->setVisible(AchievementManager::GetInstance()->IsLoggedIn());
+  // Settings tab handles its own updates ... indeed, that calls this
+  m_progress_widget->UpdateData();
+  m_tab_widget->setTabVisible(1, AchievementManager::GetInstance()->IsGameLoaded());
   update();
 }
 