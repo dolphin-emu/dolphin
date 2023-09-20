7e201a68ea3d9c6c5e9b131a8b8aefaaaac5b606
Nikhil Narayana <nikhil.narayana@live.com>
2023-09-20 16:09:29 -0700

    build(mac): don't copy codes around and skip code signing

--- build-mac.sh
+++ build-mac.sh
@@ -2,12 +2,7 @@
 # build-mac.sh
 
 QT_BREW_PATH=$(brew --prefix qt@6)
-CMAKE_FLAGS="-DQT_DIR=${QT_BREW_PATH}/lib/cmake/Qt5 -DENABLE_NOGUI=false"
-
-PLAYBACK_CODES_PATH="./Data/PlaybackGeckoCodes/"
-
-DATA_SYS_PATH="./Data/Sys/"
-BINARY_PATH="./build/Binaries/Slippi_Dolphin.app/Contents/Resources/"
+CMAKE_FLAGS="-DQT_DIR=${QT_BREW_PATH}/lib/cmake/Qt6 -DENABLE_NOGUI=false"
 
 export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib:/usr/lib/
 
@@ -20,30 +15,9 @@ else
         CMAKE_FLAGS+=" -DSLIPPI_PLAYBACK=false"
 fi
 
-if [[ -z "${CERTIFICATE_MACOS_APPLICATION}" ]]
-    then
-        echo "Building without code signing"
-else
-        echo "Building with code signing"
-        CMAKE_FLAGS+=' -DMACOS_CODE_SIGNING="ON"'
-fi
-
 # Move into the build directory, run CMake, and compile the project
 mkdir -p build
 pushd build
 cmake ${CMAKE_FLAGS} ..
 cmake --build . --target dolphin-emu -- -j$(nproc)
 popd
-
-# Copy the Sys folder in
-echo "Copying Sys files into the bundle"
-cp -Rfn "${DATA_SYS_PATH}" "${BINARY_PATH}"
-
-# Copy playback specific codes if needed
-if [ "$1" == "playback" ]
-    then
-        # Update Sys dir with playback codes
-        echo "Copying playback gecko codes into the bundle"
-		rm -rf "${BINARY_PATH}/Sys/GameSettings" # Delete netplay codes
-		cp -r "${PLAYBACK_CODES_PATH}/." "${BINARY_PATH}/Sys/GameSettings/"
-fi
