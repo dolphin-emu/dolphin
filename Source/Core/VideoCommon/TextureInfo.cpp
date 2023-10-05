c678d9fa7154dbf9d226d90e470d9100788842ae
Venomalia <Venomalia@mailbox.org>
2023-10-05 18:05:37 +0200

    C8 Texture-Tlut-Hash remains constant when the transparency flag is used.

--- Source/Core/VideoCommon/TextureInfo.cpp
+++ Source/Core/VideoCommon/TextureInfo.cpp
@@ -135,7 +135,9 @@ TextureInfo::NameDetails TextureInfo::CalculateTextureName() const
       const u32 texture_byte = m_ptr[i];
 
       min = std::min(min, texture_byte);
-      max = std::max(max, texture_byte);
+      // The value 255 can be used as a transparency flag if the tlut is smaller than 255.
+      if (texture_byte != 255 || max == 254)
+        max = std::max(max, texture_byte);
     }
     break;
   }
