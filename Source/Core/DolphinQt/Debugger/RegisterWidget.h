Narugakuruga/dolphin_EFB

Add trace widget. Adapt CodeTrace to new methods. Expand register widget trace options to be the same as CodeView

Source/Core/DolphinQt/Debugger/RegisterWidget.h
@@ -8,6 +8,7 @@
 #include <QDockWidget>
 
 #include "Common/CommonTypes.h"
+#include "Common/Debug/CodeTrace.h"
 #include "DolphinQt/Debugger/RegisterColumn.h"
 
 class QTableWidget;
@@ -22,6 +23,7 @@ public:
   ~RegisterWidget();
 
 signals:
+  void DoAutoStep(CodeTrace::AutoStop option, std::string reg);
   void RequestTableUpdate();
   void RequestViewInCode(u32 addr);
   void RequestViewInMemory(u32 addr);

dolphin-emu/dolphin

Source/Core/DolphinQt/Debugger/RegisterWidget.h
@@ -8,6 +8,7 @@
 #include <QDockWidget>
 
 #include "Common/CommonTypes.h"
+#include "Core/Debugger/CodeTrace.h"
 #include "DolphinQt/Debugger/RegisterColumn.h"
 
 class QTableWidget;
@@ -26,6 +27,7 @@ public:
   ~RegisterWidget();
 
 signals:
+  void DoAutoStep(CodeTrace::AutoStop option, std::string reg);
   void RequestTableUpdate();
   void RequestViewInCode(u32 addr);
   void RequestViewInMemory(u32 addr);