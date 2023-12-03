975df858be901535e1e6b42dd85adff7d57c81a0
iwubcode <iwubcode@users.noreply.github.com>
2023-12-03 17:45:32 -0600

    VideoCommon: move graphics mod common types to a specific file that can be shared by editor and manager alike

---
+++ Source/Core/VideoCommon/GraphicsModSystem/Types.h
@@ -0,0 +1,22 @@
+// Copyright 2023 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+namespace GraphicsMods
+{
+enum class DrawCallID : unsigned long long
+{
+  INVALID = 0
+};
+
+enum class MeshID : unsigned long long
+{
+  INVALID = 0
+};
+
+enum class LightID : unsigned long long
+{
+  INVALID = 0
+};
+}  // namespace GraphicsMods
