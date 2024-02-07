439f14aa402982a08c8f3301d31e20ab35469dbd
Nikhil Narayana <nikhil.narayana@live.com>
2024-02-06 21:16:19 -0800

    build(macOS): don't use cmake's signing on CI

--- build-mac.sh
+++ build-mac.sh
@@ -8,11 +8,16 @@ export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib:/usr/lib/
 
 # Build type
 if [ "$1" == "playback" ]
-    then
+then
         echo "Using Playback build config"
 else
         echo "Using Netplay build config"
         CMAKE_FLAGS+=" -DSLIPPI_PLAYBACK=false"
+fi
+
+if [ "$CI" == "true" ]
+then
+        CMAKE_FLAGS+=" -DMACOS_CODE_SIGNING=OFF"
 fi
 
 # Move into the build directory, run CMake, and compile the project
