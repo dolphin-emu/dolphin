0f36654d2b11a3c5fdd173b25418da6cf260af8a
mitaclaw <140017135+mitaclaw@users.noreply.github.com>
2024-03-03 18:21:31 -0800

    PPCSymbolDB: Eliminate Redundant Member
    This can be accessed through the CPUThreadGuard.

--- Source/Core/Core/PowerPC/PPCSymbolDB.h
+++ Source/Core/Core/PowerPC/PPCSymbolDB.h
@@ -11,7 +11,6 @@
 namespace Core
 {
 class CPUThreadGuard;
-class DebugInterface;
 }  // namespace Core
 
 // This has functionality overlapping Debugger_Symbolmap. Should merge that stuff in here later.
@@ -39,9 +38,6 @@ public:
   void PrintCalls(u32 funcAddr) const;
   void PrintCallers(u32 funcAddr) const;
   void LogFunctionCall(u32 addr);
-
-private:
-  Core::DebugInterface* debugger;
 };
 
 extern PPCSymbolDB g_symbolDB;
