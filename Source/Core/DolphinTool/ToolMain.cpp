d17ed3a6b2bb0bc4ffccf3026f9ceefb87c36905
Jonas Kalsvik <janaanamal@gmail.com>
2024-03-15 20:01:48 +0100

    fix ToolMain formatting (clion at its finest)

--- Source/Core/DolphinTool/ToolMain.cpp
+++ Source/Core/DolphinTool/ToolMain.cpp
@@ -19,28 +19,29 @@
 #include "DolphinTool/HeaderCommand.h"
 #include "DolphinTool/VerifyCommand.h"
 
-static void PrintUsage() {
-  fmt::print(std::cerr,
-             "usage: dolphin-tool COMMAND -h\n"
-             "\n"
-             "commands supported: [convert, verify, header, extract]\n");
+static void PrintUsage()
+{
+  fmt::print(std::cerr, "usage: dolphin-tool COMMAND -h\n"
+                        "\n"
+                        "commands supported: [convert, verify, header, extract]\n");
 }
 
 #ifdef _WIN32
 #define main app_main
 #endif
 
-int main(const int argc, char *argv[]) {
+int main(const int argc, char* argv[])
+{
   Core::DeclareAsHostThread();
 
-  if (argc < 2) {
+  if (argc < 2)
+  {
     PrintUsage();
     return EXIT_FAILURE;
   }
 
   const std::string_view command_str = argv[1];
-  // Take off the program name and command selector before passing arguments
-  // down
+  // Take off the program name and command selector before passing arguments down
   const std::vector<std::string> args(argv + 2, argv + argc);
   if (command_str == "convert")
     return DolphinTool::ConvertCommand(args);
@@ -55,11 +56,11 @@ int main(const int argc, char *argv[]) {
 }
 
 #ifdef _WIN32
-int wmain(int, wchar_t *[], wchar_t *[]) {
-  std::vector<std::string> args =
-      Common::CommandLineToUtf8Argv(GetCommandLineW());
+int wmain(int, wchar_t*[], wchar_t*[])
+{
+  std::vector<std::string> args = Common::CommandLineToUtf8Argv(GetCommandLineW());
   const int argc = static_cast<int>(args.size());
-  std::vector<char *> argv(args.size());
+  std::vector<char*> argv(args.size());
   for (size_t i = 0; i < args.size(); ++i)
     argv[i] = args[i].data();
 
