8a527c89b23eb7ed379eb0a9dfbe74e4f259d594
iwubcode <iwubcode@users.noreply.github.com>
2024-04-09 22:31:37 -0500

    graphics mod action (get/set id)

--- Source/Core/VideoCommon/GraphicsModSystem/Runtime/GraphicsModAction.h
+++ Source/Core/VideoCommon/GraphicsModSystem/Runtime/GraphicsModAction.h
@@ -27,7 +27,13 @@ public:
   virtual void OnLight(GraphicsModActionData::Light*) {}
   virtual void OnFrameEnd() {}
 
+  void SetID(u64 id) { m_id = id; }
+  u64 GetID() const { return m_id; }
+
   virtual void DrawImGui() {}
   virtual void SerializeToConfig(picojson::object* obj) {}
   virtual std::string GetFactoryName() const { return ""; }
+
+private:
+  u64 m_id = 0;
 };
