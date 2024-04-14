59d7595aaba70f669b05d40601e019160fd1c211
iwubcode <iwubcode@users.noreply.github.com>
2024-04-14 15:07:41 -0500

    Android: update UI for graphics mods 2.0

--- Source/Android/jni/Cheats/GraphicsModGroup.cpp
+++ Source/Android/jni/Cheats/GraphicsModGroup.cpp
@@ -3,21 +3,21 @@
 
 #include <jni.h>
 
-#include <set>
 #include <vector>
 
-#include "VideoCommon/GraphicsModSystem/Config/GraphicsMod.h"
 #include "VideoCommon/GraphicsModSystem/Config/GraphicsModGroup.h"
 #include "jni/AndroidCommon/AndroidCommon.h"
 #include "jni/AndroidCommon/IDCache.h"
 
-static GraphicsModGroupConfig* GetPointer(JNIEnv* env, jobject obj)
+static GraphicsModSystem::Config::GraphicsModGroup* GetPointer(JNIEnv* env, jobject obj)
 {
-  return reinterpret_cast<GraphicsModGroupConfig*>(
+  return reinterpret_cast<GraphicsModSystem::Config::GraphicsModGroup*>(
       env->GetLongField(obj, IDCache::GetGraphicsModGroupPointer()));
 }
 
-jobject GraphicsModToJava(JNIEnv* env, GraphicsModConfig* mod, jobject jGraphicsModGroup)
+jobject GraphicsModToJava(JNIEnv* env,
+                          GraphicsModSystem::Config::GraphicsModGroup::GraphicsModWithMetadata* mod,
+                          jobject jGraphicsModGroup)
 {
   return env->NewObject(IDCache::GetGraphicsModClass(), IDCache::GetGraphicsModConstructor(),
                         reinterpret_cast<jlong>(mod), jGraphicsModGroup);
@@ -36,35 +36,23 @@ JNIEXPORT jobjectArray JNICALL
 Java_org_dolphinemu_dolphinemu_features_cheats_model_GraphicsModGroup_getMods(JNIEnv* env,
                                                                               jobject obj)
 {
-  GraphicsModGroupConfig* mod_group = GetPointer(env, obj);
+  GraphicsModSystem::Config::GraphicsModGroup* mod_group = GetPointer(env, obj);
+  std::vector<GraphicsModSystem::Config::GraphicsModGroup::GraphicsModWithMetadata*> mods;
 
-  std::set<std::string> groups;
-
-  for (const GraphicsModConfig& mod : mod_group->GetMods())
+  for (auto& mod : mod_group->GetMods())
   {
-    for (const GraphicsTargetGroupConfig& group : mod.m_groups)
-      groups.insert(group.m_name);
-  }
-
-  std::vector<GraphicsModConfig*> mods;
-
-  for (GraphicsModConfig& mod : mod_group->GetMods())
-  {
-    // If no group matches the mod's features, or if the mod has no features, skip it
-    if (std::none_of(mod.m_features.begin(), mod.m_features.end(),
-                     [&groups](const GraphicsModFeatureConfig& feature) {
-                       return groups.count(feature.m_group) == 1;
-                     }))
-    {
+    if (mod.m_mod.m_actions.empty())
       continue;
-    }
 
     mods.push_back(&mod);
   }
 
   return VectorToJObjectArray(
       env, mods, IDCache::GetGraphicsModClass(),
-      [obj](JNIEnv* env, GraphicsModConfig* mod) { return GraphicsModToJava(env, mod, obj); });
+      [obj](JNIEnv* env,
+            GraphicsModSystem::Config::GraphicsModGroup::GraphicsModWithMetadata* mod) {
+        return GraphicsModToJava(env, mod, obj);
+      });
 }
 
 JNIEXPORT void JNICALL
@@ -77,7 +65,7 @@ JNIEXPORT jobject JNICALL
 Java_org_dolphinemu_dolphinemu_features_cheats_model_GraphicsModGroup_load(JNIEnv* env, jclass,
                                                                            jstring jGameId)
 {
-  auto* mod_group = new GraphicsModGroupConfig(GetJString(env, jGameId));
+  auto* mod_group = new GraphicsModSystem::Config::GraphicsModGroup(GetJString(env, jGameId));
 
   mod_group->Load();
 
