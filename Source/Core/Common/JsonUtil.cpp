010c56c546af228afd5f887b6706986a1170bd27
iwubcode <iwubcode@users.noreply.github.com>
2024-05-31 22:36:12 -0500

    Common: add file serialization/deserialization functions for JSON

--- Source/Core/Common/JsonUtil.cpp
+++ Source/Core/Common/JsonUtil.cpp
@@ -3,6 +3,10 @@
 
 #include "Common/JsonUtil.h"
 
+#include <fstream>
+
+#include "Common/FileUtil.h"
+
 picojson::object ToJsonObject(const Common::Vec3& vec)
 {
   picojson::object obj;
@@ -37,4 +41,28 @@ std::optional<bool> ReadBoolFromJson(const picojson::object& obj, const std::str
   if (!it->second.is<bool>())
     return std::nullopt;
   return it->second.get<bool>();
+}
+
+bool JsonToFile(const std::string& filename, const picojson::value& root, bool prettify)
+{
+  std::ofstream json_stream;
+  File::OpenFStream(json_stream, filename, std::ios_base::out);
+  if (!json_stream.is_open())
+  {
+    return false;
+  }
+  json_stream << root.serialize(prettify);
+  return true;
+}
+
+bool JsonFromFile(const std::string& filename, picojson::value* root, std::string* error)
+{
+  std::string json_data;
+  if (!File::ReadFileToString(filename, json_data))
+  {
+    return false;
+  }
+
+  *error = picojson::parse(*root, json_data);
+  return error->empty();
 }
