68de42074071e581e1912e782fc2cc819b98c89d
iwubcode <iwubcode@users.noreply.github.com>
2024-06-30 01:14:05 -0500

    custom pipeline action...

--- Source/Core/VideoCommon/GraphicsModSystem/Runtime/Actions/CustomPipelineAction.h
+++ Source/Core/VideoCommon/GraphicsModSystem/Runtime/Actions/CustomPipelineAction.h
@@ -6,22 +6,15 @@
 #include <memory>
 #include <string>
 #include <string_view>
-#include <vector>
 
 #include <picojson.h>
 
 #include "VideoCommon/Assets/CustomAssetLibrary.h"
-#include "VideoCommon/GraphicsModSystem/Runtime/CustomPipeline.h"
 #include "VideoCommon/GraphicsModSystem/Runtime/GraphicsModAction.h"
 
 class CustomPipelineAction final : public GraphicsModAction
 {
 public:
-  struct PipelinePassPassDescription
-  {
-    std::string m_pixel_material_asset;
-  };
-
   static constexpr std::string_view factory_name = "custom_pipeline";
   static std::unique_ptr<CustomPipelineAction>
   Create(const picojson::value& json_data,
@@ -30,7 +23,7 @@ public:
   Create(std::shared_ptr<VideoCommon::CustomAssetLibrary> library);
   explicit CustomPipelineAction(std::shared_ptr<VideoCommon::CustomAssetLibrary> library);
   CustomPipelineAction(std::shared_ptr<VideoCommon::CustomAssetLibrary> library,
-                       std::vector<PipelinePassPassDescription> pass_descriptions);
+                       std::string material_asset);
   void OnDrawStarted(GraphicsModActionData::DrawStarted*) override;
 
   void DrawImGui() override;
@@ -39,6 +32,5 @@ public:
 
 private:
   std::shared_ptr<VideoCommon::CustomAssetLibrary> m_library;
-  std::vector<PipelinePassPassDescription> m_passes_config;
-  std::vector<CustomPipeline> m_pipeline_passes;
+  std::string m_material_asset;
 };
