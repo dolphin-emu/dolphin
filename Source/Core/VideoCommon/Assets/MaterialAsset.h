bc429206786fcef33fd5088dbb642b7afbe113f1
iwubcode <iwubcode@users.noreply.github.com>
2024-07-21 17:42:16 -0500

    Rework material asset for new material system

--- Source/Core/VideoCommon/Assets/MaterialAsset.h
+++ Source/Core/VideoCommon/Assets/MaterialAsset.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <array>
+#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
@@ -13,6 +14,9 @@
 #include "Common/CommonTypes.h"
 #include "Common/EnumFormatter.h"
 #include "VideoCommon/Assets/CustomAsset.h"
+#include "VideoCommon/Assets/TextureSamplerValue.h"
+#include "VideoCommon/BPMemory.h"
+#include "VideoCommon/RenderState.h"
 
 class ShaderCode;
 
@@ -23,10 +27,20 @@ struct MaterialProperty
   static void WriteToMemory(u8*& buffer, const MaterialProperty& property);
   static std::size_t GetMemorySize(const MaterialProperty& property);
   static void WriteAsShaderCode(ShaderCode& shader_source, const MaterialProperty& property);
-  using Value = std::variant<CustomAssetLibrary::AssetID, s32, std::array<s32, 2>,
-                             std::array<s32, 3>, std::array<s32, 4>, float, std::array<float, 2>,
-                             std::array<float, 3>, std::array<float, 4>, bool>;
+  using Value = std::variant<TextureSamplerValue, s32, std::array<s32, 2>, std::array<s32, 3>,
+                             std::array<s32, 4>, float, std::array<float, 2>, std::array<float, 3>,
+                             std::array<float, 4>, bool>;
   std::string m_code_name;
+  Value m_value;
+};
+
+struct MaterialProperty2
+{
+  static void WriteToMemory(u8*& buffer, const MaterialProperty2& property);
+  static std::size_t GetMemorySize(const MaterialProperty2& property);
+  using Value =
+      std::variant<s32, std::array<s32, 2>, std::array<s32, 3>, std::array<s32, 4>, float,
+                   std::array<float, 2>, std::array<float, 3>, std::array<float, 4>, bool>;
   Value m_value;
 };
 
@@ -44,6 +58,32 @@ struct MaterialData
 // with a shader asset that is provided by name.  It is up to user of this asset to
 // use the two together to create the relevant runtime data
 class MaterialAsset final : public CustomLoadableAsset<MaterialData>
+{
+public:
+  using CustomLoadableAsset::CustomLoadableAsset;
+
+private:
+  CustomAssetLibrary::LoadInfo LoadImpl(const CustomAssetLibrary::AssetID& asset_id) override;
+};
+
+struct RasterMaterialData
+{
+  static bool FromJson(const CustomAssetLibrary::AssetID& asset_id, const picojson::object& json,
+                       RasterMaterialData* data);
+  static void ToJson(picojson::object* obj, const RasterMaterialData& data);
+  std::string shader_asset;
+  std::string next_material_asset;
+  std::vector<MaterialProperty2> vertex_properties;
+  std::vector<MaterialProperty2> pixel_properties;
+
+  std::optional<CullMode> cull_mode;
+  std::optional<DepthState> depth_state;
+  std::optional<BlendingState> blending_state;
+
+  std::vector<TextureSamplerValue> pixel_textures;
+};
+
+class RasterMaterialAsset final : public CustomLoadableAsset<RasterMaterialData>
 {
 public:
   using CustomLoadableAsset::CustomLoadableAsset;
