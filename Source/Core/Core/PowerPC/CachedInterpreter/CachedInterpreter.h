Clever-ops/Dolphin--Shark-Fin-Sleek

Cached Interpreter 2.0
It now supports variable-sized data payloads and memory range freeing. It's a little faster, too.

Source/Core/Core/PowerPC/CachedInterpreter/CachedInterpreter.h
@@ -3,14 +3,27 @@
 
 #pragma once
 
-#include <vector>
+#include <cstddef>
+
+#include <rangeset/rangesizeset.h>
 
 #include "Common/CommonTypes.h"
-#include "Core/PowerPC/CachedInterpreter/InterpreterBlockCache.h"
+#include "Core/PowerPC/CachedInterpreter/CachedInterpreterBlockCache.h"
+#include "Core/PowerPC/CachedInterpreter/CachedInterpreterEmitter.h"
 #include "Core/PowerPC/JitCommon/JitBase.h"
 #include "Core/PowerPC/PPCAnalyst.h"
 
-class CachedInterpreter : public JitBase
+namespace CoreTiming
+{
+class CoreTimingManager;
+}
+namespace CPU
+{
+enum class State;
+}
+class Interpreter;
+
+class CachedInterpreter : public JitBase, public CachedInterpreterCodeBlock
 {
 public:
   explicit CachedInterpreter(Core::System& system);
@@ -30,32 +43,94 @@ public:
   void SingleStep() override;
 
   void Jit(u32 address) override;
+  void Jit(u32 address, bool clear_cache_and_retry_on_failure);
+  bool DoJit(u32 address, JitBlock* b, u32 nextPC);
 
   JitBaseBlockCache* GetBlockCache() override { return &m_block_cache; }
   const char* GetName() const override { return "Cached Interpreter"; }
   const CommonAsmRoutinesBase* GetAsmRoutines() override { return nullptr; }
 
 private:
-  struct Instruction;
-
-  u8* GetCodePtr();
   void ExecuteOneBlock();
 
   bool HandleFunctionHooking(u32 address);
 
-  static void EndBlock(CachedInterpreter& cached_interpreter, UGeckoInstruction data);
-  static void UpdateNumLoadStoreInstructions(CachedInterpreter& cached_interpreter,
-                                             UGeckoInstruction data);
-  static void UpdateNumFloatingPointInstructions(CachedInterpreter& cached_interpreter,
-                                                 UGeckoInstruction data);
-  static void WritePC(CachedInterpreter& cached_interpreter, UGeckoInstruction data);
-  static void WriteBrokenBlockNPC(CachedInterpreter& cached_interpreter, UGeckoInstruction data);
-  static bool CheckFPU(CachedInterpreter& cached_interpreter, u32 data);
-  static bool CheckDSI(CachedInterpreter& cached_interpreter, u32 data);
-  static bool CheckProgramException(CachedInterpreter& cached_interpreter, u32 data);
-  static bool CheckBreakpoint(CachedInterpreter& cached_interpreter, u32 data);
-  static bool CheckIdle(CachedInterpreter& cached_interpreter, u32 idle_pc);
+  // Finds a free memory region and sets the code emitter to point at that region.
+  // Returns false if no free memory region can be found.
+  bool SetEmitterStateToFreeCodeRegion();
 
-  BlockCache m_block_cache{*this};
-  std::vector<Instruction> m_code;
+  void FreeRanges();
+  void ResetFreeMemoryRanges();
+
+  struct EndBlockOperands;
+  struct InterpretOperands;
+  struct HLEFunctionOperands;
+  struct WritePCOperands;
+  struct ExceptionCheckOperands;
+  struct CheckBreakpointOperands;
+  struct CheckIdleOperands;
+
+  static s32 EndBlock(PowerPC::PowerPCState& ppc_state, const EndBlockOperands& operands);
+  static s32 Interpret(PowerPC::PowerPCState& ppc_state, const InterpretOperands& operands);
+  static s32 HLEFunction(PowerPC::PowerPCState& ppc_state, const HLEFunctionOperands& operands);
+  static s32 WritePC(PowerPC::PowerPCState& ppc_state, const WritePCOperands& operands);
+  static s32 WriteBrokenBlockNPC(PowerPC::PowerPCState& ppc_state, const WritePCOperands& operands);
+  static s32 CheckFPU(PowerPC::PowerPCState& ppc_state, const ExceptionCheckOperands& operands);
+  static s32 CheckDSI(PowerPC::PowerPCState& ppc_state, const ExceptionCheckOperands& operands);
+  static s32 CheckProgramException(PowerPC::PowerPCState& ppc_state,
+                                   const ExceptionCheckOperands& operands);
+  static s32 CheckBreakpoint(PowerPC::PowerPCState& ppc_state,
+                             const CheckBreakpointOperands& operands);
+  static s32 CheckIdle(PowerPC::PowerPCState& ppc_state, const CheckIdleOperands& operands);
+
+  HyoutaUtilities::RangeSizeSet<u8*> m_free_ranges;
+  CachedInterpreterBlockCache m_block_cache;
+};
+
+struct CachedInterpreter::EndBlockOperands
+{
+  u32 downcount;
+  u32 num_load_stores;
+  u32 num_fp_inst;
+  u32 : 32;
+};
+
+struct CachedInterpreter::InterpretOperands
+{
+  Interpreter& interpreter;
+  void (*func)(Interpreter&, UGeckoInstruction);  // Interpreter::Instruction
+  u32 current_pc;
+  UGeckoInstruction inst;
+};
+
+struct CachedInterpreter::HLEFunctionOperands
+{
+  Core::System& system;
+  u32 current_pc;
+  u32 hook_index;
+};
+
+struct CachedInterpreter::WritePCOperands
+{
+  u32 current_pc;
+  u32 : 32;
+};
+
+struct CachedInterpreter::ExceptionCheckOperands
+{
+  PowerPC::PowerPCManager& power_pc;
+  u32 downcount;
+};
+
+struct CachedInterpreter::CheckBreakpointOperands
+{
+  PowerPC::PowerPCManager& power_pc;
+  const CPU::State* cpu_state;
+  u32 downcount;
+};
+
+struct CachedInterpreter::CheckIdleOperands
+{
+  CoreTiming::CoreTimingManager& core_timing;
+  u32 idle_pc;
 };

dolphin-emu/dolphin

Source/Core/Core/PowerPC/CachedInterpreter/CachedInterpreter.h
@@ -3,14 +3,27 @@
 
 #pragma once
 
-#include <vector>
+#include <cstddef>
+
+#include <rangeset/rangesizeset.h>
 
 #include "Common/CommonTypes.h"
-#include "Core/PowerPC/CachedInterpreter/InterpreterBlockCache.h"
+#include "Core/PowerPC/CachedInterpreter/CachedInterpreterBlockCache.h"
+#include "Core/PowerPC/CachedInterpreter/CachedInterpreterEmitter.h"
 #include "Core/PowerPC/JitCommon/JitBase.h"
 #include "Core/PowerPC/PPCAnalyst.h"
 
-class CachedInterpreter : public JitBase
+namespace CoreTiming
+{
+class CoreTimingManager;
+}
+namespace CPU
+{
+enum class State;
+}
+class Interpreter;
+
+class CachedInterpreter : public JitBase, public CachedInterpreterCodeBlock
 {
 public:
   explicit CachedInterpreter(Core::System& system);
@@ -30,32 +43,85 @@ public:
   void SingleStep() override;
 
   void Jit(u32 address) override;
+  void Jit(u32 address, bool clear_cache_and_retry_on_failure);
+  bool DoJit(u32 address, JitBlock* b, u32 nextPC);
 
   JitBaseBlockCache* GetBlockCache() override { return &m_block_cache; }
   const char* GetName() const override { return "Cached Interpreter"; }
   const CommonAsmRoutinesBase* GetAsmRoutines() override { return nullptr; }
 
 private:
-  struct Instruction;
-
-  u8* GetCodePtr();
   void ExecuteOneBlock();
 
   bool HandleFunctionHooking(u32 address);
 
-  static void EndBlock(CachedInterpreter& cached_interpreter, UGeckoInstruction data);
-  static void UpdateNumLoadStoreInstructions(CachedInterpreter& cached_interpreter,
-                                             UGeckoInstruction data);
-  static void UpdateNumFloatingPointInstructions(CachedInterpreter& cached_interpreter,
-                                                 UGeckoInstruction data);
-  static void WritePC(CachedInterpreter& cached_interpreter, UGeckoInstruction data);
-  static void WriteBrokenBlockNPC(CachedInterpreter& cached_interpreter, UGeckoInstruction data);
-  static bool CheckFPU(CachedInterpreter& cached_interpreter, u32 data);
-  static bool CheckDSI(CachedInterpreter& cached_interpreter, u32 data);
-  static bool CheckProgramException(CachedInterpreter& cached_interpreter, u32 data);
-  static bool CheckBreakpoint(CachedInterpreter& cached_interpreter, u32 data);
-  static bool CheckIdle(CachedInterpreter& cached_interpreter, u32 idle_pc);
+  // Finds a free memory region and sets the code emitter to point at that region.
+  // Returns false if no free memory region can be found.
+  bool SetEmitterStateToFreeCodeRegion();
 
-  BlockCache m_block_cache{*this};
-  std::vector<Instruction> m_code;
+  void FreeRanges();
+  void ResetFreeMemoryRanges();
+
+  struct EndBlockOperands;
+  struct InterpretOperands;
+  struct HLEFunctionOperands;
+  struct WritePCOperands;
+  struct CheckHaltOperands;
+  struct CheckIdleOperands;
+
+  static s32 EndBlock(PowerPC::PowerPCState& ppc_state, const EndBlockOperands& operands);
+  static s32 Interpret(PowerPC::PowerPCState& ppc_state, const InterpretOperands& operands);
+  static s32 HLEFunction(PowerPC::PowerPCState& ppc_state, const HLEFunctionOperands& operands);
+  static s32 WritePC(PowerPC::PowerPCState& ppc_state, const WritePCOperands& operands);
+  static s32 WriteBrokenBlockNPC(PowerPC::PowerPCState& ppc_state, const WritePCOperands& operands);
+  static s32 CheckFPU(PowerPC::PowerPCState& ppc_state, const CheckHaltOperands& operands);
+  static s32 CheckDSI(PowerPC::PowerPCState& ppc_state, const CheckHaltOperands& operands);
+  static s32 CheckProgramException(PowerPC::PowerPCState& ppc_state,
+                                   const CheckHaltOperands& operands);
+  static s32 CheckBreakpoint(PowerPC::PowerPCState& ppc_state, const CheckHaltOperands& operands);
+  static s32 CheckIdle(PowerPC::PowerPCState& ppc_state, const CheckIdleOperands& operands);
+
+  HyoutaUtilities::RangeSizeSet<u8*> m_free_ranges;
+  CachedInterpreterBlockCache m_block_cache;
+};
+
+struct CachedInterpreter::EndBlockOperands
+{
+  u32 downcount;
+  u32 num_load_stores;
+  u32 num_fp_inst;
+  u32 : 32;
+};
+
+struct CachedInterpreter::InterpretOperands
+{
+  Interpreter& interpreter;
+  void (*func)(Interpreter&, UGeckoInstruction);  // Interpreter::Instruction
+  u32 current_pc;
+  UGeckoInstruction inst;
+};
+
+struct CachedInterpreter::HLEFunctionOperands
+{
+  Core::System& system;
+  u32 current_pc;
+  u32 hook_index;
+};
+
+struct CachedInterpreter::WritePCOperands
+{
+  u32 current_pc;
+  u32 : 32;
+};
+
+struct CachedInterpreter::CheckHaltOperands
+{
+  PowerPC::PowerPCManager& power_pc;
+  u32 downcount;
+};
+
+struct CachedInterpreter::CheckIdleOperands
+{
+  CoreTiming::CoreTimingManager& core_timing;
+  u32 idle_pc;
 };