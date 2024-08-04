4a005a76085da78e7bfcce967975cde8e048ca13
mitaclaw <140017135+mitaclaw@users.noreply.github.com>
2024-08-04 05:44:44 -0700

    Branch Watch Tool: Toolbar Visiblity Menu
    Adds the ability to hide unneeded features of the controls toolbar.

--- Source/Core/DolphinQt/Debugger/BranchWatchDialog.h
+++ Source/Core/DolphinQt/Debugger/BranchWatchDialog.h
@@ -150,9 +150,11 @@ private:
   QMenu *m_mnu_table_context_instruction, *m_mnu_table_context_condition,
       *m_mnu_table_context_origin, *m_mnu_table_context_destin_or_symbol,
       *m_mnu_table_context_other;
-  QMenu* m_mnu_column_visibility;
+  QMenu *m_mnu_column_visibility, *m_mnu_toolbar_visibility;
 
   QToolBar* m_control_toolbar;
+  QAction *m_act_branch_type_filters, *m_act_origin_destin_filters, *m_act_condition_filters,
+      *m_act_misc_controls;
   QTableView* m_table_view;
   BranchWatchProxyModel* m_table_proxy;
   BranchWatchTableModel* m_table_model;
