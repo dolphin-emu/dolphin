4719d7bcc4a90106215c3a8db3951fe3985c48a9
mitaclaw <140017135+mitaclaw@users.noreply.github.com>
2024-09-20 22:18:04 -0700

    Modernize `std::find` with ranges

--- Source/Core/UICommon/ResourcePack/Manager.cpp
+++ Source/Core/UICommon/ResourcePack/Manager.cpp
@@ -85,7 +85,7 @@ std::vector<ResourcePack>& GetPacks()
 std::vector<ResourcePack*> GetLowerPriorityPacks(const ResourcePack& pack)
 {
   std::vector<ResourcePack*> list;
-  for (auto it = std::find(packs.begin(), packs.end(), pack) + 1; it != packs.end(); ++it)
+  for (auto it = std::ranges::find(packs, pack) + 1; it != packs.end(); ++it)
   {
     auto& entry = *it;
     if (!IsInstalled(pack))
@@ -100,7 +100,7 @@ std::vector<ResourcePack*> GetLowerPriorityPacks(const ResourcePack& pack)
 std::vector<ResourcePack*> GetHigherPriorityPacks(const ResourcePack& pack)
 {
   std::vector<ResourcePack*> list;
-  auto end = std::find(packs.begin(), packs.end(), pack);
+  auto end = std::ranges::find(packs, pack);
 
   for (auto it = packs.begin(); it != end; ++it)
   {
@@ -145,7 +145,7 @@ bool Remove(ResourcePack& pack)
   if (!result)
     return false;
 
-  auto pack_iterator = std::find(packs.begin(), packs.end(), pack);
+  auto pack_iterator = std::ranges::find(packs, pack);
 
   if (pack_iterator == packs.end())
     return false;
