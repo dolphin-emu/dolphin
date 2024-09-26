2b82c34ea81c673da4cac8227277a01021730c4d
Dentomologist <dentomologist@gmail.com>
2024-09-26 13:33:16 -0700

    Core: Remove redundant call to PopulateBackendInfo
    Remove the second of two calls to PopulateBackendInfo during emulation
    startup. This call was originally the only one, but b214e0e added an
    earlier call to handle the backend being changed by the GameINI.
    
    The PR discussion doesn't explain why the original call was left in; I
    suspect it was just overlooked.
    
    As a bonus, this removes one of the extra copies of the "Video Info" On
    Screen Display message at startup when using OpenGL.

--- Source/Core/Core/Core.cpp
+++ Source/Core/Core/Core.cpp
@@ -573,8 +573,6 @@ static void EmuThread(Core::System& system, std::unique_ptr<BootParameters> boot
     system.GetPowerPC().GetDebugInterface().Clear(guard);
   }};
 
-  VideoBackendBase::PopulateBackendInfo(wsi);
-
   if (!g_video_backend->Initialize(wsi))
   {
     PanicAlertFmt("Failed to initialize video backend!");
