8da7a86b961dd3e6a8c49c0c379eaa5d9c0b6e51
Jordan Woyak <jordan.woyak@gmail.com>
2024-11-02 16:22:54 -0500

    Config/IsSettingSaveable: Use class template argument deduction.

--- Source/Core/Core/ConfigLoaders/IsSettingSaveable.cpp
+++ Source/Core/Core/ConfigLoaders/IsSettingSaveable.cpp
@@ -17,8 +17,8 @@ namespace ConfigLoaders
 {
 bool IsSettingSaveable(const Config::Location& config_location)
 {
-  static constexpr std::array<Config::System, 3> systems_not_saveable = {
-      Config::System::GCPad, Config::System::WiiPad, Config::System::GCKeyboard};
+  static constexpr std::array systems_not_saveable = {Config::System::GCPad, Config::System::WiiPad,
+                                                      Config::System::GCKeyboard};
 
   if (std::find(begin(systems_not_saveable), end(systems_not_saveable), config_location.system) ==
       end(systems_not_saveable))
