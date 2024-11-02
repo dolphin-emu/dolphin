lckybstrd/dolphin-emu2

Jit: Skip setting bit 32 of CRs when possible
Setting SO and LT can be done without setting bit 32. Setting bit 32 is
required in situations where clearing bits in the host representation of
a CR could otherwise cause the host representation to become all zeroes,
but it's impossible to generate a host representation whose SO and/or LT
bits are set without any other bits being set. (Any attempt to do so
would require either starting with a host representation of all zeroes
and then setting the SO or LT bit, which results in bit 63 getting set,
or clearing bit 63 by setting the emulated GT bit, which results in bit
32 getting set, or clearing the lower 32 bits by setting the emulated EQ
bit, which results in bit 32 getting set.)

Source/Core/Core/PowerPC/JitArm64/JitArm64_SystemRegisters.cpp
@@ -537,10 +537,12 @@ void JitArm64::crXXX(UGeckoInstruction inst)
 
     case PowerPC::CR_EQ_BIT:
       AND(XA, XA, LogicalImm(0xFFFF'FFFF'0000'0000, GPRSize::B64));
+      ORR(XA, XA, LogicalImm(u64(1) << 32, GPRSize::B64));
       break;
 
     case PowerPC::CR_GT_BIT:
       AND(XA, XA, LogicalImm(~(u64(1) << 63), GPRSize::B64));
+      ORR(XA, XA, LogicalImm(u64(1) << 32, GPRSize::B64));
       break;
 
     case PowerPC::CR_LT_BIT:
@@ -548,7 +550,6 @@ void JitArm64::crXXX(UGeckoInstruction inst)
       break;
     }
 
-    ORR(XA, XA, LogicalImm(u64(1) << 32, GPRSize::B64));
     return;
   }
 
@@ -643,11 +644,13 @@ void JitArm64::crXXX(UGeckoInstruction inst)
 
   case PowerPC::CR_EQ_BIT:  // clear low 32 bits, set bit 0 to !input
     AND(XB, XB, LogicalImm(0xFFFF'FFFF'0000'0000, GPRSize::B64));
+    ORR(XB, XB, LogicalImm(1ULL << 32, GPRSize::B64));
     EOR(XA, XA, LogicalImm(1, GPRSize::B64));
     ORR(XB, XB, XA);
     break;
 
   case PowerPC::CR_GT_BIT:  // set bit 63 to !input
+    ORR(XB, XB, LogicalImm(1ULL << 32, GPRSize::B64));
     EOR(XA, XA, LogicalImm(1, GPRSize::B64));
     BFI(XB, XA, 63, 1);
     break;
@@ -656,8 +659,6 @@ void JitArm64::crXXX(UGeckoInstruction inst)
     BFI(XB, XA, PowerPC::CR_EMU_LT_BIT, 1);
     break;
   }
-
-  ORR(XB, XB, LogicalImm(1ULL << 32, GPRSize::B64));
 
   gpr.Unlock(WA);
 }

dolphin-emu/dolphin

Source/Core/Core/PowerPC/JitArm64/JitArm64_SystemRegisters.cpp
@@ -520,10 +520,12 @@ void JitArm64::crXXX(UGeckoInstruction inst)
 
     case PowerPC::CR_EQ_BIT:
       AND(XA, XA, LogicalImm(0xFFFF'FFFF'0000'0000, GPRSize::B64));
+      ORR(XA, XA, LogicalImm(u64(1) << 32, GPRSize::B64));
       break;
 
     case PowerPC::CR_GT_BIT:
       AND(XA, XA, LogicalImm(~(u64(1) << 63), GPRSize::B64));
+      ORR(XA, XA, LogicalImm(u64(1) << 32, GPRSize::B64));
       break;
 
     case PowerPC::CR_LT_BIT:
@@ -531,7 +533,6 @@ void JitArm64::crXXX(UGeckoInstruction inst)
       break;
     }
 
-    ORR(XA, XA, LogicalImm(u64(1) << 32, GPRSize::B64));
     return;
   }
 
@@ -626,11 +627,13 @@ void JitArm64::crXXX(UGeckoInstruction inst)
 
   case PowerPC::CR_EQ_BIT:  // clear low 32 bits, set bit 0 to !input
     AND(CR, CR, LogicalImm(0xFFFF'FFFF'0000'0000, GPRSize::B64));
+    ORR(CR, CR, LogicalImm(1ULL << 32, GPRSize::B64));
     EOR(XA, XA, LogicalImm(1, GPRSize::B64));
     ORR(CR, CR, XA);
     break;
 
   case PowerPC::CR_GT_BIT:  // set bit 63 to !input
+    ORR(CR, CR, LogicalImm(1ULL << 32, GPRSize::B64));
     EOR(XA, XA, LogicalImm(1, GPRSize::B64));
     BFI(CR, XA, 63, 1);
     break;
@@ -639,8 +642,6 @@ void JitArm64::crXXX(UGeckoInstruction inst)
     BFI(CR, XA, PowerPC::CR_EMU_LT_BIT, 1);
     break;
   }
-
-  ORR(CR, CR, LogicalImm(1ULL << 32, GPRSize::B64));
 }
 
 void JitArm64::mfcr(UGeckoInstruction inst)