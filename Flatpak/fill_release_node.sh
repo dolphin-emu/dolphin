4fc259710f68dc1efe3648dd359ff97ab473e41d
OatmealDome <julian@oatmealdome.me>
2024-11-12 16:11:32 -0500

    Flatpak: Use ScmRevGen to generate metainfo XML

--- Flatpak/fill_release_node.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DATE=$(git log -1 --pretty=%cd --date=iso8601 --date=format:'%Y-%m-%d')
-sed -i -e "s/@DATE_PLACEHOLDER/${DATE}/" ../org.DolphinEmu.dolphin-emu.metainfo.xml
-VERSION=$(git describe --tags | sed -E 's/^([0-9]+-[0-9]+).*/\1/')
-sed -i -e "s/@VERSION_PLACEHOLDER/${VERSION}/" ../org.DolphinEmu.dolphin-emu.metainfo.xml
-
-
