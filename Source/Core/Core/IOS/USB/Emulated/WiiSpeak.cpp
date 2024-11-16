8bfec928e5630c9b3b00f5e72014e760226af7de
Sepalani <sepalani@hotmail.fr>
2024-11-16 15:50:06 +0400

    IOS/USB: Add more Wii Speak log messages

--- Source/Core/Core/IOS/USB/Emulated/WiiSpeak.cpp
+++ Source/Core/Core/IOS/USB/Emulated/WiiSpeak.cpp
@@ -250,6 +250,8 @@ void WiiSpeak::SetRegister(const std::unique_ptr<CtrlMessage>& cmd)
     m_sampler.sample_on = !!arg1;
     break;
   case SAMPLER_FREQ:
+    WARN_LOG_FMT(IOS_USB, "Wii Speak SAMPLER_FREQ set (arg1={:04x}, arg2={:04x}) not implemented",
+                 arg1, arg2);
     switch (arg1)
     {
     case FREQ_8KHZ:
@@ -271,6 +273,8 @@ void WiiSpeak::SetRegister(const std::unique_ptr<CtrlMessage>& cmd)
     }
     break;
   case SAMPLER_GAIN:
+    WARN_LOG_FMT(IOS_USB, "Wii Speak SAMPLER_GAIN set (arg1={:04x}, arg2={:04x}) not implemented",
+                 arg1, arg2);
     switch (arg1 & ~0x300)
     {
     case GAIN_00dB:
