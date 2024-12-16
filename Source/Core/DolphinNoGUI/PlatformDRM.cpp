3518bbac359af162d250a6e43c52aea1885a8247
camdenorrb <me@12oclock.dev>
2024-12-15 21:03:46 -0600

    Fixes

--- Source/Core/DolphinNoGUI/PlatformDRM.cpp
+++ Source/Core/DolphinNoGUI/PlatformDRM.cpp
@@ -1,6 +1,7 @@
 // Copyright 2018 Dolphin Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#if HAVE_DRM
 #include <unistd.h>
 
 #include "DolphinNoGUI/Platform.h"
@@ -18,47 +19,46 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
-namespace
-{
-class PlatformDRM : public Platform
-{
-public:
-  void SetTitle(const std::string& string) override;
-  void MainLoop() override;
-
-  WindowSystemInfo GetWindowSystemInfo() const override;
-};
-
-
-void PlatformDRM::SetTitle(const std::string& string)
-{
-  std::fprintf(stdout, "%s\n", string.c_str());
-}
-
-void PlatformDRM::MainLoop()
-{
-  while (IsRunning())
+namespace {
+  class PlatformDRM : public Platform
   {
-    UpdateRunningFlag();
-    Core::HostDispatchJobs(Core::System::GetInstance());
 
-    // TODO: Is this sleep appropriate?
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  public:
+    void SetTitle(const std::string& string) override;
+    void MainLoop() override;
+
+    WindowSystemInfo GetWindowSystemInfo() const override;
+  };
+
+
+  void PlatformDRM::SetTitle(const std::string& string)
+  {
+    std::fprintf(stdout, "%s\n", string.c_str());
   }
-}
 
-WindowSystemInfo PlatformDRM::GetWindowSystemInfo() const
-{
-  WindowSystemInfo wsi;
-  wsi.type = WindowSystemType::DRM;
-  wsi.display_connection = nullptr;  // EGL_DEFAULT_DISPLAY
-  wsi.render_window = nullptr;
-  wsi.render_surface = nullptr;
-  return wsi;
-}
+  void PlatformDRM::MainLoop()
+  {
+    while (IsRunning())
+    {
+      UpdateRunningFlag();
+      Core::HostDispatchJobs(Core::System::GetInstance());
+
+      // TODO: Is this sleep appropriate?
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+  }
+
+  WindowSystemInfo PlatformDRM::GetWindowSystemInfo() const
+  {
+    WindowSystemInfo wsi;
+    wsi.type = WindowSystemType::DRM;
+    wsi.display_connection = nullptr;  // EGL_DEFAULT_DISPLAY
+    wsi.render_window = nullptr;
+    wsi.render_surface = nullptr;
+    return wsi;
+  }
 }  // namespace
 
-#if HAVE_DRM
 std::unique_ptr<Platform> Platform::CreateDRMPlatform()
 {
   return std::make_unique<PlatformDRM>();
