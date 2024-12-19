e4ec5c9f6c7a46d4af35db3cebe84cb5426c8d13
camdenorrb <me@12oclock.dev>
2024-12-19 03:42:47 -0600

    Fix lint

--- Source/Core/DolphinNoGUI/PlatformDRM.cpp
+++ Source/Core/DolphinNoGUI/PlatformDRM.cpp
@@ -18,44 +18,45 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
-namespace {
-  class PlatformDRM : public Platform
+namespace
+{
+class PlatformDRM : public Platform
+{
+
+public:
+  void SetTitle(const std::string& string) override;
+  void MainLoop() override;
+
+  WindowSystemInfo GetWindowSystemInfo() const override;
+};
+
+
+void PlatformDRM::SetTitle(const std::string& string)
+{
+  std::fprintf(stdout, "%s\n", string.c_str());
+}
+
+void PlatformDRM::MainLoop()
+{
+  while (IsRunning())
   {
+    UpdateRunningFlag();
+    Core::HostDispatchJobs(Core::System::GetInstance());
 
-  public:
-    void SetTitle(const std::string& string) override;
-    void MainLoop() override;
-
-    WindowSystemInfo GetWindowSystemInfo() const override;
-  };
-
-
-  void PlatformDRM::SetTitle(const std::string& string)
-  {
-    std::fprintf(stdout, "%s\n", string.c_str());
+    // TODO: Is this sleep appropriate?
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
   }
+}
 
-  void PlatformDRM::MainLoop()
-  {
-    while (IsRunning())
-    {
-      UpdateRunningFlag();
-      Core::HostDispatchJobs(Core::System::GetInstance());
-
-      // TODO: Is this sleep appropriate?
-      std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-  }
-
-  WindowSystemInfo PlatformDRM::GetWindowSystemInfo() const
-  {
-    WindowSystemInfo wsi;
-    wsi.type = WindowSystemType::DRM;
-    wsi.display_connection = nullptr;  // EGL_DEFAULT_DISPLAY
-    wsi.render_window = nullptr;
-    wsi.render_surface = nullptr;
-    return wsi;
-  }
+WindowSystemInfo PlatformDRM::GetWindowSystemInfo() const
+{
+  WindowSystemInfo wsi;
+  wsi.type = WindowSystemType::DRM;
+  wsi.display_connection = nullptr;  // EGL_DEFAULT_DISPLAY
+  wsi.render_window = nullptr;
+  wsi.render_surface = nullptr;
+  return wsi;
+}
 }  // namespace
 
 std::unique_ptr<Platform> Platform::CreateDRMPlatform()
