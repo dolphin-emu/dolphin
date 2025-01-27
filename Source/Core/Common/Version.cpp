78211a81b9d950ca52705d528a6d0316be86c065
Jas Laferriere <Fizzi36@gmail.com>
2025-01-27 15:34:15 -0500

    release: v4.0.0-mainline-beta.8

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -18,7 +18,7 @@ namespace Common
 #endif
 
 #ifndef IS_PLAYBACK
-#define SLIPPI_REV_STR "4.0.0-mainline-beta.7"  // netplay version
+#define SLIPPI_REV_STR "4.0.0-mainline-beta.8"  // netplay version
 #else
 #define SLIPPI_REV_STR "3.2.0"  // playback version
 #endif
