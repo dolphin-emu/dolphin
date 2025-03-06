0f9ca3d7d6e87181b935a3e2953a3a0d059181ed
Nikhil Narayana <nikhil.narayana@live.com>
2025-03-05 22:32:22 -0800

    release(netplay): v4.0.0-mainline-beta.9

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -20,7 +20,7 @@ namespace Common
 #endif
 
 #ifndef IS_PLAYBACK
-#define SLIPPI_REV_STR "4.0.0-mainline-beta.8"  // netplay version
+#define SLIPPI_REV_STR "4.0.0-mainline-beta.9"  // netplay version
 #else
 #define SLIPPI_REV_STR "3.2.0"  // playback version
 #endif
