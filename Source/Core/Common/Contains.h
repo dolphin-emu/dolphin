6225ef40cc76bb0fa98a158d987854bf4013349f
Joshua Vandaële <joshua@vandaele.software>
2025-03-18 13:08:58 +0100

    linter: `RequiresClausePosition: WithPreceding`

--- Source/Core/Common/Contains.h
+++ Source/Core/Common/Contains.h
@@ -11,16 +11,15 @@ namespace Common
 struct ContainsFn
 {
   template <std::input_iterator I, std::sentinel_for<I> S, class T, class Proj = std::identity>
-    requires std::indirect_binary_predicate<std::ranges::equal_to, std::projected<I, Proj>,
-                                            const T*>
+  requires std::indirect_binary_predicate<std::ranges::equal_to, std::projected<I, Proj>, const T*>
   constexpr bool operator()(I first, S last, const T& value, Proj proj = {}) const
   {
     return std::ranges::find(std::move(first), last, value, std::move(proj)) != last;
   }
 
   template <std::ranges::input_range R, class T, class Proj = std::identity>
-    requires std::indirect_binary_predicate<
-        std::ranges::equal_to, std::projected<std::ranges::iterator_t<R>, Proj>, const T*>
+  requires std::indirect_binary_predicate<
+      std::ranges::equal_to, std::projected<std::ranges::iterator_t<R>, Proj>, const T*>
   constexpr bool operator()(R&& r, const T& value, Proj proj = {}) const
   {
     return (*this)(std::ranges::begin(r), std::ranges::end(r), value, std::move(proj));
@@ -32,7 +31,7 @@ struct ContainsSubrangeFn
   template <std::forward_iterator I1, std::sentinel_for<I1> S1, std::forward_iterator I2,
             std::sentinel_for<I2> S2, class Pred = std::ranges::equal_to,
             class Proj1 = std::identity, class Proj2 = std::identity>
-    requires std::indirectly_comparable<I1, I2, Pred, Proj1, Proj2>
+  requires std::indirectly_comparable<I1, I2, Pred, Proj1, Proj2>
   constexpr bool operator()(I1 first1, S1 last1, I2 first2, S2 last2, Pred pred = {},
                             Proj1 proj1 = {}, Proj2 proj2 = {}) const
   {
@@ -45,8 +44,8 @@ struct ContainsSubrangeFn
   template <std::ranges::forward_range R1, std::ranges::forward_range R2,
             class Pred = std::ranges::equal_to, class Proj1 = std::identity,
             class Proj2 = std::identity>
-    requires std::indirectly_comparable<std::ranges::iterator_t<R1>, std::ranges::iterator_t<R2>,
-                                        Pred, Proj1, Proj2>
+  requires std::indirectly_comparable<std::ranges::iterator_t<R1>, std::ranges::iterator_t<R2>,
+                                      Pred, Proj1, Proj2>
   constexpr bool operator()(R1&& r1, R2&& r2, Pred pred = {}, Proj1 proj1 = {},
                             Proj2 proj2 = {}) const
   {
