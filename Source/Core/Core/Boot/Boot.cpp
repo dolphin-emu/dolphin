goit10042022/Dolphin-anty

Core: properly distinguish between ES title switches (Wii titles) and normal loading (GC, also called for Wii..);  update config for ES title change, generate textures for both situations

Source/Core/Core/Boot/Boot.cpp
@@ -550,7 +550,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
       if (!EmulatedBS2(system, guard, system.IsWii(), *volume, riivolution_patches))
         return false;
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleBooted(guard);
       return true;
     }
 
@@ -601,7 +601,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
         return false;
       }
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleBooted(guard);
 
       ppc_state.pc = executable.reader->GetEntryPoint();
 
@@ -621,7 +621,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
       if (!Boot_WiiWAD(system, wad))
         return false;
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleBooted(guard);
       return true;
     }
 
@@ -631,7 +631,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
       if (!BootNANDTitle(system, nand_title.id))
         return false;
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleBooted(guard);
       return true;
     }
 
@@ -657,7 +657,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
                 ipl.disc->auto_disc_change_paths);
       }
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleBooted(guard);
       return true;
     }
 

Source/Core/Core/ConfigManager.cpp
@@ -43,8 +43,11 @@
 #include "Core/HLE/HLE.h"
 #include "Core/HW/DVD/DVDInterface.h"
 #include "Core/HW/EXI/EXI_Device.h"
+#include "Core/HW/GCKeyboard.h"
+#include "Core/HW/GCPad.h"
 #include "Core/HW/SI/SI.h"
 #include "Core/HW/SI/SI_Device.h"
+#include "Core/HW/Wiimote.h"
 #include "Core/Host.h"
 #include "Core/IOS/ES/ES.h"
 #include "Core/IOS/ES/Formats.h"
@@ -197,7 +200,20 @@ void SConfig::SetRunningGameMetadata(const std::string& game_id, const std::stri
     DolphinAnalytics::Instance().ReportGameStart();
 }
 
-void SConfig::OnNewTitleLoad(const Core::CPUThreadGuard& guard)
+void SConfig::OnESTitleChanged()
+{
+  auto& system = Core::System::GetInstance();
+  Pad::LoadConfig();
+  Keyboard::LoadConfig();
+  if (system.IsWii() && !Config::Get(Config::MAIN_BLUETOOTH_PASSTHROUGH_ENABLED))
+  {
+    Wiimote::LoadConfig();
+  }
+
+  OnNewTitleLoad(system);
+}
+
+void SConfig::OnTitleBooted(const Core::CPUThreadGuard& guard)
 {
   auto& system = guard.GetSystem();
   if (!Core::IsRunningOrStarting(system))
@@ -211,9 +227,27 @@ void SConfig::OnNewTitleLoad(const Core::CPUThreadGuard& guard)
   }
   CBoot::LoadMapFromFilename(guard, ppc_symbol_db);
   HLE::Reload(system);
+
   PatchEngine::Reload(system);
-  HiresTexture::Update();
   WC24PatchEngine::Reload();
+
+  // Note: Wii is handled by ES title change
+  if (!system.IsWii())
+  {
+    OnNewTitleLoad(system);
+  }
+}
+
+void SConfig::OnNewTitleLoad(Core::System& system)
+{
+  Pad::GenerateDynamicInputTextures();
+  Keyboard::GenerateDynamicInputTextures();
+  if (system.IsWii() && !Config::Get(Config::MAIN_BLUETOOTH_PASSTHROUGH_ENABLED))
+  {
+    Wiimote::GenerateDynamicInputTextures();
+  }
+
+  HiresTexture::Update();
 }
 
 void SConfig::LoadDefaults()

Source/Core/Core/IOS/IOS.cpp
@@ -952,7 +952,7 @@ static void FinishPPCBootstrap(Core::System& system, u64 userdata, s64 cycles_la
 
   ASSERT(Core::IsCPUThread());
   Core::CPUThreadGuard guard(system);
-  SConfig::OnNewTitleLoad(guard);
+  SConfig::OnTitleBooted(guard);
 
   INFO_LOG_FMT(IOS, "Bootstrapping done.");
 }

Source/Core/Core/IOS/MIOS.cpp
@@ -103,7 +103,7 @@ bool Load(Core::System& system)
   NOTICE_LOG_FMT(IOS, "IPL ready.");
   system.SetIsMIOS(true);
   system.GetDVDInterface().UpdateRunningGameMetadata();
-  SConfig::OnNewTitleLoad(guard);
+  SConfig::OnTitleBooted(guard);
   return true;
 }
 }  // namespace IOS::HLE::MIOS

dolphin-emu/dolphin

Source/Core/Core/Boot/Boot.cpp
@@ -550,7 +550,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
       if (!EmulatedBS2(system, guard, system.IsWii(), *volume, riivolution_patches))
         return false;
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleDirectlyBooted(guard);
       return true;
     }
 
@@ -601,7 +601,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
         return false;
       }
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleDirectlyBooted(guard);
 
       ppc_state.pc = executable.reader->GetEntryPoint();
 
@@ -621,7 +621,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
       if (!Boot_WiiWAD(system, wad))
         return false;
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleDirectlyBooted(guard);
       return true;
     }
 
@@ -631,7 +631,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
       if (!BootNANDTitle(system, nand_title.id))
         return false;
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleDirectlyBooted(guard);
       return true;
     }
 
@@ -657,7 +657,7 @@ bool CBoot::BootUp(Core::System& system, const Core::CPUThreadGuard& guard,
                 ipl.disc->auto_disc_change_paths);
       }
 
-      SConfig::OnNewTitleLoad(guard);
+      SConfig::OnTitleDirectlyBooted(guard);
       return true;
     }
 

Source/Core/Core/ConfigManager.cpp
@@ -44,8 +44,11 @@
 #include "Core/HLE/HLE.h"
 #include "Core/HW/DVD/DVDInterface.h"
 #include "Core/HW/EXI/EXI_Device.h"
+#include "Core/HW/GCKeyboard.h"
+#include "Core/HW/GCPad.h"
 #include "Core/HW/SI/SI.h"
 #include "Core/HW/SI/SI_Device.h"
+#include "Core/HW/Wiimote.h"
 #include "Core/Host.h"
 #include "Core/IOS/ES/ES.h"
 #include "Core/IOS/ES/Formats.h"
@@ -239,7 +242,20 @@ void SConfig::SetRunningGameMetadata(const std::string& game_id, const std::stri
     DolphinAnalytics::Instance().ReportGameStart();
 }
 
-void SConfig::OnNewTitleLoad(const Core::CPUThreadGuard& guard)
+void SConfig::OnESTitleChanged()
+{
+  auto& system = Core::System::GetInstance();
+  Pad::LoadConfig();
+  Keyboard::LoadConfig();
+  if (system.IsWii() && !Config::Get(Config::MAIN_BLUETOOTH_PASSTHROUGH_ENABLED))
+  {
+    Wiimote::LoadConfig();
+  }
+
+  ReloadTextures(system);
+}
+
+void SConfig::OnTitleDirectlyBooted(const Core::CPUThreadGuard& guard)
 {
   auto& system = guard.GetSystem();
   if (!Core::IsRunningOrStarting(system))
@@ -253,9 +269,27 @@ void SConfig::OnNewTitleLoad(const Core::CPUThreadGuard& guard)
   }
   CBoot::LoadMapFromFilename(guard, ppc_symbol_db);
   HLE::Reload(system);
+
   PatchEngine::Reload(system);
-  HiresTexture::Update();
   WC24PatchEngine::Reload();
+
+  // Note: Wii is handled by ES title change
+  if (!system.IsWii())
+  {
+    ReloadTextures(system);
+  }
+}
+
+void SConfig::ReloadTextures(Core::System& system)
+{
+  Pad::GenerateDynamicInputTextures();
+  Keyboard::GenerateDynamicInputTextures();
+  if (system.IsWii() && !Config::Get(Config::MAIN_BLUETOOTH_PASSTHROUGH_ENABLED))
+  {
+    Wiimote::GenerateDynamicInputTextures();
+  }
+
+  HiresTexture::Update();
 }
 
 void SConfig::LoadDefaults()

Source/Core/Core/IOS/IOS.cpp
@@ -952,7 +952,7 @@ static void FinishPPCBootstrap(Core::System& system, u64 userdata, s64 cycles_la
 
   ASSERT(Core::IsCPUThread());
   Core::CPUThreadGuard guard(system);
-  SConfig::OnNewTitleLoad(guard);
+  SConfig::OnTitleDirectlyBooted(guard);
 
   INFO_LOG_FMT(IOS, "Bootstrapping done.");
 }

Source/Core/Core/IOS/MIOS.cpp
@@ -103,7 +103,7 @@ bool Load(Core::System& system)
   NOTICE_LOG_FMT(IOS, "IPL ready.");
   system.SetIsMIOS(true);
   system.GetDVDInterface().UpdateRunningGameMetadata();
-  SConfig::OnNewTitleLoad(guard);
+  SConfig::OnTitleDirectlyBooted(guard);
   return true;
 }
 }  // namespace IOS::HLE::MIOS