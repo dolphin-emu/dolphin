46063bf36c75199c8cf2ee5ff7a5e6cf418a5178
Sepalani <sepalani@hotmail.fr>
2025-04-19 23:57:08 +0400

    IOS/KBD: Move keyboard logic to Common/Keyboard
    The keypress loop was simplified and its keyboard state buffer removed.
    
    KeyboardContext helper class added.

--- Source/Core/Core/IOS/USB/USB_KBD.h
+++ Source/Core/Core/IOS/USB/USB_KBD.h
@@ -3,12 +3,11 @@
 
 #pragma once
 
-#include <array>
 #include <queue>
 #include <string>
-#include <type_traits>
 
 #include "Common/CommonTypes.h"
+#include "Common/Keyboard.h"
 #include "Core/IOS/Device.h"
 #include "Core/IOS/IOS.h"
 
@@ -20,6 +19,7 @@ public:
   USB_KBD(EmulationKernel& ios, const std::string& device_name);
 
   std::optional<IPCReply> Open(const OpenRequest& request) override;
+  std::optional<IPCReply> Close(u32 fd) override;
   std::optional<IPCReply> Write(const ReadWriteRequest& request) override;
   std::optional<IPCReply> IOCtl(const IOCtlRequest& request) override;
   void Update() override;
@@ -32,8 +32,6 @@ private:
     Event = 2
   };
 
-  using PressedKeyData = std::array<u8, 6>;
-
 #pragma pack(push, 1)
   struct MessageData
   {
@@ -41,26 +39,15 @@ private:
     u32 unk1 = 0;
     u8 modifiers = 0;
     u8 unk2 = 0;
-    PressedKeyData pressed_keys{};
+    Common::HIDPressedKeys pressed_keys{};
 
-    MessageData(MessageType msg_type, u8 modifiers, PressedKeyData pressed_keys);
+    MessageData(MessageType msg_type, const Common::HIDPressedState& state);
   };
   static_assert(std::is_trivially_copyable_v<MessageData>,
                 "MessageData must be trivially copyable, as it's copied into emulated memory.");
 #pragma pack(pop)
   std::queue<MessageData> m_message_queue;
-
-  std::array<bool, 256> m_old_key_buffer{};
-  u8 m_old_modifiers = 0;
-
-  bool IsKeyPressed(int key) const;
-
-  // This stuff should probably die
-  enum
-  {
-    KBD_LAYOUT_QWERTY = 0,
-    KBD_LAYOUT_AZERTY = 1
-  };
-  int m_keyboard_layout = KBD_LAYOUT_QWERTY;
+  Common::HIDPressedState m_previous_state;
+  std::shared_ptr<Common::KeyboardContext> m_keyboard_context;
 };
 }  // namespace IOS::HLE
