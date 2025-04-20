73a1b12472cb4ca5424fe7b94fb5c3bab3b3e401
Dr. Dystopia <jonis9898@hotmail.com>
2025-04-20 17:53:40 +0200

    DolphinNoGUI: Make function static

--- Source/Core/DolphinNoGUI/PlatformWin32.cpp
+++ Source/Core/DolphinNoGUI/PlatformWin32.cpp
@@ -33,7 +33,7 @@ private:
 
   static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-  bool RegisterRenderWindowClass();
+  static bool RegisterRenderWindowClass();
   bool CreateRenderWindow();
   void UpdateWindowPosition();
   void ProcessEvents();
