fe6fd2279c195517094538fff8667332049823ad
Tillmann Karras <tilkax@gmail.com>
2025-06-15 11:13:18 +0100

    WiiSaveBanner: fall back to $userdir/Load/WiiBanners
    Unlike custom banners which work as an override, this mechanism works as
    a fallback. The use case is if you have games you don't really play but
    want to keep around for testing purposes without filling up your NAND
    with lots of saves. For ease of use, the directory structure is the same
    but only title/$title_hi/$title_lo/data/banner.bin files are
    relevant.

--- Source/Core/Common/CommonPaths.h
+++ Source/Core/Common/CommonPaths.h
@@ -89,6 +89,7 @@
 #define GRAPHICSMOD_DIR "GraphicMods"
 #define WIISDSYNC_DIR "WiiSDSync"
 #define ASSEMBLY_DIR "SavedAssembly"
+#define WIIBANNERS_DIR "WiiBanners"
 
 // This one is only used to remove it if it was present
 #define SHADERCACHE_LEGACY_DIR "ShaderCache"
