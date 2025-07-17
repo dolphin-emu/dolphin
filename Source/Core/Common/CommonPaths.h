7c6231ae5b0ee7fd6b68199998922c798b0d3a6f
crediar <crediar@rypp.net>
2025-07-17 23:38:08 +0200

    Added support to boot the Triforce IPL Added handling of dev region for Triforce IPL

--- Source/Core/Common/CommonPaths.h
+++ Source/Core/Common/CommonPaths.h
@@ -41,6 +41,7 @@
 #define USA_DIR "USA"
 #define JAP_DIR "JAP"
 #define JPN_DIR "JPN"
+#define DEV_DIR "DEV" 
 
 // Subdirs in the User dir returned by GetUserPath(D_USER_IDX)
 #define GC_USER_DIR "GC"
