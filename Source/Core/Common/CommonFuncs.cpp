4b3d51a2e55ce54a509f3888488a7953e91ea87c
Admiral H. Curtiss <pikachu025@gmail.com>
2025-07-19 14:02:43 +0200

    Common: Move GetDeviceProperty() into its own header
    Otherwise we include Windows headers in the entire codebase through CommonFuncs.h

--- Source/Core/Common/CommonFuncs.cpp
+++ Source/Core/Common/CommonFuncs.cpp
@@ -92,28 +92,5 @@ std::optional<std::wstring> GetModuleName(void* hInstance)
   name.resize(size);
   return name;
 }
-
-std::wstring GetDeviceProperty(const HDEVINFO& device_info, const PSP_DEVINFO_DATA device_data,
-                               const DEVPROPKEY* requested_property)
-{
-  DWORD required_size = 0;
-  DEVPROPTYPE device_property_type;
-  BOOL result;
-
-  result = SetupDiGetDeviceProperty(device_info, device_data, requested_property,
-                                    &device_property_type, nullptr, 0, &required_size, 0);
-  if (!result && GetLastError() != ERROR_INSUFFICIENT_BUFFER)
-    return std::wstring();
-
-  std::vector<TCHAR> unicode_buffer(required_size / sizeof(TCHAR));
-
-  result = SetupDiGetDeviceProperty(
-      device_info, device_data, requested_property, &device_property_type,
-      reinterpret_cast<PBYTE>(unicode_buffer.data()), required_size, nullptr, 0);
-  if (!result)
-    return std::wstring();
-
-  return std::wstring(unicode_buffer.data());
-}
 #endif
 }  // namespace Common
