2ca74be32029630aa52d5102a29f0c3c8732ba93
crediar <crediar@rypp.net>
2025-07-20 23:22:48 +0200

    Fixed not booting the Triforce IPL when loading a Triforce game

--- Source/Core/Core/ConfigManager.cpp
+++ Source/Core/Core/ConfigManager.cpp
@@ -433,6 +433,12 @@ bool SConfig::SetPathsAndGameMetadata(Core::System& system, const BootParameters
   if (m_region == DiscIO::Region::Unknown)
     m_region = Config::Get(Config::MAIN_FALLBACK_REGION);
 
+  // Triforce IPL
+  if (m_triforce_id.length())
+  {
+    m_region = DiscIO::Region::DEV;
+  }
+
   // Set up paths
   const std::string region_dir = Config::GetDirectoryForRegion(Config::ToGameCubeRegion(m_region));
   m_strSRAM = File::GetUserPath(F_GCSRAM_IDX);
