77654d55c48ed0499e2472bcd226b27fff69d0ba
iwubcode <iwubcode@users.noreply.github.com>
2025-08-03 16:46:25 -0500

    texture asset

--- Source/Core/VideoCommon/Assets/TextureAsset.h
+++ Source/Core/VideoCommon/Assets/TextureAsset.h
@@ -18,16 +18,9 @@ struct TextureAndSamplerData
   static bool FromJson(const CustomAssetLibrary::AssetID& asset_id, const picojson::object& json,
                        TextureAndSamplerData* data);
   static void ToJson(picojson::object* obj, const TextureAndSamplerData& data);
-  enum class Type
-  {
-    Type_Undefined,
-    Type_Texture2D,
-    Type_TextureCube,
-    Type_Max = Type_TextureCube
-  };
-  Type m_type;
-  CustomTextureData m_texture;
-  SamplerState m_sampler;
+  AbstractTextureType type;
+  CustomTextureData texture_data;
+  SamplerState sampler;
 };
 
 class TextureAsset final : public CustomLoadableAsset<CustomTextureData>
@@ -38,11 +31,14 @@ public:
 private:
   CustomAssetLibrary::LoadInfo LoadImpl(const CustomAssetLibrary::AssetID& asset_id) override;
 };
+
+class TextureAndSamplerAsset final : public CustomLoadableAsset<TextureAndSamplerData>
+{
+public:
+  using CustomLoadableAsset::CustomLoadableAsset;
+
+private:
+  CustomAssetLibrary::LoadInfo LoadImpl(const CustomAssetLibrary::AssetID& asset_id) override;
+};
 }  // namespace VideoCommon
 
-template <>
-struct fmt::formatter<VideoCommon::TextureAndSamplerData::Type>
-    : EnumFormatter<VideoCommon::TextureAndSamplerData::Type::Type_Max>
-{
-  constexpr formatter() : EnumFormatter({"Undefined", "Texture2D", "TextureCube"}) {}
-};
