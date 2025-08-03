671c5a4a3c2a8391801e905492378b8bb85bfae7
iwubcode <iwubcode@users.noreply.github.com>
2025-08-03 16:44:39 -0500

    VideoCommon: move ApplyDriverBugs to its own function that can be called from other areas of code

---
+++ Source/Core/VideoCommon/ShaderCacheUtils.h
@@ -0,0 +1,11 @@
+// Copyright 2025 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "VideoCommon/GXPipelineTypes.h"
+
+namespace VideoCommon
+{
+GXPipelineUid ApplyDriverBugs(const GXPipelineUid& in);
+}
