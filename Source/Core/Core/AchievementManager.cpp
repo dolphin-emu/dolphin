1ad0a771d944c84b828c287592c896d446786866
LillyJadeKatrin <lilly.kitty.1988@gmail.com>
2025-08-09 21:23:48 -0400

    Delete Invalid RetroAchievements Tokens

--- Source/Core/Core/AchievementManager.cpp
+++ Source/Core/Core/AchievementManager.cpp
@@ -927,6 +927,7 @@ void AchievementManager::LoginCallback(int result, const char* error_message, rc
   {
     WARN_LOG_FMT(ACHIEVEMENTS, "Failed to login {} to RetroAchievements server.",
                  Config::Get(Config::RA_USERNAME));
+    Config::SetBaseOrCurrent(Config::RA_API_TOKEN, "");
     instance.update_event.Trigger({.failed_login_code = result});
     return;
   }
@@ -1030,6 +1031,7 @@ void AchievementManager::LoadGameCallback(int result, const char* error_message,
         OSD::Duration::VERY_LONG, OSD::Color::RED);
     OSD::AddMessage("Please close the game to log back in before continuing.",
                     OSD::Duration::VERY_LONG, OSD::Color::RED);
+    Config::SetBaseOrCurrent(Config::RA_API_TOKEN, "");
     return;
   }
 
