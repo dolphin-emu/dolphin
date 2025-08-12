2b92fda875426d1f0cbcc4e266f67315fab6db90
Joshua Vandaële <joshua@vandaele.software>
2025-08-13 00:33:52 +0200

    GameCubePane: Use ConfigControls where applicable

--- Source/Core/UICommon/UICommon.cpp
+++ Source/Core/UICommon/UICommon.cpp
@@ -114,8 +114,6 @@ static void InitCustomPaths()
                     Config::Get(Config::MAIN_WII_SD_CARD_SYNC_FOLDER_PATH));
   File::CreateFullPath(File::GetUserPath(D_WIISDCARDSYNCFOLDER_IDX));
 #ifdef HAS_LIBMGBA
-  File::SetUserPath(F_GBABIOS_IDX, Config::Get(Config::MAIN_GBA_BIOS_PATH));
-  File::SetUserPath(D_GBASAVES_IDX, Config::Get(Config::MAIN_GBA_SAVES_PATH));
   File::CreateFullPath(File::GetUserPath(D_GBASAVES_IDX));
 #endif
 }
