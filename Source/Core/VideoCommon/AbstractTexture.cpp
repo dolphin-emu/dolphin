86b4ff276505023155b31cf5cab80b077275ef83
TryTwo <taolas@gmail.com>
2025-08-12 02:00:44 -0700

    Update code to work with new version of ImGui.
    UpdateImGuiTexture now handles creating font textures and modifying them.

--- Source/Core/VideoCommon/AbstractTexture.cpp
+++ Source/Core/VideoCommon/AbstractTexture.cpp
@@ -20,6 +20,11 @@ AbstractTexture::AbstractTexture(const TextureConfig& c) : m_config(c)
 {
 }
 
+AbstractTexture::operator ImTextureRef() const
+{
+  return ImTextureRef(reinterpret_cast<AbstractTexture::imgui_texture_id>(this));
+}
+
 void AbstractTexture::FinishedRendering()
 {
 }
