c2efa5a9240d55371533cfe6834832eecfe71b09
cristian64 <cristian64@gmail.com>
2025-08-16 18:50:33 +0100

    Core/HW: Support for BBA (IPC) in macOS and FreeBSD.
    Follow-up to #13870, which introduced a new IPC-based BroadBand Adapter
    implementation for GameCube.
    
    At the time, support was added only for Windows and Linux, as the
    underlying cpp-ipc library only supports those platforms. Now support is
    added for macOS and FreeBSD using a separate implementation with ZeroMQ.

--- Source/Core/DolphinQt/Settings/GameCubePane.cpp
+++ Source/Core/DolphinQt/Settings/GameCubePane.cpp
@@ -143,9 +143,7 @@ void GameCubePane::CreateWidgets()
            EXIDeviceType::EthernetXLink,
            EXIDeviceType::EthernetTapServer,
            EXIDeviceType::EthernetBuiltIn,
-#if defined(WIN32) || (defined(__linux__) && !defined(__ANDROID__))
            EXIDeviceType::EthernetIPC,
-#endif
            EXIDeviceType::ModemTapServer,
        })
   {
