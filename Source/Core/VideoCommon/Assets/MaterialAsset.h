a84479630b06cb834bbe32819ffbddc177e995c0
iwubcode <iwubcode@users.noreply.github.com>
2025-08-22 19:09:39 -0500

    VideoCommon: update material asset to support render state properties, support multi-pass, and textures are now split out (as well as supporting a way to calculate sampler origin)

--- Source/Core/VideoCommon/Assets/MaterialAsset.h
+++ Source/Core/VideoCommon/Assets/MaterialAsset.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <array>
+#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
@@ -11,8 +12,10 @@
 #include <picojson.h>
 
 #include "Common/CommonTypes.h"
-#include "Common/EnumFormatter.h"
 #include "VideoCommon/Assets/CustomAsset.h"
+#include "VideoCommon/Assets/TextureSamplerValue.h"
+#include "VideoCommon/BPMemory.h"
+#include "VideoCommon/RenderState.h"
 
 class ShaderCode;
 
@@ -22,11 +25,9 @@ struct MaterialProperty
 {
   static void WriteToMemory(u8*& buffer, const MaterialProperty& property);
   static std::size_t GetMemorySize(const MaterialProperty& property);
-  static void WriteAsShaderCode(ShaderCode& shader_source, const MaterialProperty& property);
-  using Value = std::variant<CustomAssetLibrary::AssetID, s32, std::array<s32, 2>,
-                             std::array<s32, 3>, std::array<s32, 4>, float, std::array<float, 2>,
-                             std::array<float, 3>, std::array<float, 4>, bool>;
-  std::string m_code_name;
+  using Value =
+      std::variant<s32, std::array<s32, 2>, std::array<s32, 3>, std::array<s32, 4>, float,
+                   std::array<float, 2>, std::array<float, 3>, std::array<float, 4>, bool>;
   Value m_value;
 };
 
@@ -35,8 +36,15 @@ struct MaterialData
   static bool FromJson(const CustomAssetLibrary::AssetID& asset_id, const picojson::object& json,
                        MaterialData* data);
   static void ToJson(picojson::object* obj, const MaterialData& data);
-  std::string shader_asset;
+  CustomAssetLibrary::AssetID shader_asset;
+  CustomAssetLibrary::AssetID next_material_asset;
   std::vector<MaterialProperty> properties;
+
+  std::optional<CullMode> cull_mode;
+  std::optional<DepthState> depth_state;
+  std::optional<BlendingState> blending_state;
+
+  std::vector<TextureSamplerValue> textures;
 };
 
 // Much like Unity and Unreal materials, a Dolphin material does very little on its own
