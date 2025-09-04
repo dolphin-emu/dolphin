158172985661d9e298b08090e26f2ee38e3427a5
crediar <crediar@rypp.net>
2025-09-04 03:22:58 +0200

    Code improvements and formatting Fixed wrong value in folder path handling Fixed incorrect filenames for Triforce files error messages Added socket value check in AMMBCommand::Closesocket Fixed incorrect array offsets in the network functions of the AMBaseboard Fixed not using the randomly generated filename when a backup file for Triforce is already in use Fixed DTV flag not being set when using NTSC-J

--- Source/Core/Core/HW/VideoInterface.cpp
+++ Source/Core/Core/HW/VideoInterface.cpp
@@ -165,14 +165,7 @@ void VideoInterfaceManager::Preset(bool _bNTSC)
   m_dtv_status.component_plugged = Config::Get(Config::SYSCONF_PROGRESSIVE_SCAN);
 
   // Triforce IPL requires the DTV NTSC-J flag to be set.
-  if (m_system.IsTriforce())
-  {
-    m_dtv_status.ntsc_j = true;
-  }
-  else if (region == DiscIO::Region::NTSC_J)
-  {
-    m_dtv_status.ntsc_j = false;
-  }
+  m_dtv_status.ntsc_j = m_system.IsTriforce() || region == DiscIO::Region::NTSC_J;
 
   m_fb_width.Hex = 0;
   m_border_hblank.Hex = 0;
