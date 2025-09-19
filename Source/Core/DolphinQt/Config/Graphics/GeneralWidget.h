fcb3d2fb0594f940c8325ee1e3284be053b3c8f3
TryTwo <taolas@gmail.com>
2025-09-18 17:17:51 -0700

    Qt Setting Panes: Remove original location of options transferred to the OSD pane.

--- Source/Core/DolphinQt/Config/Graphics/GeneralWidget.h
+++ Source/Core/DolphinQt/Config/Graphics/GeneralWidget.h
@@ -52,10 +52,9 @@ private:
   ConfigBool* m_enable_fullscreen;
 
   // Options
-  ConfigBool* m_show_ping;
   ConfigBool* m_autoadjust_window_size;
-  ConfigBool* m_show_messages;
   ConfigBool* m_render_main_window;
+
   std::array<ConfigRadioInt*, 4> m_shader_compilation_mode{};
   ConfigBool* m_wait_for_shaders;
   int m_previous_backend = 0;
