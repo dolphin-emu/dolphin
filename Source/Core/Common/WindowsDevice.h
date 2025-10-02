2ac6bd9a694ceff00ce5c0e3f46d6b3451768c5f
Jordan Woyak <jordan.woyak@gmail.com>
2025-10-02 04:11:27 -0500

    WindowsDevice: Add some utility functions for getting device properties using CfgMgr32.

--- Source/Core/Common/WindowsDevice.h
+++ Source/Core/Common/WindowsDevice.h
@@ -5,6 +5,7 @@
 
 #ifdef _WIN32
 
+#include <optional>
 #include <string>
 
 #ifndef WIN32_LEAN_AND_MEAN
@@ -24,6 +25,13 @@ namespace Common
 // Obtains a device property and returns it as a wide string.
 std::wstring GetDeviceProperty(const HANDLE& device_info, const PSP_DEVINFO_DATA device_data,
                                const DEVPROPKEY* requested_property);
+
+std::optional<std::wstring> GetDevNodeStringProperty(DEVINST device,
+                                                     const DEVPROPKEY* requested_property);
+
+std::optional<std::wstring> GetDeviceInterfaceStringProperty(LPCWSTR iface,
+                                                             const DEVPROPKEY* requested_property);
+
 }  // namespace Common
 
 #endif
