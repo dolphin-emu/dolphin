4ab2b24bd779c70655ed52040a18efa890486aa9
Jordan Woyak <jordan.woyak@gmail.com>
2025-10-14 22:32:12 -0500

    WindowsDevice: Remove GetDeviceProperty function in favor of the CfgMgr32 versions. SetupAPI seems to be no longer recommended.

--- Source/Core/Common/WindowsDevice.h
+++ Source/Core/Common/WindowsDevice.h
@@ -24,10 +24,6 @@
 
 namespace Common
 {
-// Obtains a device property and returns it as a wide string.
-std::wstring GetDeviceProperty(const HANDLE& device_info, const PSP_DEVINFO_DATA device_data,
-                               const DEVPROPKEY* requested_property);
-
 std::optional<std::wstring> GetDevNodeStringProperty(DEVINST device,
                                                      const DEVPROPKEY* requested_property);
 
