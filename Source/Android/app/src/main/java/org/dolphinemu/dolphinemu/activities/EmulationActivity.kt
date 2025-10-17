27646fc530488adb1314c48d46f5834a5693a7b3
JosJuice <josjuice@gmail.com>
2025-10-17 22:38:52 +0200

    Android: Treat EmulationActivity dialog dismiss the same as OK
    In the dialog where you can choose what controller the input overlay
    should be controlling, there's an OK button. If you change controller
    but don't press OK, your selection will be saved, but the input overlay
    won't refresh to show the new controller unless you perform some other
    action that would cause it to refresh. This is not good UX.
    
    This commit changes the behavior not only of this dialog but also other
    dialogs spawned by EmulationActivity so that everything is properly
    updated when dismissing a dialog, as if you had pressed OK.

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/EmulationActivity.kt
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/EmulationActivity.kt
@@ -543,6 +543,7 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 ) { _: DialogInterface?, indexSelected: Int, isChecked: Boolean ->
                     BooleanSetting.valueOf(gcSettingBase + indexSelected)
                         .setBoolean(settings, isChecked)
+                    emulationFragment?.refreshInputOverlay()
                 }
             }
             InputOverlay.OVERLAY_WIIMOTE_CLASSIC -> {
@@ -557,6 +558,7 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 ) { _: DialogInterface?, indexSelected: Int, isChecked: Boolean ->
                     BooleanSetting.valueOf(classicSettingBase + indexSelected)
                         .setBoolean(settings, isChecked)
+                    emulationFragment?.refreshInputOverlay()
                 }
             }
             InputOverlay.OVERLAY_WIIMOTE_NUNCHUK -> {
@@ -578,6 +580,7 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 ) { _: DialogInterface?, indexSelected: Int, isChecked: Boolean ->
                     BooleanSetting.valueOf(nunchukSettingBase + translateToSettingsIndex(indexSelected))
                         .setBoolean(settings, isChecked)
+                    emulationFragment?.refreshInputOverlay()
                 }
             }
             else -> {
@@ -593,14 +596,13 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 ) { _: DialogInterface?, indexSelected: Int, isChecked: Boolean ->
                     BooleanSetting.valueOf(wiimoteSettingBase + indexSelected)
                         .setBoolean(settings, isChecked)
+                    emulationFragment?.refreshInputOverlay()
                 }
             }
         }
 
         builder
-            .setPositiveButton(R.string.ok) { _: DialogInterface?, _: Int ->
-                emulationFragment?.refreshInputOverlay()
-            }
+            .setPositiveButton(R.string.ok, null)
             .show()
     }
 
@@ -622,6 +624,7 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
             ) { _: DialogInterface?, indexSelected: Int, isChecked: Boolean ->
                 BooleanSetting
                     .valueOf(gcSettingBase + indexSelected).setBoolean(settings, isChecked)
+                emulationFragment?.refreshInputOverlay()
             }
         } else if (currentController == InputOverlay.OVERLAY_WIIMOTE_CLASSIC) {
             val wiiClassicEnabledButtons = BooleanArray(14)
@@ -635,6 +638,7 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
             ) { _: DialogInterface?, indexSelected: Int, isChecked: Boolean ->
                 BooleanSetting.valueOf(classicSettingBase + indexSelected)
                     .setBoolean(settings, isChecked)
+                emulationFragment?.refreshInputOverlay()
             }
         } else {
             val wiiEnabledButtons = BooleanArray(11)
@@ -649,6 +653,7 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 ) { _: DialogInterface?, indexSelected: Int, isChecked: Boolean ->
                     BooleanSetting
                         .valueOf(wiiSettingBase + indexSelected).setBoolean(settings, isChecked)
+                    emulationFragment?.refreshInputOverlay()
                 }
             } else {
                 builder.setMultiChoiceItems(
@@ -656,6 +661,7 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 ) { _: DialogInterface?, indexSelected: Int, isChecked: Boolean ->
                     BooleanSetting
                         .valueOf(wiiSettingBase + indexSelected).setBoolean(settings, isChecked)
+                    emulationFragment?.refreshInputOverlay()
                 }
             }
         }
@@ -664,7 +670,7 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
             .setNeutralButton(R.string.emulation_toggle_all) { _: DialogInterface?, _: Int ->
                 emulationFragment!!.toggleInputOverlayVisibility(settings)
             }
-            .setPositiveButton(R.string.ok) { _: DialogInterface?, _: Int -> emulationFragment?.refreshInputOverlay() }
+            .setPositiveButton(R.string.ok, null)
             .show()
     }
 
@@ -689,10 +695,9 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                     settings,
                     InputOverlayPointer.DOUBLE_TAP_OPTIONS[which]
                 )
-            }
-            .setPositiveButton(R.string.ok) { _: DialogInterface?, _: Int ->
                 emulationFragment?.initInputPointer()
             }
+            .setPositiveButton(R.string.ok, null)
             .show()
     }
 
@@ -703,9 +708,11 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 valueTo = 150f
                 value = IntSetting.MAIN_CONTROL_SCALE.int.toFloat()
                 stepSize = 1f
-                addOnChangeListener(Slider.OnChangeListener { _: Slider?, progress: Float, _: Boolean ->
-                    dialogBinding.inputScaleValue.text = "${(progress.toInt() + 50)}%"
-                })
+                addOnChangeListener { _: Slider?, value: Float, _: Boolean ->
+                    dialogBinding.inputScaleValue.text = "${(value.toInt() + 50)}%"
+                    IntSetting.MAIN_CONTROL_SCALE.setInt(settings, value.toInt())
+                    emulationFragment?.refreshInputOverlay()
+                }
             }
             inputScaleValue.text =
                 "${(dialogBinding.inputScaleSlider.value.toInt() + 50)}%"
@@ -714,9 +721,11 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 valueTo = 100f
                 value = IntSetting.MAIN_CONTROL_OPACITY.int.toFloat()
                 stepSize = 1f
-                addOnChangeListener(Slider.OnChangeListener { _: Slider?, progress: Float, _: Boolean ->
-                    inputOpacityValue.text = progress.toInt().toString() + "%"
-                })
+                addOnChangeListener { _: Slider?, value: Float, _: Boolean ->
+                    inputOpacityValue.text = value.toInt().toString() + "%"
+                    IntSetting.MAIN_CONTROL_OPACITY.setInt(settings, value.toInt())
+                    emulationFragment?.refreshInputOverlay()
+                }
             }
             inputOpacityValue.text = inputOpacitySlider.value.toInt().toString() + "%"
         }
@@ -724,17 +733,7 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
         MaterialAlertDialogBuilder(this)
             .setTitle(R.string.emulation_control_adjustments)
             .setView(dialogBinding.root)
-            .setPositiveButton(R.string.ok) { _: DialogInterface?, _: Int ->
-                IntSetting.MAIN_CONTROL_SCALE.setInt(
-                    settings,
-                    dialogBinding.inputScaleSlider.value.toInt()
-                )
-                IntSetting.MAIN_CONTROL_OPACITY.setInt(
-                    settings,
-                    dialogBinding.inputOpacitySlider.value.toInt()
-                )
-                emulationFragment?.refreshInputOverlay()
-            }
+            .setPositiveButton(R.string.ok, null)
             .setNeutralButton(R.string.default_values) { _: DialogInterface?, _: Int ->
                 IntSetting.MAIN_CONTROL_SCALE.delete(settings)
                 IntSetting.MAIN_CONTROL_OPACITY.delete(settings)
@@ -840,10 +839,9 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 entries.toArray(arrayOf<CharSequence>()), checkedItem
             ) { _: DialogInterface?, indexSelected: Int ->
                 controllerSetting.setInt(settings, values[indexSelected])
-            }
-            .setPositiveButton(R.string.ok) { _: DialogInterface?, _: Int ->
                 emulationFragment?.refreshInputOverlay()
             }
+            .setPositiveButton(R.string.ok, null)
             .setNeutralButton(
                 R.string.emulation_more_controller_settings
             ) { _: DialogInterface?, _: Int -> SettingsActivity.launch(this, MenuTag.SETTINGS) }
@@ -858,10 +856,9 @@ class EmulationActivity : AppCompatActivity(), ThemeProvider {
                 IntSetting.MAIN_IR_MODE.int
             ) { _: DialogInterface?, indexSelected: Int ->
                 IntSetting.MAIN_IR_MODE.setInt(settings, indexSelected)
-            }
-            .setPositiveButton(R.string.ok) { _: DialogInterface?, _: Int ->
                 emulationFragment?.refreshOverlayPointer()
             }
+            .setPositiveButton(R.string.ok, null)
             .show()
     }
 
