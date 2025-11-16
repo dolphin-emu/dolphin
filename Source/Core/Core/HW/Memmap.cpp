dolphin-emu/dolphin

commit ac84ea17a6248062e556065f56a2ae564ff5a303

Memmap: Optimize UpdateLogicalMemory by merging mappings
Instead of creating many 128 KiB mappings, we can create a few large
mappings. On my Windows PC, this speeds up GameCube (FakeVMEM) game boot
times by about 200 ms and Wii game boot times by about 60 ms. Loading
savestates is also faster, by about 45 ms for GameCube (FakeVMEM) games
and 5 ms for Wii games. The impact is presumably smaller on other OSes
because Windows is particularly slow at creating mappings.

Source/Core/Core/HW/Memmap.cpp
@@ -248,9 +248,24 @@ void MemoryManager::UpdateLogicalMemory(const PowerPC::BatTable& dbat_table)
     if (dbat_table[i] & PowerPC::BAT_PHYSICAL_BIT)
     {
       u32 logical_address = i << PowerPC::BAT_INDEX_SHIFT;
-      // TODO: Merge adjacent mappings to make this faster.
       u32 logical_size = PowerPC::BAT_PAGE_SIZE;
       u32 translated_address = dbat_table[i] & PowerPC::BAT_RESULT_MASK;
+
+      while (i + 1 < dbat_table.size())
+      {
+        if (!(dbat_table[i + 1] & PowerPC::BAT_PHYSICAL_BIT))
+        {
+          ++i;
+          break;
+        }
+
+        if ((dbat_table[i + 1] & PowerPC::BAT_RESULT_MASK) != translated_address + logical_size)
+          break;
+
+        ++i;
+        logical_size += PowerPC::BAT_PAGE_SIZE;
+      }
+
       for (const auto& physical_region : m_physical_regions)
       {
         if (!physical_region.active)

commit e00f39bb6d5b22b4de57654c1eac62cfed273259

Memmap: Fix populating m_logical_page_mappings
This fixes a regression in ac84ea17a6. That commit was intended to
reduce the number of MapInMemoryRegion calls while otherwise keeping
behavior the same, but it accidentally messed up our logic for writing
to m_logical_page_mappings. This commit restores the old
m_logical_page_mappings behavior, fixing a crash that would occur when
running JitArm64 without fastmem.

Source/Core/Core/HW/Memmap.cpp
@@ -278,12 +278,13 @@ void MemoryManager::UpdateLogicalMemory(const PowerPC::BatTable& dbat_table)
         if (intersection_start < intersection_end)
         {
           // Found an overlapping region; map it.
+          u32 mapped_logical_address = logical_address + intersection_start - translated_address;
+          u32 mapped_size = intersection_end - intersection_start;
 
           if (m_is_fastmem_arena_initialized)
           {
             u32 position = physical_region.shm_position + intersection_start - mapping_address;
-            u8* base = m_logical_base + logical_address + intersection_start - translated_address;
-            u32 mapped_size = intersection_end - intersection_start;
+            u8* base = m_logical_base + mapped_logical_address;
 
             void* mapped_pointer = m_arena.MapInMemoryRegion(position, mapped_size, base);
             if (!mapped_pointer)
@@ -297,8 +298,10 @@ void MemoryManager::UpdateLogicalMemory(const PowerPC::BatTable& dbat_table)
             m_logical_mapped_entries.push_back({mapped_pointer, mapped_size});
           }
 
-          m_logical_page_mappings[i] =
-              *physical_region.out_pointer + intersection_start - mapping_address;
+          u32 bat_index = mapped_logical_address / PowerPC::BAT_PAGE_SIZE;
+          u8* target_address = *physical_region.out_pointer + intersection_start - mapping_address;
+          for (u32 j = 0; j < mapped_size / PowerPC::BAT_PAGE_SIZE; ++j)
+            m_logical_page_mappings[bat_index + j] = target_address + j * PowerPC::BAT_PAGE_SIZE;
         }
       }
     }