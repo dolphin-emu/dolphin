48bfa09c645ccf6427020d7c8d776a1a3088892c
JosJuice <josjuice@gmail.com>
2025-12-07 10:19:35 +0100

    Jit64: cmpl flush b if very specific condition

--- Source/Core/Core/PowerPC/Jit64/Jit_Integer.cpp
+++ Source/Core/Core/PowerPC/Jit64/Jit_Integer.cpp
@@ -576,6 +576,8 @@ void Jit64::cmpXX(UGeckoInstruction inst)
   // cmp / cmpl
   case 31:
     signedCompare = (inst.SUBOP10 == 0);
+    if (!signedCompare && !gpr.IsImm(a) && gpr.IsImm(b) && gpr.Imm32(b) != 0 && merge_branch)
+      gpr.Flush(BitSet32{b});
     Rb = signedCompare ? gpr.Use(b, RCMode::Read) : gpr.Bind(b, RCMode::Read);
     RegCache::Realize(Rb);
     comparand = Rb;
