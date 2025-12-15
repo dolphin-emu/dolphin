f4b1b4c76890cf413b0c8e5ca194743b9e1bfc57
PlayXboxtion963 <org0030h@outlook.com>
2025-12-16 07:16:59 +0800

    DSU Thread headfile add to git

---
+++ Source/Android/jni/Input/DSUInput.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <stdbool.h>
+
+// Enable/disable background input execution. When enabled a background thread
+// may be created to poll controller input; when disabled the thread will be
+// stopped and joined so it doesn't prevent the CPU from sleeping.
+void SetBackgroundInputExecutionAllowed(bool allowed);
