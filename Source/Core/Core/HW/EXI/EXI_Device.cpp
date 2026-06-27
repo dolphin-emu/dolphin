b89e21e0ef11f674f3d33f644db3e0a507ec3448
Tillmann Karras <tilkax@gmail.com>
2026-06-27 21:04:03 +0100

    EXI: add Waikiki device
    work in progress, iirc the irq state machine is not quite right

--- Source/Core/Core/HW/EXI/EXI_Device.cpp
+++ Source/Core/Core/HW/EXI/EXI_Device.cpp
@@ -15,6 +15,7 @@
 #include "Core/HW/EXI/EXI_DeviceIPL.h"
 #include "Core/HW/EXI/EXI_DeviceMemoryCard.h"
 #include "Core/HW/EXI/EXI_DeviceModem.h"
+#include "Core/HW/EXI/EXI_DeviceWaikiki.h"
 #include "Core/HW/Memmap.h"
 #include "Core/System.h"
 
@@ -188,6 +189,10 @@ std::unique_ptr<IEXIDevice> EXIDevice_Create(Core::System& system, const EXIDevi
 
   case EXIDeviceType::Baseboard:
     result = std::make_unique<CEXIBaseboard>(system);
+    break;
+
+  case EXIDeviceType::Waikiki:
+    result = std::make_unique<CEXIWaikiki>(system);
     break;
 
   case EXIDeviceType::None:
