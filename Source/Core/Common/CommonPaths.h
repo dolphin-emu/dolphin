edca2a9454f6482a8bcc6e0e47c7ef6075bd8057
CySlaytor <cyslaytor@gmail.com>
2026-09-21 13:45:18 +0000

    feat(retroachievements): implement achievement notification sounds

--- Source/Core/Common/CommonPaths.h
+++ Source/Core/Common/CommonPaths.h
@@ -92,6 +92,7 @@
 #define WIISDSYNC_DIR "WiiSDSync"
 #define ASSEMBLY_DIR "SavedAssembly"
 #define WIIBANNERS_DIR "WiiBanners"
+#define SOUNDS_DIR "Sounds"
 
 // This one is only used to remove it if it was present
 #define SHADERCACHE_LEGACY_DIR "ShaderCache"
