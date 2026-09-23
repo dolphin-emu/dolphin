1e151be0c1e4ce7ad83826b7e12ccb0368e73650
NVDEMU <ndelpo318@gmail.com>
2026-09-22 23:48:51 -0400

    movie: serialize EFB clear-byte setting

--- Source/Core/Core/ConfigLoaders/MovieConfigLoader.cpp
+++ Source/Core/Core/ConfigLoaders/MovieConfigLoader.cpp
@@ -47,6 +47,8 @@ static void LoadFromDTM(Config::Layer* config_layer, Movie::DTMHeader* dtm)
 
   config_layer->Set(Config::GFX_HACK_EFB_ACCESS_ENABLE, dtm->bEFBAccessEnable);
   config_layer->Set(Config::GFX_HACK_SKIP_EFB_COPY_TO_RAM, dtm->bSkipEFBCopyToRam);
+  config_layer->Set(Config::GFX_HACK_SKIP_EFB_COPY_TO_RAM_CLEAR_BYTE,
+                    dtm->uSkipEFBCopyToRamClearByte);
   config_layer->Set(Config::GFX_HACK_EFB_EMULATE_FORMAT_CHANGES, dtm->bEFBEmulateFormatChanges);
   config_layer->Set(Config::GFX_HACK_IMMEDIATE_XFB, dtm->bImmediateXFB);
   config_layer->Set(Config::GFX_HACK_SKIP_XFB_COPY_TO_RAM, dtm->bSkipXFBCopyToRam);
