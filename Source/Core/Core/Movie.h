f6dc028d225e136bda7efc536d5ebc7b25944832
NVDEMU <ndelpo318@gmail.com>
2026-09-22 23:49:03 -0400

    movie: store EFB clear-byte setting

--- Source/Core/Core/Movie.h
+++ Source/Core/Core/Movie.h
@@ -133,7 +133,8 @@ struct DTMHeader
   u8 GBAControllers;                // GBA Controllers plugged in (the bits are ports 1-4)
   bool bWidescreen;                 // true indicates SYSCONF aspect ratio is 16:9, false for 4:3
   u8 countryCode;                   // SYSCONF country code
-  std::array<u8, 5> reserved;       // Padding for any new config options
+  u8 uSkipEFBCopyToRamClearByte;
+  std::array<u8, 4> reserved;       // Padding for any new config options
   std::array<char, 40> discChange;  // Name of iso file to switch to, for two disc games.
   std::array<u8, 20> revision;      // Git hash
   u32 DSPiromHash;
